14a7996e7044318d383b3081b48fb6566cca0f15
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-04-04 22:16:37 +0000

    [added] textures to mesh shader test

--- gdx/src/com/badlogic/gdx/tests/MeshShaderTest.java
+++ gdx/src/com/badlogic/gdx/tests/MeshShaderTest.java
@@ -4,7 +4,13 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.RenderListener;
 import com.badlogic.gdx.backends.desktop.JoglApplication;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.ShaderProgram;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.mesh.Mesh;
 import com.badlogic.gdx.graphics.mesh.VertexAttribute;
 import com.badlogic.gdx.graphics.mesh.VertexAttributes.Usage;
@@ -14,6 +20,7 @@ public class MeshShaderTest implements RenderListener
 {
 	ShaderProgram shader;
 	Mesh mesh;		
+	Texture texture;
 	Matrix matrix = new Matrix();
 
 	@Override
@@ -21,40 +28,60 @@ public class MeshShaderTest implements RenderListener
 	{
 		String vertexShader =  "attribute vec4 a_position;    \n" +
 							   "attribute vec4 a_color;\n" +
-							   "uniform mat4 worldView;\n" +
+							   "attribute vec2 a_texCoords;\n" +							   
+							   "uniform mat4 u_worldView;\n" +							   
 							   "varying vec3 v_color;" +
+							   "varying vec2 v_texCoords;" +							   
 							   "void main()                  \n" +
 							   "{                            \n" +
 							   "   v_color = vec4(a_color.x, a_color.y, a_color.z, 1); \n" +
-							   "   gl_Position =  worldView * a_position;  \n" +
+							   "   v_texCoords = a_texCoords; \n" +
+							   "   gl_Position =  u_worldView * a_position;  \n" +
 							   "}                            \n";
 		String fragmentShader = "precision mediump float;\n" +
 								"varying vec4 v_color;\n" +
+								"varying vec2 v_texCoords;\n" +								
+								"uniform sampler2D u_texture;\n" +
 								"void main()                                  \n" +
-							    "{                                            \n" +
-							    "  gl_FragColor = v_color;\n" +
+							    "{                                            \n" +							    							   
+							    "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n" +
 							    "}";  
 		
 		shader = new ShaderProgram( app.getGraphics().getGL20(), vertexShader, fragmentShader);
 		if( shader.isCompiled() == false )
+		{
 			app.log( "ShaderTest", shader.getLog() );
+			System.exit(0);
+		}
 		
 		mesh = new Mesh( app.getGraphics(), true, true, false, 3, 0, 
 						 new VertexAttribute( Usage.Position, 3, "a_position" ),
-						 new VertexAttribute( Usage.Color, 3, "a_color" ) );
+						 new VertexAttribute( Usage.Color, 3, "a_color" ),
+						 new VertexAttribute( Usage.TextureCoordinates, 2, "a_texCoords" ) );
 		
-		mesh.setVertices( new float[] { -0.5f, -0.5f, 0, 1, 0, 0,
-										 0.5f, -0.5f, 0, 0, 1, 0,
-										 0, 0.5f, 0, 0, 0, 1 } );	
+		mesh.setVertices( new float[] { -0.5f, -0.5f, 0, 1, 0, 0, 0, 0,
+										 0.5f, -0.5f, 0, 0, 1, 0, 1, 0,
+										 0, 0.5f, 0, 0, 0, 1, 0.5f, 1 } );	
 		
 		matrix.setToTranslation( 0.3f, 0.2f, 0 );
+		
+		Pixmap pixmap = app.getGraphics().newPixmap(256, 256, Format.RGBA8888 );
+		pixmap.setColor(1, 1, 1, 1 );
+		pixmap.fill();
+		pixmap.setColor(0, 0, 0, 1 );
+		pixmap.drawLine(0, 0, 256, 256);
+		pixmap.drawLine(256, 0, 0, 256);
+		texture = app.getGraphics().newTexture( pixmap, TextureFilter.MipMap, TextureFilter.Linear, TextureWrap.ClampToEdge, TextureWrap.ClampToEdge, true );
 	}
 
 	@Override
 	public void render(Application app) 
 	{
+		app.getGraphics().getGL20().glEnable( GL20.GL_TEXTURE_2D );
+		texture.bind();
 		shader.begin();		
-		shader.setUniformMatrix( "worldView", matrix );
+		shader.setUniformMatrix( "u_worldView", matrix );
+		shader.setUniformi( "u_texture", 0 );
 		mesh.render( shader, GL10.GL_TRIANGLES );
 		shader.end();
 	}
