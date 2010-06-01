da166503c6ccb4fad911aa514cace1a67bf410d5
christoph.widulle <christoph.widulle@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-06-01 00:27:00 +0000

    [fixed] blue and green switched to display correct color

--- gdx/src/com/badlogic/gdx/graphics/Color.java
+++ gdx/src/com/badlogic/gdx/graphics/Color.java
@@ -12,8 +12,8 @@ public class Color
 	public static final Color WHITE = new Color( 1, 1, 1, 1 );
 	public static final Color BLACK = new Color( 0, 0, 0, 1 );
 	public static final Color RED = new Color( 1, 0, 0, 1 );
-	public static final Color BLUE = new Color( 0, 1, 0, 1 );
-	public static final Color GREEN = new Color( 0, 0, 1, 1 );
+	public static final Color GREEN = new Color( 0, 1, 0, 1 );
+	public static final Color BLUE = new Color( 0, 0, 1, 1 );
 	
 	/** the red, green, blue and alpha components **/
 	public float r, g, b, a;
