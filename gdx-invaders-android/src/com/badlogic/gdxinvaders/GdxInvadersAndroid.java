22d8b795142f704ce8b56a977a1ddbd70a02f393
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-06-22 12:49:38 +0000

    initial commit

---
+++ gdx-invaders-android/src/com/badlogic/gdxinvaders/GdxInvadersAndroid.java
@@ -0,0 +1,18 @@
+package com.badlogic.gdxinvaders;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class GdxInvadersAndroid extends AndroidApplication {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        initialize(false, 20);    
+        getGraphics().setRenderListener(new GdxInvaders());
+    }
+}
