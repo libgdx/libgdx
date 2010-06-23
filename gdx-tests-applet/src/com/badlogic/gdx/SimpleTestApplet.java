5eb4cd1b109e766832908b241d9f7f7937e84a34
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-06-23 10:06:30 +0000

    [changed] applet test now also uses resources. seems to work well.

--- gdx-tests-applet/src/com/badlogic/gdx/SimpleTestApplet.java
+++ gdx-tests-applet/src/com/badlogic/gdx/SimpleTestApplet.java
@@ -2,91 +2,144 @@ package com.badlogic.gdx;
 
 import java.applet.Applet;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.applet.AppletApplication;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.ModelLoader;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.math.Matrix;
 
-public class SimpleTestApplet extends Applet implements RenderListener
+public class SimpleTestApplet extends Applet implements RenderListener, InputListener
 {
-	Mesh mesh;
-	PerspectiveCamera camera;
-	Matrix matrix = new Matrix();
-	float angle = 0;
-	
 	public void init()
 	{
-		AppletApplication app = new AppletApplication( this, false );		
+		AppletApplication app = new AppletApplication( this, false, false );		
 		app.getGraphics().setRenderListener( this );
 	}
 
+	PerspectiveCamera cam;
+	Mesh mesh;
+	Texture texture;
+	float angleY = 0;
+	float angleX = 0;
+	float[] lightColor = { 1, 1, 1, 0 };
+	float[] lightPosition = { 2, 5, 10, 0 }; 
+	float touchStartX = 0;
+	float touchStartY = 0;
+	
+	long frameStart;
+	int frames = 0;
+
 	@Override
 	public void surfaceCreated(Application app) 
-	{
-		mesh = new Mesh( app.getGraphics(), true, true, false, 8, 36, new VertexAttribute( Usage.Position, 3, "a_Position" ) );
-
-		float hsize = 0.5f;
-		float vsize = 0.5f;
-		float zsize = 0.5f;
-		mesh.setVertices( new float[] 
-		                            {
-				-hsize, vsize, -zsize,
-				hsize, vsize, -zsize,
-				-hsize, -vsize, -zsize,
-				hsize, -vsize, -zsize,
-				-hsize, vsize, zsize,
-				hsize, vsize, zsize,
-				-hsize, -vsize, zsize,
-				hsize, -vsize, zsize	
-		                            });
-		mesh.setIndices( new short[] {
-				0, 1, 2, 2, 1, 3,
-				1, 5, 3, 3, 5, 7,
-				5, 4, 7, 7, 4, 6,
-				4, 0, 6, 6, 0, 2,
-				0, 4, 1, 1, 4, 5,
-				2, 6, 3, 3, 6, 7 
-		});
-
-		camera = new PerspectiveCamera();
-		camera.getPosition().set( 2, 2, 2 );
-		camera.getDirection().set( -1, -1, -1 );		
-	}
-	
-	@Override
-	public void render(Application app) 
-	{
-		GL10 gl = app.getGraphics().getGL10();
-		gl.glClearColor(1, 0, 0, 0);
-		gl.glClear( GL10.GL_COLOR_BUFFER_BIT );
-		camera.setMatrices( app.getGraphics() );
-
-		angle += app.getGraphics().getDeltaTime() * 45;
-		matrix.setFromEulerAngles( angle, angle, 0 );
-		gl.glMultMatrixf( matrix.val, 0);
-
-		mesh.render( GL10.GL_TRIANGLES );
-
-		try {
-			Thread.sleep( 20 );
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	{	
+		if( mesh == null )
+		{
+			app.getInput().addInputListener( this );
+			
+			mesh = ModelLoader.loadObj( app.getGraphics(), app.getFiles().readFile( "data/cube.obj", FileType.Internal ), true, true );			
+			
+			Pixmap pixmap = app.getGraphics().newPixmap( app.getFiles().readFile( "data/badlogic.jpg", FileType.Internal));
+			texture = app.getGraphics().newTexture( pixmap, TextureFilter.MipMap, TextureFilter.Linear, TextureWrap.ClampToEdge, TextureWrap.ClampToEdge, true );					
+			
+			cam = new PerspectiveCamera();
+			cam.getPosition().set( 2, 2, 2 );
+			cam.getDirection().set( -1, -1, -1 );							
 		}
+		frameStart = System.nanoTime();
 	}
 
 	@Override
 	public void surfaceChanged(Application app, int width, int height) 
 	{	
-
+		
 	}
 	
 	@Override
+	public void render(Application app) 
+	{	
+		GL10 gl = app.getGraphics().getGL10();
+				
+		gl.glViewport( 0, 0, app.getGraphics().getWidth(), app.getGraphics().getHeight() );
+		gl.glClear( GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT );
+		gl.glEnable( GL10.GL_DEPTH_TEST );
+		gl.glEnable( GL10.GL_LIGHTING );		
+		gl.glEnable( GL10.GL_COLOR_MATERIAL );
+		gl.glEnable( GL10.GL_TEXTURE_2D );
+		
+		cam.setMatrices( app.getGraphics() );							
+		
+		gl.glEnable( GL10.GL_LIGHT0 );				
+		gl.glLightfv( GL10.GL_LIGHT0, GL10.GL_DIFFUSE, lightColor, 0 );
+		gl.glLightfv( GL10.GL_LIGHT0, GL10.GL_POSITION, lightPosition, 0 );
+		
+		gl.glRotatef(angleY, 0, 1, 0);	
+		gl.glRotatef(angleX, 1, 0, 0 );
+		texture.bind();
+		mesh.render( GL10.GL_TRIANGLES );
+		
+		if( System.nanoTime() - frameStart > 1000000000 )
+		{
+			app.log( "Obj Test", "fps: " + frames );
+			frames = 0;
+			frameStart = System.nanoTime();
+		}
+		
+		frames++;
+	}
+
+	@Override
 	public void dispose(Application app) 
 	{	
+		
+	}	
+	
 
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int x, int y, int pointer) {
+		touchStartX = x;
+		touchStartY = y;
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) 
+	{		
+		angleY += (x - touchStartX);
+		angleX += (y - touchStartY);
+		touchStartX = x;
+		touchStartY = y;
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int x, int y, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
