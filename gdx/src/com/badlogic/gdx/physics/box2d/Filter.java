cb7593b12659b81371a00a027f2a8342c1066020
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-07-03 22:06:48 +0000

    [fixed] Filter initial values had to be set. Stupid hidden init code in C++ constructors...

--- gdx/src/com/badlogic/gdx/physics/box2d/Filter.java
+++ gdx/src/com/badlogic/gdx/physics/box2d/Filter.java
@@ -10,18 +10,18 @@ public class Filter
 	/**
 	 * The collision category bits. Normally you would just set one bit.
 	 */
-	public short categoryBits;
+	public short categoryBits = 0x0001;
 	
 	/**
 	 * The collision mask bits. This states the categories that this
 	 * shape would accept for collision.
 	 */
-	public short maskBits;
+	public short maskBits = -1;
 	
 	/**
 	 * Collision groups allow a certain group of objects to never collide (negative)
 	 * or always collide (positive). Zero means no collision group. Non-zero group
 	 * filtering always wins against the mask bits.
 	 */	 
-	public short groupIndex;
+	public short groupIndex = 0;
 }
