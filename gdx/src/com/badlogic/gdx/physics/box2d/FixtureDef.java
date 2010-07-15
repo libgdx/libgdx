586d01201b88df742b68579f3710ecfea252a0f6
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-07-15 10:28:50 +0000

    [fixed] FixtureDef restitution is now set to 0.2f as in the original native code.

--- gdx/src/com/badlogic/gdx/physics/box2d/FixtureDef.java
+++ gdx/src/com/badlogic/gdx/physics/box2d/FixtureDef.java
@@ -30,19 +30,19 @@ public class FixtureDef
 	public Shape shape;
 	
 	/** The friction coefficient, usually in the range [0,1]. **/
-	public float friction;
+	public float friction = 0.2f;
 	
 	/** The restitution (elasticity) usually in the range [0,1]. **/
-	public float restitution;
+	public float restitution = 0;
 	
 	/** The density, usually in kg/m^2. **/
-	public float density;
+	public float density = 0;
 	
 	/**
 	 * A sensor shape collects contact information but never generates a collision
 	 * response.
 	 */
-	public boolean isSensor;
+	public boolean isSensor = false;
 	
 	/** Contact filtering data. **/
 	public final Filter filter = new Filter();
