644b30df80c7972d97a7bdaf698eb99ea946bec2
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-09-08 23:39:16 +0000

    [changed] added debug renderer. seems to work just fine.

--- gdx-tests-desktop/src/com/badlogic/gdx/tests/desktop/MD5Test.java
+++ gdx-tests-desktop/src/com/badlogic/gdx/tests/desktop/MD5Test.java
@@ -3,12 +3,16 @@ package com.badlogic.gdx.tests.desktop;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import com.badlogic.gdx.backends.desktop.JoglApplication;
 import com.badlogic.gdx.graphics.loaders.md5.MD5Loader;
+import com.badlogic.gdx.graphics.loaders.md5.MD5Mesh;
+import com.badlogic.gdx.graphics.loaders.md5.MD5Model;
 
 public class MD5Test 
 {
 	public static void main( String[] argv ) throws FileNotFoundException
 	{
-		MD5Loader.loadModel( new FileInputStream( "data/zfat.md5mesh" ) );
+		JoglApplication app = new JoglApplication( "MD5 Test", 480, 320, false );
+		app.getGraphics().setRenderListener( new com.badlogic.gdx.tests.MD5Test() );
 	}
 }
