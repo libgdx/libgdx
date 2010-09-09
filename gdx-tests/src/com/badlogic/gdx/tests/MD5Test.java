a232c77f2a6c0b2103323bf86f3a7b82dbb40f14
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-09-09 19:29:40 +0000

    [fixed] fucked up last commit of MD5Test it seems. Either that or subclipse is funny (which it is really often on 64-bit systems...)

--- gdx-tests/src/com/badlogic/gdx/tests/MD5Test.java
+++ gdx-tests/src/com/badlogic/gdx/tests/MD5Test.java
@@ -12,81 +12,77 @@ import com.badlogic.gdx.graphics.Font.FontStyle;
 import com.badlogic.gdx.graphics.loaders.md5.MD5Loader;
 import com.badlogic.gdx.graphics.loaders.md5.MD5Model;
 import com.badlogic.gdx.graphics.loaders.md5.MD5Renderer;
+import com.badlogic.gdx.math.Vector3;
 
 public class MD5Test implements RenderListener
 {
+	PerspectiveCamera camera;
 	MD5Model model;
 	MD5Renderer renderer;
-	PerspectiveCamera camera;
 	SpriteBatch batch;
 	Font font;
+
 	
 	@Override
 	public void surfaceCreated(Application app) 
-	{	
+	{
 		if( model == null )
 		{
-			model = MD5Loader.loadModel( app.getFiles().readFile( "data/zfat.md5mesh", FileType.Internal ) );
-			
-			app.log( "MD5 Test", "num triangles: " + model.getNumTriangles() );
-			app.log( "MD5 Test", "num vertices: " + model.getNumVertices( ) );
-			
+			model = MD5Loader.loadModel( app.getFiles().readFile( "data/zfat.md5mesh", FileType.Internal) );
 			renderer = new MD5Renderer( app.getGraphics(), model, true );
-			renderer.setSkeleton( model.baseSkeleton );
-			
+		
 			camera = new PerspectiveCamera();
-			camera.setFov( 60 );
 			camera.getPosition().set( 0, 25, 100 );
+			camera.setFov(60);
 			camera.setNear( 1 );
 			camera.setFar( 1000 );
-			
+			camera.setViewport( app.getGraphics().getWidth(), app.getGraphics().getHeight() );
+		
 			batch = new SpriteBatch( app.getGraphics() );
-			font = app.getGraphics().newFont( "Arial", 12, FontStyle.Plain, true );
-			
-			GL10 gl = app.getGraphics().getGL10();
-			gl.glViewport( 0, 0, app.getGraphics().getWidth(), app.getGraphics().getHeight() );							
+			font = app.getGraphics().newFont( "Arial", 12, FontStyle.Plain, true );		
+			app.getGraphics().getGL10().glViewport( 0, 0, app.getGraphics().getWidth(), app.getGraphics().getHeight() );
 		}
 	}
 
 	@Override
-	public void surfaceChanged(Application app, int width, int height) 
-	{	
+	public void surfaceChanged(Application app, int width, int height) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	float angle = 0;
+	
 	@Override
 	public void render(Application app) 
-	{	
+	{
 		GL10 gl = app.getGraphics().getGL10();
 		gl.glClear( GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT );
+		
 		gl.glEnable( GL10.GL_DEPTH_TEST );
-		gl.glPolygonMode( GL10.GL_FRONT_AND_BACK, GL10.GL_FILL );
-		
 		camera.setMatrices( app.getGraphics() );
-		
 		angle+=app.getGraphics().getDeltaTime() * 20;
-		for( int z = 0; z < 1000; z+= 50 )
-		{					
+		
+		for( int z = 0; z < 500; z += 50 )
+		{
 			gl.glLoadIdentity();
 			gl.glTranslatef( 0, 0, -z );
 			gl.glRotatef( angle, 0, 1, 0 );
-			gl.glRotatef( -90, 1, 0, 0 );
+			gl.glRotatef(-90, 1, 0, 0 );
 			
 			renderer.setSkeleton( model.baseSkeleton );
-			renderer.render( );
+			renderer.render();
 		}
 		
 		gl.glDisable( GL10.GL_DEPTH_TEST );
-		gl.glPolygonMode( GL10.GL_FRONT_AND_BACK, GL10.GL_FILL );	
 		batch.begin();
-		batch.drawText( font, "fps: " + app.getGraphics().getFramesPerSecond() + ", delta: " + app.getGraphics().getDeltaTime(), 0, 20, Color.WHITE );
+		batch.drawText( font, "fps: " + app.getGraphics().getFramesPerSecond(), 10, 20, Color.WHITE );
 		batch.end();
 	}
 
 	@Override
-	public void dispose(Application app) 
-	{	
+	public void dispose(Application app) {
+		// TODO Auto-generated method stub
 		
 	}
+
 }
