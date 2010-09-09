9772aab2cc53d3de1e81d6b57f6388f65617d3ad
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-09-09 19:16:44 +0000

    [fixed] removed bin directory. no idea why that was in there...

--- gdx/src/com/badlogic/gdx/graphics/loaders/md5/MD5Animation.java
+++
@@ -1,5 +0,0 @@
-package com.badlogic.gdx.graphics.loaders.md5;
-
-public class MD5Animation {
-
-}
--- gdx/src/com/badlogic/gdx/graphics/loaders/md5/MD5Triangle.java
+++
@@ -1,6 +0,0 @@
-package com.badlogic.gdx.graphics.loaders.md5;
-
-public class MD5Triangle 
-{
-	public final int[] indices = new int[3];
-}
