3a4289c047757deedbb4473263cc0daadd243f51
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-09-13 01:45:29 +0000

    [fixed] benchmark...

--- gdx-tests-android/src/com/badlogic/gdx/MicroBenchmarks.java
+++ gdx-tests-android/src/com/badlogic/gdx/MicroBenchmarks.java
@@ -103,17 +103,19 @@ public class MicroBenchmarks extends Activity
 	
 	private void toc( final String info )
 	{
+		final float time = (System.nanoTime()-start)/1000000000.0f;
+		
 		tv.post( new Runnable() {
 
 			@Override
 			public void run()
 			{
 				StringBuilder buff = new StringBuilder( tv.getText() );
-				buff.append( info ).append( ", " ).append((System.nanoTime()-start)/1000000000.0f).append( " secs\n" );
+				buff.append( info ).append( ", " ).append(time).append( " secs\n" );
 				tv.setText( buff.toString() );
 			}
 		} );
 
-		Log.d( "MicroBenchmarks", info + ", " + (System.nanoTime()-start)/1000000000.0f );
+		Log.d( "MicroBenchmarks", info + ", " + time );
 	}
 }
