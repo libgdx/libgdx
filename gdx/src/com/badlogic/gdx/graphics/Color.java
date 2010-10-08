1454218e09ad8e5bd3f06a863ab7f7986e3d40e4
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-10-08 19:28:29 +0000

    [fixed] Color.clamp(), i must have been drunk. Thanks to Shomari Prince for pointing out the issue.

--- gdx/src/com/badlogic/gdx/graphics/Color.java
+++ gdx/src/com/badlogic/gdx/graphics/Color.java
@@ -137,14 +137,14 @@ public class Color
 	
 	private void clamp( )
 	{
-		if( r < 0 ) a = 0; 
-		if( r > 1 ) a = 1;
+		if( r < 0 ) r = 0; 
+		if( r > 1 ) r = 1;
 		
-		if( g < 0 ) a = 0; 
-		if( g > 1 ) a = 1;
+		if( g < 0 ) g = 0; 
+		if( g > 1 ) g = 1;
 		
-		if( b < 0 ) a = 0; 
-		if( b > 1 ) a = 1;
+		if( b < 0 ) b = 0; 
+		if( b > 1 ) b = 1;
 		
 		if( a < 0 ) a = 0; 
 		if( a > 1 ) a = 1;
