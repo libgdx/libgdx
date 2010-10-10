fd9ffc0f4d29dd247d4eecda3282ec51c8c31f54
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-10-10 23:09:49 +0000

    [changed] main is now executing the old generic test.

--- image-packer/src/com/badlogic/gdx/imagepacker/ImagePacker.java
+++ image-packer/src/com/badlogic/gdx/imagepacker/ImagePacker.java
@@ -223,13 +223,13 @@ public class ImagePacker
 		Random rand = new Random( 0 );
 		ImagePacker packer = new ImagePacker( 512, 512, 1, true );
 		
-//		BufferedImage[] images = new BufferedImage[100];
-//		for( int i = 0; i < images.length; i++ )
-//		{
-//			Color color = new Color( (float)Math.random(), (float)Math.random(), (float)Math.random(), 1 );
-//			images[i] = createImage( rand.nextInt( 50 ) + 10, rand.nextInt( 50 ) + 10, color );
-//		}
-		BufferedImage[] images = { ImageIO.read( new File( "test.png" ) ) };
+		BufferedImage[] images = new BufferedImage[100];
+		for( int i = 0; i < images.length; i++ )
+		{
+			Color color = new Color( (float)Math.random(), (float)Math.random(), (float)Math.random(), 1 );
+			images[i] = createImage( rand.nextInt( 50 ) + 10, rand.nextInt( 50 ) + 10, color );
+		}
+//		BufferedImage[] images = { ImageIO.read( new File( "test.png" ) ) };
 		
 		Arrays.sort( images, new Comparator<BufferedImage>( ) {
 			@Override
