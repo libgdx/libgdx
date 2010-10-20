e1f30bc4fb295dc25fb36364d4e5199c5d8b7ab6
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-10-20 04:55:38 +0000

    Better textarea demo.

--- src/com/badlogic/gdx/twl/TextAreaTest.java
+++ src/com/badlogic/gdx/twl/TextAreaTest.java
@@ -19,12 +19,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
-public class TwlTest extends AndroidApplication
+public class TextAreaTest extends AndroidApplication
 {
 	public void onCreate( Bundle bundle )
 	{
 		super.onCreate(bundle);
 		initialize( false );
-		getGraphics().setRenderListener( new com.badlogic.gdx.twl.tests.TwlTest() );
+		getGraphics().setRenderListener( new com.badlogic.gdx.twl.tests.TextAreaTest() );
 	}
 }
