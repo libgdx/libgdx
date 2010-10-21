1a5b454c115a5c4ea1239834dd236d806ce0e949
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-10-21 20:11:46 +0000

    Fixed bug with scrolling Sprite textures.

--- gdx/src/com/badlogic/gdx/graphics/Sprite.java
+++ gdx/src/com/badlogic/gdx/graphics/Sprite.java
@@ -181,7 +181,7 @@ public class Sprite {
 	 */
 	public void scrollTexture (float xAmount, float yAmount) {
 		float[] vertices = this.vertices;
-		if (xAmount > 0) {
+		if (xAmount != 0) {
 			float u = (vertices[U1] + xAmount) % 1;
 			float u2 = u + width / texture.getWidth();
 			vertices[U1] = u;
@@ -189,7 +189,7 @@ public class Sprite {
 			vertices[U3] = u2;
 			vertices[U4] = u2;
 		}
-		if (yAmount > 0) {
+		if (yAmount != 0) {
 			float v = (vertices[V1] + yAmount) % 1;
 			float v2 = v + height / texture.getHeight();
 			vertices[V1] = v;
