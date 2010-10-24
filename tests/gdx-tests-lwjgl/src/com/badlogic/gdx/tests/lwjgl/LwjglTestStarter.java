9145a042a699f0fa3455ac675f9982d8175a4326
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-10-24 13:21:12 +0000

    [added] LwjglTestStarter [removed] all Lwjgl tests, use the starter... [added] some missing assets for the tests

---
+++ tests/gdx-tests-lwjgl/src/com/badlogic/gdx/tests/lwjgl/LwjglTestStarter.java
@@ -0,0 +1,56 @@
+
+package com.badlogic.gdx.tests.lwjgl;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import com.badlogic.gdx.backends.desktop.LwjglApplication;
+import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.tests.utils.GdxTests;
+
+public class LwjglTestStarter {
+	static class TestList extends JPanel {
+		public TestList () {
+			setLayout(new BorderLayout());
+			
+			final JList list = new JList(GdxTests.getNames());
+			JButton button = new JButton("Run Test");
+			JScrollPane pane = new JScrollPane(list);
+
+			DefaultListSelectionModel m = new DefaultListSelectionModel();
+			m.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			m.setLeadAnchorNotificationEnabled(false);
+			list.setSelectionModel(m);			
+			
+			button.addActionListener(new ActionListener() {
+				
+				@Override public void actionPerformed (ActionEvent e) {
+					String testName = (String)list.getSelectedValue();
+					GdxTest test = GdxTests.newTest(testName);
+					LwjglApplication app = new LwjglApplication(testName, 480, 320, test.needsGL20());
+					app.getGraphics().setRenderListener(test);
+				}
+			});
+
+			add(pane, BorderLayout.CENTER);
+			add(button, BorderLayout.SOUTH);
+		}
+	}
+
+	public static void main (String[] argv) {
+		JFrame frame = new JFrame("Gdx - LWJGL Test Launcher");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new TestList());
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
