334e2aaab5b4697ab878edee9493275af7aa47d8
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-11-01 23:36:53 +0000

    [added] LwjglNativesLoader [changed] new LwjglApplication implementation

---
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/desktop/LwjglApplication.java
@@ -0,0 +1,129 @@
+package com.badlogic.gdx.backends.desktop;
+
+import java.awt.Canvas;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public class LwjglApplication implements Application {
+	LwjglGraphics graphics;
+	LwjglAudio audio;
+	LwjglFiles files;
+	LwjglInput input;
+	final ApplicationListener listener;
+	Thread mainLoopThread;
+	boolean running = true;
+	
+	public LwjglApplication(ApplicationListener listener, String title, int width, int height, boolean useGL2) {
+		this(listener, title, width, height, useGL2, null);
+	}
+	
+	public LwjglApplication(ApplicationListener listener, String title, int width, int height, boolean useGL2, Canvas canvas) {
+		graphics = new LwjglGraphics(title, width, height, useGL2, canvas);
+		audio = new LwjglAudio();
+		files = new LwjglFiles();
+		input = new LwjglInput();
+		this.listener = listener;
+		
+		Gdx.app = this;
+		Gdx.graphics = graphics;
+		Gdx.audio = audio;
+		Gdx.files = files;
+		Gdx.input = input;
+		initialize();
+	}
+	
+	private void initialize() {
+		LwjglNativesLoader.load();
+		mainLoopThread = new Thread("LWJGL Application") {
+			public void run () {				
+				LwjglApplication.this.mainLoop();							
+			}
+		};
+		mainLoopThread.start();
+	}
+	
+	private void mainLoop( ) {
+		try {
+			graphics.setupDisplay();
+		} catch (LWJGLException e) {
+			throw new GdxRuntimeException(e);
+		}		
+		
+		Keyboard.enableRepeatEvents(true);
+		listener.create();
+		
+		while(running && !Display.isCloseRequested()) {
+			graphics.updateTime();
+			input.update();
+			listener.render();
+			input.processEvents(null);
+			Display.update();
+			Display.sync(60);
+		}
+		
+		listener.pause();
+		listener.destroy();
+		Display.destroy();
+	}
+	
+	@Override
+	public Audio getAudio() {	
+		return audio;
+	}
+
+	@Override
+	public Files getFiles() {
+		return files;
+	}
+
+	@Override
+	public Graphics getGraphics() {
+		return graphics;
+	}
+
+	@Override
+	public Input getInput() {
+		return input;
+	}
+
+	@Override
+	public ApplicationType getType() {
+		return ApplicationType.Desktop;
+	}
+
+	@Override
+	public int getVersion() {
+		return 0;
+	}
+
+	@Override
+	public void log(String tag, String message) {
+		System.out.println( tag + ": " + message );
+	}
+
+	public void stop() {
+		running = false;
+		try {
+			mainLoopThread.join();
+		}
+		catch(Exception ex) {			
+		}
+	}
+
+	public void setSize(int viewWidth, int viewHeight) {
+		graphics.width = viewWidth;
+		graphics.height = viewHeight;
+	}
+
+}
---
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/desktop/LwjglNativesLoader.java
@@ -0,0 +1,88 @@
+package com.badlogic.gdx.backends.desktop;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import com.badlogic.gdx.Version;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public class LwjglNativesLoader {
+	static void load() {
+		System.setProperty("org.lwjgl.input.Mouse.allowNegativeMouseCoords", "true");
+		Version.loadLibrary();
+
+		String os = System.getProperty("os.name");
+		String arch = System.getProperty("os.arch");
+		boolean is64Bit = false;
+
+		if (arch.equals("amd64")) is64Bit = true;
+
+		if (os.contains("Windows")) loadLibrariesWindows(is64Bit);
+		if (os.contains("Linux")) loadLibrariesLinux(is64Bit);
+		if (os.contains("Mac")) loadLibrariesMac();
+
+		System.setProperty("org.lwjgl.librarypath", new File("").getAbsolutePath());
+	}
+
+	private static void loadLibrariesWindows (boolean is64Bit) {
+		String[] libNames = null;
+		if (is64Bit)
+			libNames = new String[] {"OpenAL64.dll", "lwjgl64.dll", "jinput-raw_64.dll", "jinput-dx8_64.dll"};
+		else
+			libNames = new String[] {"OpenAL32.dll", "lwjgl.dll", "jinput-raw.dll", "jinput-dx8.dll"};
+
+		for (String libName : libNames)
+			loadLibrary(libName, "/native/windows/");
+	}
+
+	private static void loadLibrariesLinux (boolean is64Bit) {
+		String[] libNames = null;
+		if (is64Bit)
+			libNames = new String[] {"libopenal64.so", "liblwjgl64.so", "jinput-linux64.so",};
+		else
+			libNames = new String[] {"libopenal.so", "liblwjgl.so", "jinput-linux.so",};
+
+		for (String libName : libNames)
+			loadLibrary(libName, "/native/linux/");
+	}
+
+	private static void loadLibrariesMac () {
+		throw new GdxRuntimeException("loading native libs on Mac OS X not supported, mail contact@badlogicgames.com");
+	}
+
+	private static void loadLibrary (String libName, String classPath) {
+		InputStream in = null;
+		BufferedOutputStream out = null;
+
+		try {
+			in = LwjglApplication.class.getResourceAsStream(classPath + libName);
+			out = new BufferedOutputStream(new FileOutputStream(libName));
+			byte[] bytes = new byte[1024 * 4];
+			while (true) {
+				int read_bytes = in.read(bytes);
+				if (read_bytes == -1) break;
+
+				out.write(bytes, 0, read_bytes);
+			}
+			out.close();
+			out = null;
+			in.close();
+			in = null;
+		} catch (Throwable t) {
+			new GdxRuntimeException("Couldn't load lwjgl native, " + libName, t);
+		} finally {
+			if (out != null) try {
+				out.close();
+			} catch (Exception ex) {
+			}
+			;
+			if (in != null) try {
+				in.close();
+			} catch (Exception ex) {
+			}
+		}
+	}
+
+}
