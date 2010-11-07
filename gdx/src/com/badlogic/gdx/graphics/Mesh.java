0c2f4f311d59a0dc53b2e074f5e583b640566956
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-11-07 15:09:18 +0000

    [changed] exposing setAutoBind() now. Could come in handy for rendering the same mesh multiple times. Will brake on Hero if you modify the vertices/indices due to a driver bug though.

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -174,16 +174,20 @@ public class Mesh {
 		return vertices.getAttributes().vertexSize;
 	}
 
-	// /**
-	// * Sets whether to bind the underlying {@link VertexArray} or
-	// * {@link VertexBufferObject} automatically on a call to one of the {@link
-	// #render(int)}
-	// * methods or not.
-	// * @param autoBind
-	// */
-	// public void setAutoBind( boolean autoBind ) {
-	// this.autoBind = autoBind;
-	// }
+	/**
+	 * Sets whether to bind the underlying {@link VertexArray} or
+	 * {@link VertexBufferObject} automatically on a call to one of the
+	 * {@link #render(int)} methods or not. Usually you want to use autobind.
+	 * Manual binding is an expert functionality. There is a driver bug
+	 * on the MSM720xa chips that will fuck up memory if you manipulate the
+	 * vertices and indices of a Mesh multiple times while it is bound. Keep 
+	 * this in mind.
+	 * 
+	 * @param autoBind whether to autobind meshes.
+	 */
+	public void setAutoBind(boolean autoBind) {
+		this.autoBind = autoBind;
+	}
 
 	/**
 	 * Binds the underlying {@link VertexArray}/{@link VertexBufferObject} and
