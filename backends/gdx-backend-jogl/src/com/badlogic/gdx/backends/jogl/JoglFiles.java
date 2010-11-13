b74a23889df704faddd2e05632024058288bf3b9
christoph.widulle <christoph.widulle@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-11-13 01:40:34 +0000

    [changed] leading "/" in resourcePath is optional now

--- backends/gdx-backend-jogl/src/com/badlogic/gdx/backends/jogl/JoglFiles.java
+++ backends/gdx-backend-jogl/src/com/badlogic/gdx/backends/jogl/JoglFiles.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright 2010 Mario Zechner (contact@badlogicgames.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
  * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
@@ -13,23 +13,18 @@
 
 package com.badlogic.gdx.backends.jogl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
+import java.io.*;
+
 /**
  * Implementation for a desktop application of {@link Files}. Internal resources are relative to the application root directory,
  * external files are relative to the user's home directory.
- * 
+ *
  * @author mzechner
- * 
+ *
  */
 final class JoglFiles implements Files {
 	private final String externalPath = System.getProperty("user.home") + "/";
@@ -68,7 +63,8 @@ final class JoglFiles implements Files {
 		if (type == FileType.External)
 			fileName = this.externalPath + fileName;
 		else if (type == FileType.Internal) {
-			InputStream input = JoglFileHandle.class.getResourceAsStream("/" + fileName);
+            final String path = fileName.startsWith("/") ? fileName : "/" + fileName;
+            InputStream input = JoglFileHandle.class.getResourceAsStream(path);
 			if (input != null) return input;
 		}
 
