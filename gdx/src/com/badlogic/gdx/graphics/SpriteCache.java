338d4a036b25526d2e5bf53c5e49f56ccbbf356f
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-11-24 08:38:56 +0000

    [fixed] SpriteCache using the wrong vertex count.

--- gdx/src/com/badlogic/gdx/graphics/SpriteCache.java
+++ gdx/src/com/badlogic/gdx/graphics/SpriteCache.java
@@ -99,7 +99,7 @@ public class SpriteCache {
 	 */
 	public void beginCache () {
 		if (currentCache != null) throw new IllegalStateException("endCache must be called before begin.");
-		currentCache = new Cache(caches.size(), mesh.getNumVertices() / 2 * 6);
+		currentCache = new Cache(caches.size(), mesh.getNumVertices() / 4 * 6);
 		caches.add(currentCache);
 		mesh.getVerticesBuffer().compact();
 	}
@@ -536,7 +536,7 @@ public class SpriteCache {
 		if (!drawing) throw new IllegalStateException("SpriteCache.begin must be called before draw.");
 
 		Cache cache = caches.get(cacheID);
-		offset = offset * 12 + cache.offset;
+		offset = offset * 6 + cache.offset;
 		length *= 6;
 		Texture[] textures = cache.textures;
 		int[] counts = cache.counts;
