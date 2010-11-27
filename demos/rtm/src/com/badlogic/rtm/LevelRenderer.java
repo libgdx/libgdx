d32b1be72d879705f65d5ec7a1715e146da1b7f1
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-11-27 16:14:32 +0000

    [fixed] demo license headers and content license.

--- demos/rtm/src/com/badlogic/rtm/LevelRenderer.java
+++ demos/rtm/src/com/badlogic/rtm/LevelRenderer.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2010 Mario Zechner (contact@badlogicgames.com), Nathan Sweet (admin@esotericsoftware.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package com.badlogic.rtm;
 
 import java.io.BufferedReader;
@@ -199,7 +212,7 @@ public class LevelRenderer implements ApplicationListener {
 		wallMesh.render(GL10.GL_TRIANGLES);
 		
 		batch.begin();
-		font.draw(batch, "fps: " + Gdx.graphics.getFramesPerSecond() + ", delta:" + Gdx.graphics.getDeltaTime(), 10, 10, Color.WHITE);
+		font.draw(batch, "fps: " + Gdx.graphics.getFramesPerSecond() + ", delta:" + Gdx.graphics.getDeltaTime(), 10, 10);
 		batch.end();
 		
 		processInput();
