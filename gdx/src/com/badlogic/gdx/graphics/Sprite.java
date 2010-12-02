1e7dfc9675ab8c807ff98e351c316e7f8153f6f2
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-12-02 03:00:55 +0000

    [changed] small improvement to Sprite, now with 4 multiplications less in case of rotation...

--- gdx/src/com/badlogic/gdx/graphics/Sprite.java
+++ gdx/src/com/badlogic/gdx/graphics/Sprite.java
@@ -414,31 +414,39 @@ public class Sprite {
 				localY2 *= scaleY;
 			}
 			if (rotation != 0) {
-				float cos = MathUtils.cosDeg(rotation);
-				float sin = MathUtils.sinDeg(rotation);
+				final float cos = MathUtils.cosDeg(rotation);
+				final float sin = MathUtils.sinDeg(rotation);
+				final float localXCos = localX * cos;
+				final float localXSin = localX * sin;
+				final float localYCos = localY * cos;
+				final float localYSin = localY * sin;
+				final float localX2Cos = localX2 * cos;
+				final float localX2Sin = localX2 * sin;
+				final float localY2Cos = localX2 * cos;
+				final float localY2Sin = localY2 * sin;
 
-				float x1 = localX * cos - localY * sin + worldOriginX;
-				float y1 = localY * cos + localX * sin + worldOriginY;
+				final float x1 = localXCos - localYSin + worldOriginX;
+				final float y1 = localYCos + localXSin + worldOriginY;
 				vertices[X1] = x1;
 				vertices[Y1] = y1;
 
-				float x2 = localX * cos - localY2 * sin + worldOriginX;
-				float y2 = localY2 * cos + localX * sin + worldOriginY;
+				final float x2 = localXCos - localY2Sin + worldOriginX;
+				final float y2 = localY2Cos + localXSin + worldOriginY;
 				vertices[X2] = x2;
 				vertices[Y2] = y2;
 
-				float x3 = localX2 * cos - localY2 * sin + worldOriginX;
-				float y3 = localY2 * cos + localX2 * sin + worldOriginY;
+				final float x3 = localX2Cos - localY2Sin + worldOriginX;
+				final float y3 = localY2Cos + localX2Sin + worldOriginY;
 				vertices[X3] = x3;
 				vertices[Y3] = y3;
 
 				vertices[X4] = x1 + (x3 - x2);
 				vertices[Y4] = y3 - (y2 - y1);
 			} else {
-				float x1 = localX + worldOriginX;
-				float y1 = localY + worldOriginY;
-				float x2 = localX2 + worldOriginX;
-				float y2 = localY2 + worldOriginY;
+				final float x1 = localX + worldOriginX;
+				final float y1 = localY + worldOriginY;
+				final float x2 = localX2 + worldOriginX;
+				final float y2 = localY2 + worldOriginY;
 
 				vertices[X1] = x1;
 				vertices[Y1] = y1;
