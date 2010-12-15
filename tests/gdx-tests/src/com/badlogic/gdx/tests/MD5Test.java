71d2b3fff912e04fac92334165e85163ffa2a0ee
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-12-15 18:57:31 +0000

    ["fixed"] MD5Test compiles again but to doesn't work anymore :( will figure it out.

--- tests/gdx-tests/src/com/badlogic/gdx/tests/MD5Test.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/MD5Test.java
@@ -39,12 +39,12 @@ public class MD5Test extends GdxTest implements InputProcessor {
 
 	@Override public void create () {
 		Gdx.app.log("MD5 Test", "created");
-		model = MD5Loader.loadModel(Gdx.files.internal("data/zfat.md5mesh").read());
+		model = MD5Loader.loadModel(Gdx.files.internal("data/zfat.md5mesh").read(), false);
 		anim = MD5Loader.loadAnimation(Gdx.files.internal("data/walk1.md5anim").read());
 		skeleton = new MD5Joints();
 		skeleton.joints = new float[anim.frames[0].joints.length];
 		animInfo = new MD5AnimationInfo(anim.frames.length, anim.secondsPerFrame);
-		renderer = new MD5Renderer(model, true);
+		renderer = new MD5Renderer(model, false, true);
 		renderer.setSkeleton(model.baseSkeleton);
 
 		// long start = System.nanoTime();
