d81eb2aee0026253a2aff6b2d100bc40b523dbd0
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2010-12-25 10:54:09 +0000

    [added] OpenAL based audio backend. Has in-memory and streaming. Missing recording.

---
+++ backends/gdx-audio/src/com/baglogic/gdx/audio/OggSound.java
@@ -0,0 +1,22 @@
+
+package com.baglogic.gdx.audio;
+
+import java.io.ByteArrayOutputStream;
+
+import com.badlogic.gdx.files.FileHandle;
+
+public class OggSound extends OpenALSound {
+	public OggSound (OpenALAudio audio, FileHandle file) {
+		super(audio);
+
+		OggInputStream input = new OggInputStream(file.read());
+		ByteArrayOutputStream output = new ByteArrayOutputStream(4096);
+		byte[] buffer = new byte[2048];
+		while (!input.atEnd()) {
+			int length = input.read(buffer);
+			if (length == -1) break;
+			output.write(buffer, 0, length);
+		}
+		setup(output.toByteArray(), input.getChannels(), input.getSampleRate());
+	}
+}
---
+++ backends/gdx-audio/src/com/baglogic/gdx/audio/OpenALMusic.java
@@ -0,0 +1,129 @@
+
+package com.baglogic.gdx.audio;
+
+import static org.lwjgl.openal.AL10.*;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
+
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public abstract class OpenALMusic implements Music {
+	static private final int bufferSize = 4096 * 10;
+	static private final int bufferCount = 3;
+	static private final byte[] tempBytes = new byte[bufferSize];
+	static private final ByteBuffer tempBuffer = BufferUtils.createByteBuffer(bufferSize);
+
+	private final OpenALAudio audio;
+	private final IntBuffer buffers;
+	private int streamID = -1;
+	private int format, sampleRate;
+	private boolean isLooping;
+	private float volume = 1;
+
+	protected final FileHandle file;
+
+	public OpenALMusic (OpenALAudio audio, FileHandle file) {
+		this.audio = audio;
+		this.file = file;
+
+		buffers = BufferUtils.createIntBuffer(bufferCount);
+		alGenBuffers(buffers);
+		if (alGetError() != AL_NO_ERROR) throw new GdxRuntimeException("Unabe to allocate audio buffers.");
+
+		audio.music.add(this);
+	}
+
+	protected void setup (int channels, int sampleRate) {
+		this.format = channels > 1 ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16;
+		this.sampleRate = sampleRate;
+	}
+
+	public void play () {
+		if (streamID == -1) {
+			streamID = audio.getIdleStreamID();
+			if (streamID == -1) return;
+			alSourcei(streamID, AL_LOOPING, AL_FALSE);
+			alSourcef(streamID, AL_GAIN, volume);
+			for (int i = 0; i < bufferCount; i++)
+				fill(buffers.get(i));
+			alSourceQueueBuffers(streamID, buffers);
+		}
+		alSourcePlay(streamID);
+	}
+
+	public void stop () {
+		reset();
+		alSourceStop(streamID);
+		streamID = -1;
+	}
+
+	public void pause () {
+		alSourcePause(streamID);
+	}
+
+	public boolean isPlaying () {
+		return alGetSourcei(streamID, AL_SOURCE_STATE) == AL_PLAYING;
+	}
+
+	public void setLooping (boolean isLooping) {
+		this.isLooping = isLooping;
+	}
+
+	public boolean isLooping () {
+		return isLooping;
+	}
+
+	public void setVolume (float volume) {
+		this.volume = volume;
+		if (streamID != -1) alSourcef(streamID, AL_GAIN, volume);
+	}
+
+	/**
+	 * Fills as much of the buffer as possible and returns the number of bytes filled. Returns <= 0 to indicate the end of the
+	 * stream.
+	 */
+	abstract protected int read (byte[] buffer);
+
+	/**
+	 * Resets the stream to the beginning.
+	 */
+	abstract protected void reset ();
+
+	public void update () {
+		if (streamID == -1) return;
+		int buffers = alGetSourcei(streamID, AL_BUFFERS_PROCESSED);
+		while (buffers-- > 0) {
+			int bufferID = alSourceUnqueueBuffers(streamID);
+			if (bufferID == AL_INVALID_VALUE) break;
+			if (!fill(bufferID)) return;
+			alSourceQueueBuffers(streamID, bufferID);
+		}
+	}
+
+	private boolean fill (int bufferID) {
+		tempBuffer.clear();
+		int length = read(tempBytes);
+		if (length <= 0) {
+			if (isLooping) {
+				reset();
+				length = read(tempBytes);
+				if (length <= 0) return false;
+			} else
+				return false;
+		}
+		tempBuffer.put(tempBytes, 0, length).flip();
+		alBufferData(bufferID, format, tempBuffer, sampleRate);
+		return true;
+	}
+
+	public void dispose () {
+		audio.music.removeValue(this, true);
+		stop();
+		alDeleteBuffers(buffers);
+	}
+}
