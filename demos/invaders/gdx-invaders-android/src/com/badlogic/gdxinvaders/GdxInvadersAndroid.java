765bec213ce7f62186f2a5c47b5abd87738e4a10
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-01-05 16:25:09 +0000

    [fixed] GdxInvadersAndroid and BouncyAndroid uses old initialize() method. Needed to add ResolutionStrategy. Christoph, please document this stuff!

--- demos/invaders/gdx-invaders-android/src/com/badlogic/gdxinvaders/GdxInvadersAndroid.java
+++ demos/invaders/gdx-invaders-android/src/com/badlogic/gdxinvaders/GdxInvadersAndroid.java
@@ -5,12 +5,13 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.surfaceview.FillResolutionStrategy;
 
 public class GdxInvadersAndroid extends AndroidApplication {
 	/** Called when the activity is first created. */
 	@Override public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		initialize(new GdxInvaders(),false, 20);		
+		initialize(new GdxInvaders(),false, new FillResolutionStrategy(), 20);		
 	}
 }
--- demos/vector-pinball/gdx-vectorpinball-android/src/com/dozingcatsoftware/bouncy/BouncyAndroid.java
+++ demos/vector-pinball/gdx-vectorpinball-android/src/com/dozingcatsoftware/bouncy/BouncyAndroid.java
@@ -4,11 +4,12 @@ package com.dozingcatsoftware.bouncy;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.surfaceview.FillResolutionStrategy;
 
 public class BouncyAndroid extends AndroidApplication {
 	/** Called when the activity is first created. */
 	@Override public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initialize(new Bouncy(), false, 16);
+		initialize(new Bouncy(), false, new FillResolutionStrategy(), 16);
 	}
 }
