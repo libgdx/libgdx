64bfeb885b587b4ca5510bc7c0fd93b9de84010c
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-01-07 05:15:43 +0000

    [fixed] issue 79, precision modifiers in SpriteBatch shader. silly me...

--- gdx/src/com/badlogic/gdx/graphics/g2d/SpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/SpriteBatch.java
@@ -200,7 +200,10 @@ public class SpriteBatch {
 			+ "uniform mat4 u_projectionViewMatrix; \n" + "varying vec4 v_color; \n" + "varying vec2 v_texCoords; \n" + " \n"
 			+ "void main()                  \n" + "{                            \n" + "   v_color = a_color; \n"
 			+ "   v_texCoords = a_texCoords; \n" + "   gl_Position =  u_projectionViewMatrix * a_position;  \n" + "}  \n";
-		String fragmentShader = "precision mediump float;\n" + "varying vec4 v_color;\n" + "varying vec2 v_texCoords;\n"
+		String fragmentShader = "#ifdef GL_ES\n"+
+										"precision mediump float;\n" +
+										"#endif\n" + 
+										"varying vec4 v_color;\n" + "varying vec2 v_texCoords;\n"
 			+ "uniform sampler2D u_texture;\n" + "void main()                                  \n"
 			+ "{                                            \n" + "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords); \n"
 			+ "}";
