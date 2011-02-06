340ea44edd3821bc2795c9ad7d6dc32270db59e7
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-02-06 13:08:17 +0000

    [fixed] Pixmap set default color to 1,1,1,1. wtf...

--- gdx/src/com/badlogic/gdx/graphics/Pixmap.java
+++ gdx/src/com/badlogic/gdx/graphics/Pixmap.java
@@ -116,8 +116,7 @@ public class Pixmap {
 	public Pixmap(int width, int height, Format format) {
 		pixmap = new Gdx2DPixmap(width, height, Format.toGdx2DPixmapFormat(format));
 		setColor(0, 0, 0, 0);
-		fill();
-		setColor(1, 1, 1, 1);
+		fill();		
 	}
 	
 	/**
