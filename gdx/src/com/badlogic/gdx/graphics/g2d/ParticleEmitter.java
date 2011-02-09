a50c99068aa58c2919c916ad8e58327841d05daa
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-02-09 11:23:13 +0000

    Fixed issue 126. setFlip now applies to any particles that may have already been created.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -671,6 +671,10 @@ public class ParticleEmitter {
 	public void setFlip (boolean flipX, boolean flipY) {
 		this.flipX = flipX;
 		this.flipY = flipY;
+		for (int i = 0, n = particles.length; i < n; i++) {
+			Particle particle = particles[i];
+			if (particle != null) particle.flip(flipX, flipY);
+		}
 	}
 
 	public void save (Writer output) throws IOException {
