d8b940a55b7cf972c9daf44631ec873c5f8cde7b
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-03-07 23:28:49 +0000

    [fixed] Copying a particle emitter with multiple colors.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -1140,7 +1140,7 @@ public class ParticleEmitter {
 
 		public void load (GradientColorValue value) {
 			super.load(value);
-			colors = new float[3];
+			colors = new float[value.colors.length];
 			System.arraycopy(value.colors, 0, colors, 0, colors.length);
 			timeline = new float[value.timeline.length];
 			System.arraycopy(value.timeline, 0, timeline, 0, timeline.length);
