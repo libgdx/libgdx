08e5edb9a002d8eb8e3345c2308a7d33650aba24
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-03-28 07:36:19 +0000

    [fixed] Javadoc typo.

--- gdx/src/com/badlogic/gdx/Files.java
+++ gdx/src/com/badlogic/gdx/Files.java
@@ -37,8 +37,8 @@ public interface Files {
 
 		/**
 		 * Path relative to the asset directory on Android and to the application's root directory on the desktop. On the desktop,
-		 * if the file is not, the classpath is then checked. This enables files to be found when using JWS or applets. Internal
-		 * files are always readonly.
+		 * if the file is not found, then the classpath is checked. This enables files to be found when using JWS or applets.
+		 * Internal files are always readonly.
 		 */
 		Internal,
 
