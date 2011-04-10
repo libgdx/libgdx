8390744df1c109fe291e4c5ef6a8a1b10cf4cf07
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-04-10 13:07:10 +0000

    [fixed] particle editor license headers

--- extensions/particle-editor/src/com/badlogic/gdx/graphics/particles/ScaledNumericPanel.java
+++ extensions/particle-editor/src/com/badlogic/gdx/graphics/particles/ScaledNumericPanel.java
@@ -1,4 +1,18 @@
-
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.badlogic.gdx.graphics.particles;
 
 import java.awt.Dimension;
@@ -110,7 +124,7 @@ class ScaledNumericPanel extends EditorPanel {
 				GridBagConstraints expandButtonConstraints = layout.getConstraints(expandButton);
 				if (expanded) {
 					chart.setPreferredSize(new Dimension(150, 200));
-					expandButton.setText("–");
+					expandButton.setText("ï¿½");
 					chartConstraints.weightx = 1;
 					expandButtonConstraints.weightx = 0;
 				} else {
