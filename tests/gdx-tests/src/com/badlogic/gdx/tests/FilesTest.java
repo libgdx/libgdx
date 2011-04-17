0d7b37b8f79be6bcb69a7b19eddeabe71f287d78
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-04-17 11:30:03 +0000

    [changed] Linked the desktop test projects to the Android test project's "assets" dir. No more juggling multiple copies of assets!

--- tests/gdx-tests/src/com/badlogic/gdx/tests/FilesTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/FilesTest.java
@@ -1,18 +1,16 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  ******************************************************************************/
+
 package com.badlogic.gdx.tests;
 
 import java.io.BufferedReader;
@@ -24,6 +22,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL10;
@@ -168,8 +167,11 @@ public class FilesTest extends GdxTest {
 			fail();
 		} catch (Exception ignored) {
 		}
-		FileHandle dir = Gdx.files.internal("data");
-		if (!dir.path().equals("data")) fail();
+		FileHandle dir;
+		if (Gdx.app.getType() == ApplicationType.Android)
+			dir = Gdx.files.internal("data");
+		else
+			dir = Gdx.files.internal("../gdx-tests-android/assets/data");
 		if (!dir.exists()) fail();
 		if (!dir.isDirectory()) fail();
 		if (dir.list().length == 0) fail();
