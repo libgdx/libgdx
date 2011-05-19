89a0b1f8e82d25625317ab0f5ef7722429e7c0ff
eric.spitz2000@gmail.com <eric.spitz2000@gmail.com@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-05-19 18:43:04 +0000

    [Moved] ObjLoader in extensions to .g3d.loaders.wavefront to avoid conflicts with ObjLoader in core

--- extensions/model-loaders/src/com/badlogic/gdx/graphics/g3d/loaders/wavefront/ObjLoader.java
+++ extensions/model-loaders/src/com/badlogic/gdx/graphics/g3d/loaders/wavefront/ObjLoader.java
@@ -11,7 +11,7 @@
  * governing permissions and limitations under the License.
  */
 
-package com.badlogic.gdx.graphics.g3d.loaders.obj;
+package com.badlogic.gdx.graphics.g3d.loaders.wavefront;
 
 import java.io.BufferedReader;
 import java.io.IOException;
