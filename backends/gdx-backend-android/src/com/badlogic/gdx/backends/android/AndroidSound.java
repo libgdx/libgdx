15a583cd34b59e63b6f252fb06763bd29c60ea67
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-05-26 07:10:01 +0000

    Fixed issue 249. We now store the last 8 IDs for each sound. Calling stop will stop all instances of the sound.

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidSound.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidSound.java
@@ -13,17 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
+
 package com.badlogic.gdx.backends.android;
 
 import android.media.AudioManager;
 import android.media.SoundPool;
 
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.IntArray;
 
 final class AndroidSound implements Sound {
 	final SoundPool soundPool;
 	final AudioManager manager;
 	final int soundId;
+	final IntArray streamIds = new IntArray(8);
 
 	AndroidSound (SoundPool pool, AudioManager manager, int soundId) {
 		this.soundPool = pool;
@@ -40,10 +43,12 @@ final class AndroidSound implements Sound {
 	}
 
 	@Override public void play (float volume) {
-		soundPool.play(soundId, volume, volume, 1, 0, 1);
+		if (streamIds.size == 8) streamIds.pop();
+		streamIds.add(soundPool.play(soundId, volume, volume, 1, 0, 1));
 	}
 
 	public void stop () {
-		soundPool.stop(soundId);
+		for (int i = 0, n = streamIds.size; i < n; i++)
+			soundPool.stop(streamIds.get(i));
 	}
 }
