c03ad18ca93d2c460983e9f84bd387939f70950e
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-06-02 21:10:08 +0000

    [added] new lightmaps...

--- extensions/model-loaders/model-loaders/src/com/badlogic/gdx/graphics/g3d/test/QbobViewer.java
+++ extensions/model-loaders/model-loaders/src/com/badlogic/gdx/graphics/g3d/test/QbobViewer.java
@@ -2,7 +2,7 @@ package com.badlogic.gdx.graphics.g3d.test;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.backends.jogl.JoglApplication;
+import com.badlogic.gdx.backends.jogl.JoglApplication;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.GL11;
@@ -140,7 +140,7 @@ public class QbobViewer implements ApplicationListener {
 	@Override public void dispose () {		
 	}	
 	
-//	public static void main(String[] argv) {
-//		new JoglApplication(new QbobViewer(), "Qbob Viewer", 800, 480, false);
-//	}
+	public static void main(String[] argv) {
+		new JoglApplication(new QbobViewer(), "Qbob Viewer", 800, 480, false);
+	}
 }
