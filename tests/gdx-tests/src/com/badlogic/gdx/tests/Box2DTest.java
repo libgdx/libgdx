1945e95eee4a1d2e59b94843fdea187b09ce5bc3
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-06-15 17:15:44 +0000

    [added] post/pre solve stuff to Box2DTest. Major log output ahead :D

--- tests/gdx-tests/src/com/badlogic/gdx/tests/Box2DTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/Box2DTest.java
@@ -34,8 +34,12 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.Manifold.ManifoldPoint;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.QueryCallback;
 import com.badlogic.gdx.physics.box2d.World;
@@ -129,6 +133,39 @@ public class Box2DTest extends GdxTest implements InputProcessor {
 		groundPoly.dispose();
 
 		createBoxes();
+		
+		world.setContactListener(new ContactListener() {
+			@Override
+			public void beginContact(Contact contact) {
+				System.out.println("begin contact");				
+			}
+
+			@Override
+			public void endContact(Contact contact) {
+				System.out.println("end contact");
+			}
+
+			@Override
+			public void preSolve(Contact contact, Manifold oldManifold) {
+				Manifold.ManifoldType type = oldManifold.getType();
+				Vector2 localPoint = oldManifold.getLocalPoint();
+				Vector2 localNormal = oldManifold.getLocalNormal();
+				int pointCount = oldManifold.getPointCount();
+				ManifoldPoint[] points = oldManifold.getPoints();
+				System.out.println("pre solve, " + type + 
+									", point: " + localPoint +
+									", local normal: " + localNormal + 
+									", #points: " + pointCount + 
+									", [" + points[0] + ", " + points[1] + "]");				
+			}
+
+			@Override
+			public void postSolve(Contact contact, ContactImpulse impulse) {
+				float[] ni = impulse.getNormalImpulses();
+				float[] ti = impulse.getTangentImpulses();		
+				System.out.println("post solve, normal impulses: " + ni[0] + ", " + ni[1] + ", tangent impulses: " + ti[0] + ", " + ti[1]);				
+			}			
+		});
 	}
 
 	private void createBoxes () {
