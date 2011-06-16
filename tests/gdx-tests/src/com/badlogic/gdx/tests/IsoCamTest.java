04d80f1e2a3fd5c82a41ab49a6e635bd7e8601db
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-06-16 22:33:56 +0000

    [changed] IsoCamTest is now a fully working isometric tile rendering example with camera drag. tiles are asumed to be 1x1 units in size. The spritebatch used to render is using a rotation matrix so everything is rendered in the x/z plane instead of the usual x/y plane. the camera itself is located above the x/z plane, looking down, left, along the negative z-axis for a 45 degree iso effect.

--- tests/gdx-tests/src/com/badlogic/gdx/tests/IsoCamTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/IsoCamTest.java
@@ -16,51 +16,113 @@
 package com.badlogic.gdx.tests;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Plane;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
 import com.badlogic.gdx.tests.utils.GdxTest;
 
-public class IsoCamTest extends GdxTest {
+public class IsoCamTest extends GdxTest implements InputProcessor {
 
 	@Override public boolean needsGL20 () {
 		return false;
 	}
 
 	Texture texture;
-	Mesh mesh;
 	OrthographicCamera cam;
-//	Matrix4 projection;
-//	Matrix4 view;
+	SpriteBatch batch;	
+	final Sprite[][] sprites = new Sprite[10][10];
+	final Matrix4 matrix = new Matrix4();
+	final Plane xzPlane = new Plane(new Vector3(0, 1, 0), 0);
+	final Vector3 intersection = new Vector3();
+	Sprite lastSelectedTile = null;
 	
 	@Override public void create() {
-		mesh = new Mesh(true, 4, 6, new VertexAttribute(Usage.Position, 3, "a_pos"));
-		mesh.setVertices(new float[] {
-			-1, 0,  1,
-			 1, 0,  1,
-			 1, 0, -1,
-			-1, 0, -1
-		});
-		mesh.setIndices(new short[] { 0, 1, 2, 2, 3, 0 });		
-		cam = new OrthographicCamera(10, 10);		
-		cam.position.set(3, 3, 3);
-		cam.lookAt(0, 0, 0);			
+		texture = new Texture(Gdx.files.internal("data/badlogicsmall.jpg"));		
+		cam = new OrthographicCamera(10, 10 * (Gdx.graphics.getHeight() / (float)Gdx.graphics.getWidth()));			
+		cam.position.set(5, 5, 10);
+		cam.direction.set(-1, -1, -1);
+		cam.near = 1;
+		cam.far = 100;
+		
+		// rotation matrix so we rotate the x/y plane spritebatch
+		// operates on to tze x/z plane.
+		matrix.setToRotation(new Vector3(1, 0, 0), 90);
+		
+		for(int z = 0; z < 10; z++) {
+			for(int x = 0; x < 10; x++) {
+				sprites[x][z] = new Sprite(texture);
+				sprites[x][z].setPosition(x,z);
+				sprites[x][z].setSize(1, 1);
+			}
+		}
+		
+		batch = new SpriteBatch();
+		
+		Gdx.input.setInputProcessor(this);
 	}
 	
 	@Override public void render() {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
 		cam.update();		
-		Gdx.gl10.glMatrixMode(GL10.GL_PROJECTION);
-		Gdx.gl10.glLoadMatrixf(cam.projection.val, 0);
-		Gdx.gl10.glMatrixMode(GL10.GL_MODELVIEW);
-		Gdx.gl10.glLoadMatrixf(cam.view.val, 0);
+				
+		batch.setProjectionMatrix(cam.combined);
+		batch.setTransformMatrix(matrix);
+		batch.begin();
+		for(int z = 0; z < 10; z++) {
+			for(int x = 0; x < 10; x++) {
+				sprites[x][z].draw(batch);
+			}
+		}
+		batch.end();
 		
-		mesh.render(GL10.GL_TRIANGLES);
+		if(Gdx.input.justTouched()) {
+			Ray pickRay = cam.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+			Intersector.intersectRayPlane(pickRay, xzPlane, intersection);
+			int x = (int)intersection.x;
+			int z = (int)intersection.z;
+			if(x >= 0 && x < 10 && z >= 0 && z < 10) {
+				if(lastSelectedTile != null) lastSelectedTile.setColor(1, 1, 1, 1);
+				Sprite sprite = sprites[x][z];
+				sprite.setColor(1, 0, 0, 1);
+				lastSelectedTile = sprite;
+			}
+		}
 	}
+	
+	final Vector3 curr = new Vector3();
+	final Vector3 last = new Vector3(-1, -1, -1);
+	final Vector3 delta = new Vector3();
+	@Override public boolean touchDragged (int x, int y, int pointer) {
+		Ray pickRay = cam.getPickRay(x, y);
+		Intersector.intersectRayPlane(pickRay, xzPlane, curr);
+		
+		if(!(last.x == -1 && last.y == -1 && last.z == -1)) {
+			pickRay = cam.getPickRay(last.x, last.y);
+			Intersector.intersectRayPlane(pickRay, xzPlane, delta);			
+			delta.sub(curr);
+			cam.position.add(delta.x, delta.y, delta.z);
+		}
+		last.set(x, y, 0);
+		return false;
+	}
+	
+	@Override public boolean touchUp(int x, int y, int pointer, int button) {
+		last.set(-1, -1, -1);
+		return false;
+	}
+
+	@Override public boolean keyDown (int keycode) { return false; }	
+	@Override public boolean keyUp (int keycode) { return false; }
+	@Override public boolean keyTyped (char character) { return false; }
+	@Override public boolean touchDown (int x, int y, int pointer, int button) { return false; }		
+	@Override public boolean touchMoved (int x, int y) { return false; }
+	@Override public boolean scrolled (int amount) { return false; }
 }
