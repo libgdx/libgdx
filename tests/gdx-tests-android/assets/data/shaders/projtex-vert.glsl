0f49d488d9e0450609c21f618bbaad7ea5cf5dba
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-07-03 13:30:00 +0000

    [fixed] silly projective texturing shader...

--- tests/gdx-tests-android/assets/data/shaders/projtex-vert.glsl
+++ tests/gdx-tests-android/assets/data/shaders/projtex-vert.glsl
@@ -15,12 +15,12 @@ void main() {
 	vec4 worldPos = u_model * a_Position;
 	vec3 worldNormal = (u_modelNormal * vec4(a_Normal, 1)).xyz;	
 	vec3 lightDir = u_projectorPos - worldPos.xyz;
-	normalize(lightDir);	
-	normalize(worldNormal);
+	lightDir = normalize(lightDir);	
+	worldNormal = normalize(worldNormal);
 		
 	float dotL = dot(lightDir, worldNormal);
 	if(dotL < 0.0) dotL = 0.0;
 	v_color = u_color * dotL;
-	v_texcoords = u_projector * worldPos;	
+	v_texcoords = u_projector * worldPos;
 	gl_Position = u_camera * worldPos;
 }
