aa153c05af20461798e8acaa4931174c752caff6
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-07-04 08:42:14 +0000

    [fixed] issue 324, Button checked condition for SpriteBatch.draw method selection incorrectly.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actors/Button.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actors/Button.java
@@ -83,7 +83,7 @@ public class Button extends Actor implements Layout {
 		TextureRegion region = pressed ? pressedRegion : unpressedRegion;
 		batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
 		if (region.getTexture() != null) {
-			if (scaleX == 0 && scaleY == 0 && rotation == 0)
+			if (scaleX == 1 && scaleY == 1 && rotation == 0)
 				batch.draw(region, x, y, width, height);
 			else
 				batch.draw(region, x, y, originX, originY, width, height, scaleX, scaleY, rotation);
