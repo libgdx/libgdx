61ce9ce3aa1b22b4eba9e9438d1eca2158ce2074
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-07-24 15:50:58 +0000

    [added] GwtApplication stuff. the log methods are far from optimal, should allow user to define where things go (e.g. Surface3D and log label).

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtApplication.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtApplication.java
@@ -2,16 +2,31 @@ package com.badlogic.gdx.backends.gwt;
 
 import gwt.g2d.client.util.FpsTimer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 
-public abstract class GwtApplication implements EntryPoint {
+public abstract class GwtApplication implements EntryPoint, Application {
 	private ApplicationListener listener;
 	private GwtApplicationConfiguration config;
 	private GwtGraphics graphics;
+	private Label log = null;
+	private int logLevel = LOG_ERROR;
+	private List<Runnable> runnables = new ArrayList<Runnable>();
 	
 	@Override
 	public void onModuleLoad() {
@@ -43,6 +58,10 @@ public abstract class GwtApplication implements EntryPoint {
 		FpsTimer timer = new FpsTimer(config.fps) {
 			@Override
 			public void update() {
+				for(int i = 0; i < runnables.size(); i++) {
+					runnables.get(i).run();
+				}
+				runnables.clear();
 				listener.render();
 			}
 		};
@@ -51,4 +70,106 @@ public abstract class GwtApplication implements EntryPoint {
 
 	public abstract GwtApplicationConfiguration getConfig();
 	public abstract ApplicationListener getApplicationListener();
+
+	@Override
+	public Graphics getGraphics() {
+		return graphics;
+	}
+
+	@Override
+	public Audio getAudio() {
+		// FIXME
+		throw new GdxRuntimeException("not implemented");
+	}
+
+	@Override
+	public Input getInput() {
+		// FIXME
+		throw new GdxRuntimeException("not implemented");
+	}
+
+	@Override
+	public Files getFiles() {
+		// FIXME
+		throw new GdxRuntimeException("not implemented");
+	}
+
+	private void checkLogLabel() {
+		if(log == null) {
+			log = new Label();
+			RootPanel.get().add(log);
+		}
+	}
+	
+	@Override
+	public void log(String tag, String message) {
+		if(logLevel >= LOG_INFO) {
+			checkLogLabel();
+			log.setText(log.getText() + "\n" + tag + ": " + message);
+		}
+	}
+
+	@Override
+	public void log(String tag, String message, Exception exception) {
+		if(logLevel >= LOG_INFO) {
+			checkLogLabel();
+			log.setText(log.getText() + "\n" + tag + ": " + message + "\n" + exception.getMessage());
+		}	
+	}
+
+	@Override
+	public void error(String tag, String message) {
+		if(logLevel >= LOG_ERROR) {
+			checkLogLabel();
+			log.setText(log.getText() + "\n" + tag + ": " + message);
+		}
+	}
+
+	@Override
+	public void error(String tag, String message, Exception exception) {
+		if(logLevel >= LOG_ERROR) {
+			checkLogLabel();
+			log.setText(log.getText() + "\n" + tag + ": " + message + "\n" + exception.getMessage());
+		}		
+	}
+
+	@Override
+	public void setLogLevel(int logLevel) {
+		this.logLevel = logLevel;
+	}
+
+	@Override
+	public ApplicationType getType() {
+		return ApplicationType.WebGL;
+	}
+
+	@Override
+	public int getVersion() {
+		return 0;
+	}
+
+	@Override
+	public long getJavaHeap() {
+		return 0;
+	}
+
+	@Override
+	public long getNativeHeap() {
+		return 0;
+	}
+
+	@Override
+	public Preferences getPreferences(String name) {
+		// FIXME
+		throw new GdxRuntimeException("not implemented");
+	}
+
+	@Override
+	public void postRunnable(Runnable runnable) {
+		runnables.add(runnable);
+	}
+
+	@Override
+	public void exit() {		
+	}
 }
