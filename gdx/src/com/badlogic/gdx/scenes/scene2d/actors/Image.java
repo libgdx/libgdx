3921b4761e32964f15705f0de5a40f080119d40e
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-07-26 17:28:14 +0000

    [fixed] tiny bozo in Image, used slow path all the time.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actors/Image.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actors/Image.java
@@ -48,7 +48,7 @@ public class Image extends Actor {
 	@Override protected void draw (SpriteBatch batch, float parentAlpha) {
 		if (region.getTexture() != null) {
 			batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-			if (scaleX == 0 && scaleY == 0 && rotation == 0)
+			if (scaleX == 1 && scaleY == 1 && rotation == 0)
 				batch.draw(region, x, y, width, height);
 			else
 				batch.draw(region, x, y, originX, originY, width, height, scaleX, scaleY, rotation);
