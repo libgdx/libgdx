2333e1ff1c5f97a976778b9607b512f85f77bcc8
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-08-04 05:53:31 +0000

    [fixed] issue 371, thanks Kurt!

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
@@ -112,7 +112,11 @@ public class AndroidApplication extends Activity implements Application {
        Gdx.files = this.getFiles();
        Gdx.graphics = this.getGraphics();
 
-       requestWindowFeature(Window.FEATURE_NO_TITLE);
+       try {
+      	 requestWindowFeature(Window.FEATURE_NO_TITLE);
+       } catch(Exception ex) {
+      	 log("AndroidApplication", "Content already displayed, cannot request FEATURE_NO_TITLE", ex);
+       }
        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
        setContentView(graphics.getView(), createLayoutParams());
