1b94114257683ffc1ad75c8cc466a647d49754ea
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-08-10 22:27:06 +0000

    [added] ETC1TextureData, not operational yet.

---
+++ gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
@@ -0,0 +1,77 @@
+package com.badlogic.gdx.graphics.glutils;
+
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.TextureData;
+import com.badlogic.gdx.graphics.glutils.ETC1.ETC1Data;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public class ETC1TextureData implements TextureData {
+	FileHandle file;
+	int width = 0;
+	int height = 0;
+	
+	public ETC1TextureData(FileHandle file) {
+		this.file = file;
+	}
+
+	@Override
+	public TextureDataType getType () {
+		return TextureDataType.Compressed;
+	}
+
+	@Override
+	public Pixmap getPixmap () {
+		throw new GdxRuntimeException("This TextureData implementation does not return a Pixmap");
+	}
+
+	@Override
+	public boolean disposePixmap () {
+		throw new GdxRuntimeException("This TextureData implementation does not return a Pixmap");
+	}
+
+	@Override
+	public void uploadCompressedData () {
+		ETC1Data data = new ETC1Data(file);
+		width = data.width;
+		height = data.height;
+		
+		if(Gdx.app.getType() == ApplicationType.Desktop) {
+			Pixmap pixmap = ETC1.decodeImage(data, Format.RGB565);
+			Gdx.gl.glTexImage2D(GL10.GL_TEXTURE_2D, 0, pixmap.getGLInternalFormat(), pixmap.getWidth(), pixmap.getHeight(), 0, pixmap.getGLFormat(), pixmap.getGLType(), pixmap.getPixels());
+			pixmap.dispose();
+		} else {
+			
+		}
+		data.dispose();
+	}
+
+	@Override
+	public int getWidth () {
+		return width;
+	}
+
+	@Override
+	public int getHeight () {
+		return height;
+	}
+
+	@Override
+	public Format getFormat () {
+		return Format.RGB565;
+	}
+
+	@Override
+	public boolean useMipMaps () {
+		return false;
+	}
+
+	@Override
+	public boolean isManaged () {
+		return true;
+	}
+}
