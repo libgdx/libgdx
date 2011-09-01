262bb46fd60dc16cf85289c02ee3b932d0bb51e3
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-09-01 05:58:22 +0000

    [fixed] AssetManager.preload() checks if asset with given name is in preload queue/task list/already loaded but has different type. Results in GdxRuntimeException.

--- gdx/src/com/badlogic/gdx/assets/AssetManager.java
+++ gdx/src/com/badlogic/gdx/assets/AssetManager.java
@@ -191,6 +191,25 @@ public class AssetManager implements Disposable {
 			loaded = 0;
 			toLoad = 0;
 		}
+		
+		// check if an asset with the same name but a different type has already been added.
+		
+		// check preload queue
+		for(int i = 0; i < preloadQueue.size; i++) {
+			AssetDescriptor desc = preloadQueue.get(i);
+			if(desc.fileName.equals(fileName) && !desc.type.equals(type)) throw new GdxRuntimeException("Asset with name '" + fileName + "' already in preload queue, but has different type (expected: " + type.getSimpleName() + ", found: " + desc.type.getSimpleName());
+		}
+		
+		// check task list
+		for(int i = 0; i < tasks.size(); i++) {
+			AssetDescriptor desc = tasks.get(i).assetDesc;
+			if(desc.fileName.equals(fileName) && !desc.type.equals(type)) throw new GdxRuntimeException("Asset with name '" + fileName + "' already in task list, but has different type (expected: " + type.getSimpleName() + ", found: " + desc.type.getSimpleName());
+		}
+		
+		// check loaded assets
+		Class otherType = assetTypes.get(fileName);
+		if(otherType != null && !otherType.equals(type)) throw new GdxRuntimeException("Asset with name '" + fileName + "' already loaded, but has different type (expected: " + type.getSimpleName() + ", found: " + otherType.getSimpleName());
+		
 		toLoad++;
 		AssetDescriptor assetDesc = new AssetDescriptor(fileName, type, parameter);
 		preloadQueue.add(assetDesc);
