7c9b790f6e22526b8af4633c9699f314562972c0
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-09-04 00:02:30 +0000

    [fixed] issue 434

--- gdx/src/com/badlogic/gdx/physics/box2d/RayCastCallback.java
+++ gdx/src/com/badlogic/gdx/physics/box2d/RayCastCallback.java
@@ -26,7 +26,10 @@ public interface RayCastCallback {
 
 	/** Called for each fixture found in the query. You control how the ray cast proceeds by returning a float: return -1: ignore
 	 * this fixture and continue return 0: terminate the ray cast return fraction: clip the ray to this point return 1: don't clip
-	 * the ray and continue
+	 * the ray and continue.
+	 * 
+	 * The {@link Vector2} instances passed to the callback will be reused for future calls so make a copy of them!
+	 * 
 	 * @param fixture the fixture hit by the ray
 	 * @param point the point of initial intersection
 	 * @param normal the normal vector at the point of intersection
