488289b00d87d4f6b8673985014732b6f9beb8b6
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-09-05 07:00:01 +0000

    [added] contact rendering

--- gdx/src/com/badlogic/gdx/physics/box2d/Box2DDebugRenderer.java
+++ gdx/src/com/badlogic/gdx/physics/box2d/Box2DDebugRenderer.java
@@ -218,7 +218,10 @@ public class Box2DDebugRenderer {
 	}
 
 	private void drawContact (Contact contact) {
-		
+		WorldManifold worldManifold = contact.getWorldManifold();
+		if(worldManifold.getNumberOfContactPoints() == 0) return;
+		Vector2 point = worldManifold.getPoints()[0];
+		renderer.point(point.x, point.y, 0);
 	}
 
 	public void dispose () {
