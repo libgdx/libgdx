f820a8094d1454959540cc9bda40d7e617a6b88b
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-09-20 08:38:06 +0000

    [changed] played around with StbTrueTypeTest a little. Quality sucks...

--- tests/gdx-tests/src/com/badlogic/gdx/tests/StbTrueTypeTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/StbTrueTypeTest.java
@@ -31,7 +31,7 @@ public class StbTrueTypeTest extends GdxTest {
 		System.out.println(font.getCodePointBox('e'));
 		Bitmap glyphBitmap = font.makeCodepointBitmap(scale, scale, 'e');
 		glyphBitmap.dispose();
-		int glyphIndex = font.findGlyphIndex('e');
+		int glyphIndex = font.findGlyphIndex('T');
 		glyphBitmap = font.makeGlyphBitmap(scale, scale, 0, 0, glyphIndex);
 		texture = new Texture(glyphBitmap.pixmap);
 		texture.bind();
@@ -41,11 +41,11 @@ public class StbTrueTypeTest extends GdxTest {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
-		batch.setColor(1, 0, 0, 1);
+		batch.setColor(0, 0, 0, 1);
 		batch.setBlendFunction(GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA);
 		batch.draw(texture, 100, 100);
 		batch.end();
