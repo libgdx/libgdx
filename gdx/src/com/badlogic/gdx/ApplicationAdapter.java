50b4f08ef8107c4aeaad0f3a79ebce4d1e198659
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-09-26 02:28:07 +0000

    [added] ApplicationAdapter.

---
+++ gdx/src/com/badlogic/gdx/ApplicationAdapter.java
@@ -0,0 +1,47 @@
+package com.badlogic.gdx;
+
+/**
+ * Convenience implementation of {@link ApplicationListener}. Derrive from this and only
+ * override what you need.
+ * @author mzechner
+ *
+ */
+public abstract class ApplicationAdapter implements ApplicationListener {
+
+	@Override
+	public void create () {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize (int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render () {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause () {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume () {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose () {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
