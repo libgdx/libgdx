6b80162dedcbab591bdd217b10a1cb0c96195dda
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-12-03 18:23:03 +0000

    [fixed] issue 538, LwjglGL10.glTexImage2D exploded when passing in a null pointer.

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglGL10.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglGL10.java
@@ -421,8 +421,10 @@ class LwjglGL10 implements GL10 {
 	public final void glTexImage2D (int target, int level, int internalformat, int width, int height, int border, int format,
 		int type, Buffer pixels) {
 
-		if (pixels instanceof ByteBuffer)
-			GL11.glTexImage2D(target, level, internalformat, width, height, border, format, type, (ByteBuffer)pixels);
+		if (pixels == null)
+			GL11.glTexImage2D(target, level, internalformat, width, height, border, format, type, (ByteBuffer)null);
+		else if (pixels instanceof ByteBuffer)
+			 GL11.glTexImage2D(target, level, internalformat, width, height, border, format, type, (ByteBuffer)pixels);
 		else if (pixels instanceof ShortBuffer)
 			GL11.glTexImage2D(target, level, internalformat, width, height, border, format, type, (ShortBuffer)pixels);
 		else if (pixels instanceof IntBuffer)
