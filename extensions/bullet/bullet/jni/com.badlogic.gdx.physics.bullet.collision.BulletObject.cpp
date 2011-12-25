c1a3b429f0f12948acb1ec6273516b8479b358bb
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-12-25 23:24:25 +0000

    [fixed] bullet object with new jnigen.

--- extensions/bullet/bullet/jni/com.badlogic.gdx.physics.bullet.collision.BulletObject.cpp
+++ extensions/bullet/bullet/jni/com.badlogic.gdx.physics.bullet.collision.BulletObject.cpp
@@ -3,13 +3,13 @@
 (JNIEnv* env, jclass clazz, jlong addr, jobject obj_buffer, jfloatArray obj_floats, jbyteArray obj_bytes, jstring obj_ohgod) {
 	char* buffer = (char*)env->GetDirectBufferAddress(obj_buffer);
 	char* ohgod = (char*)env->GetStringUTFChars(obj_ohgod, 0);
-	float* floats = (float*)env->GetPrimitiveArrayListCritical(obj_floats, 0);
-	char* bytes = (char*)env->GetPrimitiveArrayListCritical(obj_bytes, 0);
+	float* floats = (float*)env->GetPrimitiveArrayCritical(obj_floats, 0);
+	char* bytes = (char*)env->GetPrimitiveArrayCritical(obj_bytes, 0);
 
 	printf("%f %d %s\n", floats[0], bytes[0], ohgod); asdhasdkjhash
 	
-	env->ReleasePrimitiveArrayListCritical(obj_floats, floats, 0);
-	env->ReleasePrimitiveArrayListCritical(obj_bytes, bytes, 0);
+	env->ReleasePrimitiveArrayCritical(obj_floats, floats, 0);
+	env->ReleasePrimitiveArrayCritical(obj_bytes, bytes, 0);
 	env->ReleaseStringUTFChars(obj_ohgod, ohgod);
 }
 JNIEXPORT jint JNICALL Java_com_badlogic_gdx_physics_bullet_collision_BulletObject_testJni2
