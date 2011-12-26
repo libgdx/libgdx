e37b7d68b5274ad7f6b186f0e83af0643a4fced5
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-12-26 02:24:33 +0000

    [fixed] stbtt build now includes android target

---
+++ extensions/stb-truetype/jni/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+ 
+LOCAL_MODULE    := stbtruetype
+LOCAL_C_INCLUDES := 
+ 
+LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
+LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
+LOCAL_LDLIBS := -lm
+LOCAL_ARM_MODE  := arm
+ 
+LOCAL_SRC_FILES := com.badlogic.gdx.graphics.g2d.stbtt.StbTrueType.cpp
+ 
+include $(BUILD_SHARED_LIBRARY)
