4b6dd2f8ae5c0d46abb904722c65b7b25562f045
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-12-26 12:11:21 +0000

    [removed] vorbis and mpg123 decoder from gdx core

--- tests/gdx-tests/src/com/badlogic/gdx/tests/Mpg123Test.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/Mpg123Test.java
@@ -16,41 +16,33 @@
 
 package com.badlogic.gdx.tests;
 
-import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
-
-import com.badlogic.gdx.Application.ApplicationType;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.analysis.AudioTools;
-import com.badlogic.gdx.audio.analysis.KissFFT;
-import com.badlogic.gdx.audio.io.Mpg123Decoder;
 import com.badlogic.gdx.tests.utils.GdxTest;
 
 public class Mpg123Test extends GdxTest {
 
 	@Override
 	public void create () {
-		String file = null;
-		if (Gdx.app.getType() == ApplicationType.Android)
-			file = "/sdcard/audio/schism.mp3";
-		else
-			file = "data/threeofaperfectpair.mp3";
-
-		Mpg123Decoder decoder = new Mpg123Decoder(file);
-		ShortBuffer stereoSamples = AudioTools.allocateShortBuffer(1024, decoder.getNumChannels());
-		ShortBuffer monoSamples = AudioTools.allocateShortBuffer(1024, 1);
-		FloatBuffer spectrum = AudioTools.allocateFloatBuffer(1024 / 2 + 1, 1);
-		KissFFT fft = new KissFFT(1024);
-
-		Gdx.app.log("Mpg123",
-			"rate: " + decoder.getRate() + ", channels: " + decoder.getNumChannels() + ", length: " + decoder.getLength());
-
-		long start = System.nanoTime();
-		while (decoder.readSamples(stereoSamples) > 0) {
-		}
-
-		Gdx.app.log("Mpg123", "took " + (System.nanoTime() - start) / 1000000000.0);
-		decoder.dispose();
+//		String file = null;
+//		if (Gdx.app.getType() == ApplicationType.Android)
+//			file = "/sdcard/audio/schism.mp3";
+//		else
+//			file = "data/threeofaperfectpair.mp3";
+//
+//		Mpg123Decoder decoder = new Mpg123Decoder(file);
+//		ShortBuffer stereoSamples = AudioTools.allocateShortBuffer(1024, decoder.getNumChannels());
+//		ShortBuffer monoSamples = AudioTools.allocateShortBuffer(1024, 1);
+//		FloatBuffer spectrum = AudioTools.allocateFloatBuffer(1024 / 2 + 1, 1);
+//		KissFFT fft = new KissFFT(1024);
+//
+//		Gdx.app.log("Mpg123",
+//			"rate: " + decoder.getRate() + ", channels: " + decoder.getNumChannels() + ", length: " + decoder.getLength());
+//
+//		long start = System.nanoTime();
+//		while (decoder.readSamples(stereoSamples) > 0) {
+//		}
+//
+//		Gdx.app.log("Mpg123", "took " + (System.nanoTime() - start) / 1000000000.0);
+//		decoder.dispose();
 	}
 
 	@Override
--- tests/gdx-tests/src/com/badlogic/gdx/tests/VorbisTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/VorbisTest.java
@@ -16,35 +16,29 @@
 
 package com.badlogic.gdx.tests;
 
-import java.nio.ShortBuffer;
-
-import com.badlogic.gdx.Application.ApplicationType;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.analysis.AudioTools;
-import com.badlogic.gdx.audio.io.VorbisDecoder;
 import com.badlogic.gdx.tests.utils.GdxTest;
 
 public class VorbisTest extends GdxTest {
 
 	@Override
 	public void create () {
-		VorbisDecoder decoder = null;
-		if (Gdx.app.getType() == ApplicationType.Android)
-			decoder = new VorbisDecoder("/sdcard/audio/schism.ogg");
-		else
-			decoder = new VorbisDecoder("data/cloudconnected.ogg");
-		Gdx.app.log("Vorbis",
-			"channels: " + decoder.getNumChannels() + ", rate: " + decoder.getRate() + ", length: " + decoder.getLength());
-		;
-
-		ShortBuffer samplesBuffer = AudioTools.allocateShortBuffer(1024, 2);
-
-		long start = System.nanoTime();
-		while (decoder.readSamples(samplesBuffer) > 0) {
-
-		}
-		Gdx.app.log("Vorbis", "took " + (System.nanoTime() - start) / 1000000000.0);
-		decoder.dispose();
+//		VorbisDecoder decoder = null;
+//		if (Gdx.app.getType() == ApplicationType.Android)
+//			decoder = new VorbisDecoder("/sdcard/audio/schism.ogg");
+//		else
+//			decoder = new VorbisDecoder("data/cloudconnected.ogg");
+//		Gdx.app.log("Vorbis",
+//			"channels: " + decoder.getNumChannels() + ", rate: " + decoder.getRate() + ", length: " + decoder.getLength());
+//		;
+//
+//		ShortBuffer samplesBuffer = AudioTools.allocateShortBuffer(1024, 2);
+//
+//		long start = System.nanoTime();
+//		while (decoder.readSamples(samplesBuffer) > 0) {
+//
+//		}
+//		Gdx.app.log("Vorbis", "took " + (System.nanoTime() - start) / 1000000000.0);
+//		decoder.dispose();
 	}
 
 	@Override
