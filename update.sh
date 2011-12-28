328f98f8ba55456de056e0e6ff716b332e1f6f21
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2011-12-28 19:33:15 +0000

    [updated] natives from new build server :D [removed] old build script :D

--- update.sh
+++ update.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/sh 
 # Execute this in the root directory of the trunk to update all jars and natives from 
 # the CI server.
-wget http://libgdx.l33tlabs.org/libgdx-nightly-latest.zip -O libgdx-nightlies.zip
+wget http://libgdx.badlogicgames.com/nightlies/libgdx-nightly-latest.zip -O libgdx-nightlies.zip
 unzip libgdx-nightlies.zip -d nightlies
 unzip nightlies/gdx-natives.jar -d nightlies/libs
 
 # copy natives to gdx/libs
-cp nightlies/libs/ gdx/libs
-cp -rf nightlies/libs/armeabi nightlies/libs/armeabi-v7a gdx/libs
+cp nightlies/gdx-natives.jar gdx/libs
+cp -rf nightlies/armeabi nightlies/armeabi-v7a gdx/libs
 
 # copy android natives to tests
 cp -rf nightlies/armeabi nightlies/armeabi-v7a tests/gdx-tests-android/libs
 
 # copy jars and natives to demos
-cp -rf nightlies/gdx.jar nightlies/gdx-natives.jar nightlies/gdx-backend-jogl.jar nightlies/gdx-backend-jogl-natives.jar nightlies/gdx-sources.jar demos/helloworld/gdx-helloworld/libs
+cp -rf nightlies/gdx.jar nightlies/gdx-natives.jar nightlies/gdx-backend-jogl.jar nightlies/gdx-backend-jogl-natives.jar nightlies/sources/gdx-sources.jar demos/helloworld/gdx-helloworld/libs
 cp -rf nightlies/gdx.jar nightlies/gdx-backend-android.jar nightlies/armeabi nightlies/armeabi-v7a demos/helloworld/gdx-helloworld-android/libs
 cp -rf nightlies/armeabi nightlies/armeabi-v7a demos/invaders/gdx-invaders-android/libs
 cp -rf nightlies/armeabi nightlies/armeabi-v7a demos/vector-pinball/gdx-vectorpinball-android/libs
