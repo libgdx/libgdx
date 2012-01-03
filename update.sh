aa4833947a3683a5d03c092382b59aeeb58cb067
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-01-03 11:36:50 +0000

    [fixed] update.sh script

--- update.sh
+++ update.sh
@@ -5,12 +5,23 @@ wget http://libgdx.badlogicgames.com/nightlies/libgdx-nightly-latest.zip -O libg
 unzip libgdx-nightlies.zip -d nightlies
 unzip nightlies/gdx-natives.jar -d nightlies/libs
 
-# copy natives to gdx/libs
+# copy natives to gdx/libs and extensions libs
 cp nightlies/gdx-natives.jar gdx/libs
 cp -rf nightlies/armeabi nightlies/armeabi-v7a gdx/libs
 
+# gdx-audio natives
+cp -rf nightlies/extensions/gdx-audio-natives.jar extensions/gdx-audio/libs
+cp -rf nightlies/extensions/armeabi/libgdx-audio.so extensions/gdx-audio/libs/armeabi
+cp -rf nightlies/extensions/armeabi-v7a/libgdx-audio.so extensions/gdx-audio/libs/armeabi-v7a
+
+# gdx-stb-truetype natives
+cp -rf nightlies/extensions/gdx-stb-truetype-natives.jar extensions/gdx-stb-truetype/libs
+cp -rf nightlies/extensions/armeabi/libgdx-stb-truetype.so extensions/gdx-stb-truetype/libs/armeabi
+cp -rf nightlies/extensions/armeabi-v7a/libgdx-stb-truetype.so extensions/gdx-stb-truetype/libs/armeabi-v7a
+
 # copy android natives to tests
 cp -rf nightlies/armeabi nightlies/armeabi-v7a tests/gdx-tests-android/libs
+cp -rf nightlies/extensions/armeabi nightlies/extensions/armeabi-v7a tests/gdx-tests-android/libs
 
 # copy jars and natives to demos
 cp -rf nightlies/gdx.jar nightlies/gdx-natives.jar nightlies/gdx-backend-jogl.jar nightlies/gdx-backend-jogl-natives.jar nightlies/sources/gdx-sources.jar demos/helloworld/gdx-helloworld/libs
