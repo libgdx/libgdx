dd1ff7c8c7c853cc340783466aad13923c8e22a6
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-01-03 11:50:51 +0000

    [fixed] stb truetype build.

--- extensions/gdx-stb-truetype/jni/Android.mk
+++ extensions/gdx-stb-truetype/jni/Android.mk
@@ -9,5 +9,6 @@ LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
  
-LOCAL_SRC_FILES :=  
+LOCAL_SRC_FILES := com.badlogic.gdx.graphics.g2d.stbtt.StbTrueType.cpp
+ 
 include $(BUILD_SHARED_LIBRARY)
