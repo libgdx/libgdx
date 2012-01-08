5899f8465d9c98c8a3a035358247f6a08ff735c1
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-01-08 00:16:55 +0000

    [added] hashCode and equals to Vector2, even though it's a fantastically stupid idea :D

--- gdx/src/com/badlogic/gdx/math/Vector2.java
+++ gdx/src/com/badlogic/gdx/math/Vector2.java
@@ -259,6 +259,31 @@ public class Vector2 implements Serializable {
 		return r;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector2 other = (Vector2) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
+
 	/**
 	 * Compares this vector with the other vector, using the supplied
 	 * epsilon for fuzzy equality testing.
