edcc72580064c79537e59ebe9fd071371bb28db5
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-01-14 00:42:50 +0000

    [removed] iosgl2

---
+++ backends/gdx-backend-ios/gdx-ios/gdx-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gdx-ios
+//
+//  Created by badlogic on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ backends/gdx-backend-ios/gdx-ios/gdx-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gdx-ios
+//
+//  Created by badlogic on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
