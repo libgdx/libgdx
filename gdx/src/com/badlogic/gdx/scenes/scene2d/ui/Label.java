43b2fa49ca351c05c453c8cd0eec32ce6277c830
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-02-28 15:56:25 +0000

    [fixed] issue 702, Label works with CharSequence instead of String.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
@@ -34,7 +34,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
 public class Label extends Widget {
 	private LabelStyle style;
 	private final TextBounds bounds = new TextBounds();
-	private String text;
+	private CharSequence text;
 	private BitmapFontCache cache;
 	private float prefWidth, prefHeight;
 	private int labelAlign = Align.LEFT;
@@ -46,27 +46,27 @@ public class Label extends Widget {
 		this("", skin);
 	}
 
-	public Label (String text, Skin skin) {
+	public Label (CharSequence text, Skin skin) {
 		this(text, skin.getStyle(LabelStyle.class), null);
 	}
 
 	/** Creates a label, using a {@link LabelStyle} that has a BitmapFont with the specified name from the skin and the specified
 	 * color. */
-	public Label (String text, String fontName, Color color, Skin skin) {
+	public Label (CharSequence text, String fontName, Color color, Skin skin) {
 		this(text, new LabelStyle(skin.getFont(fontName), color), null);
 	}
 
 	/** Creates a label, using a {@link LabelStyle} that has a BitmapFont with the specified name and the specified color from the
 	 * skin. */
-	public Label (String text, String fontName, String colorName, Skin skin) {
+	public Label (CharSequence text, String fontName, String colorName, Skin skin) {
 		this(text, new LabelStyle(skin.getFont(fontName), skin.getColor(colorName)), null);
 	}
 
-	public Label (String text, LabelStyle style) {
+	public Label (CharSequence text, LabelStyle style) {
 		this(text, style, null);
 	}
 
-	public Label (String text, LabelStyle style, String name) {
+	public Label (CharSequence text, LabelStyle style, String name) {
 		super(name);
 		if (text == null) text = "";
 		this.text = text;
@@ -91,7 +91,7 @@ public class Label extends Widget {
 		return style;
 	}
 
-	public void setText (String text) {
+	public void setText (CharSequence text) {
 		if (text == null) throw new IllegalArgumentException("text cannot be null.");
 		if (text.equals(this.text)) return;
 		this.text = text;
@@ -99,7 +99,7 @@ public class Label extends Widget {
 		invalidateHierarchy();
 	}
 
-	public String getText () {
+	public CharSequence getText () {
 		return text;
 	}
 
