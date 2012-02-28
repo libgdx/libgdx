013fdb468dce7453ce60d28e7ac0505e97c17649
kalle.hamalainen85@gmail.com <kalle.hamalainen85@gmail.com@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-02-28 23:20:14 +0000

    fixed nasty assert bug if len < small value, cleaned and documented.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitterBox2D.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitterBox2D.java
@@ -1,4 +1,3 @@
-
 package com.badlogic.gdx.graphics.g2d;
 
 import java.io.BufferedReader;
@@ -10,65 +9,134 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.RayCastCallback;
 import com.badlogic.gdx.physics.box2d.World;
 
-/** @author pitbuller */
+/**
+ * @author kalle_h
+ * 
+ *         ParticleEmitterBox2D use box2d rayCast:ing to achieve continuous
+ *         collision detection against box2d fixtures. If particle detect
+ *         collision it change it's direction before actual collision would
+ *         occur. Velocity is 100% reflected.
+ * 
+ *         These particles does not have any other physical attributes or
+ *         functionality. Particles can't collide to other particles.
+ */
 public class ParticleEmitterBox2D extends ParticleEmitter {
 	final World world;
-	final Vector2 start = new Vector2();
-	final Vector2 end = new Vector2();
-	float fraction, normal;
+	final Vector2 startPoint = new Vector2();
+	final Vector2 endPoint = new Vector2();
+	/** collision flag */
+	boolean particleCollided;
+	float normalAngle;
+	/**
+	 * If velocities squared is shorter than this it could lead 0 length rayCast
+	 * that cause c++ assertion at box2d
+	 */
+	private final static float EPSILON = 0.001f;
 
-	final RayCastCallback ray = new RayCastCallback() {
-		public float reportRayFixture (Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
-			ParticleEmitterBox2D.this.fraction = fraction;
-			ParticleEmitterBox2D.this.normal = MathUtils.atan2(normal.y, normal.x) * MathUtils.radiansToDegrees;
+	/** default visibility to prevent synthetic accesor creation*/
+	final RayCastCallback rayCallBack = new RayCastCallback() {
+		public float reportRayFixture(Fixture fixture, Vector2 point,
+				Vector2 normal, float fraction) {
+			ParticleEmitterBox2D.this.particleCollided = true;
+			ParticleEmitterBox2D.this.normalAngle = MathUtils.atan2(normal.y,
+					normal.x) * MathUtils.radiansToDegrees;
 			return fraction;
 		}
 	};
 
-	public ParticleEmitterBox2D (World world) {
+	/**
+	 * Constructs default ParticleEmitterBox2D. Box2d World is used for
+	 * rayCasting. Assumes that particles use same unit system that box2d world
+	 * does.
+	 * 
+	 * @param world
+	 */
+	public ParticleEmitterBox2D(World world) {
 		super();
 		this.world = world;
 	}
 
-	public ParticleEmitterBox2D (World world, BufferedReader reader) throws IOException {
+	/**
+	 * /**Constructs ParticleEmitterBox2D using bufferedReader. Box2d World is
+	 * used for rayCasting. Assumes that particles use same unit system that
+	 * box2d world does.
+	 * 
+	 * @param world
+	 * @param reader
+	 * @throws IOException
+	 */
+	public ParticleEmitterBox2D(World world, BufferedReader reader)
+			throws IOException {
 		super(reader);
 		this.world = world;
 	}
 
-	public ParticleEmitterBox2D (World world, ParticleEmitter emitter) {
+	/**
+	 * Constructs ParticleEmitterBox2D fully copying given emitter attributes.
+	 * Box2d World is used for rayCasting. Assumes that particles use same unit
+	 * system that box2d world does.
+	 * 
+	 * @param world
+	 * @param emitter
+	 */
+	public ParticleEmitterBox2D(World world, ParticleEmitter emitter) {
 		super(emitter);
 		this.world = world;
 	}
 
 	@Override
-	protected Particle newParticle (Sprite sprite) {
+	protected Particle newParticle(Sprite sprite) {
 		return new ParticleBox2D(sprite);
 	}
 
+	/**
+	 * Particle that can collide to box2d fixtures
+	 * 
+	 */
 	private class ParticleBox2D extends Particle {
-		public ParticleBox2D (Sprite sprite) {
+		public ParticleBox2D(Sprite sprite) {
 			super(sprite);
 		}
 
-		public void translate (float velocityX, float velocityY) {
-			if (velocityX == 0 && velocityY == 0) return;
+		/**
+		 * translate particle given amount. Continuous collision detection
+		 * achieved by using RayCast from oldPos to newPos.
+		 * 
+		 * @param velocityX
+		 * @param velocityY
+		 */
+		@Override
+		public void translate(float velocityX, float velocityY) {
+			/**
+			 * If velocities squares summed is shorter than Epsilon it could
+			 * lead ~0 length rayCast that cause nasty c++ assertion inside
+			 * box2d. This is so short distance that moving particle has no
+			 * effect so this return early.
+			 */
+			if ((velocityX * velocityX + velocityY * velocityY) < EPSILON)
+				return;
 
-			// Position offset is half of sprite texture size. Currently this does not work right with textureAtlas.
+			/** Position offset is half of sprite texture size. */
 			final float x = getX() + getWidth() / 2f;
 			final float y = getY() + getHeight() / 2f;
 
-			fraction = 1f;
-			start.set(x, y);
-			end.set(x + velocityX, y + velocityY);
-			world.rayCast(ray, start, end);
+			/** collision flag to false */
+			particleCollided = false;
+			startPoint.set(x, y);
+			endPoint.set(x + velocityX, y + velocityY);
+			if (world != null)
+				world.rayCast(rayCallBack, startPoint, endPoint);
 
-			if (fraction < 1f) {
-				angle = 2f * normal - angle - 180f;
+			/** If ray collided boolean has set to true at rayCallBack */
+			if (!particleCollided) {
+				// perfect reflection
+				angle = 2f * normalAngle - angle - 180f;
 				angleCos = MathUtils.cosDeg(angle);
 				angleSin = MathUtils.sinDeg(angle);
 				velocityX = velocity * angleCos;
 				velocityY = velocity * angleSin;
 			}
+
 			super.translate(velocityX, velocityY);
 		}
 	}
