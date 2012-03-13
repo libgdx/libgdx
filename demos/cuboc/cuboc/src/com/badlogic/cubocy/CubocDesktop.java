82108b6645565229949061d8f7385e26f0adf053
seraphim6x7@gmail.com <seraphim6x7@gmail.com@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-03-13 14:25:29 +0000

    [changed] Set the log level to debug for the Cubocy demo

--- demos/cuboc/cuboc/src/com/badlogic/cubocy/CubocDesktop.java
+++ demos/cuboc/cuboc/src/com/badlogic/cubocy/CubocDesktop.java
@@ -1,9 +1,14 @@
+
 package com.badlogic.cubocy;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class CubocDesktop {
 	public static void main (String[] argv) {
+		// Set the log level so we can see the output of calls to Gdx.app.debug
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		new LwjglApplication(new Cubocy(), "Cubocy", 480, 320, true);
 	}
 }
