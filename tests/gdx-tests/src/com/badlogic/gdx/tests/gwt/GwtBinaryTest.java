0dd4dc758d2bfe1d14db8d6e40188eaca24ebe76
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-03-15 17:04:40 +0000

    [added] binary stuff...

--- tests/gdx-tests/src/com/badlogic/gdx/tests/gwt/GwtBinaryTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/gwt/GwtBinaryTest.java
@@ -4,19 +4,30 @@ import java.io.DataInputStream;
 import java.io.IOException;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.tests.utils.GdxTest;
 
 public class GwtBinaryTest extends GdxTest {
+	byte[] bytes;
 
 	@Override
 	public void create () {
-		DataInputStream in = new DataInputStream(Gdx.files.internal("data/arial.ttf").read());
-		for(int i = 0; i < 10; i++) {
+		FileHandle handle = Gdx.files.internal("data/arial.ttf");
+		bytes = new byte[(int)handle.length()];
+		DataInputStream in = new DataInputStream(handle.read());
+		for(int i = 0; i < 100; i++) {
 			try {
-				System.out.println(in.read());
+				bytes[i] = in.readByte();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 	}
 }
