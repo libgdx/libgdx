f06500e902523ec57262939ce52156da57958926
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-03-29 11:17:03 +0000

    fixed setup.sh

--- backends/gdx-backend-iosmonotouch/ikvm/setup.sh
+++ backends/gdx-backend-iosmonotouch/ikvm/setup.sh
@@ -1,4 +1,3 @@
 git clone https://badlogic@github.com/badlogic/ikvm-monotouch.git
 git clone https://badlogic@github.com/badlogic/ikvm-openjdk.git
 sudo ln -s /Developer/MonoTouch/usr/lib/mono/2.1 /Library/Frameworks/Mono.framework/Home/lib/mono/2.1
-nant -t:moonlight-2.0
