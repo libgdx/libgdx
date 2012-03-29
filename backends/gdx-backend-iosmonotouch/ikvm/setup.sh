3b7cd569602fe2bd9da169c0cdd539a75c2acbd9
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-03-29 11:11:59 +0000

    [added] monotouch/ikvm stub directory. Execute setup.sh to pull in the git repositories

---
+++ backends/gdx-backend-iosmonotouch/ikvm/setup.sh
@@ -0,0 +1,4 @@
+git clone https://badlogic@github.com/badlogic/ikvm-monotouch.git
+git clone https://badlogic@github.com/badlogic/ikvm-openjdk.git
+sudo ln -s /Developer/MonoTouch/usr/lib/mono/2.1 /Library/Frameworks/Mono.framework/Home/lib/mono/2.1
+nant -t:moonlight-2.0
