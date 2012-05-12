254017757c84162138bad057d9e9e53e97759c2e
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-05-12 21:52:39 +0000

    [added] FileHandle#pathWithoutExtension(), returns dir/dir2/file for dir/dir2/file.png

--- gdx/src/com/badlogic/gdx/files/FileHandle.java
+++ gdx/src/com/badlogic/gdx/files/FileHandle.java
@@ -94,6 +94,16 @@ public class FileHandle {
 		if (dotIndex == -1) return name;
 		return name.substring(0, dotIndex);
 	}
+	
+	/**
+	 * @return the path and filename without the extension, e.g. dir/dir2/file.png -> dir/dir2/file
+	 */
+	public String pathWithoutExtension() {
+	    String path = file.getPath();
+	    int dotIndex = path.lastIndexOf('.');
+	    if (dotIndex == -1) return path;
+	    return path.substring(0, dotIndex);
+	}
 
 	public FileType type () {
 		return type;
