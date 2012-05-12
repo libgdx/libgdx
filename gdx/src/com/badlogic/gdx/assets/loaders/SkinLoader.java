e6bf561b6532c12c8bb202ba7e03aa3f2e6b8a2b
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-05-12 21:52:03 +0000

    [fixed] issue 776, SkinLoader didn't cope with directory nesting for texture files.

--- gdx/src/com/badlogic/gdx/assets/loaders/SkinLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/SkinLoader.java
@@ -47,7 +47,7 @@ public class SkinLoader extends AsynchronousAssetLoader<Skin, SkinLoader.SkinPar
 		textureParam.minFilter = TextureFilter.Linear;
 		textureParam.magFilter = TextureFilter.Linear;
 		if (parameter == null)
-			deps.add(new AssetDescriptor(Gdx.files.internal(fileName).nameWithoutExtension() + ".png", Texture.class, textureParam));
+			deps.add(new AssetDescriptor(Gdx.files.internal(fileName).pathWithoutExtension() + ".png", Texture.class, textureParam));
 		else
 			deps.add(new AssetDescriptor(parameter.texturePath, Texture.class, textureParam));
 		return deps;
@@ -61,7 +61,7 @@ public class SkinLoader extends AsynchronousAssetLoader<Skin, SkinLoader.SkinPar
 	public Skin loadSync (AssetManager manager, String fileName, SkinParameter parameter) {
 		String texturePath;
 		if (parameter == null)
-			texturePath = Gdx.files.internal(fileName).nameWithoutExtension() + ".png";
+			texturePath = Gdx.files.internal(fileName).pathWithoutExtension() + ".png";
 		else
 			texturePath = parameter.texturePath;
 		Texture texture = manager.get(texturePath, Texture.class);
