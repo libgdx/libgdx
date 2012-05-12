e329ec8c94a938475cc98f805a4e54c809fa5740
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-05-12 22:03:50 +0000

    [added] GwtFileHandle#pathWithoutExtension.

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtFileHandle.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtFileHandle.java
@@ -60,6 +60,16 @@ public class GwtFileHandle extends FileHandle {
 		if (dotIndex == -1) return name;
 		return name.substring(0, dotIndex);
 	}
+	
+	/**
+	 * @return the path and filename without the extension, e.g. dir/dir2/file.png -> dir/dir2/file
+	 */
+	public String pathWithoutExtension() {
+		String path = file;
+		int dotIndex = path.lastIndexOf('.');
+		if (dotIndex == -1) return path;
+		return path.substring(0, dotIndex);
+	}
 
 	public FileType type () {
 		return type;
