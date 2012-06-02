34834c55c81e608ac032d5b9e9467f04d110c81b
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-06-02 15:41:45 +0000

    killed setup.sh

--- backends/gdx-backend-iosmonotouch/setup.sh
+++
@@ -1,5 +0,0 @@
-# fetch ikvm distri
-curl "http://netcologne.dl.sourceforge.net/project/ikvm/ikvm/7.0.4335.0/ikvmbin-7.0.4335.0.zip" > ikvmbin.zip
-unzip ikvmbin.zip
-mv ikvm-7.0.4335.0 ikvm
-rm ikvmbin.zip
