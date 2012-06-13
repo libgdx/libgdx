c36900c168aee55ab5c5438787da55beb844a031
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-06-13 23:15:17 +0000

    [added] stubs for music and sound for ios backend

---
+++ backends/gdx-backend-iosmonotouch/src/com/badlogic/gdx/backends/ios/IOSMusic.java
@@ -0,0 +1,61 @@
+package com.badlogic.gdx.backends.ios;
+
+import com.badlogic.gdx.audio.Music;
+
+public class IOSMusic implements Music {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPlaying() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setLooping(boolean isLooping) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isLooping() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setVolume(float volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public float getPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ backends/gdx-backend-iosmonotouch/src/com/badlogic/gdx/backends/ios/IOSSound.java
@@ -0,0 +1,85 @@
+package com.badlogic.gdx.backends.ios;
+
+import com.badlogic.gdx.audio.Sound;
+
+public class IOSSound implements Sound {
+
+	@Override
+	public long play() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long play(float volume) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long play(float volume, float pitch, float pan) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long loop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long loop(float volume) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long loop(float volume, float pitch, float pan) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop(long soundId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLooping(long soundId, boolean looping) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPitch(long soundId, float pitch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setVolume(long soundId, float volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPan(long soundId, float pan, float volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
