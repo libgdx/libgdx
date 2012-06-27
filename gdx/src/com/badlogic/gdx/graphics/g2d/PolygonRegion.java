26be2fe85f1087a5692f9e079e3eb36d0169db46
st.lebach@gmail.com <st.lebach@gmail.com@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-06-27 13:25:18 +0000

    WIP: PolygonSpriteBatch initial commit. Some loose ends, use with care.

---
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PolygonRegion.java
@@ -0,0 +1,139 @@
+package com.badlogic.gdx.graphics.g2d;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+/** 
+ * Defines a polygon shape on top of a #TextureRegion for minimising pixel drawing. Can either be 
+ * constructed through a .psh file from an external editor or programmatically through a list of vertices
+ * defining a polygon.
+ * 
+ * THIS STUFF IS WIP
+ * 
+ * @author Stefan Bachmann
+ */
+public class PolygonRegion {
+	// texture coordinates in atlas coordinates
+	private float[] texCoords;
+   // pixel coordinates relative to source image.
+	private float[] localVertices;
+	// the underlying TextureRegion
+	private TextureRegion region;
+	
+	/**
+	 * Creates a PolygonRegion by reading in the vertices and texture coordinates from the external file. 
+	 * TextureRegion can come from an atlas.
+	 * @param region the region used for drawing
+	 * @param file polygon shape definition file
+	 */
+	public PolygonRegion(TextureRegion region, FileHandle file) {
+		this.region = region;
+		
+		if(file == null)
+			throw new IllegalArgumentException("region cannot be null.");
+		
+		loadPolygonDefinition(file);
+	}
+
+	/**
+	 * Creates a PolygonRegin by triangulating the polygon coordinates in vertices and calculates uvs 
+	 * based on that. TextureRegion can come from an atlas.
+	 * @param region the region used for drawing
+	 * @param vertices contains 2D polygon coordinates in pixels relative to source region
+	 */
+	public PolygonRegion(TextureRegion region, float[] vertices) {
+		
+	}
+
+	
+   /**
+    * Loads the vertices and texture data from an external file. The file should look something like this:
+    * 
+    * ------------
+    * // Triangulated vertices data (x, y) in pixel coordinates with origin bottom-left, y-up
+    * v 230.0, 230.0, ...
+    * // UVs with origin top-left 
+    * u 0.23, 0.123, ...  
+    * -------------
+    * 
+    * Anything not prefixed with "u" or "v" will be ignored.
+    * @param file file handle to the shape definition file
+    */
+   private void loadPolygonDefinition (FileHandle file) {
+   	String line;
+   	BufferedReader reader = new BufferedReader(new InputStreamReader(file.read()), 64);
+   	
+   	try{
+	   	while(true){
+	   		line = reader.readLine();
+	   		
+	   		if(line == null)
+	   			break;
+	   		else if(line.startsWith("v")){
+   				// read in vertices
+	   			String[] vertices = line.substring(1).trim().split(",");
+	   			localVertices = new float[vertices.length];
+	   			for(int i=0; i<vertices.length; i+=2){
+	   				localVertices[i] = Float.parseFloat(vertices[i]);
+	   				localVertices[i+1] = Float.parseFloat(vertices[i+1]);
+	   			}
+	   		}
+	   		else if(line.startsWith("u")){
+   				// read in uvs
+	   			String[] texCoords = line.substring(1).trim().split(",");
+	   			float localTexCoords[] = new float[texCoords.length];
+	   			for(int i=0; i<texCoords.length; i+=2){
+	   				localTexCoords[i] = Float.parseFloat(texCoords[i]);
+	   				localTexCoords[i+1] = Float.parseFloat(texCoords[i+1]);
+	   			}
+	   			
+	   			this.texCoords = calculateAtlasTexCoords(localTexCoords);
+	   		}
+	   	}
+   	}
+   	catch (IOException ex) {
+			throw new GdxRuntimeException("Error reading polygon shape file: " + file);
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException ignored) {
+			}
+		}
+	}
+
+	/**
+	 * @param localTexCoords texture coordinates relative to the image
+	 * @return the texture coordinates relative to the Texture (atlas) the region is from
+	 */
+	private float[] calculateAtlasTexCoords (float[] localTexCoords) {
+		
+		float uvWidth = this.region.u2 - this.region.u;
+		float uvHeight = this.region.v2 - this.region.v;
+		
+		for(int i=0; i<localTexCoords.length; i+=2){
+			localTexCoords[i] = this.region.u + (localTexCoords[i] * uvWidth);
+			localTexCoords[i+1] = this.region.v + (localTexCoords[i+1] * uvHeight);
+		}
+		
+		return localTexCoords;
+	}
+	
+	// Returns the vertices in local space
+	public float[] getLocalVertices(){
+		return localVertices;
+	}
+	
+	// Returns the texture coordinates
+	public float[] getTextureCoords(){
+		return texCoords;
+	}
+	
+	// Returns the underlying TextureRegion
+	public TextureRegion getRegion(){
+		return region;
+	}
+}
---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/PolygonRegionTest.java
@@ -0,0 +1,260 @@
+package com.badlogic.gdx.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.EarClippingTriangulator;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+
+public class PolygonRegionTest extends GdxTest {
+	@Override
+	public boolean needsGL20 () {
+		return false;
+	}
+
+	PolygonSpriteBatch batch;
+	PolygonRegionDebugRenderer debugRenderer;
+	
+	Texture texture;
+	OrthographicCamera camera;
+	PolygonRegion region;
+	
+	boolean usePolygonBatch = true;
+
+	@Override
+	public void create () {
+		texture = new Texture(Gdx.files.internal("data/tree.png"));
+		region = new PolygonRegion(new TextureRegion(texture), 
+			Gdx.files.internal("data/tree.psh"));
+		
+		camera = new OrthographicCamera(480, 320);
+		camera.position.x = 0;
+		camera.position.y = 0;
+		camera.update();
+		
+		batch = new PolygonSpriteBatch();
+		debugRenderer = new PolygonRegionDebugRenderer();
+		
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override
+	public void render () {
+		GL10 gl = Gdx.gl10;
+		
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+		
+		camera.apply(Gdx.gl10);
+		batch.setProjectionMatrix(camera.combined);
+		
+		batch.begin();
+		
+		batch.draw(region, -128, -128, 256, 256);
+		
+		batch.end();
+		
+		debugRenderer.setProjectionMatrix(camera.combined);
+		debugRenderer.draw(region, -128, -128, 0, 0, 256, 256, 1, 1, 0);
+	}
+
+	@Override
+	public void dispose () {
+		debugRenderer.dispose();
+		texture.dispose();
+		batch.dispose();
+	}
+	
+	public class PolygonRegionDebugRenderer implements Disposable {
+		ShapeRenderer renderer;
+		
+		public PolygonRegionDebugRenderer(){
+			renderer = new ShapeRenderer();
+		}
+		
+		public void draw (PolygonRegion region, float x, float y, float originX, float originY, float width, float height,
+			float scaleX, float scaleY, float rotation) {
+			
+			float[] localVertices = region.getLocalVertices();
+			float[] texCoords = region.getTextureCoords();
+
+			// bottom left and top right corner points relative to origin
+			final float worldOriginX = x + originX;
+			final float worldOriginY = y + originY;
+			float sX = width / region.getRegion().getRegionWidth();
+			float sY = height / region.getRegion().getRegionHeight();
+			float fx1, fx2, fx3, px1, px2, px3;
+			float fy1, fy2, fy3, py1, py2, py3;
+			
+			final float cos = MathUtils.cosDeg(rotation);
+			final float sin = MathUtils.sinDeg(rotation);
+			
+			renderer.setColor(Color.RED);
+			renderer.begin(ShapeType.Line);
+
+			for(int i=0; i<localVertices.length; i+=6){
+				fx1 = localVertices[i] * sX;
+				fy1 = localVertices[i+1] * sY;
+				fx2 = localVertices[i+2] * sX;
+				fy2 = localVertices[i+3] * sY;
+				fx3 = localVertices[i+4] * sX;
+				fy3 = localVertices[i+5] * sY;
+				
+				fx1 -= originX;
+				fy1 -= originY;
+				fx2 -= originX;
+				fy2 -= originY;
+				fx3 -= originX;
+				fy3 -= originY;
+				
+				if(scaleX != 1 || scaleY != 1){
+					fx1 *= scaleX;
+					fy1 *= scaleY;
+					fx2 *= scaleX;
+					fy2 *= scaleY;
+					fx3 *= scaleX;
+					fy3 *= scaleY;
+				}
+				
+				px1 = cos * fx1 - sin * fy1;
+				py1 = sin * fx1 + cos * fy1;
+				px2 = cos * fx2 - sin * fy2;
+				py2 = sin * fx2 + cos * fy2;
+				px3 = cos * fx3 - sin * fy3;
+				py3 = sin * fx3 + cos * fy3;
+				
+				px1 += worldOriginX;
+				py1 += worldOriginY;
+				px2 += worldOriginX;
+				py2 += worldOriginY;
+				px3 += worldOriginX;
+				py3 += worldOriginY;
+				
+				renderer.line(px1, py1, px2, py2);
+				renderer.line(px2, py2, px3, py3);
+				renderer.line(px3, py3, px1, py1);
+			}
+			
+			renderer.end();
+			
+			renderer.setColor(Color.BLUE);
+			renderer.begin(ShapeType.FilledCircle);
+			
+			renderer.filledCircle(worldOriginX, worldOriginY, 4);
+			
+			renderer.end();
+			
+			// Calculate the bounding rect, is there a better way?!
+			// bottom left and top right corner points relative to origin
+			fx1 = -originX;
+			fy1 = -originY;
+			fx2 = width - originX;
+			fy2 = height - originY;
+
+			// scale
+			if (scaleX != 1 || scaleY != 1) {
+				fx1 *= scaleX;
+				fy1 *= scaleY;
+				fx2 *= scaleX;
+				fy2 *= scaleY;
+			}
+
+			// construct corner points, start from top left and go counter clockwise
+			final float p1x = fx1;
+			final float p1y = fy1;
+			final float p2x = fx1;
+			final float p2y = fy2;
+			final float p3x = fx2;
+			final float p3y = fy2;
+			final float p4x = fx2;
+			final float p4y = fy1;
+
+			float x1;
+			float y1;
+			float x2;
+			float y2;
+			float x3;
+			float y3;
+			float x4;
+			float y4;
+
+			// rotate
+			if (rotation != 0) {
+				x1 = cos * p1x - sin * p1y;
+				y1 = sin * p1x + cos * p1y;
+
+				x2 = cos * p2x - sin * p2y;
+				y2 = sin * p2x + cos * p2y;
+
+				x3 = cos * p3x - sin * p3y;
+				y3 = sin * p3x + cos * p3y;
+
+				x4 = x1 + (x3 - x2);
+				y4 = y3 - (y2 - y1);
+			} else {
+				x1 = p1x;
+				y1 = p1y;
+
+				x2 = p2x;
+				y2 = p2y;
+
+				x3 = p3x;
+				y3 = p3y;
+
+				x4 = p4x;
+				y4 = p4y;
+			}
+
+			x1 += worldOriginX;
+			y1 += worldOriginY;
+			x2 += worldOriginX;
+			y2 += worldOriginY;
+			x3 += worldOriginX;
+			y3 += worldOriginY;
+			x4 += worldOriginX;
+			y4 += worldOriginY;
+			
+			// Draw the bounding rectangle
+			renderer.setColor(Color.GREEN);
+			renderer.begin(ShapeType.Line);
+			
+			renderer.line(x1, y1, x2, y2);
+			renderer.line(x2, y2, x3, y3);
+			renderer.line(x3, y3, x4, y4);
+			renderer.line(x4, y4, x1, y1);
+			
+			renderer.end();
+		}
+		
+		public void setProjectionMatrix(Matrix4 matrix){
+			this.renderer.setProjectionMatrix(matrix);
+		}
+
+		@Override
+		public void dispose () {
+			renderer.dispose();
+		}
+	}
+}
