3443f8ce0496f3e1d9ea6458f17c2c5148ac8114
aurelien.ribon <aurelien.ribon@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-07-03 08:28:27 +0000

    [gdx-setup-ui][updated] index.html page of the html backend to remove text cursor and yellow outline, [gdx-setup-ui][updated] support for gdx 0.9.6 [gdx-setup-ui][updated] version to 2.0.2

--- extensions/gdx-setup-ui/src/aurelienribon/libgdx/ui/MainPanel.java
+++ extensions/gdx-setup-ui/src/aurelienribon/libgdx/ui/MainPanel.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2011 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package aurelienribon.libgdx.ui;
 
 import aurelienribon.ui.components.PaintedPanel;
@@ -55,7 +40,7 @@ public class MainPanel extends PaintedPanel {
     }
 
 	private void checkUpdates() {
-		final String version = "2.0.1";
+		final String version = "2.0.2";
 		versionLabel.setText("v" + version + " (...)");
 
 		URL tmpUrl;
