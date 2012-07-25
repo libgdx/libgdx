7644e67e694e819b03f851285a53f4f01d907e02
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-07-25 08:47:31 +0000

    Some idiot had broken Sprite. Who runs this circus anyway?!

--- gdx/src/com/badlogic/gdx/graphics/g2d/Sprite.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/Sprite.java
@@ -505,10 +505,7 @@ public class Sprite extends TextureRegion {
 	}
 
 	public void setRegion (float u, float v, float u2, float v2) {
-		this.u = u;
-		this.v = v;
-		this.u2 = u2;
-		this.v2 = v2;
+		super.setRegion(u, v, u2, v2);
 
 		float[] vertices = Sprite.this.vertices;
 		vertices[U1] = u;
@@ -525,25 +522,25 @@ public class Sprite extends TextureRegion {
 	}
 
 	public void setU (float u) {
-		this.u = u;
+		super.setU(u);
 		vertices[U1] = u;
 		vertices[U2] = u;
 	}
 
 	public void setV (float v) {
-		this.v = v;
+		super.setV(u);
 		vertices[V2] = v;
 		vertices[V3] = v;
 	}
 
 	public void setU2 (float u2) {
-		this.u2 = u2;
+		super.setU2(u2);
 		vertices[U3] = u2;
 		vertices[U4] = u2;
 	}
 
 	public void setV2 (float v2) {
-		this.v2 = v2;
+		super.setV2(v2);
 		vertices[V1] = v2;
 		vertices[V4] = v2;
 	}
