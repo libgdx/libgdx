0dd6d34268482b3b1a72439b111e9dbbe605852f
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-07-29 23:01:29 +0000

    Fixed issue 977, Array ctor should default to ordered.

--- gdx/src/com/badlogic/gdx/utils/Array.java
+++ gdx/src/com/badlogic/gdx/utils/Array.java
@@ -64,7 +64,7 @@ public class Array<T> implements Iterable<T> {
 
 	/** Creates an ordered array with {@link #items} of the specified type and a capacity of 16. */
 	public Array (Class<T> arrayType) {
-		this(false, 16, arrayType);
+		this(true, 16, arrayType);
 	}
 
 	/** Creates a new array containing the elements in the specified array. The new array will have the same type of backing array
