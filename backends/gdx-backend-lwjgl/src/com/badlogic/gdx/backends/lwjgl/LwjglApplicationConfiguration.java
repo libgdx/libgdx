0e8c3fb1e206294396cbc488ac7377cb7673a532
nathan.sweet <nathan.sweet@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-08-01 10:55:39 +0000

    Javadoc.

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglApplicationConfiguration.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglApplicationConfiguration.java
@@ -65,7 +65,8 @@ public class LwjglApplicationConfiguration {
 	Array<String> iconPaths = new Array();
 	Array<FileType> iconFileTypes = new Array();
 
-	/** Adds a window icon. Icons are tried in the order added, the first one that works is used. */
+	/** Adds a window icon. Icons are tried in the order added, the first one that works is used. Typically three icons should be
+	 * provided: 128x128 (for Mac), 32x32 (for Windows and Linux), and 16x16 (for Windows). */
 	public void addIcon (String path, FileType fileType) {
 		iconPaths.add(path);
 		iconFileTypes.add(fileType);
