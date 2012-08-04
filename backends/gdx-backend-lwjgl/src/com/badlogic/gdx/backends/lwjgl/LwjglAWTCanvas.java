8dafbe6a28bab61a71d4046da1a262d165d6080f
badlogicgames <badlogicgames@6c4fd544-2939-11df-bb46-9574ba5d0bfa>
2012-08-04 20:44:46 +0000

    [fixed] lwjgl/LwjglAWTCanvas has a makeCurrent method now.

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglAWTCanvas.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglAWTCanvas.java
@@ -336,4 +336,17 @@ public class LwjglAWTCanvas implements Application {
 			}
 		});
 	}
+	
+   /**
+    * Make the canvas' context current. It is highly recommended that the context
+    * is only made current inside the AWT thread (for example in an overridden paintGL()).
+    */
+   public void makeCurrent() {
+      try {
+         canvas.makeCurrent();
+         setGlobals();
+      } catch (LWJGLException ex) {
+         throw new GdxRuntimeException(ex);
+      }
+   }
 }
