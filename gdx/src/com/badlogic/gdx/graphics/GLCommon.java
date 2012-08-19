6c2c29fa4ba57aea3c243434a4a2b36477afe2e5
trung <trungnt13@gmail.com>
2012-08-20 00:07:57 +0700

    more opengl docs

--- gdx/src/com/badlogic/gdx/graphics/GLCommon.java
+++ gdx/src/com/badlogic/gdx/graphics/GLCommon.java
@@ -30,6 +30,11 @@ public interface GLCommon {
 
 	public void glBindTexture (int target, int texture);
 
+	/**
+	 * glBlendFunc — specify pixel arithmetic
+	 * @param sfactor Specifies how the red, green, blue, and alpha source blending factors are computed. The following symbolic constants are accepted: GL_ZERO, GL_ONE, GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR, GL_DST_COLOR, GL_ONE_MINUS_DST_COLOR, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA, and GL_SRC_ALPHA_SATURATE. The initial value is GL_ONE. <p>
+	 * @param dfactor Specifies how the red, green, blue, and alpha destination blending factors are computed. The following symbolic constants are accepted: GL_ZERO, GL_ONE, GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR, GL_DST_COLOR, GL_ONE_MINUS_DST_COLOR, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA. GL_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_COLOR, GL_CONSTANT_ALPHA, and GL_ONE_MINUS_CONSTANT_ALPHA. The initial value is GL_ZERO.
+	 */
 	public void glBlendFunc (int sfactor, int dfactor);
 
 	public void glClear (int mask);
@@ -68,6 +73,64 @@ public interface GLCommon {
 
 	public void glDrawElements (int mode, int count, int type, Buffer indices);
 
+	/**
+	 * glEnable — enable or disable server-side GL capabilities
+	 * @param cap <p>
+	 * GL_BLEND<p>
+	 * If enabled, blend the computed fragment color values with the values in the color buffers. See {@link GLCommon#glBlendFunc}.<p>
+	 * GL_CLIP_DISTANCEi<p>
+	 * If enabled, clip geometry against user-defined half space i.<p>
+	 * GL_COLOR_LOGIC_OP<p>
+	 * If enabled, apply the currently selected logical operation to the computed fragment color and color buffer values. See {@link GL10#glLogicOp}.<p>
+	 * GL_CULL_FACE<p>
+	 * If enabled, cull polygons based on their winding in window coordinates. See {@link GLCommon#glCullFace}.<p>
+	 * GL_DEBUG_OUTPUT<p>
+	 * If enabled, debug messages are produced by a debug context. When disabled, the debug message log is silenced. Note that in a non-debug context, very few, if any messages might be produced, even when GL_DEBUG_OUTPUT is enabled.<p>
+	 * GL_DEBUG_OUTPUT_SYNCHRONOUS<p>
+	 * If enabled, debug messages are produced synchronously by a debug context. If disabled, debug messages may be produced asynchronously. In particular, they may be delayed relative to the execution of GL commands, and the debug callback function may be called from a thread other than that in which the commands are executed. See glDebugMessageCallback.<p>
+	 * GL_DEPTH_CLAMP<p>
+	 * If enabled, the -wc≤zc≤wc plane equation is ignored by view volume clipping (effectively, there is no near or far plane clipping). See {@link GLCommon#glDepthRangef}.<p>
+	 * GL_DEPTH_TEST<p>
+	 * If enabled, do depth comparisons and update the depth buffer. Note that even if the depth buffer exists and the depth mask is non-zero, the depth buffer is not updated if the depth test is disabled. See glDepthFunc and {@link GLCommon#glDepthRangef}.<p>
+	 * GL_DITHER<p>
+	 * If enabled, dither color components or indices before they are written to the color buffer.<p>
+	 * GL_FRAMEBUFFER_SRGB<p>
+	 * If enabled and the value of GL_FRAMEBUFFER_ATTACHMENT_COLOR_ENCODING for the framebuffer attachment corresponding to the destination buffer is GL_SRGB, the R, G, and B destination color values (after conversion from fixed-point to floating-point) are considered to be encoded for the sRGB color space and hence are linearized prior to their use in blending.<p>
+	 * GL_LINE_SMOOTH<p>
+	 * If enabled, draw lines with correct filtering. Otherwise, draw aliased lines. See {@link GLCommon#glLineWidth}.<p>
+	 * GL_MULTISAMPLE<p>
+	 * If enabled, use multiple fragment samples in computing the final color of a pixel. See {@link GL10#glSampleCoverage}.<p>
+	 * GL_POLYGON_OFFSET_FILL<p>
+	 * If enabled, and if the polygon is rendered in GL_FILL mode, an offset is added to depth values of a polygon's fragments before the depth comparison is performed. See {@link GLCommon#glPolygonOffset}.<p>
+	 * GL_POLYGON_OFFSET_LINE<p>
+	 * If enabled, and if the polygon is rendered in GL_LINE mode, an offset is added to depth values of a polygon's fragments before the depth comparison is performed. See {@link GLCommon#glPolygonOffset}.<p>
+	 * GL_POLYGON_OFFSET_POINT<p>
+	 * If enabled, an offset is added to depth values of a polygon's fragments before the depth comparison is performed, if the polygon is rendered in GL_POINT mode. See {@link GLCommon#glPolygonOffset}.<p>
+	 * GL_POLYGON_SMOOTH<p>
+	 * If enabled, draw polygons with proper filtering. Otherwise, draw aliased polygons. For correct antialiased polygons, an alpha buffer is needed and the polygons must be sorted front to back.<p>
+	 * GL_PRIMITIVE_RESTART<p>
+	 * Enables primitive restarting. If enabled, any one of the draw commands which transfers a set of generic attribute array elements to the GL will restart the primitive when the index of the vertex is equal to the primitive restart index. See glPrimitiveRestartIndex.<p>
+	 * GL_PRIMITIVE_RESTART_FIXED_INDEX<p>
+	 * Enables primitive restarting with a fixed index. If enabled, any one of the draw commands which transfers a set of generic attribute array elements to the GL will restart the primitive when the index of the vertex is equal to the fixed primitive index for the specified index type. The fixed index is equal to 2n−1 where n is equal to 8 for GL_UNSIGNED_BYTE, 16 for GL_UNSIGNED_SHORT and 32 for GL_UNSIGNED_INT.<p>
+	 * GL_SAMPLE_ALPHA_TO_COVERAGE<p>
+	 * If enabled, compute a temporary coverage value where each bit is determined by the alpha value at the corresponding sample location. The temporary coverage value is then ANDed with the fragment coverage value.<p>
+	 * GL_SAMPLE_ALPHA_TO_ONE<p>
+	 * If enabled, each sample alpha value is replaced by the maximum representable alpha value.<p>
+	 * GL_SAMPLE_COVERAGE<p>
+	 * If enabled, the fragment's coverage is ANDed with the temporary coverage value. If GL_SAMPLE_COVERAGE_INVERT is set to GL_TRUE, invert the coverage value. See {@link GL10#glSampleCoverage}.<p>
+	 * GL_SAMPLE_SHADING<p>
+	 * If enabled, the active fragment shader is run once for each covered sample, or at fraction of this rate as determined by the current value of GL_MIN_SAMPLE_SHADING_VALUE. See glMinSampleShading.<p>
+	 * GL_SAMPLE_MASK<p>
+	 * If enabled, the sample coverage mask generated for a fragment during rasterization will be ANDed with the value of GL_SAMPLE_MASK_VALUE before shading occurs. See glSampleMaski.<p>
+	 * GL_SCISSOR_TEST<p>
+	 * If enabled, discard fragments that are outside the scissor rectangle. See glScissor.<p>
+	 * GL_STENCIL_TEST<p>
+	 * If enabled, do stencil testing and update the stencil buffer. See glStencilFunc and glStencilOp.<p>
+	 * GL_TEXTURE_CUBE_MAP_SEAMLESS<p>
+	 * If enabled, cubemap textures are sampled such that when linearly sampling from the border between two adjacent faces, texels from both faces are used to generate the final sample value. When disabled, texels from only a single face are used to construct the final sample value.<p>
+	 * GL_PROGRAM_POINT_SIZE<p>
+	 * If enabled and a vertex or geometry shader is active, then the derived point size is taken from the (potentially clipped) shader builtin gl_PointSize and clamped to the implementation-dependent point size range.<p>
+	 */
 	public void glEnable (int cap);
 
 	public void glFinish ();
@@ -84,6 +147,11 @@ public interface GLCommon {
 
 	public String glGetString (int name);
 
+	/**
+	 * glHint — specify implementation-specific hints
+	 * @param target Specifies a symbolic constant indicating the behavior to be controlled. GL_FOG_HINT,	 GL_GENERATE_MIPMAP_HINT, GL_LINE_SMOOTH_HINT, GL_PERSPECTIVE_CORRECTION_HINT, GL_POINT_SMOOTH_HINT, GL_POLYGON_SMOOTH_HINT, GL_TEXTURE_COMPRESSION_HINT, and GL_FRAGMENT_SHADER_DERIVATIVE_HINT are accepted.<p>
+	 * @param mode Specifies a symbolic constant indicating the desired behavior. GL_FASTEST, GL_NICEST, and GL_DONT_CARE are accepted.
+	 */
 	public void glHint (int target, int mode);
 
 	public void glLineWidth (float width);
