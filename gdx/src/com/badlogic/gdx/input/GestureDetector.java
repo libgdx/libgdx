baae43f40448dd9b051e745254eb8443496bad62
NathanSweet <nathan.sweet@gmail.com>
2012-08-18 18:36:32 -0700

    Fixed GestureDetector setTapCountInterval.

--- gdx/src/com/badlogic/gdx/input/GestureDetector.java
+++ gdx/src/com/badlogic/gdx/input/GestureDetector.java
@@ -231,8 +231,9 @@ public class GestureDetector extends InputAdapter {
 		this.tapSquareSize = tapSquareSize;
 	}
 
-	public void setTapCountInterval (long tapCountInterval) {
-		this.tapCountInterval = tapCountInterval;
+	/** @param tapCountInterval time in seconds that must pass for two touch down/up sequences to be detected as consecutive taps. */
+	public void setTapCountInterval (float tapCountInterval) {
+		this.tapCountInterval = (long)(tapCountInterval * 1000000000l);
 	}
 
 	public void setLongPressSeconds (float longPressSeconds) {
