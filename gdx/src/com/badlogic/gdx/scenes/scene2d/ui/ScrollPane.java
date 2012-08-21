182a97e70a371802dce7dbe9bec6bc8189932169
NathanSweet <nathan.sweet@gmail.com>
2012-08-20 21:12:30 -0700

    ScrollPane, fixed hit() logic.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
@@ -548,7 +548,7 @@ public class ScrollPane extends WidgetGroup {
 	}
 
 	public Actor hit (float x, float y) {
-		if (x < 0 && x >= getWidth() && y < 0 && y >= getHeight()) return null;
+		if (x < 0 || x >= getWidth() || y < 0 || y >= getHeight()) return null;
 		if (scrollX && hScrollBounds.contains(x, y)) return this;
 		if (scrollY && vScrollBounds.contains(x, y)) return this;
 		return super.hit(x, y);
