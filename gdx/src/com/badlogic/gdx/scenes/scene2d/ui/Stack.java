ab3afc3bbb699034c79a3f75ce8e931d51bfba77
NathanSweet <nathan.sweet@gmail.com>
2012-09-03 21:42:03 -0700

    scene2d, avoid invalidating widgets in layout() that have not changed size.
    Previously layout() invalidated every child. invalidateHierarchy happens often and invalidates to the root, which means ALL tables are invalidated. This optimization prevents an enormous number of unnecessary invalidations. In a very complex app with 71 tables, resizing the window triggers only 5 layouts.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Stack.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Stack.java
@@ -97,11 +97,18 @@ public class Stack extends WidgetGroup {
 		Array<Actor> children = getChildren();
 		for (int i = 0, n = children.size; i < n; i++) {
 			Actor child = children.get(i);
-			child.setBounds(0, 0, width, height);
-			if (child instanceof Layout) {
-				Layout layout = (Layout)child;
-				layout.invalidate();
-				layout.validate();
+			child.setX(0);
+			child.setY(0);
+			if (child.getWidth() != width || child.getHeight() != height) {
+				child.setWidth(width);
+				child.setHeight(height);
+				if (child instanceof Layout) {
+					Layout layout = (Layout)child;
+					layout.invalidate();
+					layout.validate();
+				}
+			} else {
+				if (child instanceof Layout) ((Layout)child).validate();
 			}
 		}
 	}
