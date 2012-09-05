0274e787eef2fd07db5ca8aa0568b89009cfe94a
NathanSweet <nathan.sweet@gmail.com>
2012-09-04 18:13:19 -0700

    ParticleEditor, changed back to spinners instead of sliders for now.

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/particleeditor/Slider.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/particleeditor/Slider.java
@@ -16,6 +16,7 @@
 
 package com.badlogic.gdx.tools.particleeditor;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -29,99 +30,37 @@ import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+// BOZO - Slider is a placeholder that uses a spinner until the slider (in NewSlider) is complete.
 class Slider extends JPanel {
-	static private final int KNOB_WIDTH = 10;
-
-	float value, min, max, stepSize, sliderMin, sliderMax;
-	ChangeListener listener;
-	int border = 2;
-	Color bgColor = new Color(0.6f, 0.6f, 0.6f);
-	Color knobColor = Color.lightGray;
+	private JSpinner spinner;
 
 	public Slider (float initialValue, final float min, final float max, float stepSize, final float sliderMin,
 		final float sliderMax) {
-		this.min = min;
-		this.max = max;
-		this.stepSize = stepSize;
-		this.sliderMin = sliderMin;
-		this.sliderMax = sliderMax;
-		value = Math.max(min, Math.min(max, initialValue));
-
-		setLayout(new GridBagLayout());
-
-		addMouseListener(new MouseAdapter() {
-			public void mousePressed (MouseEvent event) {
-				float width = getWidth() - KNOB_WIDTH - border * 2;
-				float mouseX = event.getX() - KNOB_WIDTH / 2 - border;
-				setValue(sliderMin + (sliderMax - sliderMin) * Math.max(0, Math.min(width, mouseX)) / width);
-			}
-
-			public void mouseReleased (MouseEvent event) {
-
-			}
-
-			public void mouseClicked (MouseEvent event) {
-				repaint();
-			}
-		});
-		addMouseMotionListener(new MouseMotionListener() {
-			public void mouseDragged (MouseEvent event) {
-				float width = getWidth() - KNOB_WIDTH - border * 2;
-				float mouseX = event.getX() - KNOB_WIDTH / 2 - border;
-				setValue(sliderMin + (sliderMax - sliderMin) * Math.max(0, Math.min(width, mouseX)) / width);
-			}
-
-			public void mouseMoved (MouseEvent event) {
-				int mouseX = event.getX();
-				int mouseY = event.getY();
-
-			}
-		});
-	}
-
-	protected void paintComponent (Graphics graphics) {
-		super.paintComponent(graphics);
-
-		Graphics2D g = (Graphics2D)graphics;
-		int width = getWidth();
-		int height = getHeight();
-
-		g.setColor(bgColor);
-		g.fillRect(border, border, width - border * 2, height - border * 2);
-
-		int maxKnobX = width - border - KNOB_WIDTH;
-		int knobX = (int)((width - border * 2 - KNOB_WIDTH) * (value - sliderMin) / (sliderMax - sliderMin)) + border;
-		g.setColor(knobColor);
-		g.fillRect(Math.max(border, Math.min(maxKnobX, knobX)), 0, KNOB_WIDTH, height);
-
-		float displayValue = (int)(value * 10) / 10f;
-		String label = displayValue == (int)displayValue ? String.valueOf((int)displayValue) : String.valueOf(displayValue);
-		FontMetrics metrics = g.getFontMetrics();
-		int labelWidth = metrics.stringWidth(label);
-		g.setColor(Color.white);
-		g.drawString(label, width / 2 - labelWidth / 2, height / 2 + metrics.getAscent() / 2);
+		spinner = new JSpinner(new SpinnerNumberModel(initialValue, min, max, stepSize));
+		setLayout(new BorderLayout());
+		add(spinner);
 	}
 
 	public void setValue (float value) {
-		this.value = (int)(Math.max(min, Math.min(max, value)) / stepSize) * stepSize;
-		repaint();
-		if (listener != null) listener.stateChanged(new ChangeEvent(this));
+		spinner.setValue((double)value);
 	}
 
 	public float getValue () {
-		return value;
+		return ((Double)spinner.getValue()).floatValue();
 	}
 
 	public void addChangeListener (ChangeListener listener) {
-		this.listener = listener;
+		spinner.addChangeListener(listener);
 	}
 
 	public Dimension getPreferredSize () {
 		Dimension size = super.getPreferredSize();
-		size.width = 150;
+		size.width = 75;
 		size.height = 26;
 		return size;
 	}
