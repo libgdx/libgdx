40218e3f74aa53cf5d6bc18690f6a5d9a24f42dd
NathanSweet <nathan.sweet@gmail.com>
2012-09-12 16:27:21 -0700

    Fixed 1053, FileHandle readBytes allocation.

--- gdx/src/com/badlogic/gdx/files/FileHandle.java
+++ gdx/src/com/badlogic/gdx/files/FileHandle.java
@@ -222,7 +222,7 @@ public class FileHandle {
 				int count = input.read(buffer, position, buffer.length - position);
 				if (count == -1) break;
 				position += count;
-				if (position == buffer.length) {
+				if (count == 0 && position == buffer.length) {
 					// Grow buffer.
 					byte[] newBuffer = new byte[buffer.length * 2];
 					System.arraycopy(buffer, 0, newBuffer, 0, position);
