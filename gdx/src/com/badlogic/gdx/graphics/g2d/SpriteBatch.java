eae5a4bfaf0387ea1dd24d2faa6076bc6c4eb3a6
Mario Zechner <contact@badlogicgames.com>
2012-11-04 14:13:38 +0100

    fixed javadocs of SpriteBatch#setShader

--- gdx/src/com/badlogic/gdx/graphics/g2d/SpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/SpriteBatch.java
@@ -1162,8 +1162,7 @@ public class SpriteBatch implements Disposable {
 	/** Sets the shader to be used in a GLES 2.0 environment. Vertex position attribute is called "a_position", the texture
 	 * coordinates attribute is called called "a_texCoords0", the color attribute is called "a_color". See
 	 * {@link ShaderProgram#POSITION_ATTRIBUTE}, {@link ShaderProgram#COLOR_ATTRIBUTE} and {@link ShaderProgram#TEXCOORD_ATTRIBUTE}
-	 * which gets "0" appened to indicate the use of the first texture unit. The projection matrix is uploaded via a mat4 uniform
-	 * called "u_proj", the transform matrix is uploaded via a uniform called "u_trans", the combined transform and projection
+	 * which gets "0" appened to indicate the use of the first texture unit. The combined transform and projection
 	 * matrx is is uploaded via a mat4 uniform called "u_projTrans". The texture sampler is passed via a uniform called
 	 * "u_texture".</p>
 	 * 
