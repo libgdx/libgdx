4970f1690e966f41f29899cb176211b6bfb473d7
badlogic <contact@badlogicgames.com>
2012-11-12 00:05:14 +0100

    and another fix, cause i'm dumb...

--- extensions/gdx-image/jni/memcpy_wrap.c
+++ extensions/gdx-image/jni/memcpy_wrap.c
@@ -1,3 +1,7 @@
+#ifndef __ANDROID__
+#ifdef __linux__
+#ifdef__x86_64__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,3 +12,7 @@ void *__wrap_memcpy(void * destination, const void * source, size_t num)
 {
         return memcpy(destination, source, num);
 }
+
+#endif
+#endif
+#endif
