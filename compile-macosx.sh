5cb590fb3b50bb86e212b68a4ea9fd3300dd332d
badlogic <contact@badlogicgames.com>
2012-11-12 23:58:46 +0100

    updated mac natives..

--- compile-macosx.sh
+++ compile-macosx.sh
@@ -1,14 +1,22 @@
 # compiles all the natives for mac os x...
 cd gdx/jni
+ant -f build-macosx32.xml clean
 ant -f build-macosx32.xml
 cd ../..
 cd extensions
 cd gdx-freetype/jni
+ant -f build-macosx32.xml clean
 ant -f build-macosx32.xml
 cd ../..
 cd gdx-audio/jni
+ant -f build-macosx32.xml clean
 ant -f build-macosx32.xml
 cd ../..
 cd gdx-image/jni
+ant -f build-macosx32.xml clean
+ant -f build-macosx32.xml
+cd ../..
+cd gdx-bullet/jni
+ant -f build-macosx32.xml
 ant -f build-macosx32.xml
 cd ../../../
