d4ad535a861f0d63517c64b38ee310bfdeccd3d3
NathanSweet <nathan.sweet@gmail.com>
2012-11-16 16:30:11 -0800

    ShapeRenderer, fixed filled circle segment estimation.

--- gdx/src/com/badlogic/gdx/graphics/glutils/ShapeRenderer.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/ShapeRenderer.java
@@ -513,7 +513,7 @@ public class ShapeRenderer {
 
 	/** Calls {@link #filledCircle(float, float, float, int)} by estimating the number of segments needed for a smooth circle. */
 	public void filledCircle (float x, float y, float radius) {
-		filledCircle(x, y, radius, (int)(4 * (float)Math.sqrt(radius)));
+		filledCircle(x, y, radius, (int)(6 * (float)Math.cbrt(radius)));
 	}
 
 	public void filledCircle (float x, float y, float radius, int segments) {
