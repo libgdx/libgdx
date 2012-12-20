9c6966710d1a99f926e72e3b920efa41d32b3f5d
Xoppa <contact@xoppa.nl>
2012-12-20 21:05:33 +0100

    Add SoftBodyTest

--- tests/gdx-tests/src/com/badlogic/gdx/tests/BulletTestCollection.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/BulletTestCollection.java
@@ -35,13 +35,15 @@ import com.badlogic.gdx.tests.bullet.ConstraintsTest;
 import com.badlogic.gdx.tests.bullet.ConvexHullTest;
 import com.badlogic.gdx.tests.bullet.KinematicTest;
 import com.badlogic.gdx.tests.bullet.MeshShapeTest;
+import com.badlogic.gdx.tests.bullet.RayPickRagdollTest;
 import com.badlogic.gdx.tests.bullet.RayCastTest;
 import com.badlogic.gdx.tests.bullet.ShootTest;
+import com.badlogic.gdx.tests.bullet.SoftBodyTest;
 import com.badlogic.gdx.tests.utils.GdxTest;
 
 /** @author xoppa */
 public class BulletTestCollection extends GdxTest implements InputProcessor, GestureListener {
-	private final BulletTest[] tests = {new ShootTest(), new KinematicTest(), new ConstraintsTest(), new MeshShapeTest(), new ConvexHullTest(), new RayCastTest()};
+	private final BulletTest[] tests = {new ShootTest(), new KinematicTest(), new ConstraintsTest(), new MeshShapeTest(), new ConvexHullTest(), new RayCastTest(), new RayPickRagdollTest(), new SoftBodyTest()};
 	
 	private int testIndex = 0;
 	
