f2bc59297d85d6772465845700f3a8485dbe97b4
Alex Vega <semtiko@gmail.com>
2012-12-27 05:55:14 +0400

    overlapRectangles clean

--- gdx/src/com/badlogic/gdx/math/Intersector.java
+++ gdx/src/com/badlogic/gdx/math/Intersector.java
@@ -596,10 +596,7 @@ public final class Intersector {
 	}
 
 	public static boolean overlapRectangles (Rectangle r1, Rectangle r2) {
-		if (r1.x < r2.x + r2.width && r1.x + r1.width > r2.x && r1.y < r2.y + r2.height && r1.y + r1.height > r2.y)
-			return true;
-		else
-			return false;
+		return (r1.x < r2.x + r2.width && r1.x + r1.width > r2.x && r1.y < r2.y + r2.height && r1.y + r1.height > r2.y);
 	}
 
 	public static boolean overlapCircleRectangle (Circle c, Rectangle r) {
