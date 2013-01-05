50e465277177aaead9cda1b193f5b81eff9b5a46
badlogic <badlogic@hadoop-vm2.(none)>
2013-01-05 23:23:19 +0100

    fixed linux builds

--- extensions/gdx-controllers/gdx-controllers-desktop/src/com/badlogic/gdx/controllers/DesktopControllersBuild.java
+++ extensions/gdx-controllers/gdx-controllers-desktop/src/com/badlogic/gdx/controllers/DesktopControllersBuild.java
@@ -12,24 +12,64 @@ public class DesktopControllersBuild {
 	public static void main (String[] args) throws Exception {
 		new NativeCodeGenerator().generate("src/", "bin/", "jni/");
 		BuildConfig buildConfig = new BuildConfig("gdx-controllers-desktop");
-		BuildTarget win32home = BuildTarget.newDefaultTarget(TargetOs.Windows, false);
-		win32home.buildFileName = "build-windows32home.xml";
-		win32home.is64Bit = false;
-		win32home.compilerPrefix = "";
-		win32home.cppIncludes = new String[] {
-			"*.cpp",
+		
+		String[] windowsSrc = {"*.cpp",
 			"ois-v1-4svn/src/*.cpp",
 			"ois-v1-4svn/src/win32/*.cpp"
 		};
-		win32home.headerDirs = new String[] {
+		
+		String[] linuxSrc = { "*.cpp",
+			"ois-v1-4svn/src/*.cpp",
+			"ois-v1-4svn/src/linux/*.cpp"
+		};
+		
+		String[] macSrc = { "*.cpp",
+			"ois-v1-4svn/src/*.cpp",
+			"ois-v1-4svn/src/linux/*.cpp"
+		};
+		
+		String[] includes = new String[] {
 			"ois-v1-4svn/includes",
 			"dinput/"
 		};
+		
+		BuildTarget win32home = BuildTarget.newDefaultTarget(TargetOs.Windows, false);
+		win32home.buildFileName = "build-windows32home.xml";
+		win32home.excludeFromMasterBuildFile = true;
+		win32home.is64Bit = false;
+		win32home.compilerPrefix = "";
+		win32home.cppIncludes = windowsSrc;
+		win32home.headerDirs = includes;
 		win32home.cIncludes = new String[0];
 		win32home.libraries = "-ldinput8 -ldxguid";
+		
+		BuildTarget win32 = BuildTarget.newDefaultTarget(TargetOs.Windows, false);
+		win32.cppIncludes = windowsSrc;
+		win32.headerDirs = includes;
+		win32.libraries = "-ldinput8 -ldxguid";
+		
+		BuildTarget win64 = BuildTarget.newDefaultTarget(TargetOs.Windows, true);
+		win64.cppIncludes = windowsSrc;
+		win64.headerDirs = includes;
+		win64.libraries = "-ldinput8 -ldxguid";
+		
+		BuildTarget lin32 = BuildTarget.newDefaultTarget(TargetOs.Linux, false);
+		lin32.cppIncludes = linuxSrc;
+		lin32.headerDirs = includes;
+		lin32.libraries = "-lX11";
 
-		new AntScriptGenerator().generate(buildConfig, win32home);
-		if(!BuildExecutor.executeAnt("jni/build-windows32home.xml", "-Dhas-compiler=true -v postcompile")) {
+		BuildTarget lin64 = BuildTarget.newDefaultTarget(TargetOs.Linux, true);
+		lin64.cppIncludes = linuxSrc;
+		lin64.headerDirs = includes;
+		lin64.libraries = "-lX11";
+		
+		BuildTarget mac = BuildTarget.newDefaultTarget(TargetOs.MacOsX, false);
+		mac.cppIncludes = macSrc;
+		mac.headerDirs = includes;
+		mac.libraries = ""; // FIXME
+		
+		new AntScriptGenerator().generate(buildConfig, win32home, win32, win64, lin32, lin64, mac);
+		if(!BuildExecutor.executeAnt("jni/build-linux32.xml", "-Dhas-compiler=true -v postcompile")) {
 			throw new Exception("build failed");
 		}
 		BuildExecutor.executeAnt("jni/build.xml", "pack-natives");
--- extensions/gdx-controllers/gdx-controllers-desktop/src/com/badlogic/gdx/controllers/desktop/ois/OisTest.java
+++ extensions/gdx-controllers/gdx-controllers-desktop/src/com/badlogic/gdx/controllers/desktop/ois/OisTest.java
@@ -17,32 +17,34 @@ public class OisTest {
 			@Override
 			public void create () {
 				this.ois = new Ois();
-				ois.getJoysticks().get(0).setCallback(new OisCallback() {
-					@Override
-					public void sliderMoved (OisJoystick joystick, int slider, boolean x, boolean y) {
-						System.out.println("slider " + slider + " moved, " + x + ", " + y);
-					}
-					
-					@Override
-					public void povMoved (OisJoystick joystick, int pov, OisPov direction) {
-						System.out.println("pov " + pov + " moved, " + direction);
-					}
-					
-					@Override
-					public void buttonReleased (OisJoystick joystick, int button) {
-						System.out.println("button " + button + " released");
-					}
-					
-					@Override
-					public void buttonPressed (OisJoystick joystick, int button) {
-						System.out.println("button " + button + " pressed");
-					}
-					
-					@Override
-					public void axisMoved (OisJoystick joystick, int axis, float value) {
-						System.out.println("axis " + axis + " moved, " + value);
-					}
-				});
+				if(ois.getJoysticks().size > 0) {
+					ois.getJoysticks().get(0).setCallback(new OisCallback() {
+						@Override
+						public void sliderMoved (OisJoystick joystick, int slider, boolean x, boolean y) {
+							System.out.println("slider " + slider + " moved, " + x + ", " + y);
+						}
+						
+						@Override
+						public void povMoved (OisJoystick joystick, int pov, OisPov direction) {
+							System.out.println("pov " + pov + " moved, " + direction);
+						}
+						
+						@Override
+						public void buttonReleased (OisJoystick joystick, int button) {
+							System.out.println("button " + button + " released");
+						}
+						
+						@Override
+						public void buttonPressed (OisJoystick joystick, int button) {
+							System.out.println("button " + button + " pressed");
+						}
+						
+						@Override
+						public void axisMoved (OisJoystick joystick, int axis, float value) {
+							System.out.println("axis " + axis + " moved, " + value);
+						}
+					});
+				}
 			}
 			
 			public void render() {
