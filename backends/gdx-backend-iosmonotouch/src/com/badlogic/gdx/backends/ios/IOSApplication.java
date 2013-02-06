0c7dab7dc32d77de55e51311a68d59f8b66928f0
Tam Dinh Thai Minh <tamdinh@gmail.com>
2013-02-06 23:12:05 +0700

    Add a method for getting UIViewController.

--- backends/gdx-backend-iosmonotouch/src/com/badlogic/gdx/backends/ios/IOSApplication.java
+++ backends/gdx-backend-iosmonotouch/src/com/badlogic/gdx/backends/ios/IOSApplication.java
@@ -168,6 +168,14 @@ public class IOSApplication extends UIApplicationDelegate implements Application
 		Gdx.app.debug("IOSApplication", "created");
 		return true;
 	}
+	
+	/**
+	 * Return the UI view controller of IOSApplication
+	 * @return the view controller of IOSApplication
+	 */
+	public UIViewController getUIViewController(){
+		return uiViewController;
+	}
 
 	/** Returns our real display dimension based on screen orientation.
 	 * 
