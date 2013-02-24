8fb6db621f6a84ea8c6c5c5672c1c89f6eac9dbc
Justin Shapcott <support@mobidevelop.com>
2013-02-23 21:35:37 -0700

    Fix loading MapProperties from tmx file.

--- gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java
@@ -529,7 +529,7 @@ public class TmxMapLoader extends SynchronousAssetLoader<TiledMap, TmxMapLoader.
 		if (element.getName().equals("properties")) {
 			for (Element property : element.getChildrenByName("property")) {
 				String name = property.getAttribute("name", null);
-				String value = property.getAttribute(name, null);
+				String value = property.getAttribute("value", null);
 				if (value == null) {
 					value = property.getText();
 				}
