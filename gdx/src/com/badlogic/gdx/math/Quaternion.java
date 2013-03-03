89dd159e6c635cedbd7c6daeb3a14d26b8f36e75
Xoppa <contact@xoppa.nl>
2013-03-03 22:22:01 +0100

    Small bugfix, Quaternion#setFromCross should convert to degrees as setFromAxis expects it.

--- gdx/src/com/badlogic/gdx/math/Quaternion.java
+++ gdx/src/com/badlogic/gdx/math/Quaternion.java
@@ -360,7 +360,7 @@ public class Quaternion implements Serializable {
 	 * @return This quaternion for chaining */
 	public Quaternion setFromCross (final Vector3 v1, final Vector3 v2) {
 		final float dot = MathUtils.clamp(Vector3.tmp.set(v1).nor().dot(Vector3.tmp2.set(v2).nor()), -1f, 1f);
-		return setFromAxis(Vector3.tmp.crs(Vector3.tmp2), (float)Math.acos(dot));
+		return setFromAxis(Vector3.tmp.crs(Vector3.tmp2), (float)Math.acos(dot) * MathUtils.radiansToDegrees);
 	}
 	
 	/** Set this quaternion to the rotation between two vectors.
@@ -373,7 +373,7 @@ public class Quaternion implements Serializable {
 	 * @return This quaternion for chaining */
 	public Quaternion setFromCross (final float x1, final float y1, final float z1, final float x2, final float y2, final float z2) {
 		final float dot = MathUtils.clamp(Vector3.tmp.set(x1, y1, z1).nor().dot(Vector3.tmp2.set(x2, y2, z2).nor()), -1f, 1f);
-		return setFromAxis(Vector3.tmp.crs(Vector3.tmp2), (float)Math.acos(dot));
+		return setFromAxis(Vector3.tmp.crs(Vector3.tmp2), (float)Math.acos(dot) * MathUtils.radiansToDegrees);
 	}
 
 	/** Spherical linear interpolation between this quaternion and the other quaternion, based on the alpha value in the range
