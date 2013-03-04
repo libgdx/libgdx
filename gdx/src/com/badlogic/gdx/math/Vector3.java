fcda34896499d9f0c29ea5f000aee28854bdbfe2
Xoppa <contact@xoppa.nl>
2013-03-04 18:56:50 +0100

    Fix typo #2

--- gdx/src/com/badlogic/gdx/math/Vector3.java
+++ gdx/src/com/badlogic/gdx/math/Vector3.java
@@ -255,7 +255,7 @@ public class Vector3 implements Serializable, Vector<Vector3> {
 		final float b = vector.y - y;
 		final float c = vector.z - z;
 
-		return (float)Math.sqrt(a * a + b * b + c * b);
+		return (float)Math.sqrt(a * a + b * b + c * c);
 	}
 
 	/** Normalizes this vector to unit length
