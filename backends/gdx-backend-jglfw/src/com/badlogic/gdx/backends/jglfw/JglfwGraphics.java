7e64b8399ef45194a0129ae9427925ccded13f0a
NathanSweet <nathan.sweet@gmail.com>
2013-03-05 12:33:19 +0100

    GL10

--- backends/gdx-backend-jglfw/src/com/badlogic/gdx/backends/jglfw/JglfwGraphics.java
+++ backends/gdx-backend-jglfw/src/com/badlogic/gdx/backends/jglfw/JglfwGraphics.java
@@ -285,6 +285,8 @@ public class JglfwGraphics implements Graphics {
 		return config.fullscreen;
 	}
 
+	/** Returns the JGLFW window handle. Note this should not be stored externally as it may change if the window is recreated to
+	 * enter/exit fullscreen. */
 	public long getWindow () {
 		return window;
 	}
