3348e30cdaf5a2d4378d63c43369105b96afb59b
badlogic <badlogicgames@gmail.com>
2013-03-17 00:10:11 +0100

    fixed issue 1239, MathUtils.ceil was broken in GWT as NumberUtils isn't fully implemented there

--- tests/gdx-tests-lwjgl/src/com/badlogic/gdx/tests/lwjgl/LwjglDebugStarter.java
+++ tests/gdx-tests-lwjgl/src/com/badlogic/gdx/tests/lwjgl/LwjglDebugStarter.java
@@ -41,7 +41,6 @@ public class LwjglDebugStarter {
 		new SharedLibraryLoader("../../extensions/gdx-controllers/gdx-controllers-desktop/libs/gdx-controllers-desktop-natives.jar").load("gdx-controllers-desktop");
 		new SharedLibraryLoader("../../gdx/libs/gdx-natives.jar").load("gdx");
 
-		MathUtils.ceil(1);
 		GdxTest test = new AnimationTest();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = test.needsGL20();
