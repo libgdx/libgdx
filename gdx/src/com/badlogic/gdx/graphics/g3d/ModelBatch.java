483c45545c0d0620612c0977a5fda6d61c8f6051
badlogic <badlogicgames@gmail.com>
2013-03-24 16:42:28 +0100

    added ModelBatch, adaption of RenderBatch so i can experiment, see NewModelTest

---
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
@@ -0,0 +1,99 @@
+package com.badlogic.gdx.graphics.g3d;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g3d.model.Model;
+import com.badlogic.gdx.graphics.g3d.test.Light;
+import com.badlogic.gdx.graphics.g3d.test.NewModel;
+import com.badlogic.gdx.graphics.g3d.utils.ExclusiveTextures;
+import com.badlogic.gdx.graphics.g3d.utils.RenderInstancePool;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+
+public class ModelBatch {
+	protected RenderContext context;
+	protected RenderBatchListener listener;
+	protected Camera camera;
+	protected final Array<RenderInstance> instances = new Array<RenderInstance>();	
+	protected final Pool<Renderable> renderablesPool = new Pool<Renderable>() {
+		@Override
+		protected Renderable newObject () {
+			return new Renderable();
+		}
+	};
+	private final RenderInstancePool renderInstancePool = new RenderInstancePool();
+	/** used in {@link #addModel(Model, Matrix4, Light[], Shader)} to retrieve Renderables **/
+	protected final Array<Renderable> renderables = new Array<Renderable>();
+	
+	/** Construct a BaseRenderBatch with the specified listener */
+	public ModelBatch(RenderBatchListener listener, ExclusiveTextures textures) {
+		this.listener = listener;
+		this.context = new RenderContext(textures);
+	}
+	
+	/** Construct a BaseRenderBatch with the default implementation and the specified texture range */
+	public ModelBatch(ExclusiveTextures textures) {
+		this(new RenderBatchAdapter(), textures);
+	}
+	
+	/** Construct a BaseRenderBatch with the default implementation */
+	public ModelBatch() {
+		this(new ExclusiveTextures(ExclusiveTextures.ROUNDROBIN));
+	}
+
+	public void begin (Camera cam) {
+		this.camera = cam;
+	}
+
+	public void end () {
+		instances.sort(listener);
+		context.begin();
+		Shader currentShader = null;
+		for (int i = 0; i < instances.size; i++) {
+			final RenderInstance instance = instances.get(i);
+			if (currentShader != instance.shader) {
+				if (currentShader != null)
+					currentShader.end();
+				currentShader = instance.shader;
+				currentShader.begin(camera, context);
+			}
+			currentShader.render(instance);
+		}
+		if (currentShader != null)
+			currentShader.end();
+		context.end();
+		renderablesPool.freeAll(renderables);
+		renderables.clear();
+		renderInstancePool.freeAll(instances);
+		instances.clear();
+		camera = null;
+	}
+
+	/** Add an instance to render, the shader property of the instance will be overwritten by this method */
+	protected void addInstance(final RenderInstance instance, final Shader shader) {
+		instance.shader = listener.getShader(instance, shader);
+		instance.renderable.mesh.setAutoBind(false);
+		instances.add(instance);
+	}
+	
+	public void addRenderable(final Renderable renderable, final Matrix4 transform, final float distance, final Light[] lights, final Shader shader) {
+		addInstance(renderInstancePool.obtain(renderable, transform, distance, lights, null), shader);
+	}
+	
+	public void addModel(final Model model, final Matrix4 transform) {
+		addModel(model, transform, null, null);
+	}
+	
+	public void addModel(final Model model, final Matrix4 transform, final Light[] lights) {
+		addModel(model, transform, lights, null);
+	}
+	
+	public void addModel(final Model model, final Matrix4 transform, final Light[] lights, final Shader shader) {
+		int offset = renderables.size;
+		model.getRenderables(renderables, renderablesPool);
+		for (int i = offset; i < renderables.size; i++) {
+			addRenderable(renderables.get(i), transform, 0, lights, null);
+		}
+	}
+}
---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/NewModelTest.java
@@ -0,0 +1,100 @@
+package com.badlogic.gdx.tests.g3d;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.RenderBatch;
+import com.badlogic.gdx.graphics.g3d.loader.JsonModelLoader;
+import com.badlogic.gdx.graphics.g3d.model.Model;
+import com.badlogic.gdx.graphics.g3d.test.Light;
+import com.badlogic.gdx.graphics.g3d.test.TestShader;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.tests.utils.GdxTest;
+
+public class NewModelTest extends GdxTest {
+	PerspectiveCamera cam;
+	ModelBatch modelBatch;
+	Model model;
+	Matrix4 transform = new Matrix4();
+	ShapeRenderer shapeRenderer;
+	
+	Light[] lights = new Light[] {
+		new Light(Color.WHITE, Vector3.tmp.set(-10f, 10f, -10f), 15f),
+		new Light(Color.BLUE, Vector3.tmp.set(10f, 5f, 0f), 10f),
+		new Light(Color.GREEN, Vector3.tmp.set(0f, 10f, 5f), 5f)
+	};
+	
+	float touchStartX = 0;
+	float touchStartY = 0;
+	
+	@Override
+	public void create () {
+		JsonModelLoader loader = new JsonModelLoader();
+		model = new Model(loader.parseModel(Gdx.files.internal("data/g3d/head.g3dj"),  null));
+		modelBatch = new ModelBatch();
+		TestShader.ignoreUnimplemented = true;
+		shapeRenderer = new ShapeRenderer();
+		
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.direction.set(-1, -1, -1);
+		cam.near = 0.1f;
+		cam.far = 300f;
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+//		Gdx.gl.glDisable(GL20.GL_CULL_FACE);
+
+		cam.update();
+		shapeRenderer.setProjectionMatrix(cam.combined);
+		shapeRenderer.begin(ShapeType.Line);
+		shapeRenderer.setColor(Color.RED);
+		shapeRenderer.line(-100, 0, 0, 100, 0, 0);
+		shapeRenderer.setColor(Color.GREEN);
+		shapeRenderer.line(0, -100, 0, 0, 100, 0);
+		shapeRenderer.setColor(Color.BLUE);
+		shapeRenderer.line(0, 0, -100, 0, 0, 100);
+		shapeRenderer.end();
+		
+		modelBatch.begin(cam);
+		modelBatch.addModel(model, transform, lights);
+		modelBatch.end();
+	}
+
+	@Override
+	public boolean touchDown (int x, int y, int pointer, int newParam) {
+		touchStartX = x;
+		touchStartY = y;
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged (int x, int y, int pointer) {
+		cam.rotateAround(new Vector3(), Vector3.Y, x - touchStartX);
+//		cam.rotateAround(new Vector3(), Vector3.X, y - touchStartY);
+		touchStartX = x;
+		touchStartY = y;
+		return false;
+	}
+
+	@Override
+	public boolean scrolled (int amount) {
+		cam.fieldOfView -= -amount * Gdx.graphics.getDeltaTime() * 100;
+		return false;
+	}
+
+	@Override
+	public boolean needsGL20 () {
+		return true;
+	}
+}
