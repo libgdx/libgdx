9402efb9633cc3c4de670f87854bfa9ca0e8c69e
Xoppa <contact@xoppa.nl>
2013-03-30 01:20:46 +0100

    Add GLES10Shader

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultShaderProvider.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultShaderProvider.java
@@ -3,7 +3,8 @@ package com.badlogic.gdx.graphics.g3d.utils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.graphics.g3d.Shader;
-import com.badlogic.gdx.graphics.g3d.materials.NewMaterial;
+import com.badlogic.gdx.graphics.g3d.materials.Material;
+import com.badlogic.gdx.graphics.g3d.test.GLES10Shader;
 import com.badlogic.gdx.graphics.g3d.test.TestShader;
 import com.badlogic.gdx.utils.Array;
 
@@ -25,9 +26,11 @@ public class DefaultShaderProvider implements ShaderProvider {
 		return result;
 	}
 	
-	protected Shader createShader(final NewMaterial material) {
+	protected Shader createShader(final Material material) {
 		Gdx.app.log("DefaultShaderProvider", "Creating new shader");
-		return new TestShader(material);
+		if (Gdx.graphics.isGL20Available())
+			return new TestShader(material);
+		return new GLES10Shader();
 	}
 
 	@Override
