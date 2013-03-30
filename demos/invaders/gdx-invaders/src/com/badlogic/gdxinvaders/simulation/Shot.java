c8735e6fc82a51c335fa9a0170e0286027108d1b
Xoppa <contact@xoppa.nl>
2013-03-30 01:24:57 +0100

    - Update gdxinvaders to use new 3d api (todo: explosions)

--- demos/invaders/gdx-invaders/src/com/badlogic/gdxinvaders/simulation/Shot.java
+++ demos/invaders/gdx-invaders/src/com/badlogic/gdxinvaders/simulation/Shot.java
@@ -13,26 +13,30 @@
 
 package com.badlogic.gdxinvaders.simulation;
 
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Vector3;
 
-public class Shot {
+public class Shot extends ModelInstance {
 	public static float SHOT_VELOCITY = 10;
-	public final Vector3 position = new Vector3();
 	public boolean isInvaderShot;
 	public boolean hasLeftField = false;
+	private final static Vector3 tmpV = new Vector3();
 
-	public Shot (Vector3 position, boolean isInvaderShot) {
-		this.position.set(position);
+	public Shot (Model model, Vector3 position, boolean isInvaderShot) {
+		super(model, position);
+		// this.position.set(position);
 		this.isInvaderShot = isInvaderShot;
 	}
 
 	public void update (float delta) {
 		if (isInvaderShot)
-			position.z += SHOT_VELOCITY * delta;
+			transform.trn(0, 0, SHOT_VELOCITY * delta);
 		else
-			position.z -= SHOT_VELOCITY * delta;
+			transform.trn(0, 0, -SHOT_VELOCITY * delta);
 
-		if (position.z > Simulation.PLAYFIELD_MAX_Z) hasLeftField = true;
-		if (position.z < Simulation.PLAYFIELD_MIN_Z) hasLeftField = true;
+		transform.getTranslation(tmpV);
+		if (tmpV.z > Simulation.PLAYFIELD_MAX_Z) hasLeftField = true;
+		if (tmpV.z < Simulation.PLAYFIELD_MIN_Z) hasLeftField = true;
 	}
 }
