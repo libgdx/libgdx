ed3af3ca0f1ce678dc7290f50bace47f0dd44a26
Xoppa <contact@xoppa.nl>
2013-04-22 22:47:01 +0200

    Add g3dbmodelloader

--- tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/NewModelTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/NewModelTest.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.lights.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.lights.Lights;
 import com.badlogic.gdx.graphics.g3d.lights.PointLight;
+import com.badlogic.gdx.graphics.g3d.loader.G3dbModelLoader;
 import com.badlogic.gdx.graphics.g3d.loader.JsonModelLoader;
 import com.badlogic.gdx.graphics.g3d.materials.Material;
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
@@ -33,9 +34,9 @@ import com.badlogic.gdx.utils.Array;
 public class NewModelTest extends BaseG3dHudTest {
 	Lights lights = new Lights(0.2f, 0.2f, 0.2f).add(
 		//new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -1f, 0f)
-		new PointLight().set(1f, 0f, 0f, 5f, 5f, 5f, 100f),
-		new PointLight().set(0f, 0f, 1f, -5f, 5f, 5f, 100f),
-		new PointLight().set(0f, 1f, 0f, 0f, 5f, -5f, 100f)
+		new PointLight().set(1f, 0f, 0f, 5f, 5f, 5f, 15f),
+		new PointLight().set(0f, 0f, 1f, -5f, 5f, 5f, 15f),
+		new PointLight().set(0f, 1f, 0f, 0f, 5f, -5f, 7f)
 		//new Light(0.5f, 0.5f, 0.5f, 1f),
 		//new Light(0.5f, 0.5f, 0.5f, 1f, -1f, -2f, -3f)
 	);
@@ -43,8 +44,7 @@ public class NewModelTest extends BaseG3dHudTest {
 	@Override
 	public void create () {
 		super.create();
-
-		onModelClicked("g3d/head.g3dj");
+		onModelClicked("g3d/teapot.g3db");
 	}
 
 	@Override
