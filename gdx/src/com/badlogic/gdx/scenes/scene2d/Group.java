bf4973d3df8a77304c071f5ddb35ea5022e96888
aurel2152 <aurelien.catinon@gmail.com>
2013-05-04 22:00:39 +0300

    Possible bug in Group
    I have random NullPointerException at com.badlogic.gdx.scenes.scene2d.Group.drawChildren (SourceFile:79).
    
    What happens if an actor is added between line 68 and line 77 ?
    
    I think children.size is incremented, but the actor is obviously not added in actors SnapshotArray, so the last child is null.
    
    What do you think ?
    
    Thanks for your time :)

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Group.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Group.java
@@ -74,7 +74,7 @@ public class Group extends Actor implements Cullable {
 			float cullBottom = cullingArea.y;
 			float cullTop = cullBottom + cullingArea.height;
 			if (transform) {
-				for (int i = 0, n = children.size; i < n; i++) {
+				for (int i = 0, n = actors.length; i < n; i++) {
 					Actor child = actors[i];
 					if (!child.isVisible()) continue;
 					float cx = child.x, cy = child.y;
@@ -87,7 +87,7 @@ public class Group extends Actor implements Cullable {
 				float offsetX = x, offsetY = y;
 				x = 0;
 				y = 0;
-				for (int i = 0, n = children.size; i < n; i++) {
+				for (int i = 0, n = actors.length; i < n; i++) {
 					Actor child = actors[i];
 					if (!child.isVisible()) continue;
 					float cx = child.x, cy = child.y;
@@ -105,7 +105,7 @@ public class Group extends Actor implements Cullable {
 		} else {
 			// No culling, draw all children.
 			if (transform) {
-				for (int i = 0, n = children.size; i < n; i++) {
+				for (int i = 0, n = actors.length; i < n; i++) {
 					Actor child = actors[i];
 					if (!child.isVisible()) continue;
 					child.draw(batch, parentAlpha);
@@ -116,7 +116,7 @@ public class Group extends Actor implements Cullable {
 				float offsetX = x, offsetY = y;
 				x = 0;
 				y = 0;
-				for (int i = 0, n = children.size; i < n; i++) {
+				for (int i = 0, n = actors.length; i < n; i++) {
 					Actor child = actors[i];
 					if (!child.isVisible()) continue;
 					float cx = child.x, cy = child.y;
