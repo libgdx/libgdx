c3fd42aeb5999cea7c5742eaa3eb0d6c3a88c825
badlogic <badlogicgames@gmail.com>
2013-05-20 23:56:06 +0200

    removed model-loaders extension, replaced by new 3D api

--- extensions/model-loaders/model-loaders/generate-beans.sh
+++
@@ -1,3 +0,0 @@
-#xjc -dtd -no-header -p "com.badlogic.gdx.graphics.g3d.orgrexml.mesh" -d src "mesh.dtd"
-xjc -dtd -no-header -p com.badlogic.gdx.graphics.g3d.orgrexml.skeleton -d src skeleton.dtd
-xjc -dtd -no-header -p com.badlogic.gdx.graphics.g3d.orgrexml.mesh -d src mesh.dtd
