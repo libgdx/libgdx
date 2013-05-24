9def89f7003f55b4c3807de130da4a3a0abb6f62
NathanSweet <nathan.sweet@gmail.com>
2013-05-24 13:39:22 +0200

    Added updateVisualScroll for setting scroll position without animating.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
@@ -657,6 +657,12 @@ public class ScrollPane extends WidgetGroup {
 		return amountY;
 	}
 
+	/** Sets the visual scroll amount equal to the scroll amount. This can be used when setting the scroll amount without animating. */
+	public void updateVisualScroll () {
+		visualAmountX = amountX;
+		visualAmountY = amountY;
+	}
+
 	public float getVisualScrollX () {
 		return !scrollX ? 0 : visualAmountX;
 	}
