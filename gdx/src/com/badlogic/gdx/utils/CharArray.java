b73112717db734c71c7be9122fbc4c48555e53d2
NathanSweet <nathan.sweet@gmail.com>
2013-05-27 18:44:10 +0200

    Fixed array addAll.

--- gdx/src/com/badlogic/gdx/utils/CharArray.java
+++ gdx/src/com/badlogic/gdx/utils/CharArray.java
@@ -94,7 +94,7 @@ public class CharArray {
 
 	public void addAll (char[] array, int offset, int length) {
 		char[] items = this.items;
-		int sizeNeeded = size + length - offset;
+		int sizeNeeded = size + length ;
 		if (sizeNeeded >= items.length) items = resize(Math.max(8, (int)(sizeNeeded * 1.75f)));
 		System.arraycopy(array, offset, items, size, length);
 		size += length;
