d988d00d970784129a75e54ccf0369dd3df53a7a
badlogic <badlogicgames@gmail.com>
2013-05-30 19:16:02 +0200

    added simple cube with two textures, added ModelLoaderTest to reproduce loader issue

---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/ModelLoaderTest.java
@@ -0,0 +1,54 @@
+package com.badlogic.gdx.tests.g3d;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.tests.utils.GdxTest;
+
+public class ModelLoaderTest extends GdxTest {
+	AssetManager assets;
+	PerspectiveCamera camera;
+	ModelBatch batch;
+	Model model;
+	ModelInstance instance;
+
+	@Override
+	public void create () {
+		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(0, 0, 5);
+		camera.near = 1;
+		camera.far = 100;
+		camera.update();
+		assets = new AssetManager();
+		assets.load("data/g3d/cube.g3dj", Model.class);
+		assets.finishLoading();
+		model = assets.get("data/g3d/cube.g3dj", Model.class);
+		instance = new ModelInstance(model);
+		batch = new ModelBatch();
+	}
+
+	@Override
+	public boolean needsGL20 () {
+		return true;
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+		
+		batch.begin(camera);
+		batch.render(instance);
+		batch.end();
+	}
+
+	@Override
+	public void dispose () {
+		assets.dispose();
+		batch.dispose();
+	}
+}
