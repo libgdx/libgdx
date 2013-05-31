35f6f88936ad12fb05f0b2e1f821d491dc92d2b7
badlogic <badlogicgames@gmail.com>
2013-05-31 13:55:01 +0200

    fixed fetch script to pull in ios robovm natives into proper location

--- demos/superjumper/superjumper/src/com/badlogicgames/superjumper/SuperJumper.java
+++ demos/superjumper/superjumper/src/com/badlogicgames/superjumper/SuperJumper.java
@@ -28,14 +28,14 @@ public class SuperJumper extends Game {
 	public void create () {
 		Settings.load();
 		Assets.load();
-		setScreen(new MainMenuScreen(this));
+		setScreen(new GameScreen(this));
 		fps = new FPSLogger();
 	}
 	
 	@Override
 	public void render() {
 		super.render();
-//		fps.log();
+		fps.log();
 	}
 
 	/** {@link Game#dispose()} only calls {@link Screen#hide()} so you need to override {@link Game#dispose()} in order to call
