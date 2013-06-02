ac1d233076b036fa6df3f95b6277ff28e2ed7b8d
SG57 <jordansg57@gmail.com>
2013-06-02 03:47:11 -0600

    Wrong method name copy-paste error xD

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ListFilterable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ListFilterable.java
@@ -70,7 +70,7 @@ public class ListFilterable extends List {
 	 * @param case_sensitive Whether the filter and items are compared case sensitive
 	 */
 	public void filterList(String filter, boolean case_sensitive) {
-		if (filter.isEmpty() && getItems().length == getItemsDisplayed().length) // don't need to filter
+		if (filter.isEmpty() && getItems().length == getItemsUnfiltered().length) // don't need to filter
 			return;
 		
 		if (!case_sensitive)
