34fea4ea3ab85914902ab1deafa3df63608bef68
Yonsh <yonsh@live.com>
2013-06-07 12:06:39 +0800

    Fix SequenceAction skipping first action under certain circumstance
    If this SequenceAction is freed during actions.get(index).act(delta), next time this SequenceAction being obtained, index will start from 1.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/SequenceAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/SequenceAction.java
@@ -59,8 +59,8 @@ public class SequenceAction extends ParallelAction {
 	public boolean act (float delta) {
 		if (index >= actions.size) return true;
 		if (actions.get(index).act(delta)) {
+			if (index >= actions.size) return true;
 			index++;
-			if (index > actions.size) return true;
 		}
 		return false;
 	}
