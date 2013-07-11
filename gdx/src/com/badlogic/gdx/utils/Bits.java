f0e67a1474fab9528737cc703fde6c35fdd5e288
Justin Shapcott <support@mobidevelop.com>
2013-07-11 15:46:16 -0700

    Fix Issue 1561

--- gdx/src/com/badlogic/gdx/utils/Bits.java
+++ gdx/src/com/badlogic/gdx/utils/Bits.java
@@ -39,7 +39,7 @@ public class Bits {
 	}
 	
 	private void checkCapacity(int len) {
-		if(len> bits.length) {
+		if(len>=bits.length) {
 			long[] newBits = new long[len+1];
 			System.arraycopy(bits, 0, newBits, 0, bits.length);
 			bits = newBits;
