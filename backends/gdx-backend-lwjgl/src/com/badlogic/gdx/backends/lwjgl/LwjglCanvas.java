8a436bdfd87bcf159009e9590034c7c7891aaca4
dphang <dphang12@gmail.com>
2013-07-23 20:35:00 -0700

    Fixed getFrameRate()

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglCanvas.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglCanvas.java
@@ -256,7 +256,7 @@ public class LwjglCanvas implements Application {
 		if (frameRate == -1) frameRate = 10;
 		if (frameRate == 0) frameRate = graphics.config.backgroundFPS;
 		if (frameRate == 0) frameRate = 30;
-		return 0;
+		return frameRate;
 	}
 
 	protected void exception (Throwable ex) {
