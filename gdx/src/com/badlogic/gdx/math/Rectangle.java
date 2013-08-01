e5f871636e226e0d2eb7110b70c91cfaec16644b
Jonathan Pearl <jonathantpearl@gmail.com>
2013-08-01 16:37:38 -0600

    Forgot to change centerTo->setCenter and more chains

--- gdx/src/com/badlogic/gdx/math/Rectangle.java
+++ gdx/src/com/badlogic/gdx/math/Rectangle.java
@@ -54,11 +54,18 @@ public class Rectangle implements Serializable {
 		height = rect.height;
 	}
 
-	public void set (float x, float y, float width, float height) {
+	/** @param x bottom-left x coordinate
+	 * @param y bottom-left y coordinate
+	 * @param width width
+	 * @param height height
+	 * @return this rectangle for chaining */
+	public Rectangle set (float x, float y, float width, float height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		
+		return this;
 	}
 
 	/** @return the x-coordinate of the bottom left corner */
@@ -67,9 +74,12 @@ public class Rectangle implements Serializable {
 	}
 
 	/** Sets the x-coordinate of the bottom left corner
-	 * @param x The x-coordinate */
-	public void setX (float x) {
+	 * @param x The x-coordinate
+	 * @return this rectangle for chaining */
+	public Rectangle setX (float x) {
 		this.x = x;
+		
+		return this;
 	}
 
 	/** @return the y-coordinate of the bottom left corner */
@@ -78,9 +88,12 @@ public class Rectangle implements Serializable {
 	}
 
 	/** Sets the y-coordinate of the bottom left corner
-	 * @param y The y-coordinate */
-	public void setY (float y) {
+	 * @param y The y-coordinate 
+	 * @return this rectangle for chaining */
+	public Rectangle setY (float y) {
 		this.y = y;
+		
+		return this;
 	}
 
 	/** @return the width */
@@ -89,9 +102,12 @@ public class Rectangle implements Serializable {
 	}
 
 	/** Sets the width of this rectangle
-	 * @param width The width */
-	public void setWidth (float width) {
+	 * @param width The width 
+	 * @return this rectangle for chaining */
+	public Rectangle setWidth (float width) {
 		this.width = width;
+		
+		return this;
 	}
 
 	/** @return the height */
@@ -100,9 +116,12 @@ public class Rectangle implements Serializable {
 	}
 
 	/** Sets the height of this rectangle
-	 * @param height The height */
-	public void setHeight (float height) {
+	 * @param height The height 
+	 * @return this rectangle for chaining */
+	public Rectangle setHeight (float height) {
 		this.height = height;
+		
+		return this;
 	}
 
 	/** return the Vector2 with coordinates of this rectangle
@@ -112,33 +131,45 @@ public class Rectangle implements Serializable {
 	}
 
 	/** Sets the x and y-coordinates of the bottom left corner from vector
-	 * @param position The position vector */
-	public void setPosition (Vector2 position) {
+	 * @param position The position vector
+	 * @return this rectangle for chaining */
+	public Rectangle setPosition (Vector2 position) {
 		this.x = position.x;
 		this.y = position.y;
+		
+		return this;
 	}
 
 	/** Sets the x and y-coordinates of the bottom left corner
 	 * @param x The x-coordinate
-	 * @param y The y-coordinate */
-	public void setPosition (float x, float y) {
+	 * @param y The y-coordinate 
+	 * @return this rectangle for chaining */
+	public Rectangle setPosition (float x, float y) {
 		this.x = x;
 		this.y = y;
+		
+		return this;
 	}
 
 	/** Sets the width and height of this rectangle
 	 * @param width The width
-	 * @param height The height */
-	public void setSize (float width, float height) {
+	 * @param height The height 
+	 * @return this rectangle for chaining */
+	public Rectangle setSize (float width, float height) {
 		this.width = width;
 		this.height = height;
+		
+		return this;
 	}
 
 	/** Sets the squared size of this rectangle
-	 * @param sizeXY The size */
-	public void setSize (float sizeXY) {
+	 * @param sizeXY The size 
+	 * @return this rectangle for chaining */
+	public Rectangle setSize (float sizeXY) {
 		this.width = sizeXY;
 		this.height = sizeXY;
+		
+		return this;
 	}
 
 	/** @return the Vector2 with size of this rectangle
@@ -180,17 +211,21 @@ public class Rectangle implements Serializable {
 	}
 
 	/** Sets the values of the given rectangle to this rectangle.
-	 * @param rect the other rectangle */
-	public void set (Rectangle rect) {
+	 * @param rect the other rectangle
+	 * @return this rectangle for chaining */
+	public Rectangle set (Rectangle rect) {
 		this.x = rect.x;
 		this.y = rect.y;
 		this.width = rect.width;
 		this.height = rect.height;
+		
+		return this;
 	}
 
 	/** Merges this rectangle with the other rectangle.
-	 * @param rect the other rectangle */
-	public void merge (Rectangle rect) {
+	 * @param rect the other rectangle
+	 * @return this rectangle for chaining */
+	public Rectangle merge (Rectangle rect) {
 		float minX = Math.min(x, rect.x);
 		float maxX = Math.max(x + width, rect.x + rect.width);
 		x = minX;
@@ -200,6 +235,8 @@ public class Rectangle implements Serializable {
 		float maxY = Math.max(y + height, rect.y + rect.height);
 		y = minY;
 		height = maxY - minY;
+		
+		return this;
 	}
 	
 	/** Calculates the aspect ratio ( width / height ) of this rectangle
@@ -222,7 +259,7 @@ public class Rectangle implements Serializable {
 	 * @param x the position's x
 	 * @param y the position's y
 	 * @return this for chaining */
-	public Rectangle centerTo (float x, float y) {
+	public Rectangle setCenter (float x, float y) {
 		setPosition(x - width / 2, y - height / 2);
 		return this;
 	}
@@ -230,7 +267,7 @@ public class Rectangle implements Serializable {
 	/** Moves this rectangle so that its center point is located at a given position
 	 * @param position the position
 	 * @return this for chaining */
-	public Rectangle centerTo (Vector2 position) {
+	public Rectangle setCenter (Vector2 position) {
 		setPosition(position.x - width / 2, position.y - height / 2);
 		return this;
 	}
