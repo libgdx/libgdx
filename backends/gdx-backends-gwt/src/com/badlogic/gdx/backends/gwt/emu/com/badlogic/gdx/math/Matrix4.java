100bdc6dc2fb9cfc4b8c37783bd04d0fcd8a488b
Xoppa <contact@xoppa.nl>
2013-08-08 21:29:26 +0200

    pre multiply

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/math/Matrix4.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/math/Matrix4.java
@@ -196,24 +196,24 @@ public class Matrix4 implements Serializable {
 	 * @param scale The scale
 	 * @return This matrix for chaining */
 	public Matrix4 set (Vector3 position, Quaternion orientation, Vector3 scale) {
-		final float xs = orientation.x,			ys = orientation.y,  		zs = orientation.z;
+		final float xs = orientation.x * 2f,	ys = orientation.y * 2f, 	zs = orientation.z * 2f;
 		final float wx = orientation.w * xs,	wy = orientation.w * ys, 	wz = orientation.w * zs;
 		final float xx = orientation.x * xs,	xy = orientation.x * ys, 	xz = orientation.x * zs;
 		final float yy = orientation.y * ys,	yz = orientation.y * zs,	zz = orientation.z * zs;
 	
-		val[M00] = scale.x * (1.0f - 2 * (yy + zz));
-		val[M01] = scale.x * 2 * (xy - wz);
-		val[M02] = scale.x * 2 * (xz + wy);
+		val[M00] = scale.x * (1.0f - (yy + zz));
+		val[M01] = scale.x * (xy - wz);
+		val[M02] = scale.x * (xz + wy);
 		val[M03] = position.x;
 		
-		val[M10] = scale.y * 2 * (xy + wz);
-		val[M11] = scale.y * (1.0f - 2 * (xx + zz));
-		val[M12] = scale.y * 2 * (yz - wx);
+		val[M10] = scale.y * (xy + wz);
+		val[M11] = scale.y * (1.0f - (xx + zz));
+		val[M12] = scale.y * (yz - wx);
 		val[M13] = position.y;
 		
-		val[M20] = scale.z * 2 * (xz - wy);
-		val[M21] = scale.z * 2 * (yz + wx);
-		val[M22] = scale.z * (1.0f - 2 * (xx + yy));
+		val[M20] = scale.z * (xz - wy);
+		val[M21] = scale.z * (yz + wx);
+		val[M22] = scale.z * (1.0f - (xx + yy));
 		val[M23] = position.z;
 		
 		val[M30] = 0.0f;
