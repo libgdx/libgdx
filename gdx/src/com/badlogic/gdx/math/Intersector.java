b244bf9a4097cdc5c072674f398a6884e935d049
NathanSweet <nathan.sweet@gmail.com>
2013-09-07 16:36:07 +0200

    Fixed triangleCentroid.

--- gdx/src/com/badlogic/gdx/math/Intersector.java
+++ gdx/src/com/badlogic/gdx/math/Intersector.java
@@ -98,7 +98,7 @@ public final class Intersector {
 
 	public static Vector2 triangleCentroid (float x1, float y1, float x2, float y2, float x3, float y3, Vector2 centroid) {
 		centroid.x = (x1 + x2 + x3) / 3;
-		centroid.y = (x1 + x2 + x3) / 3;
+		centroid.y = (y1 + y2 + y3) / 3;
 		return centroid;
 	}
 
