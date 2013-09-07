f36cc96f7a1398470d199047ea4d207a4d46c2e3
Xoppa <contact@xoppa.nl>
2013-09-07 22:05:23 +0200

    Add basic shadow mapping

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultShaderProvider.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultShaderProvider.java
@@ -30,6 +30,7 @@ public class DefaultShaderProvider extends BaseShaderProvider {
 		Gdx.app.log("DefaultShaderProvider", "Creating new shader");
 		if (Gdx.graphics.isGL20Available()) {
             return new DefaultShader(vertexShader, fragmentShader, renderable, renderable.lights != null,
+            	renderable.lights != null && renderable.lights.shadowMap != null, 
             	renderable.lights != null && renderable.lights.fog != null, 2, 5, 3, renderable.bones == null ? 0 : 12);
 		}
 		return new GLES10Shader();
