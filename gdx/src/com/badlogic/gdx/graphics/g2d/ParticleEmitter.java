04f4ee199a23b1d072c92bff0ac2fa2546651cfc
hneuer <hanno.neuer@gmail.com>
2013-09-07 15:07:55 +0200

    ParticleEmitter now resets the active particles too.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -307,6 +307,10 @@ public class ParticleEmitter {
 	public void reset () {
 		emissionDelta = 0;
 		durationTimer = duration;
+		boolean[] active = this.active;
+		for (int i = 0, n = active.length; i < n; i++)
+			active[i] = false;
+		activeCount = 0;
 		start();
 	}
 
