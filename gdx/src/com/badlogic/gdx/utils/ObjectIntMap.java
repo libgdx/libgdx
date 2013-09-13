4281cc1c5d3aa51b56db9d9e251c761c52a855e3
hneuer <hanno.neuer@gmail.com>
2013-09-13 10:51:48 +0200

    Fixed capacity check in collection classes

--- gdx/src/com/badlogic/gdx/utils/ObjectIntMap.java
+++ gdx/src/com/badlogic/gdx/utils/ObjectIntMap.java
@@ -64,7 +64,7 @@ public class ObjectIntMap<K> {
 	 * before growing the backing table. */
 	public ObjectIntMap (int initialCapacity, float loadFactor) {
 		if (initialCapacity < 0) throw new IllegalArgumentException("initialCapacity must be >= 0: " + initialCapacity);
-		if (capacity > 1 << 30) throw new IllegalArgumentException("initialCapacity is too large: " + initialCapacity);
+		if (initialCapacity > 1 << 30) throw new IllegalArgumentException("initialCapacity is too large: " + initialCapacity);
 		capacity = MathUtils.nextPowerOfTwo(initialCapacity);
 
 		if (loadFactor <= 0) throw new IllegalArgumentException("loadFactor must be > 0: " + loadFactor);
