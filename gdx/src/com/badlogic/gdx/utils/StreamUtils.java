ce8e7434575e52cd6dbd1e1d0c059d46505206c9
hneuer <hanno.neuer@gmail.com>
2013-09-15 11:48:52 +0200

    Some tweaks in NetJavaImpl

--- gdx/src/com/badlogic/gdx/utils/StreamUtils.java
+++ gdx/src/com/badlogic/gdx/utils/StreamUtils.java
@@ -1,6 +1,7 @@
 
 package com.badlogic.gdx.utils;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,6 +14,7 @@ import com.badlogic.gdx.Net.HttpRequest;
 
 /** Provides utility methods to copy streams */
 public class StreamUtils {
+	public static final byte[] EMPTY_BYTES = new byte[0];
 
 	/** Copy the data from an {@link InputStream} to an {@link OutputStream}.
 	 * @throws IOException */
@@ -27,6 +29,15 @@ public class StreamUtils {
 		int bytesRead;
 		while ((bytesRead = input.read(buffer)) != -1) {
 			output.write(buffer, 0, bytesRead);
+		}
+	}
+	
+	/** Close and ignore all errors. */
+	public static void closeQuietly (Closeable c) {
+		if (c != null) try {
+			c.close();
+		} catch (IOException e) {
+			// ignore
 		}
 	}
 }
