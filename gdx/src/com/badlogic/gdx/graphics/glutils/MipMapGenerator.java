3fef117185804f03a19d49b248fbfa90e44218d7
NathanSweet <nathan.sweet@gmail.com>
2013-09-15 15:38:49 +0200

    Javadocs.

--- gdx/src/com/badlogic/gdx/graphics/glutils/MipMapGenerator.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/MipMapGenerator.java
@@ -41,9 +41,7 @@ public class MipMapGenerator {
 	}
 	
 	/** Sets the image data of the {@link Texture} based on the {@link Pixmap}. The texture must be bound for this to work. If
-	 * <code>disposePixmap</code> is true, the pixmap will be disposed at the end of the method.
-	 * @param pixmap the Pixmap
-	 * @param disposePixmap whether to dispose the Pixmap after upload */
+	 * <code>disposePixmap</code> is true, the pixmap will be disposed at the end of the method. */
 	public static void generateMipMap (int target, Pixmap pixmap, int textureWidth, int textureHeight) {
 		if (!useHWMipMap) {
 			generateMipMapCPU(target, pixmap, textureWidth, textureHeight);
