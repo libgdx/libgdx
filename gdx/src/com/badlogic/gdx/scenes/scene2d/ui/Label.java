95d6d9197f269d9d35d6f0d72f9e30d4f121095f
NathanSweet <nathan.sweet@gmail.com>
2013-10-06 18:49:33 +0200

    Javadoc

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
@@ -228,7 +228,11 @@ public class Label extends Widget {
 
 	/** If false, the text will only wrap where it contains newlines (\n). The preferred size of the label will be the text bounds.
 	 * If true, the text will word wrap using the width of the label. The preferred width of the label will be 0, it is expected
-	 * that the something external will set the width of the label. Default is false. */
+	 * that the something external will set the width of the label. Default is false.
+	 * <p>
+	 * When wrap is enabled, the label's preferred height depends on the width of the label. In some cases the parent of the label
+	 * will need to layout twice: once to set the width of the label and a second time to adjust to the label's new preferred
+	 * height. */
 	public void setWrap (boolean wrap) {
 		this.wrap = wrap;
 		invalidateHierarchy();
