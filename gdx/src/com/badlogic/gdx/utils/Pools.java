fa187b882171fc0c6cf049a85bcaae26d2261328
MobiDevelop <support@mobidevelop.com>
2013-10-22 14:30:51 -0700

    Fix null check

--- gdx/src/com/badlogic/gdx/utils/Pools.java
+++ gdx/src/com/badlogic/gdx/utils/Pools.java
@@ -51,7 +51,7 @@ public class Pools {
 		if (objects == null) throw new IllegalArgumentException("objects cannot be null.");
 		for (int i = 0, n = objects.size; i < n; i++) {
 			Object object = objects.get(i);
-			if (objects == null) continue;
+			if (object == null) continue;
 			ReflectionPool pool = typePools.get(object.getClass());
 			if (pool == null) return; // Ignore freeing an object that was never retained.
 			pool.free(object);
