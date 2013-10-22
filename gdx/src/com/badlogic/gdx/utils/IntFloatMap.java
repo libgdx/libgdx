eab196531d022ac42556bd4bc0ab6ee0d02cf0b4
hneuer <hanno.neuer@gmail.com>
2013-10-22 11:49:39 +0200

    Use epsilon for zeroValue comparison too.

--- gdx/src/com/badlogic/gdx/utils/IntFloatMap.java
+++ gdx/src/com/badlogic/gdx/utils/IntFloatMap.java
@@ -445,7 +445,7 @@ public class IntFloatMap {
 	/** Returns true if the specified value is in the map. Note this traverses the entire map and compares every value, which may be
 	 * an expensive operation. */
 	public boolean containsValue (float value, float epsilon) {
-		if (hasZeroValue && zeroValue == value) return true;
+		if (hasZeroValue && Math.abs(zeroValue - value) <= epsilon) return true;
 		float[] valueTable = this.valueTable;
 		for (int i = capacity + stashSize; i-- > 0;)
 			if (Math.abs(valueTable[i] - value) <= epsilon) return true;
