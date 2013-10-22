6dd782d7616c7ae954df37e1efb9c7dae9bb879b
NathanSweet <nathan.sweet@gmail.com>
2013-10-22 20:42:51 +0200

    closes 857

--- gdx/src/com/badlogic/gdx/utils/Pools.java
+++ gdx/src/com/badlogic/gdx/utils/Pools.java
@@ -45,11 +45,13 @@ public class Pools {
 		pool.free(object);
 	}
 
-	/** Frees the specified objects from the {@link #get(Class) pool}. */
+	/** Frees the specified objects from the {@link #get(Class) pool}. Null objects within the array are silently ignored. Object
+	 * don't need to be from the same pool. */
 	static public void freeAll (Array objects) {
 		if (objects == null) throw new IllegalArgumentException("objects cannot be null.");
 		for (int i = 0, n = objects.size; i < n; i++) {
 			Object object = objects.get(i);
+			if (objects == null) continue;
 			ReflectionPool pool = typePools.get(object.getClass());
 			if (pool == null) return; // Ignore freeing an object that was never retained.
 			pool.free(object);
