279c7983eefdb7be79c00aea5223e5908c06b867
badlogic <badlogicgames@gmail.com>
2013-10-26 13:34:16 +0200

    fixed issue 1662, ortho cam setToOrtho didn't set up/dir properly

--- gdx/src/com/badlogic/gdx/graphics/OrthographicCamera.java
+++ gdx/src/com/badlogic/gdx/graphics/OrthographicCamera.java
@@ -82,6 +82,9 @@ public class OrthographicCamera extends Camera {
 		if (yDown) {
 			up.set(0, -1, 0);
 			direction.set(0, 0, 1);
+		} else {
+			up.set(0, 1, 0);
+			direction.set(0, 0, 1);
 		}
 		position.set(zoom * viewportWidth / 2.0f, zoom * viewportHeight / 2.0f, 0);
 		this.viewportWidth = viewportWidth;
