5386453a79c272db3ae77f5beee9b9567577d836
NathanSweet <nathan.sweet@gmail.com>
2013-11-08 23:01:09 +0100

    Don't allow 0 max check count.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ButtonGroup.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ButtonGroup.java
@@ -137,6 +137,7 @@ public class ButtonGroup {
 
 	/** Sets the maximum number of buttons that can be checked. Set to -1 for no maximum. Default is 1. */
 	public void setMaxCheckCount (int maxCheckCount) {
+		if (maxCheckCount == 0) maxCheckCount = -1;
 		this.maxCheckCount = maxCheckCount;
 	}
 
