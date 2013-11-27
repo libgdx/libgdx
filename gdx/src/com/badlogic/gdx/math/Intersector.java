8f55fb8a65cab1468fbae9e41ecaedde9fccd356
NathanSweet <nathan.sweet@gmail.com>
2013-11-27 15:02:40 +0100

    intersectSegments with floats.

--- gdx/src/com/badlogic/gdx/math/Intersector.java
+++ gdx/src/com/badlogic/gdx/math/Intersector.java
@@ -778,10 +778,29 @@ public final class Intersector {
 		float d = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
 		if (d == 0) return false;
 
-		float ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / d;
+		float yd = y1 - y3;
+		float xd = x1 - x3;
+		float ua = ((x4 - x3) * yd - (y4 - y3) * xd) / d;
 		if (ua < 0 || ua > 1) return false;
 
-		float ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / d;
+		float ub = ((x2 - x1) * yd - (y2 - y1) * xd) / d;
+		if (ub < 0 || ub > 1) return false;
+
+		if (intersection != null) intersection.set(x1 + (x2 - x1) * ua, y1 + (y2 - y1) * ua);
+		return true;
+	}
+
+	public static boolean intersectSegments (float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4,
+		Vector2 intersection) {
+		float d = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+		if (d == 0) return false;
+
+		float yd = y1 - y3;
+		float xd = x1 - x3;
+		float ua = ((x4 - x3) * yd - (y4 - y3) * xd) / d;
+		if (ua < 0 || ua > 1) return false;
+
+		float ub = ((x2 - x1) * yd - (y2 - y1) * xd) / d;
 		if (ub < 0 || ub > 1) return false;
 
 		if (intersection != null) intersection.set(x1 + (x2 - x1) * ua, y1 + (y2 - y1) * ua);
