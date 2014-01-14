eaa2759dc952fb76c83071ec8ccca95e1aaffab2
Jon Renner <rennerjc@gmail.com>
2014-01-14 10:36:29 +0800

    move package declaration

--- backends/gdx-backend-headless/src/com/badlogic/gdx/backends/headless/HeadlessApplication.java
+++ backends/gdx-backend-headless/src/com/badlogic/gdx/backends/headless/HeadlessApplication.java
@@ -1,4 +1,4 @@
-package com.badlogic.gdx.backends.headless; /*******************************************************************************
+/*******************************************************************************
  * Copyright 2011 See AUTHORS file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,8 @@ package com.badlogic.gdx.backends.headless; /***********************************
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
+
+package com.badlogic.gdx.backends.headless;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
