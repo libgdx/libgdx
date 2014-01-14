libgdx/libgdx

commit d08ff17f2519c17883f1234de0afdf086da7cc3f

Remove Vector2.tmp() and its variations (.tmp{1,2,3})
Down with public static mutable objects.  Up with private static mutable objects ... I guess.
Remove Vector2.tmp usage from PathTest and StageTest
Add a static scratch vector in pax-britannica's Ship.java
Replace Vector2.tmp usage with local static scratch variables throughout scene2d.

This breaks the API since Vector2.tmp* were all public.

demos/pax-britannica/pax-britannica/src/de/swagner/paxbritannica/Ship.java
@@ -66,7 +66,7 @@ public class Ship extends Sprite {
 		collisionPoints.get(2).set( this.getVertices()[10], this.getVertices()[11]);
 		collisionPoints.get(3).set( this.getVertices()[15], this.getVertices()[16]);
 		
-		collisionCenter.set(collisionPoints.get(0).tmp().add(collisionPoints.get(2)).mul(0.5f));
+		collisionCenter.set(collisionPoints.get(2)).mul(0.5f).add(collisionPoints.get(0));
 
 		velocity.mul( (float) Math.pow(0.97f, delta * 30.f));
 		position.add(velocity.x * delta, velocity.y * delta);
@@ -105,8 +105,14 @@ public class Ship extends Sprite {
 				+ MathUtils.random(-this.getHeight() / 2, this.getHeight() / 2));
 	}
 
+	/*
+	 * Scratch space for computing a target's direction. This is safe (as a static) because
+	 * its only used in goTowardsOrAway which is only called on the render thread (via update).
+	 */
+	private static final Vector2 target_direction = new Vector2(); 
+
 	public void goTowardsOrAway(Vector2 targetPos, boolean forceThrust, boolean isAway) {
-		Vector2 target_direction = targetPos.tmp().sub(collisionCenter);
+		target_direction.set(targetPos).sub(collisionCenter);
 		if (isAway) {
 			target_direction.mul(-1);
 		}
@@ -144,7 +150,7 @@ public class Ship extends Sprite {
 
 	public void factoryDestruct() {
 		delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
-		
+
 		if (deathCounter > 0) {
 			((FactoryProduction) this).production.halt_production = true;
 			this.setColor(1, 1, 1, Math.min(1, opacity));

gdx/src/com/badlogic/gdx/math/Vector2.java
@@ -25,10 +25,6 @@ import com.badlogic.gdx.utils.NumberUtils;
 public class Vector2 implements Serializable, Vector<Vector2> {
 	private static final long serialVersionUID = 913902788239530931L;
 
-	/** Static temporary vector. Use with care! Use only when sure other code will not also use this.
-	 * @see #tmp() **/
-	public final static Vector2 tmp = new Vector2(), tmp2 = new Vector2(), tmp3 = new Vector2();
-
 	public final static Vector2 X = new Vector2(1, 0);
 	public final static Vector2 Y = new Vector2(0, 1);
 	public final static Vector2 Zero = new Vector2(0, 0);
@@ -161,15 +157,15 @@ public class Vector2 implements Serializable, Vector<Vector2> {
 	}
 
 	public Vector2 div (float value) {
-		return this.mul(1/value);
+		return this.mul(1 / value);
 	}
 
 	public Vector2 div (float vx, float vy) {
-		return this.mul(1/vx, 1/vy);
+		return this.mul(1 / vx, 1 / vy);
 	}
 
 	public Vector2 div (Vector2 other) {
-		return this.mul(1/other.x, 1/other.y);
+		return this.mul(1 / other.x, 1 / other.y);
 	}
 
 	/** @param v The other vector
@@ -220,15 +216,6 @@ public class Vector2 implements Serializable, Vector<Vector2> {
 		return this;
 	}
 
-	/** NEVER EVER SAVE THIS REFERENCE! Do not use this unless you are aware of the side-effects, e.g. other methods might call this
-	 * as well.
-	 * 
-	 * @return a temporary copy of this vector. Use with care as this is backed by a single static Vector2 instance. v1.tmp().add(
-	 *         v2.tmp() ) will not work! */
-	public Vector2 tmp () {
-		return tmp.set(this);
-	}
-
 	/** Multiplies this vector by the given matrix
 	 * @param mat the matrix
 	 * @return this vector */
@@ -262,7 +249,7 @@ public class Vector2 implements Serializable, Vector<Vector2> {
 		if (angle < 0) angle += 360;
 		return angle;
 	}
-	
+
 	/** Sets the angle of the vector.
 	 * @param angle The angle to set. */
 	public void setAngle (float angle) {

gdx/src/com/badlogic/gdx/scenes/scene2d/InputListener.java
@@ -33,6 +33,10 @@ import com.badlogic.gdx.math.Vector2;
  * </pre>
  */
 public class InputListener implements EventListener {
+	
+	/** Scratch object used by handle().  Event dispatch should only be invoked by the render thread. */
+	private static final Vector2 coords = new Vector2();
+	
 	public boolean handle (Event e) {
 		if (!(e instanceof InputEvent)) return false;
 		InputEvent event = (InputEvent)e;
@@ -46,7 +50,7 @@ public class InputListener implements EventListener {
 			return keyTyped(event, event.getCharacter());
 		}
 
-		Vector2 coords = Vector2.tmp.set(event.getStageX(), event.getStageY());
+		coords.set(event.getStageX(), event.getStageY());
 		event.getListenerActor().stageToLocalCoordinates(coords);
 
 		switch (event.getType()) {

gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
@@ -46,6 +46,10 @@ import com.badlogic.gdx.utils.SnapshotArray;
  * {@link Input#setInputProcessor(com.badlogic.gdx.InputProcessor) Gdx.input.setInputProcessor}. An {@link InputMultiplexer} may be
  * used to handle input events before or after the stage does. If an actor handles an event by returning true from the input
  * method, then the stage's input method will also return true, causing subsequent InputProcessors to not receive the event.
+ * <p>
+ * The Stage and its constituents (like Actors and Listeners) are not thread-safe and should only be updated and queried from a
+ * single thread (presumably the main render thread). Methods should be reentrant, so you can update Actors and Stages from within
+ * callbacks and handlers.
  * 
  * @author mzechner
  * @author Nathan Sweet */
@@ -66,6 +70,12 @@ public class Stage extends InputAdapter implements Disposable {
 	private Actor mouseOverActor;
 	private Actor keyboardFocus, scrollFocus;
 	private SnapshotArray<TouchFocus> touchFocuses = new SnapshotArray(true, 4, TouchFocus.class);
+
+	/* Scratch object used by hit(). */
+	private static final Vector2 actorCoords = new Vector2();
+
+	/* Scratch object used by screenToStageCoordinates() and stageToScreenCoordinates(). */
+	private static final Vector3 cameraCoords = new Vector3();
 
 	/** Creates a stage with a {@link #setViewport(float, float, boolean) viewport} equal to the device screen resolution. The stage
 	 * will use its own {@link SpriteBatch}. */
@@ -638,26 +648,28 @@ public class Stage extends InputAdapter implements Disposable {
 	 * @param touchable If true, the hit detection will respect the {@link Actor#setTouchable(Touchable) touchability}.
 	 * @return May be null if no actor was hit. */
 	public Actor hit (float stageX, float stageY, boolean touchable) {
-		Vector2 actorCoords = Vector2.tmp;
 		root.parentToLocalCoordinates(actorCoords.set(stageX, stageY));
 		return root.hit(actorCoords.x, actorCoords.y, touchable);
 	}
 
 	/** Transforms the screen coordinates to stage coordinates.
-	 * @param screenCoords Stores the result. */
+	 * 
+	 * @param screenCoords input screen coordinates and output for resulting stage coordinates. */
 	public Vector2 screenToStageCoordinates (Vector2 screenCoords) {
-		camera.unproject(Vector3.tmp.set(screenCoords.x, screenCoords.y, 0));
-		screenCoords.x = Vector3.tmp.x;
-		screenCoords.y = Vector3.tmp.y;
+		camera.unproject(cameraCoords.set(screenCoords.x, screenCoords.y, 0));
+		screenCoords.x = cameraCoords.x;
+		screenCoords.y = cameraCoords.y;
 		return screenCoords;
 	}
 
-	/** Transforms the stage coordinates to screen coordinates. */
+	/** Transforms the stage coordinates to screen coordinates. 
+	 * 
+	 * @param stageCoords input stage coordinates and output for resulting screen coordinates */
 	public Vector2 stageToScreenCoordinates (Vector2 stageCoords) {
-		Vector3.tmp.set(stageCoords.x, stageCoords.y, 0);
-		camera.project(Vector3.tmp);
-		stageCoords.x = Vector3.tmp.x;
-		stageCoords.y = Vector3.tmp.y;
+		cameraCoords.set(stageCoords.x, stageCoords.y, 0);
+		camera.project(cameraCoords);
+		stageCoords.x = cameraCoords.x;
+		stageCoords.y = cameraCoords.y;
 		return stageCoords;
 	}
 

gdx/src/com/badlogic/gdx/scenes/scene2d/ui/SelectBox.java
@@ -52,6 +52,9 @@ public class SelectBox extends Widget {
 	private float prefWidth, prefHeight;
 	private ClickListener clickListener;
 
+	/** Scratch space for converting to/from stage coordinates. Only used in listener callbacks (so only on render thread). */
+	static final Vector2 tmpCoords = new Vector2();
+
 	public SelectBox (Object[] items, Skin skin) {
 		this(items, skin.get(SelectBoxStyle.class));
 	}
@@ -74,9 +77,8 @@ public class SelectBox extends Widget {
 					return true;
 				}
 				Stage stage = getStage();
-				Vector2 stageCoords = Vector2.tmp;
-				stage.screenToStageCoordinates(stageCoords.set(screenCoords.x, screenCoords.y));
-				list = new SelectList(stageCoords.x, stageCoords.y);
+				stage.screenToStageCoordinates(tmpCoords.set(screenCoords));
+				list = new SelectList(tmpCoords.x, tmpCoords.y);
 				stage.addActor(list);
 				return true;
 			}
@@ -207,9 +209,9 @@ public class SelectBox extends Widget {
 		InputListener stageListener = new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
 				if (pointer == 0 && button != 0) return false;
-				stageToLocalCoordinates(Vector2.tmp.set(event.getStageX(), event.getStageY()));
-				x = Vector2.tmp.x;
-				y = Vector2.tmp.y;
+				stageToLocalCoordinates(tmpCoords.set(event.getStageX(), event.getStageY()));
+				x = tmpCoords.x;
+				y = tmpCoords.y;
 				if (x > 0 && x < getWidth() && y > 0 && y < getHeight()) {
 					listSelectedIndex = (int)((getHeight() - y) / itemHeight);
 					listSelectedIndex = Math.max(0, listSelectedIndex);
@@ -230,9 +232,9 @@ public class SelectBox extends Widget {
 			}
 
 			public boolean mouseMoved (InputEvent event, float x, float y) {
-				stageToLocalCoordinates(Vector2.tmp.set(event.getStageX(), event.getStageY()));
-				x = Vector2.tmp.x;
-				y = Vector2.tmp.y;
+				stageToLocalCoordinates(tmpCoords.set(event.getStageX(), event.getStageY()));
+				x = tmpCoords.x;
+				y = tmpCoords.y;
 				if (x > 0 && x < getWidth() && y > 0 && y < getHeight()) {
 					listSelectedIndex = (int)((getHeight() - style.listBackground.getTopHeight() - y) / itemHeight);
 					listSelectedIndex = Math.max(0, listSelectedIndex);

gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
@@ -583,20 +583,28 @@ public class TextField extends Widget {
 		cursor = minIndex;
 		clearSelection();
 	}
+	
+	/*
+	 * Three hacks to work around the inability to stack allocate small scratch-space objects in Java.  
+	 * Used by #next() and #findNextTextField.
+	 */
+	private static final Vector2 tmp1 = new Vector2();
+	private static final Vector2 tmp2 = new Vector2();
+	private static final Vector2 tmp3 = new Vector2();
 
 	/** Focuses the next TextField. If none is found, the keyboard is hidden. Does nothing if the text field is not in a stage.
 	 * @param up If true, the TextField with the same or next smallest y coordinate is found, else the next highest. */
 	public void next (boolean up) {
 		Stage stage = getStage();
 		if (stage == null) return;
-		getParent().localToStageCoordinates(Vector2.tmp.set(getX(), getY()));
-		TextField textField = findNextTextField(stage.getActors(), null, Vector2.tmp2, Vector2.tmp, up);
+		getParent().localToStageCoordinates(tmp1.set(getX(), getY()));
+		TextField textField = findNextTextField(stage.getActors(), null, tmp2, tmp1, up);
 		if (textField == null) { // Try to wrap around.
 			if (up)
-				Vector2.tmp.set(Float.MIN_VALUE, Float.MIN_VALUE);
+				tmp1.set(Float.MIN_VALUE, Float.MIN_VALUE);
 			else
-				Vector2.tmp.set(Float.MAX_VALUE, Float.MAX_VALUE);
-			textField = findNextTextField(getStage().getActors(), null, Vector2.tmp2, Vector2.tmp, up);
+				tmp1.set(Float.MAX_VALUE, Float.MAX_VALUE);
+			textField = findNextTextField(getStage().getActors(), null, tmp2, tmp1, up);
 		}
 		if (textField != null)
 			stage.setKeyboardFocus(textField);
@@ -609,7 +617,7 @@ public class TextField extends Widget {
 			Actor actor = actors.get(i);
 			if (actor == this) continue;
 			if (actor instanceof TextField) {
-				Vector2 actorCoords = actor.getParent().localToStageCoordinates(Vector2.tmp3.set(actor.getX(), actor.getY()));
+				Vector2 actorCoords = actor.getParent().localToStageCoordinates(tmp3.set(actor.getX(), actor.getY()));
 				if ((actorCoords.y < currentCoords.y || (actorCoords.y == currentCoords.y && actorCoords.x > currentCoords.x)) ^ up) {
 					if (best == null
 						|| (actorCoords.y > bestCoords.y || (actorCoords.y == bestCoords.y && actorCoords.x < bestCoords.x)) ^ up) {

gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -135,15 +135,22 @@ public class Window extends Table {
 		}
 		super.draw(batch, parentAlpha);
 	}
+	
+	/*
+	 * Hacks to work around the inability to stack allocate small scratch objects in Java.
+	 */
+	
+	private static final Vector2 tmpPosition = new Vector2();
+	private static final Vector2 tmpSize = new Vector2();
 
 	protected void drawBackground (SpriteBatch batch, float parentAlpha) {
 		if (style.stageBackground != null) {
 			Color color = getColor();
 			batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
 			Stage stage = getStage();
-			Vector2 position = stageToLocalCoordinates(Vector2.tmp.set(0, 0));
-			Vector2 size = stageToLocalCoordinates(Vector2.tmp2.set(stage.getWidth(), stage.getHeight()));
-			style.stageBackground.draw(batch, getX() + position.x, getY() + position.y, getX() + size.x, getY() + size.y);
+			stageToLocalCoordinates(/*in/out*/tmpPosition.set(0, 0));
+			stageToLocalCoordinates(/*in/out*/tmpSize.set(stage.getWidth(), stage.getHeight()));
+			style.stageBackground.draw(batch, getX() + tmpPosition.x, getY() + tmpPosition.y, getX() + tmpSize.x, getY() + tmpSize.y);
 		}
 
 		super.drawBackground(batch, parentAlpha);

gdx/src/com/badlogic/gdx/scenes/scene2d/utils/ActorGestureListener.java
@@ -32,7 +32,10 @@ public class ActorGestureListener implements EventListener {
 	private final GestureDetector detector;
 	InputEvent event;
 	Actor actor, touchDownTarget;
-
+	
+	/** Scratch coordinates used to convert event coordinates into local coordinates. */
+	static final Vector2 tmpCoords = new Vector2();
+	
 	/** @see GestureDetector#GestureDetector(com.badlogic.gdx.input.GestureDetector.GestureListener) */
 	public ActorGestureListener () {
 		this(20, 0.4f, 1.1f, 0.15f);
@@ -45,14 +48,14 @@ public class ActorGestureListener implements EventListener {
 			private final Vector2 pointer1 = new Vector2(), pointer2 = new Vector2();
 
 			public boolean tap (float stageX, float stageY, int count, int button) {
-				actor.stageToLocalCoordinates(Vector2.tmp.set(stageX, stageY));
-				ActorGestureListener.this.tap(event, Vector2.tmp.x, Vector2.tmp.y, count, button);
+				actor.stageToLocalCoordinates(tmpCoords.set(stageX, stageY));
+				ActorGestureListener.this.tap(event, tmpCoords.x, tmpCoords.y, count, button);
 				return true;
 			}
 
 			public boolean longPress (float stageX, float stageY) {
-				actor.stageToLocalCoordinates(Vector2.tmp.set(stageX, stageY));
-				return ActorGestureListener.this.longPress(actor, Vector2.tmp.x, Vector2.tmp.y);
+				actor.stageToLocalCoordinates(tmpCoords.set(stageX, stageY));
+				return ActorGestureListener.this.longPress(actor, tmpCoords.x, tmpCoords.y);
 			}
 
 			public boolean fling (float velocityX, float velocityY, int button) {
@@ -61,8 +64,8 @@ public class ActorGestureListener implements EventListener {
 			}
 
 			public boolean pan (float stageX, float stageY, float deltaX, float deltaY) {
-				actor.stageToLocalCoordinates(Vector2.tmp.set(stageX, stageY));
-				ActorGestureListener.this.pan(event, Vector2.tmp.x, Vector2.tmp.y, deltaX, deltaY);
+				actor.stageToLocalCoordinates(tmpCoords.set(stageX, stageY));
+				ActorGestureListener.this.pan(event, tmpCoords.x, tmpCoords.y, deltaX, deltaY);
 				return true;
 			}
 
@@ -92,15 +95,15 @@ public class ActorGestureListener implements EventListener {
 			actor = event.getListenerActor();
 			touchDownTarget = event.getTarget();
 			detector.touchDown(event.getStageX(), event.getStageY(), event.getPointer(), event.getButton());
-			actor.stageToLocalCoordinates(Vector2.tmp.set(event.getStageX(), event.getStageY()));
-			touchDown(event, Vector2.tmp.x, Vector2.tmp.y, event.getPointer(), event.getButton());
+			actor.stageToLocalCoordinates(tmpCoords.set(event.getStageX(), event.getStageY()));
+			touchDown(event, tmpCoords.x, tmpCoords.y, event.getPointer(), event.getButton());
 			return true;
 		case touchUp:
 			this.event = event;
 			actor = event.getListenerActor();
 			detector.touchUp(event.getStageX(), event.getStageY(), event.getPointer(), event.getButton());
-			actor.stageToLocalCoordinates(Vector2.tmp.set(event.getStageX(), event.getStageY()));
-			touchUp(event, Vector2.tmp.x, Vector2.tmp.y, event.getPointer(), event.getButton());
+			actor.stageToLocalCoordinates(tmpCoords.set(event.getStageX(), event.getStageY()));
+			touchUp(event, tmpCoords.x, tmpCoords.y, event.getPointer(), event.getButton());
 			return true;
 		case touchDragged:
 			this.event = event;

commit 376e6647bb5cb36b5dd37d303a5ad7fb1460daa0

pax-britannica: Fix Ship collisionCenter calculation position error.
regression introduced by d08ff17f2519c17883f1234de0afdf086da7cc3f

Signed-off-by: Xerxes Rånby <xerxes@zafena.se>

demos/pax-britannica/pax-britannica/src/de/swagner/paxbritannica/Ship.java
@@ -68,7 +68,7 @@ public class Ship extends Sprite {
 		collisionPoints.get(2).set( this.getVertices()[10], this.getVertices()[11]);
 		collisionPoints.get(3).set( this.getVertices()[15], this.getVertices()[16]);
 		
-		collisionCenter.set(collisionPoints.get(2)).scl(0.5f).add(collisionPoints.get(0));
+		collisionCenter.set(collisionPoints.get(0)).add(collisionPoints.get(2)).scl(0.5f);
 
 		velocity.scl( (float) Math.pow(0.97f, delta * 30.f));
 		position.add(velocity.x * delta, velocity.y * delta);