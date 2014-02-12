95d4c04533d7eaa38b3ac96e5d12100818ede98a
David Saltares <david.saltares@gmail.com>
2014-02-12 13:52:13 +0000

    Fixes compile error in GWTNet

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtNet.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtNet.java
@@ -162,16 +162,14 @@ public class GwtNet implements Net {
 	
 	@Override
 	public void cancelHttpRequest (HttpRequest httpRequest) {
-		synchronized (lock) {
-			HttpResponseListener httpResponseListener = listeners.get(httpRequest);
-			Request request = requests.get(httpRequest);
-			
-			if (httpResponseListener != null && request != null) {
-				request.cancel();
-				httpResponseListener.cancelled();
-				requests.remove(httpRequest);
-				listeners.remove(httpRequest);
-			}
+		HttpResponseListener httpResponseListener = listeners.get(httpRequest);
+		Request request = requests.get(httpRequest);
+		
+		if (httpResponseListener != null && request != null) {
+			request.cancel();
+			httpResponseListener.cancelled();
+			requests.remove(httpRequest);
+			listeners.remove(httpRequest);
 		}
 	}
 
