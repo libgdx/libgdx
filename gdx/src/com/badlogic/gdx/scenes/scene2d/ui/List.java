db3f3dcc8278297eda4600375a25309016e6ec78
NathanSweet <nathan.sweet@gmail.com>
2014-02-22 20:04:16 +0100

    Fixed List touchDown.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/List.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/List.java
@@ -75,7 +75,12 @@ public class List<T> extends Widget implements Cullable {
 	}
 
 	void touchDown (float y) {
-		int index = (int)((getHeight() - y) / itemHeight);
+		float height = getHeight();
+		if (style.background != null) {
+			height -= style.background.getTopHeight() + style.background.getBottomHeight();
+			y -= style.background.getBottomHeight();
+		}
+		int index = (int)((height - y) / itemHeight);
 		index = Math.max(0, index);
 		index = Math.min(items.size - 1, index);
 		selection.choose(items.get(index));
