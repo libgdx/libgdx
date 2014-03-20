ef911e1c37dc6dbede10152bda4fc21708e288b4
NathanSweet <nathan.sweet@gmail.com>
2014-03-20 14:32:44 +0100

    Button should still fire the change listener when disabled and checked state is changed programmatically.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Button.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Button.java
@@ -108,11 +108,9 @@ public class Button extends Table implements Disableable {
 		if (this.isChecked == isChecked) return;
 		if (buttonGroup != null && !buttonGroup.canCheck(this, isChecked)) return;
 		this.isChecked = isChecked;
-		if (!isDisabled) {
-			ChangeEvent changeEvent = Pools.obtain(ChangeEvent.class);
-			if (fire(changeEvent)) this.isChecked = !isChecked;
-			Pools.free(changeEvent);
-		}
+		ChangeEvent changeEvent = Pools.obtain(ChangeEvent.class);
+		if (fire(changeEvent)) this.isChecked = !isChecked;
+		Pools.free(changeEvent);
 	}
 
 	/** Toggles the checked state. This method changes the checked state, which fires a {@link ChangeEvent}, so can be used to
