d802e3d199435e27eebc11fb4371609600db5590
davebaol <davix7@yahoo.it>
2014-03-24 16:08:12 +0100

    Cosmetic: override in place of switch/case.

--- gdx/src/com/badlogic/gdx/utils/Scaling.java
+++ gdx/src/com/badlogic/gdx/utils/Scaling.java
@@ -23,82 +23,94 @@ import com.badlogic.gdx.math.Vector2;
 public enum Scaling {
 	/** Scales the source to fit the target while keeping the same aspect ratio. This may cause the source to be smaller than the
 	 * target in one direction. */
-	fit,
-	/** Scales the source to fill the target while keeping the same aspect ratio. This may cause the source to be larger than the
-	 * target in one direction. */
-	fill,
-	/** Scales the source to fill the target in the x direction while keeping the same aspect ratio. This may cause the source to be
-	 * smaller or larger than the target in the y direction. */
-	fillX,
-	/** Scales the source to fill the target in the y direction while keeping the same aspect ratio. This may cause the source to be
-	 * smaller or larger than the target in the x direction. */
-	fillY,
-	/** Scales the source to fill the target. This may cause the source to not keep the same aspect ratio. */
-	stretch,
-	/** Scales the source to fill the target in the x direction, without changing the y direction. This may cause the source to not
-	 * keep the same aspect ratio. */
-	stretchX,
-	/** Scales the source to fill the target in the y direction, without changing the x direction. This may cause the source to not
-	 * keep the same aspect ratio. */
-	stretchY,
-	/** The source is not scaled. */
-	none;
-
-	static private final Vector2 temp = new Vector2();
-
-	/** Returns the size of the source scaled to the target. Note the same Vector2 instance is always returned and should never be
-	 * cached. */
-	public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
-		switch (this) {
-		case fit: {
+	fit() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float targetRatio = targetHeight / targetWidth;
 			float sourceRatio = sourceHeight / sourceWidth;
 			float scale = targetRatio > sourceRatio ? targetWidth / sourceWidth : targetHeight / sourceHeight;
 			temp.x = sourceWidth * scale;
 			temp.y = sourceHeight * scale;
-			break;
+			return temp;
 		}
-		case fill: {
+	},
+	/** Scales the source to fill the target while keeping the same aspect ratio. This may cause the source to be larger than the
+	 * target in one direction. */
+	fill() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float targetRatio = targetHeight / targetWidth;
 			float sourceRatio = sourceHeight / sourceWidth;
 			float scale = targetRatio < sourceRatio ? targetWidth / sourceWidth : targetHeight / sourceHeight;
 			temp.x = sourceWidth * scale;
 			temp.y = sourceHeight * scale;
-			break;
+			return temp;
 		}
-		case fillX: {
-			float targetRatio = targetHeight / targetWidth;
-			float sourceRatio = sourceHeight / sourceWidth;
+	},
+	/** Scales the source to fill the target in the x direction while keeping the same aspect ratio. This may cause the source to be
+	 * smaller or larger than the target in the y direction. */
+	fillX() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float scale = targetWidth / sourceWidth;
 			temp.x = sourceWidth * scale;
 			temp.y = sourceHeight * scale;
-			break;
+			return temp;
 		}
-		case fillY: {
-			float targetRatio = targetHeight / targetWidth;
-			float sourceRatio = sourceHeight / sourceWidth;
+	},
+	/** Scales the source to fill the target in the y direction while keeping the same aspect ratio. This may cause the source to be
+	 * smaller or larger than the target in the x direction. */
+	fillY() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float scale = targetHeight / sourceHeight;
 			temp.x = sourceWidth * scale;
 			temp.y = sourceHeight * scale;
-			break;
+			return temp;
 		}
-		case stretch:
+	},
+	/** Scales the source to fill the target. This may cause the source to not keep the same aspect ratio. */
+	stretch() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = targetWidth;
 			temp.y = targetHeight;
-			break;
-		case stretchX:
+			return temp;
+		}
+	},
+	/** Scales the source to fill the target in the x direction, without changing the y direction. This may cause the source to not
+	 * keep the same aspect ratio. */
+	stretchX() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = targetWidth;
 			temp.y = sourceHeight;
-			break;
-		case stretchY:
+			return temp;
+		}
+	},
+	/** Scales the source to fill the target in the y direction, without changing the x direction. This may cause the source to not
+	 * keep the same aspect ratio. */
+	stretchY() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = sourceWidth;
 			temp.y = targetHeight;
-			break;
-		case none:
+			return temp;
+		}
+	},
+	/** The source is not scaled. */
+	none() {
+		@Override
+		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = sourceWidth;
 			temp.y = sourceHeight;
-			break;
+			return temp;
 		}
-		return temp;
-	}
+	};
+
+	static private final Vector2 temp = new Vector2();
+
+	/** Returns the size of the source scaled to the target. Note the same Vector2 instance is always returned and should never be
+	 * cached. */
+	public abstract Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight);
 }
