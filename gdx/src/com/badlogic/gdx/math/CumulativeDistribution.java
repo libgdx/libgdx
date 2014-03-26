0210f915097a3612a9c79561aa63b9721e98f542
lordjone <drodrigu86@gmail.com>
2014-03-26 18:52:24 +0100

    Added Jdoc

--- gdx/src/com/badlogic/gdx/math/CumulativeDistribution.java
+++ gdx/src/com/badlogic/gdx/math/CumulativeDistribution.java
@@ -2,7 +2,15 @@ package com.badlogic.gdx.math;
 
 import com.badlogic.gdx.utils.Array;
 
-/** @author Inferno */
+/** This class represents a cumulative distribution.
+ * It can be used in scenarios where there are values with different probabilities
+ * and it's required to pick one of those respecting the probability.
+ * For example one could represent the frequency of the alphabet letters using a cumulative distribution
+ * and use it to randomly pick a letter respecting their probabilities (useful when generating random words).
+ * Another example could be point generation on a mesh surface: one could generate a cumulative distribution using
+ * triangles areas as interval size, in this way triangles with a large area will be picked more often than triangles with a smaller one.
+ * See <a href="http://en.wikipedia.org/wiki/Cumulative_distribution_function">Wikipedia</a> for a detailed explanation.
+ * @author Inferno */
 public class CumulativeDistribution <T>{
 	public class CumulativeValue{
 		public T value;
@@ -20,12 +28,14 @@ public class CumulativeDistribution <T>{
 		values = new Array<CumulativeValue>(false, 10, CumulativeValue.class);
 	}
 
-	public void add(T obj, float intervalSize){
-		values.add(new CumulativeValue(obj, 0, intervalSize));
+	/** Adds a value with a given interval size to the distribution */
+	public void add(T value, float intervalSize){
+		values.add(new CumulativeValue(value, 0, intervalSize));
 	}
 
-	public void add(T obj){
-		values.add(new CumulativeValue(obj, 0, 0));
+	/** Adds a value with interval size equal to zero to the distribution*/
+	public void add(T value){
+		values.add(new CumulativeValue(value, 0, 0));
 	}
 
 	/** Generate the cumulative distribution */
@@ -37,7 +47,7 @@ public class CumulativeDistribution <T>{
 		}
 	}
 
-	/** Generate the cumulative distribution in [0,1] where each interval will get a frequency */
+	/** Generate the cumulative distribution in [0,1] where each interval will get a frequency between [0,1] */
 	public void generateNormalized(){	
 		float sum = 0;
 		for(int i=0; i < values.size; ++i){
@@ -50,7 +60,7 @@ public class CumulativeDistribution <T>{
 		}		
 	}
 
-	/** Generate the cumulative distribution in [0,1] where each value will have the same interval size */
+	/** Generate the cumulative distribution in [0,1] where each value will have the same frequency and interval size*/
 	public void generateUniform(){
 		float freq = 1f/values.size;
 		for(int i=0; i < values.size; ++i){
@@ -61,7 +71,9 @@ public class CumulativeDistribution <T>{
 	}
 
 
-	/** @param probability is the value used to search the right interval inside the distribution 
+	/** Finds the value whose interval contains the given probability
+	 * Binary search algorithm is used to find the value.
+	 * @param probability 
 	 * @return the value whose interval contains the probability */
 	public T value(float probability){
 		CumulativeValue value = null;
@@ -79,25 +91,28 @@ public class CumulativeDistribution <T>{
 		return value.value;
 	}
 
-	/** @return a random value whose probability lies in [0,1] */
+	/** @return the value whose interval contains a random probability in [0,1] */
 	public T value(){
 		return value(MathUtils.random());
 	}
 
+	/** @return the amount of values */
 	public int size(){
 		return values.size;
 	}
 
+	/**@return the interval size for the value at the given position */
 	public float getInterval(int index){
 		return values.items[index].interval;
 	}
 
+	/**@return the value at the given position */
 	public T getValue(int index){
 		return values.items[index].value;
 	}
 
 	/** Set the interval size on the passed in object.
-	 *  The object has to be inside the distribution. */
+	 *  The object must be present in the distribution. */
 	public void setInterval(T obj, float intervalSize){
 		for(CumulativeValue value : values)
 			if(value.value == obj){
@@ -106,10 +121,12 @@ public class CumulativeDistribution <T>{
 			}
 	}
 
+	/** Sets the interval size for the value at the given index */
 	public void setInterval(int index, float intervalSize){
 		values.items[index].interval = intervalSize;
 	}
 
+	/** Removes all the values from the distribution */
 	public void clear(){
 		values.clear();
 	}
