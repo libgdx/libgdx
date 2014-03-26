c515a7b6f98c33bfe396024fa76334eca3f542ae
NathanSweet <nathan.sweet@gmail.com>
2014-03-26 23:04:23 +0100

    Fixed stageToScreenCoordinates to match old behavior.
    Not sure it's right to flip the y axis, but it's what it used to do...

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
@@ -605,6 +605,7 @@ public class Stage extends InputAdapter implements Disposable {
 	 * @param stageCoords Input stage coordinates and output for resulting screen coordinates. */
 	public Vector2 stageToScreenCoordinates (Vector2 stageCoords) {
 		viewport.project(stageCoords);
+		stageCoords.y = viewport.getViewportHeight() - stageCoords.y;
 		return stageCoords;
 	}
 
