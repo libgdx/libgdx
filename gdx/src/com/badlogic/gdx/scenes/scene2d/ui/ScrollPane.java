56ba4ea7deadefee08515549c99610dc5c44c82a
karan1213 <nagpalkaran.jpr@gmail.com>
2014-03-28 22:38:04 +0530

    Added fling gesture to scrollpane.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
@@ -588,8 +588,12 @@ public class ScrollPane extends WidgetGroup {
 		resetTransform(batch);
 	}
 
-	//generate flick gesture
-	public void flick(float flingTime, float velocityX, float velocityY){
+	/** Generate fling gesture.
+	 * 
+	 * @param flingTime Time in seconds for which you want to fling last.
+	 * @param velocityX Velocity for horizontal direction.
+	 * @param velocityY Velocity for vertical direction. */
+	public void fling(float flingTime, float velocityX, float velocityY){
 		this.flingTimer = flingTime;
 		this.velocityX = velocityX;
 		this.velocityY = velocityY;
