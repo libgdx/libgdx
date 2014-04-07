ccfc22e8d49fc3674fd805c2b213efa2de65be80
davebaol <davix7@yahoo.it>
2014-04-07 23:30:23 +0200

    Fixed wrong invocation to setPreserveEGLContextOnPause via reflection.

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidGraphics.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidGraphics.java
@@ -110,8 +110,9 @@ public class AndroidGraphics implements Graphics, Renderer {
 		int sdkVersion = android.os.Build.VERSION.SDK_INT;
 		if ((sdkVersion >= 11 && view instanceof GLSurfaceView20) || view instanceof GLSurfaceView20API18) {
 			try {
-				view.getClass().getDeclaredMethod("setPreserveEGLContextOnPause", Boolean.class).invoke(view, true);
+				view.getClass().getMethod("setPreserveEGLContextOnPause", boolean.class).invoke(view, true);
 			} catch (Exception e) {
+				Gdx.app.log(LOG_TAG, "Method GLSurfaceView.setPreserveEGLContextOnPause not found");
 			}
 		}
 	}
