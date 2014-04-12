5dccc3cfc10bd6176b87c0c93cdc0028399d601c
alex55i <agentlex11@yandex.ru>
2014-04-12 09:01:31 +0600

    Add mulLeft to matrices

--- gdx/src/com/badlogic/gdx/math/Matrix4.java
+++ gdx/src/com/badlogic/gdx/math/Matrix4.java
@@ -321,6 +321,20 @@ public class Matrix4 implements Serializable {
 		return this;
 	}
 
+	/** Premultiplies this matrix with the given matrix, storing the result in this matrix. For example:
+	 * 
+	 * <pre>
+	 * A.mulLeft(B) results in A := BA.
+	 * </pre>
+	 * 
+	 * @param matrix The other matrix to multiply by.
+	 * @return This matrix for the purpose of chaining operations together. */
+	public Matrix4 mulLeft (Matrix4 matrix) {
+		tmpMat.set(matrix);
+		mul(tmpMat.val, this.val);
+		return set(tmpMat);
+	}
+
 	/** Transposes the matrix.
 	 * 
 	 * @return This matrix for the purpose of chaining methods together. */
