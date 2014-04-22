966c5870804d792bb4393b721612fbefb67e191f
Justin Shapcott <support@mobidevelop.com>
2014-04-22 08:34:34 -0700

    Use floats instead of ints
    Fixes #1748

--- gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java
@@ -444,11 +444,11 @@ public class TmxMapLoader extends AsynchronousAssetLoader<TiledMap, TmxMapLoader
 			float scaleX = convertObjectToTileSpace ? 1.0f / mapTileWidth : 1.0f;
 			float scaleY = convertObjectToTileSpace ? 1.0f / mapTileHeight : 1.0f;
 
-			float x = element.getIntAttribute("x", 0) * scaleX;
-			float y = (yUp ? mapHeightInPixels - element.getIntAttribute("y", 0) : element.getIntAttribute("y", 0)) * scaleY;
+			float x = element.getFloatAttribute("x", 0) * scaleX;
+			float y = (yUp ? mapHeightInPixels - element.getFloatAttribute("y", 0) : element.getFloatAttribute("y", 0)) * scaleY;
 
-			float width = element.getIntAttribute("width", 0) * scaleX;
-			float height = element.getIntAttribute("height", 0) * scaleY;
+			float width = element.getFloatAttribute("width", 0) * scaleX;
+			float height = element.getFloatAttribute("height", 0) * scaleY;
 
 			if (element.getChildCount() > 0) {
 				Element child = null;
@@ -457,8 +457,8 @@ public class TmxMapLoader extends AsynchronousAssetLoader<TiledMap, TmxMapLoader
 					float[] vertices = new float[points.length * 2];
 					for (int i = 0; i < points.length; i++) {
 						String[] point = points[i].split(",");
-						vertices[i * 2] = Integer.parseInt(point[0]) * scaleX;
-						vertices[i * 2 + 1] = Integer.parseInt(point[1]) * scaleY;
+						vertices[i * 2] = Float.parseFloat(point[0]) * scaleX;
+						vertices[i * 2 + 1] = Float.parseFloat(point[1]) * scaleY;
 						if (yUp) {
 							vertices[i * 2 + 1] *= -1;
 						}
@@ -471,8 +471,8 @@ public class TmxMapLoader extends AsynchronousAssetLoader<TiledMap, TmxMapLoader
 					float[] vertices = new float[points.length * 2];
 					for (int i = 0; i < points.length; i++) {
 						String[] point = points[i].split(",");
-						vertices[i * 2] = Integer.parseInt(point[0]) * scaleX;
-						vertices[i * 2 + 1] = Integer.parseInt(point[1]) * scaleY;
+						vertices[i * 2] = Float.parseFloat(point[0]) * scaleX;
+						vertices[i * 2 + 1] = Float.parseFloat(point[1]) * scaleY;
 						if (yUp) {
 							vertices[i * 2 + 1] *= -1;
 						}
