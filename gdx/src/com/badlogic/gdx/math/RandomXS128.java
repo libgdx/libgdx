bc4f0fa0532c57fb10992a1cc1f69fcdac4fa9d4
inferno <infernalbits@gmail.com>
2014-05-08 12:28:00 +0200

    jdoc random x128

--- gdx/src/com/badlogic/gdx/math/RandomXS128.java
+++ gdx/src/com/badlogic/gdx/math/RandomXS128.java
@@ -2,6 +2,12 @@ package com.badlogic.gdx.math;
 
 import java.util.Random;
 
+/** This class implements the xorshift 128+ algorithm known as the fastest among the PRNG, 
+ * the comparisons between this and the java default random show an average difference of 10 ms on an input
+ * of 1 millions of floating point numbers.
+ * To use it inside the code one can simply assign an instance of this class to {@link MathUtils#random} member
+ * at application setup and generate the random numbers using the methods of {@link MathUtils} class. 
+ * More information and algorithms can be found <a href="http://xorshift.di.unimi.it/">here</a> */ 
 public class RandomXS128 extends Random{
 	long[] s = new long[2];
 	
@@ -10,9 +16,9 @@ public class RandomXS128 extends Random{
 		s[1] = seed1;
 	}
 	
+	/** It will use {@link System#nanoTime()} as seeds*/
 	public RandomXS128(){
-		s[0] = System.nanoTime();
-		s[1] = System.nanoTime();
+		this(System.nanoTime(), System.nanoTime());
 	}
 	
 	@Override
