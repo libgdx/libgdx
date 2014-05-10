10954484ccf6d1b8dc0131a356167f1f49d6881f
badlogic <badlogicgames@gmail.com>
2014-05-10 13:11:14 +0200

    fixed locale emulation utf issues

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/java/util/Locale.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/java/util/Locale.java
@@ -436,8 +436,8 @@ public final class Locale implements Cloneable, Serializable {
 //     * <li>{@code new Locale("en", "US").getDisplayName(Locale.US)} -> {@code English (United States)}
 //     * <li>{@code new Locale("en", "US", "POSIX").getDisplayName(Locale.US)} -> {@code English (United States,Computer)}
 //     * <li>{@code new Locale("en").getDisplayName(Locale.FRANCE)} -> {@code anglais}
-//     * <li>{@code new Locale("en", "US").getDisplayName(Locale.FRANCE)} -> {@code anglais (États-Unis)}
-//     * <li>{@code new Locale("en", "US", "POSIX").getDisplayName(Locale.FRANCE)} -> {@code anglais (États-Unis,informatique)}.
+//     * <li>{@code new Locale("en", "US").getDisplayName(Locale.FRANCE)} -> {@code anglais (tats-Unis)}
+//     * <li>{@code new Locale("en", "US", "POSIX").getDisplayName(Locale.FRANCE)} -> {@code anglais (tats-Unis,informatique)}.
 //     * </ul>
 //     */
 //    public String getDisplayName(Locale locale) {
