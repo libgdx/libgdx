e90f75987d2bc383c5b425bd9ed3a0071bd5e83a
Gustavo Maciel <gustavo.knu@gmail.com>
2014-05-11 21:26:26 -0300

    Update Rectangle.java
    Formatted, not using varargs to avoid array allocation, assigning vecs[i] to a local variable

--- gdx/src/com/badlogic/gdx/math/Rectangle.java
+++ gdx/src/com/badlogic/gdx/math/Rectangle.java
@@ -268,17 +268,17 @@ public class Rectangle implements Serializable {
 	 * The rectangle should not have negative width or negative height.
 	 * @param vecs the vectors describing the points
 	 * @return this rectangle for chaining */
-	public Rectangle merge (Vector2... vecs) {
+	public Rectangle merge (Vector2[] vecs) {
 		float minX = x;
 		float maxX = x+width;
 		float minY = y;
 		float maxY = y+height;
-		for(int i = 0; i < vecs.length; ++i)
-		{
-			minX = Math.min(minX, vecs[i].x);
-			maxX = Math.max(maxX, vecs[i].x);
-			minY = Math.min(minY, vecs[i].y);
-			maxY = Math.max(maxY, vecs[i].y);
+		for(int i = 0; i < vecs.length; ++i) {
+			Vector2 v = vecs[i];
+			minX = Math.min(minX, v.x);
+			maxX = Math.max(maxX, v.x);
+			minY = Math.min(minY, v.y);
+			maxY = Math.max(maxY, v.y);
 		}
 		x = minX;
 		width = maxX - minX;
