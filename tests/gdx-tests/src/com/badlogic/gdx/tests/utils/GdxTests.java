7aab199db9b9abc851dcff24ae88678f9b40ffa2
badlogic <badlogicgames@gmail.com>
2014-05-17 12:38:50 +0200

    tests/gdx-tests/src/com/

--- tests/gdx-tests/src/com/badlogic/gdx/tests/utils/GdxTests.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/utils/GdxTests.java
@@ -63,6 +63,7 @@ import java.util.List;
 public class GdxTests {
 	public static final List<Class<? extends GdxTest>> tests = new ArrayList<Class<? extends GdxTest>>(Arrays.asList(
 		// @off
+		IssueTest.class,
 		AccelerometerTest.class,
 		ActionSequenceTest.class,
 		ActionTest.class,
