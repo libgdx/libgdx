23ac1ed1e14005752ea349f1d0e1d45e0ec7454e
Thorsten Schleinzer <git@schleinzer.com>
2014-06-06 15:56:21 +0200

    libGDX game crashes on ART, Google's successor for dalvik.
    Reason is the allocation of a ByteBuffer of size "0" in "IndexBufferObject". This breaks all libGDX Meshes which don't make use of indexes and set the "maxIndexes" to 0 (which in turn breaks quite a lot of libGDX apps on ART I assume.)
    
    While allocating a 0-sized ByteBuffer is perfectly fine according to the javadoc, ART has a bug that makes programs crash if they try to do so. Only sizes >0 do work.
    
    This is a bug in ART admitted and even fixed by Google with https://android-review.googlesource.com/#/c/73175/ .
    
    Unfortunately, that fix is (although committed half a year ago...) not even part of current Android (4.4.3 as of this writing), and it's still unknown (to me) in which release it will finally be available.
    
    So I kindly ask you to accept this pull request to work around that problem for the time being.
    
    Thank you!

--- gdx/src/com/badlogic/gdx/graphics/glutils/IndexBufferObject.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/IndexBufferObject.java
@@ -56,11 +56,27 @@ public class IndexBufferObject implements IndexData {
 	boolean isBound = false;
 	final int usage;
 
+	// used to work around bug: https://android-review.googlesource.com/#/c/73175/
+	private final boolean empty;
+
+	/** Creates a new static IndexBufferObject to be used with vertex arrays.
+	 * 
+	 * @param maxIndices the maximum number of indices this buffer can hold */
+	public IndexBufferObject (int maxIndices) {
+		this(true, maxIndices);
+	}
+
 	/** Creates a new IndexBufferObject.
 	 * 
 	 * @param isStatic whether the index buffer is static
 	 * @param maxIndices the maximum number of indices this buffer can hold */
 	public IndexBufferObject (boolean isStatic, int maxIndices) {
+
+		empty = maxIndices == 0;
+		if (empty) {
+			maxIndices = 1; // avoid allocating a zero-sized buffer because of bug in Android's ART
+		}
+
 		byteBuffer = BufferUtils.newUnsafeByteBuffer(maxIndices * 2);
 		isDirect = true;
 
@@ -71,20 +87,6 @@ public class IndexBufferObject implements IndexData {
 		usage = isStatic ? GL20.GL_STATIC_DRAW : GL20.GL_DYNAMIC_DRAW;
 	}
 
-	/** Creates a new IndexBufferObject to be used with vertex arrays.
-	 * 
-	 * @param maxIndices the maximum number of indices this buffer can hold */
-	public IndexBufferObject (int maxIndices) {
-		byteBuffer = BufferUtils.newUnsafeByteBuffer(maxIndices * 2);
-		this.isDirect = true;
-
-		buffer = byteBuffer.asShortBuffer();
-		buffer.flip();
-		byteBuffer.flip();
-		bufferHandle = createBufferObject();
-		usage = GL20.GL_STATIC_DRAW;
-	}
-
 	private int createBufferObject () {
 		Gdx.gl20.glGenBuffers(1, tmpHandle);
 		return tmpHandle.get(0);
@@ -92,12 +94,12 @@ public class IndexBufferObject implements IndexData {
 
 	/** @return the number of indices currently stored in this buffer */
 	public int getNumIndices () {
-		return buffer.limit();
+		return empty ? 0 : buffer.limit();
 	}
 
 	/** @return the maximum number of indices this IndexBufferObject can store. */
 	public int getNumMaxIndices () {
-		return buffer.capacity();
+		return empty ? 0 : buffer.capacity();
 	}
 
 	/** <p>
@@ -139,7 +141,9 @@ public class IndexBufferObject implements IndexData {
 
 	/** Binds this IndexBufferObject for rendering with glDrawElements. */
 	public void bind () {
-		if (bufferHandle == 0) throw new GdxRuntimeException("No buffer allocated!");
+		if (bufferHandle == 0) {
+			throw new GdxRuntimeException("No buffer allocated!");
+		}
 
 		Gdx.gl20.glBindBuffer(GL20.GL_ELEMENT_ARRAY_BUFFER, bufferHandle);
 		if (isDirty) {
