38a2e8fd76fd15a3ff607bca64ad69a44cde841f
William Guss <carlostnb@gmail.com>
2014-06-15 22:29:13 -0600

    Cleaning up JavaDoc comments.
    As to follow the file's javadoc style (see toString above), I remove the @Override marker.

--- gdx/src/com/badlogic/gdx/math/Rectangle.java
+++ gdx/src/com/badlogic/gdx/math/Rectangle.java
@@ -360,10 +360,6 @@ public class Rectangle implements Serializable {
 		return x + "," + y + "," + width + "," + height;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
 	public int hashCode () {
 		final int prime = 31;
 		int result = 1;
@@ -374,10 +370,6 @@ public class Rectangle implements Serializable {
 		return result;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
 	public boolean equals (Object obj) {
 		if (this == obj) return true;
 		if (obj == null) return false;
