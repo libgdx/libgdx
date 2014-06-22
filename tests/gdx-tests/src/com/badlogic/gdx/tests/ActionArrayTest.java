a400c6b9497333d8a549bdc3fc5ba9804941fdc5
Angergard <portalprogramming@gmail.com>
2014-06-22 02:46:49 +0200

    Added two tests whichs just tests if the arrays and Iterable works!

---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/ActionArrayTest.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.badlogic.gdx.tests;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.forever;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.utils.Array;
+
+public class ActionArrayTest extends GdxTest implements Runnable {
+	Stage stage;
+	Texture texture;
+
+	@Override
+	public void create () {
+		stage = new Stage();
+		texture = new Texture(Gdx.files.internal("data/badlogic.jpg"), false);
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		final Image img = new Image(new TextureRegion(texture));
+		img.setSize(100, 100);
+		img.setOrigin(50, 50);
+		img.setPosition(100, 100);
+
+		Array<Action> actions = new Array<Action>();
+
+		actions.add(forever(sequence(delay(1.0f), new Action() {
+			public boolean act (float delta) {
+				System.out.println(1);
+				img.clearActions();
+				return true;
+			}
+		})));
+
+		Action[] actions2 = new Action[actions.size];
+
+		for (int i = 0; i < actions.size; i++) {
+			actions2[i] = actions.get(i);
+		}
+
+		// Works
+		// img.addActions(actions);
+		//Works
+		img.addActions(actions2);
+			
+		stage.addActor(img);
+
+		// img.action(Forever.$(Sequence.$(ScaleTo.$(1.1f,
+		// 1.1f,0.3f),ScaleTo.$(1f, 1f, 0.3f))));
+		// img.action(Forever.$(Parallel.$(RotateTo.$(1, 1))));
+		// img.action(Delay.$(RotateBy.$(45, 2),
+		// 1).setCompletionListener(this));
+// // Action actionMoveBy = MoveBy.$(30, 0, 0.5f).setCompletionListener(
+// // new OnActionCompleted() {
+// //
+// // @Override
+// // public void completed(Action action) {
+// // System.out.println("move by complete");
+// // }
+// // });
+// //
+// // Action actionDelay = Delay.$(actionMoveBy, 1).setCompletionListener(
+// // new OnActionCompleted() {
+// //
+// // @Override
+// // public void completed(Action action) {
+// // System.out.println("delay complete");
+// // }
+// // });
+// //
+// // img.action(actionDelay);
+//
+// // img.action(Repeat.$(Sequence.$(MoveBy.$(50, 0, 1), MoveBy.$(0, 50, 1), MoveBy.$(-50, 0, 1), MoveBy.$(0, -50, 1)), 3));
+// // img.action(Sequence.$(FadeOut.$(1),
+// // FadeIn.$(1),
+// // Delay.$(MoveTo.$(100, 100, 1), 2),
+// // ScaleTo.$(0.5f, 0.5f, 1),
+// // FadeOut.$(0.5f),
+// // Delay.$(Parallel.$( RotateTo.$(360, 1),
+// // FadeIn.$(1),
+// // ScaleTo.$(1, 1, 1)), 1)));
+// // OnActionCompleted listener = new OnActionCompleted() {
+// // @Override public void completed (Action action) {
+// // img.action(Parallel.$(Sequence.$(FadeOut.$(2), FadeIn.$(2)),
+// // Sequence.$(ScaleTo.$(0.1f, 0.1f, 1.5f), ScaleTo.$(1.0f, 1.0f, 1.5f))).setCompletionListener(this));
+// // }
+// // };
+// //
+// // img.action(Parallel.$(Sequence.$(FadeOut.$(2), FadeIn.$(2)),
+// // Sequence.$(ScaleTo.$(0.1f, 0.1f, 1.5f), ScaleTo.$(1.0f, 1.0f, 1.5f))).setCompletionListener(listener));
+//
+// // img.action(
+// // Sequence.$(
+// // Parallel.$(RotateBy.$(180, 2), ScaleTo.$(1.4f, 1.4f, 2), FadeTo.$(0.7f, 2)),
+// // Parallel.$(RotateBy.$(180, 2), ScaleTo.$(1.0f, 1.0f, 2), FadeTo.$(1.0f, 2)),
+// // Remove.$()
+// // )
+// // );
+// //
+// // Action action = Repeat.$(Sequence.$(
+// // MoveBy.$(8, 0, 0.5f),
+// // MoveBy.$(0, 8, 0.5f),
+// // MoveBy.$(-8, 0, 0.5f),
+// // MoveBy.$(0, -8, 0.5f)), 20);
+// // Action action2 = action.copy();
+// // img.action(action2);
+//
+// // float scale = 1;
+// // float showDuration = 1;
+// // ScaleTo scaleCountdown = ScaleTo.$(scale * 1.0f, scale * 1.0f, 1.0f);
+// // scaleCountdown.setInterpolator(DecelerateInterpolator.$(3.0f));
+// // Parallel parallel = Parallel.$(scaleCountdown);
+// // // Sequence.$(FadeIn.$(0.25f), Delay.$(FadeOut.$(0.25f), 0.5f)));
+// // Sequence cdAnim = Sequence.$(Delay.$(parallel, showDuration), Remove.$());
+// // cdAnim.setCompletionListener(this);
+// // img.action(cdAnim);
+// //
+// // Delay delay = Delay.$(MoveBy.$(100, 100, 1).setCompletionListener(this), 1);
+// // delay.setCompletionListener(this);
+// // img.action(Sequence.$(delay).setCompletionListener(this));
+
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
+	}
+
+	@Override
+	public void run () {
+		System.out.println("completed action");
+	}
+
+	@Override
+	public void dispose () {
+		stage.dispose();
+		texture.dispose();
+	}
+}
---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/StageArrayTest.java
@@ -0,0 +1,245 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.badlogic.gdx.tests;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.forever;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Scaling;
+
+public class StageArrayTest extends GdxTest implements InputProcessor {
+	private static final int NUM_GROUPS = 5;
+	private static final int NUM_SPRITES = (int)Math.sqrt(400 / NUM_GROUPS);
+	private static final float SPACING = 5;
+	ShapeRenderer renderer;
+	Stage stage;
+	Stage ui;
+	Texture texture;
+	Texture uiTexture;
+	BitmapFont font;
+
+	boolean rotateSprites = false;
+	boolean scaleSprites = false;
+	float angle;
+	List<Image> images = new ArrayList<Image>();
+	float scale = 1;
+	float vScale = 1;
+	Label fps;
+
+	@Override
+	public void create () {
+		texture = new Texture(Gdx.files.internal("data/badlogicsmall.jpg"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		font = new BitmapFont(Gdx.files.internal("data/arial-15.fnt"), false);
+
+		stage = new Stage();
+
+		float loc = (NUM_SPRITES * (32 + SPACING) - SPACING) / 2;
+		for (int i = 0; i < NUM_GROUPS; i++) {
+			Group group = new Group();
+			group.setX((float)Math.random() * (stage.getWidth() - NUM_SPRITES * (32 + SPACING)));
+			group.setY((float)Math.random() * (stage.getHeight() - NUM_SPRITES * (32 + SPACING)));
+			group.setOrigin(loc, loc);
+
+			fillGroup(group, texture);
+			stage.addActor(group);
+		}
+
+		uiTexture = new Texture(Gdx.files.internal("data/ui.png"));
+		uiTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			
+		Array<Actor> actors = new Array<Actor>();
+		
+		ui = new Stage();
+
+		Image blend = new Image(new TextureRegion(uiTexture, 0, 0, 64, 32));
+		blend.setAlign(Align.center);
+		blend.setScaling(Scaling.none);
+		blend.addListener(new InputListener() {
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				if (stage.getBatch().isBlendingEnabled())
+					stage.getBatch().disableBlending();
+				else
+					stage.getBatch().enableBlending();
+				return true;
+			}
+		});
+		blend.setY(ui.getHeight() - 64);
+
+		Image rotate = new Image(new TextureRegion(uiTexture, 64, 0, 64, 32));
+		rotate.setAlign(Align.center);
+		rotate.setScaling(Scaling.none);
+		rotate.addListener(new InputListener() {
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				rotateSprites = !rotateSprites;
+				return true;
+			}
+		});
+		rotate.setPosition(64, blend.getY());
+
+		Image scale = new Image(new TextureRegion(uiTexture, 64, 32, 64, 32));
+		scale.setAlign(Align.center);
+		scale.setScaling(Scaling.none);
+		scale.addListener(new InputListener() {
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				scaleSprites = !scaleSprites;
+				return true;
+			}
+		});
+		scale.setPosition(128, blend.getY());
+
+		actors.add(blend);
+		actors.add(rotate);
+		actors.add(scale);
+		
+		fps = new Label("fps: 0", new Label.LabelStyle(font, Color.WHITE));
+		fps.setPosition(10, 30);
+		fps.setColor(0, 1, 0, 1);
+		actors.add(fps);
+
+		renderer = new ShapeRenderer();
+		Gdx.input.setInputProcessor(this);
+		
+		Actor[] actors2 = new Actor[actors.size];
+		
+		for(int i = 0; i < actors.size; i++){
+			actors2[i] = actors.get(i);
+		}
+		//works
+		ui.addActors(actors2);
+		//works
+		//ui.addActors(actors);
+	}
+
+	private void fillGroup (Group group, Texture texture) {
+		float advance = 32 + SPACING;
+		for (int y = 0; y < NUM_SPRITES * advance; y += advance)
+			for (int x = 0; x < NUM_SPRITES * advance; x += advance) {
+				Image img = new Image(new TextureRegion(texture));
+				img.setAlign(Align.center);
+				img.setScaling(Scaling.none);
+				img.setBounds(x, y, 32, 32);
+				img.setOrigin(16, 16);
+				group.addActor(img);
+				images.add(img);
+			}
+	}
+
+	private final Vector2 stageCoords = new Vector2();
+
+	@Override
+	public void render () {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isTouched()) {
+			stage.screenToStageCoordinates(stageCoords.set(Gdx.input.getX(), Gdx.input.getY()));
+			Actor actor = stage.hit(stageCoords.x, stageCoords.y, true);
+			if (actor instanceof Image)
+				((Image)actor).setColor((float)Math.random(), (float)Math.random(), (float)Math.random(),
+					0.5f + 0.5f * (float)Math.random());
+		}
+
+		Array<Actor> actors = stage.getActors();
+		int len = actors.size;
+		if (rotateSprites) {
+			for (int i = 0; i < len; i++)
+				actors.get(i).rotateBy(Gdx.graphics.getDeltaTime() * 10);
+		}
+
+		scale += vScale * Gdx.graphics.getDeltaTime();
+		if (scale > 1) {
+			scale = 1;
+			vScale = -vScale;
+		}
+		if (scale < 0.5f) {
+			scale = 0.5f;
+			vScale = -vScale;
+		}
+
+		len = images.size();
+		for (int i = 0; i < len; i++) {
+			Image img = images.get(i);
+			if (rotateSprites)
+				img.rotateBy(-40 * Gdx.graphics.getDeltaTime());
+			else
+				img.setRotation(0);
+
+			if (scaleSprites) {
+				img.setScale(scale);
+			} else {
+				img.setScale(1);
+			}
+			img.invalidate();
+		}
+
+		stage.draw();
+
+		renderer.begin(ShapeType.Point);
+		renderer.setColor(1, 0, 0, 1);
+		len = actors.size;
+		for (int i = 0; i < len; i++) {
+			Group group = (Group)actors.get(i);
+			renderer.point(group.getX() + group.getOriginX(), group.getY() + group.getOriginY(), 0);
+		}
+		renderer.end();
+
+		fps.setText("fps: " + Gdx.graphics.getFramesPerSecond() + ", actors " + images.size() + ", groups " + actors.size);
+		ui.draw();
+	}
+
+	@Override
+	public boolean touchDown (int x, int y, int pointer, int button) {
+		return ui.touchDown(x, y, pointer, button);
+	}
+
+	@Override
+	public void dispose () {
+		ui.dispose();
+		renderer.dispose();
+		texture.dispose();
+		uiTexture.dispose();
+		font.dispose();
+	}
+}
