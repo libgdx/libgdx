ff3920460558e92dc7fe0cba2fe200b7840abd59
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-06-22 16:43:27 -0400

    Changed uses of floatToIntBits to floatToRawIntBits
    -Oops

--- gdx/src/com/badlogic/gdx/math/Circle.java
+++ gdx/src/com/badlogic/gdx/math/Circle.java
@@ -215,9 +215,9 @@ public class Circle implements Serializable {
 	public int hashCode () {
 		final int prime = 41;
 		int result = 1;
-		result = prime * result + NumberUtils.floatToIntBits(radius);
-		result = prime * result + NumberUtils.floatToIntBits(x);
-		result = prime * result + NumberUtils.floatToIntBits(y);
+		result = prime * result + NumberUtils.floatToRawIntBits(radius);
+		result = prime * result + NumberUtils.floatToRawIntBits(x);
+		result = prime * result + NumberUtils.floatToRawIntBits(y);
 		return result;
 	}
 }
--- gdx/src/com/badlogic/gdx/math/Ellipse.java
+++ gdx/src/com/badlogic/gdx/math/Ellipse.java
@@ -211,10 +211,10 @@ public class Ellipse implements Serializable {
 	public int hashCode () {
 		final int prime = 53;
 		int result = 1;
-		result = prime * result + NumberUtils.floatToIntBits(this.height);
-		result = prime * result + NumberUtils.floatToIntBits(this.width);
-		result = prime * result + NumberUtils.floatToIntBits(this.x);
-		result = prime * result + NumberUtils.floatToIntBits(this.y);
+		result = prime * result + NumberUtils.floatToRawIntBits(this.height);
+		result = prime * result + NumberUtils.floatToRawIntBits(this.width);
+		result = prime * result + NumberUtils.floatToRawIntBits(this.x);
+		result = prime * result + NumberUtils.floatToRawIntBits(this.y);
 		return result;
 	}
 }
