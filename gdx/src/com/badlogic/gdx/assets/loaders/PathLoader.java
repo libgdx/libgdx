218aa9da020b1bed79bcd4ead66fc1a3bd744962
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-06-29 14:19:45 -0400

    Improved the PathLoader
    -Added documentation to PathParameters
    -Added the ability to flip the path across its central horizontal axis
    -Throw exceptions instead of returning null for invalid input
    -Improved the reliability of load()

--- gdx/src/com/badlogic/gdx/assets/loaders/PathLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/PathLoader.java
@@ -1,22 +1,21 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  ******************************************************************************/
 
 package com.badlogic.gdx.assets.loaders;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetLoaderParameters;
 import com.badlogic.gdx.assets.AssetManager;
@@ -30,17 +29,20 @@ import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Path;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.Json.Serializer;
+import com.badlogic.gdx.utils.SerializationException;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 
 /** Use this to load {@link Path}s created with the included path editor. {@link Bezier}s, {@link BSpline}s, and
- * {@link CatmullRomSpline}s are supported. Only 2D paths (i.e. with {@code Vector2}s) are supported.
+ * {@link CatmullRomSpline}s are supported. Only 2D paths (i.e. with {@link Vector2}s) are supported.
+ * 
  * @author Jesse Talavera-Greenberg */
 public class PathLoader extends SynchronousAssetLoader<Path, PathLoader.PathParameters> {
 	private Json json;
+	private static final PathParameters DEFAULT = new PathParameters();
 
 	public PathLoader (FileHandleResolver resolver) {
 		super(resolver);
@@ -54,49 +56,60 @@ public class PathLoader extends SynchronousAssetLoader<Path, PathLoader.PathPara
 	}
 
 	public static class PathParameters extends AssetLoaderParameters<Path> {
-		public Matrix3 transform = new Matrix3();
+		/** When loading a {@link Path}, every one of its control points will be multiplied by this transformation. Useful for
+		 * switching between different coordinate systems (such as switching between the path editor's pixels as units and Box2D's
+		 * meters as units). Defaults to the identity matrix. */
+		public final Matrix3 transform = new Matrix3();
+
+		/** Defaults to {@code true}. If true, will flip the path vertically along the axis between its highest y coordinate and its
+		 * lowest <em>after</em> applying {@link PathParameters#transform}. The built-in path editor uses the top-left corner as (0,
+		 * 0) as the origin, but most of LibGDX uses the bottom-left. This flag sovles that problem. */
+		public boolean flipVertically = true;
 	}
 
 	@Override
 	public Path<Vector2> load (AssetManager assetManager, String fileName, FileHandle file, PathParameters parameter) {
-		Path<Vector2> path = this.json.fromJson(Path.class, file);
-		if (parameter != null) {
-			if (path instanceof Bezier) {
-				Bezier<Vector2> bezier = (Bezier<Vector2>)path;
-				Array<Vector2> points = bezier.points;
-				for (Vector2 v : points) {
-					v.mul(parameter.transform);
-				}
+		JsonValue path = new JsonReader().parse(file).get("path");
+		Path out = null;
+		if (parameter == null) {
+			parameter = DEFAULT;
+		}
+		String type = path.getString("type", null);
+		JsonValue points = path.get("points");
+		Vector2[] pointarray = new Vector2[points.size];
+		int i = 0;
+		for (JsonValue point = points.get(0); point != null; point = point.next(), ++i) {
+			pointarray[i] = new Vector2(point.getFloat("x", 0), point.getFloat("y", 0)).mul(parameter.transform);
+		}
 
-				bezier.set(points, 0, points.size);
-			} else if (path instanceof BSpline) {
-				BSpline<Vector2> bspline = (BSpline<Vector2>)path;
-				Vector2[] points = bspline.controlPoints;
-
-				for (Vector2 v : points) {
-					v.mul(parameter.transform);
-				}
-
-				bspline.set(points, bspline.degree, bspline.continuous);
-			} else if (path instanceof CatmullRomSpline) {
-				CatmullRomSpline<Vector2> crs = (CatmullRomSpline<Vector2>)path;
-				Vector2[] points = crs.controlPoints;
-
-				for (Vector2 v : points) {
-					v.mul(parameter.transform);
-				}
-
-				crs.set(points, crs.continuous);
-			} else {
-				Gdx.app.error("PathLoader", "Unknown path type " + path.getClass() + ", not transforming");
+		if (parameter.flipVertically) {
+			float minY = Float.POSITIVE_INFINITY, maxY = Float.NEGATIVE_INFINITY;
+			for (Vector2 v : pointarray) {
+				minY = Math.min(minY, v.y);
+				maxY = Math.max(maxY, v.y);
+			}
+			float diff = maxY - minY;
+			for (Vector2 v : pointarray) {
+				v.y = diff - v.y;
 			}
 		}
-		return path;
+
+		if ("Bezier".equals(type)) {
+			out = new Bezier(pointarray, 0, pointarray.length);
+		} else if ("BSpline".equals(type)) {
+			out = new BSpline(pointarray, path.getInt("degree", 3), path.getBoolean("continuous", false));
+		} else if ("CatmullRomSpline".equals(type)) {
+			out = new CatmullRomSpline(pointarray, path.getBoolean("continuous", false));
+		} else {
+			throw new SerializationException("Unknown path type " + path.getClass());
+		}
+
+		return out;
 	}
 
 	@Override
 	public Array<AssetDescriptor> getDependencies (String fileName, FileHandle file, PathParameters parameter) {
-		// TODO Auto-generated method stub
+		// No dependencies; once the Path is created, the JSON file can go to Hell for all it matters
 		return null;
 	}
 
@@ -124,7 +137,7 @@ public class PathLoader extends SynchronousAssetLoader<Path, PathLoader.PathPara
 						points = new Array<Vector2>(catmullrom.controlPoints);
 						continuous = catmullrom.continuous;
 					} else {
-						throw new GdxRuntimeException("Unknown path type " + object.getClass());
+						throw new SerializationException("Unknown path type " + object.getClass());
 					}
 
 					if (degree != null) {
@@ -171,11 +184,11 @@ public class PathLoader extends SynchronousAssetLoader<Path, PathLoader.PathPara
 						path = new Bezier<Vector2>(pointarray, 0, pointarray.size);
 					} else if ("BSpline".equals(type)) {
 						int degree = pathjson.getInt("degree", 3);
-						path = new BSpline(pointarray.shrink(), degree, continuous);
+						path = new BSpline<Vector2>(pointarray.shrink(), degree, continuous);
 					} else if ("CatmullRomSpline".equals(type)) {
-						path = new CatmullRomSpline(pointarray.shrink(), continuous);
+						path = new CatmullRomSpline<Vector2>(pointarray.shrink(), continuous);
 					} else {
-						throw new GdxRuntimeException("Unknown Path type " + type);
+						throw new SerializationException("Unknown Path type " + type);
 					}
 				}
 			}
