4ece5299cd868eed85f205b45f36462e673e328d
Justin Shapcott <support@mobidevelop.com>
2014-07-10 08:54:33 -0700

    Apply fix  from #2094
    Leaving everything else as is.

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
@@ -211,7 +211,8 @@ public class AndroidApplication extends Activity implements AndroidApplicationBa
 
 		try {
 			Method m = View.class.getMethod("setSystemUiVisibility", int.class);
-			m.invoke(rootView, 0x0);
+			if (getVersion() <= 13)
+				m.invoke(rootView, 0x0);
 			m.invoke(rootView, 0x1);
 		} catch (Exception e) {
 			log("AndroidApplication", "Can't hide status bar", e);
