3e1b741cd3d6c9b47f33c61eddb6f9f0a78c3759
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-07-17 14:28:05 -0400

    Changed InterpolationTest.getInterpolation to use Interpolation.get()

--- tests/gdx-tests/src/com/badlogic/gdx/tests/InterpolationTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/InterpolationTest.java
@@ -65,11 +65,7 @@ public class InterpolationTest extends GdxTest {
 
 	/** @return the {@link #selectedInterpolation selected} interpolation */
 	private Interpolation getInterpolation (String name) {
-		try {
-			return (Interpolation)Interpolation.class.getField(name).get(null);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		return Interpolation.get(name);
 	}
 
 	@Override
