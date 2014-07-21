017e146ea187be8e441d7997825b6eba3d9e6b6e
Gustavo Maciel <gustavo.knu@gmail.com>
2014-07-21 05:34:06 -0300

    Added angle calculation methods to Vector2
    Added two methods to the Vector2 class, allowing to calculate angle between two vectors, and not only between the x-axis.
    
    I'm not using the standard acos solution, math explained here: http://geomalgorithms.com/vector_products.html
    
    Since the dot product is the cosine of the angle between two vectors, and the 2d cross product (perp dot) is the sine of the same angle, plugging both on a `atan2(crs, dot)` call can give the angle on the full circle (-180~+180, instead of `acos(dot)` solution, which gives only 0~180) and also avoid the two sqrts involved in normalising the vectors.
    
    Here's another explanation on the matter: http://johnblackburne.blogspot.com.br/2012/01/angle-between-two-vectors.html

--- gdx/src/com/badlogic/gdx/math/Vector2.java
+++ gdx/src/com/badlogic/gdx/math/Vector2.java
@@ -285,12 +285,23 @@ public class Vector2 implements Serializable, Vector<Vector2> {
 		if (angle < 0) angle += 360;
 		return angle;
 	}
+	
+	/** @return the angle in degrees of this vector (point) relative to the given vector. Angles are towards the positive y-axis (typically
+	 * 	    counter-clockwise.) between -180 and +180 */
+	 public float angle(Vector reference) {
+	 	return (float)Math.atan2(crs(reference), dot(reference)) * MathUtils.radiansToDegrees;
+	 }
 
 	/** @return the angle in radians of this vector (point) relative to the x-axis. Angles are towards the positive y-axis.
 	 *         (typically counter-clockwise) */
 	public float angleRad () {
 		return (float)Math.atan2(y, x);
 	}
+	/** @return the angle in radians of this vector (point) relative to the given vector. Angles are towards the positive y-axis.
+	 * 	   (typically counter-clockwise.)*/
+	 public float angleRad(Vector reference) {
+	 	return (float)Math.atan2(crs(reference), dot(reference)) * MathUtils.radiansToDegrees;
+	 }
 
 	/** Sets the angle of the vector in degrees relative to the x-axis, towards the positive y-axis (typically counter-clockwise).
 	 * @param degrees The angle in degrees to set. */
