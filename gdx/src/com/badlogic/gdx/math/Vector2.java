a3a87f5b108bf3e0f3141eb1649475a2b214e18c
Gustavo Maciel <gustavo.knu@gmail.com>
2014-07-21 07:25:42 -0300

    Update Vector2.java
    shouldn't have a conversion here

--- gdx/src/com/badlogic/gdx/math/Vector2.java
+++ gdx/src/com/badlogic/gdx/math/Vector2.java
@@ -300,7 +300,7 @@ public class Vector2 implements Serializable, Vector<Vector2> {
 	/** @return the angle in radians of this vector (point) relative to the given vector. Angles are towards the positive y-axis.
 	 * 	   (typically counter-clockwise.)*/
 	 public float angleRad(Vector reference) {
-	 	return (float)Math.atan2(crs(reference), dot(reference)) * MathUtils.radiansToDegrees;
+	 	return (float)Math.atan2(crs(reference), dot(reference));
 	 }
 
 	/** Sets the angle of the vector in degrees relative to the x-axis, towards the positive y-axis (typically counter-clockwise).
