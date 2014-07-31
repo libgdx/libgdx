fe6949476080c74f812a4496f858cfaec01e359f
badlogic <badlogicgames@gmail.com>
2014-07-31 15:52:58 +0200

    fixed logging in iOS backend

--- backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSApplication.java
+++ backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSApplication.java
@@ -290,14 +290,14 @@ public class IOSApplication implements Application {
 	@Override
 	public void log (String tag, String message) {
 		if (logLevel > LOG_NONE) {
-			Foundation.NSLog("[info] " + tag + ": " + message);
+			Foundation.log("[info] " + tag + ": " + message);
 		}
 	}
 
 	@Override
 	public void log (String tag, String message, Throwable exception) {
 		if (logLevel > LOG_NONE) {
-			Foundation.NSLog("[info] " + tag + ": " + message);
+			Foundation.log("[info] " + tag + ": " + message);
 			exception.printStackTrace();
 		}
 	}
@@ -305,14 +305,14 @@ public class IOSApplication implements Application {
 	@Override
 	public void error (String tag, String message) {
 		if (logLevel >= LOG_ERROR) {
-			Foundation.NSLog("[error] " + tag + ": " + message);
+			Foundation.log("[error] " + tag + ": " + message);
 		}
 	}
 
 	@Override
 	public void error (String tag, String message, Throwable exception) {
 		if (logLevel >= LOG_ERROR) {
-			Foundation.NSLog("[error] " + tag + ": " + message);
+			Foundation.log("[error] " + tag + ": " + message);
 			exception.printStackTrace();
 		}
 	}
@@ -320,14 +320,14 @@ public class IOSApplication implements Application {
 	@Override
 	public void debug (String tag, String message) {
 		if (logLevel >= LOG_DEBUG) {
-			Foundation.NSLog("[debug] " + tag + ": " + message);
+			Foundation.log("[debug] " + tag + ": " + message);
 		}
 	}
 
 	@Override
 	public void debug (String tag, String message, Throwable exception) {
 		if (logLevel >= LOG_DEBUG) {
-			Foundation.NSLog("[error] " + tag + ": " + message);
+			Foundation.log("[error] " + tag + ": " + message);
 			exception.printStackTrace();
 		}
 	}
