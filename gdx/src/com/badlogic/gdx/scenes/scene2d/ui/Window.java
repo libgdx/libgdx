1f53d184d684b7e9fdc8c6d7c52fcb51fa995639
NathanSweet <nathan.sweet@gmail.com>
2014-08-05 15:32:08 +0200

    Fixed title bar touches getting the Window children.
    Really this time.
    #2202

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -270,7 +270,13 @@ public class Window extends Table {
 		if (hit == null && isModal && (!touchable || getTouchable() == Touchable.enabled)) return this;
 		float height = getHeight();
 		if (hit == null || hit == this) return hit;
-		if (y <= height && y >= height - getPadTop() && x >= 0 && x <= getWidth() && getCell(hit) != null) return this;
+		if (y <= height && y >= height - getPadTop() && x >= 0 && x <= getWidth()) {
+			// Hit the title bar, don't use the hit child if it is in the Window's table.
+			Actor current = hit;
+			while (current.getParent() != this)
+				current = current.getParent();
+			if (getCell(current) != null) return this;
+		}
 		return hit;
 	}
 
