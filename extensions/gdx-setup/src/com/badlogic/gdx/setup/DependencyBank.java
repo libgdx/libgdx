cd83221feb37386943325763833e6c99d0cae6b6
badlogic <badlogicgames@gmail.com>
2014-08-09 19:21:49 +0200

    updated libgdx version in dependency bank

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -7,9 +7,9 @@ import java.util.LinkedHashMap;
 public class DependencyBank {
 
 	//Versions
-	static String libgdxVersion = "1.2.0";
+	static String libgdxVersion = "1.3.0";
 	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
-	static String libgdxNightlyVersion = "1.2.1-SNAPSHOT";
+	static String libgdxNightlyVersion = "1.3.1-SNAPSHOT";
 	static String roboVMVersion = "0.0.14";
 	static String buildToolsVersion = "19.1.0";
 	static String androidAPILevel = "19";
