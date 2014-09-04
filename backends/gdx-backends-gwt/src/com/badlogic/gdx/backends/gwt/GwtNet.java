d262d6762113b3a3e81cd3df24a9ce7c0ffd708c
Kaj Toet <kvtoet@gmail.com>
2014-09-04 22:01:39 +0200

    Revert "Binary data support for GWTNet"
    This reverts commit d41807511c2d992bbb73a3b65f4bb27e74309db3.

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtNet.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtNet.java
@@ -16,7 +16,6 @@
 
 package com.badlogic.gdx.backends.gwt;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,85 +32,41 @@ import com.badlogic.gdx.net.SocketHints;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.google.gwt.http.client.Header;
-import com.google.gwt.typedarrays.shared.Int8Array;
-import com.google.gwt.typedarrays.shared.TypedArrays;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.xhr.client.ReadyStateChangeHandler;
-import com.google.gwt.xhr.client.XMLHttpRequest;
 
 public class GwtNet implements Net {
 
-	ObjectMap<HttpRequest, XMLHttpRequest> requests;
+	ObjectMap<HttpRequest, Request> requests;
 	ObjectMap<HttpRequest, HttpResponseListener> listeners;
 
 	private final class HttpClientResponse implements HttpResponse {
 
-		private Int8Array response;
+		private Response response;
 		private HttpStatus status;
-        private XMLHttpRequest request;
 
-		public HttpClientResponse(int status, Int8Array response, XMLHttpRequest request) {
+		public HttpClientResponse (Response response) {
 			this.response = response;
-			this.status = new HttpStatus(status);
-            this.request = request;
+			this.status = new HttpStatus(response.getStatusCode());
 		}
 
 		@Override
 		public byte[] getResult () {
-            byte[] array = new byte[response.length()];
-            for (int i = 0; i < array.length; i++) {
-                array[i] = response.get(i);
-            }
-			return array;
+			return null;
 		}
-
-        @Override
-        public String getHeader(String header) {
-            return request.getResponseHeader(header);
-        }
-
-        public Header[] getHeadersArray() {
-            String allHeaders = request.getAllResponseHeaders();
-            String[] unparsedHeaders = allHeaders.split("\n");
-            Header[] parsedHeaders = new Header[unparsedHeaders.length];
-            for (int i = 0, n = unparsedHeaders.length; i < n; ++i) {
-                String unparsedHeader = unparsedHeaders[i];
-                if (unparsedHeader.length() == 0) {
-                    continue;
-                }
-                int endOfNameIdx = unparsedHeader.indexOf(':');
-                if (endOfNameIdx < 0) {
-                    continue;
-                }
-                final String name = unparsedHeader.substring(0, endOfNameIdx).trim();
-                final String value = unparsedHeader.substring(endOfNameIdx + 1).trim();
-                Header header = new Header() {
-                    @Override
-                    public String getName() {
-                        return name;
-                    }
-                    @Override
-                    public String getValue() {
-                        return value;
-                    }
-                    @Override
-                    public String toString() {
-                        return name + " : " + value;
-                    }
-                };
-                parsedHeaders[i] = header;
-            }
-            return parsedHeaders;
-        }
 
 		@Override
 		public String getResultAsString () {
-			return response.toString();
+			return response.getText();
 		}
 
 		@Override
 		public InputStream getResultAsStream () {
-			return new ByteArrayInputStream(getResult());
+			return null;
 		}
 
 		@Override
@@ -120,24 +75,29 @@ public class GwtNet implements Net {
 		}
 
 		@Override
-        public Map<String, List<String>> getHeaders () {
-            Map<String, List<String>> headers = new HashMap<String, List<String>>();
-            Header[] responseHeaders = getHeadersArray();
-            for (int i = 0; i < responseHeaders.length; i++) {
-                String headerName = responseHeaders[i].getName();
-                List<String> headerValues = headers.get(headerName);
-                if (headerValues == null) {
-                    headerValues = new ArrayList<String>();
-                    headers.put(headerName, headerValues);
-                }
-                headerValues.add(responseHeaders[i].getValue());
-            }
-            return headers;
-        }
+		public Map<String, List<String>> getHeaders () {
+			Map<String, List<String>> headers = new HashMap<String, List<String>>();
+			Header[] responseHeaders = response.getHeaders();
+			for (int i = 0; i < responseHeaders.length; i++) {
+				String headerName = responseHeaders[i].getName();
+				List<String> headerValues = headers.get(headerName);
+				if (headerValues == null) {
+					headerValues = new ArrayList<String>();
+					headers.put(headerName, headerValues);
+				}
+				headerValues.add(responseHeaders[i].getValue());
+			}
+			return headers;
+		}
+
+		@Override
+		public String getHeader (String name) {
+			return response.getHeader(name);
+		}
 	}
 
 	public GwtNet () {
-		requests = new ObjectMap<HttpRequest, XMLHttpRequest>();
+		requests = new ObjectMap<HttpRequest, Request>();
 		listeners = new ObjectMap<HttpRequest, HttpResponseListener>();
 	}
 
@@ -151,67 +111,69 @@ public class GwtNet implements Net {
 		final String method = httpRequest.getMethod();		
 		final String value = httpRequest.getContent();
 		final boolean valueInBody = method.equalsIgnoreCase(HttpMethods.POST) || method.equals(HttpMethods.PUT);
+		
+		RequestBuilder builder;
+		
+		String url = httpRequest.getUrl();
+		if (method.equalsIgnoreCase(HttpMethods.GET)) {
+			if (value != null) {
+				url += "?" + value;
+			}			
+			builder = new RequestBuilder(RequestBuilder.GET, url);
+		} else if (method.equalsIgnoreCase(HttpMethods.POST)) {
+			builder = new RequestBuilder(RequestBuilder.POST, url);
+		} else if (method.equalsIgnoreCase(HttpMethods.DELETE)) {
+			if (value != null) {
+				url += "?" + value;
+			}
+			builder = new RequestBuilder(RequestBuilder.DELETE, url);
+		} else if (method.equalsIgnoreCase(HttpMethods.PUT)) {
+			builder = new RequestBuilder(RequestBuilder.PUT, url);
+		} else {
+			throw new GdxRuntimeException("Unsupported HTTP Method");
+		}
 
-        String url = httpRequest.getUrl();
-        if (method.equalsIgnoreCase(HttpMethods.GET)) {
-            if (value != null) {
-                url += "?" + value;
-            }
-        } else if (method.equalsIgnoreCase(HttpMethods.POST)) {
-        } else if (method.equalsIgnoreCase(HttpMethods.DELETE)) {
-            if (value != null) {
-                url += "?" + value;
-            }
-        } else if (method.equalsIgnoreCase(HttpMethods.PUT)) {
-        } else {
-            throw new GdxRuntimeException("Unsupported HTTP Method");
-        }
+		Map<String, String> content = httpRequest.getHeaders();
+		Set<String> keySet = content.keySet();
+		for (String name : keySet) {
+			builder.setHeader(name, content.get(name));
+		}
 
-        XMLHttpRequest request = XMLHttpRequest.create();
+		builder.setTimeoutMillis(httpRequest.getTimeOut());
 
-        request.setOnReadyStateChange(new ReadyStateChangeHandler() {
-            @Override
-            public void onReadyStateChange(XMLHttpRequest xhr) {
-                if (xhr.getReadyState() == XMLHttpRequest.DONE) {
-                    if (xhr.getStatus() != 200) {
-                        httpResultListener.failed(new Throwable(xhr.getStatusText()));
-                        requests.remove(httpRequest);
-                        listeners.remove(httpRequest);
-                    } else {
-                        Int8Array data = TypedArrays.createInt8Array(xhr.getResponseArrayBuffer());
-                        httpResultListener.handleHttpResponse(new HttpClientResponse(xhr.getStatus(), data, xhr));
-                        requests.remove(httpRequest);
-                        listeners.remove(httpRequest);
-                    }
-                }
-            }
-        });
-        request.open(method, url);
+		try {
+			Request request = builder.sendRequest(valueInBody ? value : null, new RequestCallback() {
 
-        Map<String, String> content = httpRequest.getHeaders();
-        Set<String> keySet = content.keySet();
-        for (String name : keySet) {
-            request.setRequestHeader(name, content.get(name));
-        }
-        request.setResponseType(XMLHttpRequest.ResponseType.ArrayBuffer);
+				@Override
+				public void onResponseReceived (Request request, Response response) {					
+						httpResultListener.handleHttpResponse(new HttpClientResponse(response));
+						requests.remove(httpRequest);
+						listeners.remove(httpRequest);
+				}
 
-        if (valueInBody)
-            request.send(value);
-        else
-            request.send();
+				@Override
+				public void onError (Request request, Throwable exception) {
+						httpResultListener.failed(exception);
+						requests.remove(httpRequest);
+						listeners.remove(httpRequest);
+				}
+			});
+			requests.put(httpRequest, request);
+			listeners.put(httpRequest, httpResultListener);
 
+		} catch (RequestException e) {
+			httpResultListener.failed(e);
+		}
 
-        requests.put(httpRequest, request);
-        listeners.put(httpRequest, httpResultListener);
 	}
 
 	@Override
 	public void cancelHttpRequest (HttpRequest httpRequest) {
 		HttpResponseListener httpResponseListener = listeners.get(httpRequest);
-		XMLHttpRequest request = requests.get(httpRequest);
+		Request request = requests.get(httpRequest);
 
 		if (httpResponseListener != null && request != null) {
-			request.abort();
+			request.cancel();
 			httpResponseListener.cancelled();
 			requests.remove(httpRequest);
 			listeners.remove(httpRequest);
