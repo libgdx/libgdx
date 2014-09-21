c62a03e59532837e5c5b9f371d61f0048062e864
NathanSweet <nathan.sweet@gmail.com>
2014-09-21 19:36:10 +0200

    Added combinations for convenience.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/Align.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/Align.java
@@ -24,4 +24,9 @@ public class Align {
 	static public final int bottom = 1 << 2;
 	static public final int left = 1 << 3;
 	static public final int right = 1 << 4;
+
+	static public final int topLeft = top | left;
+	static public final int topRight = top | right;
+	static public final int bottomLeft = bottom | left;
+	static public final int bottomRight = bottom | right;
 }
