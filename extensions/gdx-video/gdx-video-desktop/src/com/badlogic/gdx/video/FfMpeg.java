fe1025a763435f5fd4dbc67cd37d198190b2c7ae
Rob Bogie <bogie.rob@gmail.com>
2014-09-23 15:56:30 +0200

    Made loading simpler as suggested by @manuel-freire

--- extensions/gdx-video/gdx-video-desktop/src/com/badlogic/gdx/video/FfMpeg.java
+++ extensions/gdx-video/gdx-video-desktop/src/com/badlogic/gdx/video/FfMpeg.java
@@ -22,7 +22,7 @@ public class FfMpeg {
 	public static final String NATIVE_LIBRARY_NAME = "gdx-video-desktop";
 
 	private static boolean loaded = false;
-	private static String libraryPath = "";
+	private static String libraryPath;
 
 	/**
 	 * This will set the path in which it tries to find the native library.
@@ -38,99 +38,25 @@ public class FfMpeg {
 	/**
 	 * This method will load the libraries from the path given with setLibraryFilePath.
 	 *
-	 * @return whether loading was succesfull
+	 * @return whether loading was successful
 	 */
 	public static boolean loadLibraries() {
 		if (loaded) {
 			return true;
 		}
 
-		if (libraryPath == null || libraryPath.equals("")) {
-			libraryPath = System.getProperty("java.io.tmpdir") + "/" + NATIVE_LIBRARY_NAME + "-java-natives/" + NATIVE_LIBRARY_NAME
-							+ "-natives.jar";
-			File outputFile = new File(libraryPath);
-			outputFile.getParentFile()
-						.mkdirs();
-
-			InputStream input = FfMpeg.class.getResourceAsStream("/" + NATIVE_LIBRARY_NAME + "-natives.jar");
-			if (input == null) {
-				return false;
-			}
-			try {
-				FileOutputStream output = new FileOutputStream(outputFile);
-				int size = 0;
-				while (size != -1) {
-					byte[] buffer = new byte[4048];
-					size = input.read(buffer);
-					if (size > 0) {
-						output.write(buffer, 0, size);
-					}
-				}
-				input.close();
-				output.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		JniGenSharedLibraryLoader libLoader;
+		if (libraryPath == null) {
+			libLoader = new JniGenSharedLibraryLoader();
+		} else {
+			libLoader = new JniGenSharedLibraryLoader(libraryPath);
 		}
 
-		JniGenSharedLibraryLoader libLoader = new JniGenSharedLibraryLoader(libraryPath, new SharedLibraryFinder() {
-			@Override
-			public String getSharedLibraryNameWindows(String sharedLibName, boolean is64Bit, ZipFile nativesJar) {
-				Enumeration<? extends ZipEntry> entries = nativesJar.entries();
-				while (entries.hasMoreElements()) {
-					ZipEntry entry = entries.nextElement();
-					String filename = entry.getName();
-
-					if (filename.equals(sharedLibName + (is64Bit ? "64.dll" : ".dll"))
-						|| filename.matches("windows" + (is64Bit ? "64" : "32") + "\\/" + sharedLibName + "[-]?\\d+\\.dll*")) {
-						return filename;
-					}
-				}
-
-				return null;
-			}
-
-			@Override
-			public String getSharedLibraryNameLinux(String sharedLibName, boolean is64Bit, ZipFile nativesJar) {
-				Enumeration<? extends ZipEntry> entries = nativesJar.entries();
-				while (entries.hasMoreElements()) {
-					ZipEntry entry = entries.nextElement();
-					String filename = entry.getName();
-
-					if (filename.equals("lib" + sharedLibName + (is64Bit ? "64.so" : ".so"))
-						|| filename.matches("linux" + (is64Bit ? "64" : "32") + "\\/lib" + sharedLibName + "\\.so[.\\d]*")) {
-						return filename;
-					}
-				}
-
-				return null;
-			}
-
-			@Override
-			public String getSharedLibraryNameMac(String sharedLibName, boolean is64Bit, ZipFile nativesJar) {
-				return null;
-			}
-
-			@Override
-			public String getSharedLibraryNameAndroid(String sharedLibName, ZipFile nativesJar) {
-				return null;
-			}
-		});
 		try {
-
-			if (System.getProperty("os.name")
-						.startsWith("Windows")) {
-				libLoader.load("libwinpthread");
-			}
-			libLoader.load("avutil");
-			libLoader.load("avcodec");
-			libLoader.load("avformat");
-			libLoader.load("swresample");
-			libLoader.load("swscale");
 			libLoader.load(NATIVE_LIBRARY_NAME);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+			e.printStackTrace();
 			loaded = false;
 			return false;
 		}
