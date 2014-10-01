11f4b5e6a364ee38ab557f90b80823c0d661b279
Ivan Perevezentsev <needhash@gmail.com>
2014-10-01 13:39:24 +0400

    Fixed formatting

--- tests/gdx-tests/src/com/badlogic/gdx/tests/gles2/SimpleVertexShader.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/gles2/SimpleVertexShader.java
@@ -37,12 +37,21 @@ public class SimpleVertexShader extends GdxTest {
 
 	@Override
 	public void create () {
-		String vertexShader = "uniform mat4 u_mvpMatrix;                   \n" + "attribute vec4 a_position;                  \n"
-			+ "void main()                                 \n" + "{                                           \n"
-			+ "   gl_Position = u_mvpMatrix * a_position;  \n" + "}                            \n";
-		String fragmentShader = "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n"
-			+ "void main()                                  \n" + "{                                            \n"
-			+ "  gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n" + "}";
+		String vertexShader =
+			  "uniform mat4 u_mvpMatrix;                   \n"
+			+ "attribute vec4 a_position;                  \n"
+			+ "void main()                                 \n"
+			+ "{                                           \n"
+			+ "   gl_Position = u_mvpMatrix * a_position;  \n"
+			+ "}                            \n";
+		String fragmentShader =
+			"#ifdef GL_ES\n"
+			+ "precision mediump float;\n"
+			+ "#endif\n"
+			+ "void main()                                  \n"
+			+ "{                                            \n"
+			+ "  gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n"
+			+ "}";
 
 		shader = new ShaderProgram(vertexShader, fragmentShader);
 		mesh = Shapes.genCube();
