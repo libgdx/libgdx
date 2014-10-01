6df8c9591f39f57bf7453530760269c1c1e32e0d
Ivan Perevezentsev <needhash@gmail.com>
2014-10-02 03:10:44 +0400

    Revert "Ignore false warning"
    This reverts commit a8e29b5e45e66745a3f3478096cde28cca112026.

--- extensions/gdx-jnigen/src/com/badlogic/gdx/jnigen/JniGenSharedLibraryLoader.java
+++ extensions/gdx-jnigen/src/com/badlogic/gdx/jnigen/JniGenSharedLibraryLoader.java
@@ -147,8 +147,6 @@ public class JniGenSharedLibraryLoader {
 	}
 
 	private InputStream getFromJar (String jarFile, String sharedLibrary) throws IOException {
-		// File will be closed by caller with inputStream.close()
-		@SuppressWarnings("resource")
 		ZipFile file = new ZipFile(nativesJar);
 		ZipEntry entry = file.getEntry(sharedLibrary);
 		return file.getInputStream(entry);
