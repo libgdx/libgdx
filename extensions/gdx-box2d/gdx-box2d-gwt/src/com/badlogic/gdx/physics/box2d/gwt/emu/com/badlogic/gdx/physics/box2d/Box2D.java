b5832eaf830b03dbaf31fbff63ba025ab341df0f
Daniel Holderbaum <danielholderbaum@gmail.com>
2014-10-04 18:00:53 +0200

    Box2D is final and has a private ctor.

--- extensions/gdx-box2d/gdx-box2d-gwt/src/com/badlogic/gdx/physics/box2d/gwt/emu/com/badlogic/gdx/physics/box2d/Box2D.java
+++ extensions/gdx-box2d/gdx-box2d-gwt/src/com/badlogic/gdx/physics/box2d/gwt/emu/com/badlogic/gdx/physics/box2d/Box2D.java
@@ -18,7 +18,10 @@ package com.badlogic.gdx.physics.box2d;
 
 /** A stub that does nothing at all, since there is no initialization necessary in case of GWT.
  * @author Daniel Holderbaum */
-public abstract class Box2D {
+public final class Box2D {
+
+	private Box2D () {
+	}
 
 	/** Does nothing on GWT. */
 	public static void init () {
--- extensions/gdx-box2d/gdx-box2d/src/com/badlogic/gdx/physics/box2d/Box2D.java
+++ extensions/gdx-box2d/gdx-box2d/src/com/badlogic/gdx/physics/box2d/Box2D.java
@@ -20,7 +20,10 @@ import com.badlogic.gdx.utils.SharedLibraryLoader;
 
 /** This class's only purpose is to initialize Box2D by calling its {@link #init()} method.
  * @author Daniel Holderbaum */
-public abstract class Box2D {
+public final class Box2D {
+
+	private Box2D () {
+	}
 
 	/** Loads the Box2D native library and initializes the gdx-box2d extension. Must be called before any of the box2d
 	 * classes/methods can be used. Currently with the exception of the {@link World} class, which will also cause the Box2D
