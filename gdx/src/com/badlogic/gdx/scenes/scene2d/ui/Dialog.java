d9994f018ff0a6ba78fbcd1ff5da400435e95edf
Vincent Bousquet <vcbousquet@gmail.com>
2014-10-05 22:46:15 +0200

    Fix a little bug with default dialog fading

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Dialog.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Dialog.java
@@ -204,7 +204,8 @@ public class Dialog extends Window {
 
 	/** {@link #pack() Packs} the dialog and adds it to the stage, centered with default fadeIn action */
 	public Dialog show (Stage stage) {
-		show(stage, sequence(Actions.alpha(0), Actions.fadeIn(0.4f, Interpolation.fade)));
+		getColor().a = 0;
+		show(stage, Actions.fadeIn(0.4f, Interpolation.fade));
 		setPosition(Math.round((stage.getWidth() - getWidth()) / 2), Math.round((stage.getHeight() - getHeight()) / 2));
 		return this;
 	}
