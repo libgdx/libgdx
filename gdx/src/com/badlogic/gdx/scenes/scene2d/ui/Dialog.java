752254b2b6673e3fc0788f5f7c9a85149cdfc944
James Durie <duriej@gmail.com>
2014-10-07 20:56:11 +0100

    Also check for previousKeyboardFocus and previousScrollFocus before cancelling a focus change

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Dialog.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Dialog.java
@@ -107,7 +107,9 @@ public class Dialog extends Window {
 				if (isModal && stage != null && stage.getRoot().getChildren().size > 0
 					&& stage.getRoot().getChildren().peek() == Dialog.this) { // Dialog is top most actor.
 					Actor newFocusedActor = event.getRelatedActor();
-					if (newFocusedActor != null && !newFocusedActor.isDescendantOf(Dialog.this)) event.cancel();
+					if (newFocusedActor != null && !newFocusedActor.isDescendantOf(Dialog.this) &&
+						!(newFocusedActor.equals(previousKeyboardFocus) || newFocusedActor.equals(previousScrollFocus)) )
+						event.cancel();
 				}
 			}
 		};
