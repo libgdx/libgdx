4374760010679cae216f1c512564626520e00238
NathanSweet <nathan.sweet@gmail.com>
2014-10-10 12:20:18 +0200

    Fixed setting type name to null for enums and collections.
    #2435

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -460,7 +460,7 @@ public class Json {
 				return;
 			}
 			if (value instanceof Collection) {
-				if (knownType != null && actualType != knownType && actualType != ArrayList.class) {
+				if (typeName != null && actualType != ArrayList.class && (knownType == null || knownType != actualType)) {
 					writeObjectStart(actualType, knownType);
 					writeArrayStart("items");
 					for (Object item : (Collection)value)
@@ -520,7 +520,7 @@ public class Json {
 
 			// Enum special case.
 			if (ClassReflection.isAssignableFrom(Enum.class, actualType)) {
-				if (knownType == null || knownType != actualType) {
+				if (typeName != null && (knownType == null || knownType != actualType)) {
 					// Ensures that enums with specific implementations (abstract logic) serialize correctly.
 					if (actualType.getEnumConstants() == null) actualType = actualType.getSuperclass();
 
@@ -837,7 +837,7 @@ public class Json {
 				return readValue("value", type, jsonData);
 			}
 
-			if (ClassReflection.isAssignableFrom(Collection.class, type)) {
+			if (typeName != null && ClassReflection.isAssignableFrom(Collection.class, type)) {
 				// JSON object wrapper to specify type.
 				jsonData = jsonData.get("items");
 			} else {
