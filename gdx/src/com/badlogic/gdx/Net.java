44dd5351dcc2dccea60a5f0baab2f6214ebc21c9
Daniel Holderbaum <danielholderbaum@gmail.com>
2014-10-25 23:06:46 +0200

    Removed the new HTTP methods.

--- gdx/src/com/badlogic/gdx/Net.java
+++ gdx/src/com/badlogic/gdx/Net.java
@@ -104,10 +104,6 @@ public interface Net {
 		public static final String POST = "POST";
 		public static final String PUT = "PUT";
 		public static final String DELETE = "DELETE";
-		public static final String OPTIONS = "OPTIONS";
-		public static final String HEAD = "HEAD";
-		public static final String TRACE = "TRACE";
-		public static final String CONNECT = "CONNECT";
 
 	}
 
