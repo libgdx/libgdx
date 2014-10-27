217865dbc49493190fff98b525eaa7b3c1e0d7c9
James Pilfold <jamespilfold@googlemail.com>
2014-10-27 19:24:52 +0000

    Allow Label to wrap at specified characters
    This commit provides the option to specify additional break characters.

--- gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFont.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFont.java
@@ -67,6 +67,7 @@ public class BitmapFont implements Disposable {
 	private boolean integer;
 	private boolean ownsTexture;
 	boolean markupEnabled;
+	private char[] breakChars;
 
 	/** Creates a BitmapFont using the default 15pt Arial font included in the libgdx JAR file. This is convenient to easily display
 	 * text without bothering with generating a bitmap font. */
@@ -769,6 +770,22 @@ public class BitmapFont implements Disposable {
 		for (; start < n; start++)
 			if (text.charAt(start) == ch) return start;
 		return n;
+	}
+	
+	/**
+	 * Provide any additional characters that should act as break characters when the label is wrapped.
+	 * By default, only whitespace characters act as break chars.
+	 */
+	public void setBreakChars(char[] breakChars) {
+		this.breakChars = breakChars;
+	}
+	
+	public boolean isBreakChar(char c) {
+		if (breakChars == null) return false;
+		for (char br: breakChars) {
+			if (c == br) return true;
+		}
+		return false;
 	}
 
 	static boolean isWhitespace (char c) {
