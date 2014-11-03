d8312c4fdaa79e81bc8a6064704ed1a5e24e6e2a
NathanSweet <nathan.sweet@gmail.com>
2014-11-03 15:58:39 +0100

    Added alignment to MoveToAction.
    closes #2541

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/Actions.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/Actions.java
@@ -79,6 +79,22 @@ public class Actions {
 		return action;
 	}
 
+	static public MoveToAction moveTo (float x, float y, int alignment) {
+		return moveTo(x, y, alignment, 0, null);
+	}
+
+	static public MoveToAction moveTo (float x, float y, int alignment, float duration) {
+		return moveTo(x, y, alignment, duration, null);
+	}
+
+	static public MoveToAction moveTo (float x, float y, int alignment, float duration, Interpolation interpolation) {
+		MoveToAction action = action(MoveToAction.class);
+		action.setPosition(x, y, alignment);
+		action.setDuration(duration);
+		action.setInterpolation(interpolation);
+		return action;
+	}
+
 	/** Moves the actor instantly. */
 	static public MoveByAction moveBy (float amountX, float amountY) {
 		return moveBy(amountX, amountY, 0, null);
