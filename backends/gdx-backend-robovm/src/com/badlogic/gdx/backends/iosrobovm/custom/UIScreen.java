d01dc27c6bdad8364b022ebf31542ab6f7fdca2f
badlogic <badlogicgames@gmail.com>
2014-12-04 20:17:48 +0100

    backported accelerometer classes, deprecated and removed in lastest RoboVM. Opened issue for CoreMotion implementation

--- backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/custom/UIScreen.java
+++ backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/custom/UIScreen.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.badlogic.gdx.backends.iosrobovm;
+package com.badlogic.gdx.backends.iosrobovm.custom;
 
 /*<imports>*/
 import java.io.*;
