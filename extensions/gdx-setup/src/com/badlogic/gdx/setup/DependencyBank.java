8a9e0f2cc8a4a451332ac269a96a51ad92008a36
Tomski <tomaszwojciechowski@hotmail.co.uk>
2014-12-08 18:05:01 +0000

    Update DependencyBank.java

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -9,7 +9,7 @@ public class DependencyBank {
 	//Versions
 	static String libgdxVersion = "1.5.0";
 	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
-	static String libgdxNightlyVersion = "1.4.2-SNAPSHOT";
+	static String libgdxNightlyVersion = "1.5.1-SNAPSHOT";
 	static String roboVMVersion = "1.0.0-beta-01";
 	static String buildToolsVersion = "20.0.0";
 	static String androidAPILevel = "20";
