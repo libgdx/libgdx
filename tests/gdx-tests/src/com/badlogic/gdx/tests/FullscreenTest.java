0b3d80ca6e67766b64bd9295a5cc5efa30ee6c29
badlogic <badlogicgames@gmail.com>
2014-12-29 14:31:03 +0100

    Updated to LWJGL 2.9.2 for retina issue, yet FullscreenTest still doesn't work :/

--- tests/gdx-tests/src/com/badlogic/gdx/tests/FullscreenTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/FullscreenTest.java
@@ -19,18 +19,19 @@ package com.badlogic.gdx.tests;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.tests.utils.GdxTest;
 
 public class FullscreenTest extends GdxTest {
-
+	SpriteBatch batch;
+	Texture tex;
 	boolean fullscreen = false;
 
 	@Override
 	public void create () {
+		batch = new SpriteBatch();
+		tex = new Texture(Gdx.files.internal("data/badlogic.jpg"));
 		DisplayMode[] modes = Gdx.graphics.getDisplayModes();
 		for (DisplayMode mode : modes) {
 			System.out.println(mode);
@@ -45,16 +46,24 @@ public class FullscreenTest extends GdxTest {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor((float)Math.random(), 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+		
+		batch.begin();
+		batch.draw(tex, (float)Math.random() * Gdx.graphics.getWidth(), 0);
+		batch.end();
+		
 		if (Gdx.input.justTouched()) {
 			if (fullscreen) {
 				Gdx.graphics.setDisplayMode(480, 320, false);
+				batch.getProjectionMatrix().setToOrtho2D(0, 0, 480, 320);
+				Gdx.gl.glViewport(0, 0, 480, 320);
 				fullscreen = false;
 			} else {
 				DisplayMode desktopDisplayMode = Gdx.graphics.getDesktopDisplayMode();
 				Gdx.graphics.setDisplayMode(desktopDisplayMode.width, desktopDisplayMode.height, true);
+				batch.getProjectionMatrix().setToOrtho2D(0, 0, desktopDisplayMode.width, desktopDisplayMode.height);
+				Gdx.gl.glViewport(0, 0, desktopDisplayMode.width, desktopDisplayMode.height);
 				fullscreen = true;
 			}
 		}
