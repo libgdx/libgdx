083775cbfd6229b8c8998a0321cb0fabca50efe8
Daniel Holderbaum <danielholderbaum@gmail.com>
2015-01-02 11:47:56 +0100

    Small change.

--- gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java
+++ gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java
@@ -108,7 +108,8 @@ public class HttpRequestBuilder {
 		return this;
 	}
 
-	/** Sets the correct {@code ContentType} and encodes the given parameter map, then sets it as the content. */
+	/** Sets the correct {@code ContentType} and encodes the given parameter map (via {@link HttpParametersUtils}), then sets it as
+	 * the content. */
 	public HttpRequestBuilder formEncodedContent (Map<String, String> content) {
 		validate();
 		httpRequest.setHeader(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
