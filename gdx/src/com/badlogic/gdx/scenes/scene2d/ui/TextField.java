caa84a10b09c3e7105f05f03c553a48e7f668a7a
AlbertoCejas <alberto.cejas.sanchez@gmail.com>
2015-01-10 20:15:50 +0100

    adds horizontal text alignment feature for textfields

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
@@ -29,6 +29,7 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Disableable;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
@@ -85,7 +86,8 @@ public class TextField extends Widget implements Disableable {
 	TextFieldListener listener;
 	TextFieldFilter filter;
 	OnscreenKeyboard keyboard = new DefaultOnscreenKeyboard();
-	boolean focusTraversal = true, onlyFontChars = true, disabled, rightAligned;
+	boolean focusTraversal = true, onlyFontChars = true, disabled;
+	private int textHAlign = Align.left;
 	private float selectionX, selectionWidth;
 
 	boolean passwordMode;
@@ -248,8 +250,9 @@ public class TextField extends Widget implements Disableable {
 			selectionWidth = maxX - minX;
 		}
 
-		if (rightAligned) {
+		if (textHAlign == Align.center || textHAlign == Align.right) {
 			textOffset = visibleWidth - (glyphPositions[visibleTextEnd] - startPos);
+			if (textHAlign == Align.center) textOffset *= .5f;
 			if (hasSelection) selectionX += textOffset;
 		}
 	}
@@ -613,8 +616,10 @@ public class TextField extends Widget implements Disableable {
 		return prefHeight;
 	}
 
-	public void setRightAligned (boolean rightAligned) {
-		this.rightAligned = rightAligned;
+	/** Sets text horizontal alignment (left, center or right). */
+	public void setTextAlignment (int alignment) {
+		if (alignment == Align.left || alignment == Align.center || alignment == Align.right) 
+			this.textHAlign = alignment;
 	}
 
 	/** If true, the text in this text field will be shown as bullet characters.
