60492d68c72bf624a9fa1c8a02cc990d9b6fd3ac
Vincent Bousquet <vcbousquet@gmail.com>
2015-01-16 21:57:59 +0100

    Add the ability to set alpha component to BitmapFontCache keeping RGB components untouched

--- gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java
@@ -132,6 +132,27 @@ public class BitmapFontCache {
 		}
 	}
 
+	/** Sets the alpha component of all text currently in the cache. Does not affect subsequently added text. */
+	public void setAlphas (float alpha) {
+		int alphaBits = ((int)(254 * alpha)) << 24;
+		float prev = 0, newColor = 0;
+		for (int j = 0, length = vertexData.length; j < length; j++) {
+			float[] vertices = vertexData[j];
+			for (int i = 2, n = idx[j]; i < n; i += 5) {
+				float c = vertices[i];
+				if (c == prev && i != 2) {
+					vertices[i] = newColor;
+				} else {
+					prev = c;
+					int rgba = NumberUtils.floatToIntColor(c);
+					rgba = (rgba & 0x00FFFFFF) | alphaBits;
+					newColor = NumberUtils.intToFloatColor(rgba);
+					vertices[i] = newColor;
+				}
+			}
+		}
+	}
+
 	/** Sets the color of all text currently in the cache. Does not affect subsequently added text. */
 	public void setColors (float color) {
 		for (int j = 0, length = vertexData.length; j < length; j++) {
