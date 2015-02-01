a96d5f48a78e45c2c34c3a4427d28fd6938bd172
Thorsten Schleinzer <git@schleinzer.com>
2015-02-01 10:18:27 +0100

    - Removed another unnecessary "toLowerCase" call. - Made the heavy check for lost window handle only being called once per second, not on each frame

--- extensions/gdx-controllers/gdx-controllers-desktop/src/com/badlogic/gdx/controllers/desktop/OisControllers.java
+++ extensions/gdx-controllers/gdx-controllers-desktop/src/com/badlogic/gdx/controllers/desktop/OisControllers.java
@@ -16,6 +16,13 @@
 
 package com.badlogic.gdx.controllers.desktop;
 
+import java.awt.Component;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import javax.swing.SwingUtilities;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics.GraphicsType;
 import com.badlogic.gdx.controllers.ControlType;
@@ -30,21 +37,16 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
-import java.awt.Component;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-
-import javax.swing.SwingUtilities;
-
 /** @author Nathan Sweet */
 public class OisControllers {
 	final DesktopControllerManager manager;
 	long hwnd = getWindowHandle();
 	Ois ois = new Ois(hwnd);
 	OisController[] controllers;
-	
+
 	private static final boolean IS_MAC = System.getProperty("os.name").toLowerCase().contains("mac");
+	private static final boolean IS_WINDOWS = System.getProperty("os.name").toLowerCase().contains("windows");
+	private static final long CHECK_FOR_LOST_WINDOW_HANDLE_INTERVAL = 1000000000L; // in nanoseconds. 1000000000 == 1 second
 
 	public OisControllers (final DesktopControllerManager manager) {
 		this.manager = manager;
@@ -57,19 +59,28 @@ public class OisControllers {
 		}
 
 		new Runnable() {
+
+			private long lastCheckForLostWindowHandleTime;
+
 			public void run () {
-				long newWindowHandle = getWindowHandle();
-				if(hwnd != newWindowHandle){
-					hwnd = newWindowHandle;
-					ois = new Ois(newWindowHandle);
-					
-					ArrayList<OisJoystick> joysticks = ois.getJoysticks();
-					controllers = new OisController[joysticks.size()];
-					manager.controllers.clear();
-					for (int i = 0, n = joysticks.size(); i < n; i++) {
-						OisJoystick joystick = joysticks.get(i);
-						controllers[i] = new OisController(joystick);
-						manager.controllers.add(controllers[i]);
+				// we won't do the rather heavy check for a lost window handle each and every frame, but rather each second only
+				long now = System.nanoTime();
+				if (now - lastCheckForLostWindowHandleTime > CHECK_FOR_LOST_WINDOW_HANDLE_INTERVAL) {
+					lastCheckForLostWindowHandleTime = now;
+
+					long newWindowHandle = getWindowHandle();
+					if (hwnd != newWindowHandle) {
+						hwnd = newWindowHandle;
+						ois = new Ois(newWindowHandle);
+
+						ArrayList<OisJoystick> joysticks = ois.getJoysticks();
+						controllers = new OisController[joysticks.size()];
+						manager.controllers.clear();
+						for (int i = 0, n = joysticks.size(); i < n; i++) {
+							OisJoystick joystick = joysticks.get(i);
+							controllers[i] = new OisController(joystick);
+							manager.controllers.add(controllers[i]);
+						}
 					}
 				}
 				ois.update();
@@ -88,51 +99,63 @@ public class OisControllers {
 			joystick.setListener(new OisListener() {
 				public void buttonReleased (OisJoystick joystick, int buttonIndex) {
 					Array<ControllerListener> allListeners = manager.listeners;
-					for (int ii = 0, nn = allListeners.size; ii < nn; ii++)
+					for (int ii = 0, nn = allListeners.size; ii < nn; ii++) {
 						allListeners.get(ii).buttonUp(OisController.this, buttonIndex);
-					for (int ii = 0, nn = listeners.size; ii < nn; ii++)
+					}
+					for (int ii = 0, nn = listeners.size; ii < nn; ii++) {
 						listeners.get(ii).buttonUp(OisController.this, buttonIndex);
+					}
 				}
 
 				public void buttonPressed (OisJoystick joystick, int buttonIndex) {
 					Array<ControllerListener> allListeners = manager.listeners;
-					for (int ii = 0, nn = allListeners.size; ii < nn; ii++)
+					for (int ii = 0, nn = allListeners.size; ii < nn; ii++) {
 						allListeners.get(ii).buttonDown(OisController.this, buttonIndex);
-					for (int ii = 0, nn = listeners.size; ii < nn; ii++)
+					}
+					for (int ii = 0, nn = listeners.size; ii < nn; ii++) {
 						listeners.get(ii).buttonDown(OisController.this, buttonIndex);
+					}
 				}
 
 				public void axisMoved (OisJoystick joystick, int axisIndex, float value) {
 					Array<ControllerListener> allListeners = manager.listeners;
-					for (int ii = 0, nn = allListeners.size; ii < nn; ii++)
+					for (int ii = 0, nn = allListeners.size; ii < nn; ii++) {
 						allListeners.get(ii).axisMoved(OisController.this, axisIndex, value);
-					for (int ii = 0, nn = listeners.size; ii < nn; ii++)
+					}
+					for (int ii = 0, nn = listeners.size; ii < nn; ii++) {
 						listeners.get(ii).axisMoved(OisController.this, axisIndex, value);
+					}
 				}
 
 				public void povMoved (OisJoystick joystick, int povIndex, OisPov ignored) {
 					PovDirection value = getPov(povIndex);
 					Array<ControllerListener> allListeners = manager.listeners;
-					for (int ii = 0, nn = allListeners.size; ii < nn; ii++)
+					for (int ii = 0, nn = allListeners.size; ii < nn; ii++) {
 						allListeners.get(ii).povMoved(OisController.this, povIndex, value);
-					for (int ii = 0, nn = listeners.size; ii < nn; ii++)
+					}
+					for (int ii = 0, nn = listeners.size; ii < nn; ii++) {
 						listeners.get(ii).povMoved(OisController.this, povIndex, value);
+					}
 				}
 
 				public void xSliderMoved (OisJoystick joystick, int sliderIndex, boolean value) {
 					Array<ControllerListener> allListeners = manager.listeners;
-					for (int ii = 0, nn = allListeners.size; ii < nn; ii++)
+					for (int ii = 0, nn = allListeners.size; ii < nn; ii++) {
 						allListeners.get(ii).xSliderMoved(OisController.this, sliderIndex, value);
-					for (int ii = 0, nn = listeners.size; ii < nn; ii++)
+					}
+					for (int ii = 0, nn = listeners.size; ii < nn; ii++) {
 						listeners.get(ii).xSliderMoved(OisController.this, sliderIndex, value);
+					}
 				}
 
 				public void ySliderMoved (OisJoystick joystick, int sliderIndex, boolean value) {
 					Array<ControllerListener> allListeners = manager.listeners;
-					for (int ii = 0, nn = allListeners.size; ii < nn; ii++)
+					for (int ii = 0, nn = allListeners.size; ii < nn; ii++) {
 						allListeners.get(ii).ySliderMoved(OisController.this, sliderIndex, value);
-					for (int ii = 0, nn = listeners.size; ii < nn; ii++)
+					}
+					for (int ii = 0, nn = listeners.size; ii < nn; ii++) {
 						listeners.get(ii).ySliderMoved(OisController.this, sliderIndex, value);
+					}
 				}
 			});
 		}
@@ -211,6 +234,7 @@ public class OisControllers {
 			return joystick.getName();
 		}
 
+		@Override
 		public String toString () {
 			return joystick.getName();
 		}
@@ -224,8 +248,9 @@ public class OisControllers {
 		}
 
 		try {
-			if (Gdx.graphics.getType() == GraphicsType.JGLFW)
+			if (Gdx.graphics.getType() == GraphicsType.JGLFW) {
 				return (Long)Gdx.graphics.getClass().getDeclaredMethod("getWindow").invoke(null);
+			}
 
 			if (Gdx.graphics.getType() == GraphicsType.LWJGL) {
 				if (Gdx.app.getClass().getName().equals("com.badlogic.gdx.backends.lwjgl.LwjglCanvas")) {
@@ -238,8 +263,7 @@ public class OisControllers {
 				Method getImplementation = displayClass.getDeclaredMethod("getImplementation", new Class[0]);
 				getImplementation.setAccessible(true);
 				Object display = getImplementation.invoke(null, (Object[])null);
-				String fieldName = System.getProperty("os.name").toLowerCase().contains("windows") ? "hwnd" : "parent_window";
-				Field field = display.getClass().getDeclaredField(fieldName);
+				Field field = display.getClass().getDeclaredField(IS_WINDOWS ? "hwnd" : "parent_window");
 				field.setAccessible(true);
 				return (Long)field.get(display);
 			}
@@ -251,8 +275,11 @@ public class OisControllers {
 	}
 
 	static private Object invokeMethod (Object object, String methodName) throws Exception {
-		for (Method m : object.getClass().getMethods())
-			if (m.getName().equals(methodName)) return m.invoke(object);
+		for (Method m : object.getClass().getMethods()) {
+			if (m.getName().equals(methodName)) {
+				return m.invoke(object);
+			}
+		}
 		throw new RuntimeException("Could not find method '" + methodName + "' on class: " + object.getClass());
 	}
 
