40fc5f68317eddc0158875c89d2ad416b3d79ec0
TomSki <tomaszwojciechowski@hotmail.co.uk>
2015-02-03 11:50:39 +0000

    AlSource may be null, resolve #2790

--- backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSSound.java
+++ backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSSound.java
@@ -64,6 +64,7 @@ public class IOSSound implements Sound {
 	public long play (float volume, float pitch, float pan, boolean loop) {
 		if (streamIds.size == 8) streamIds.pop();
 		ALSource soundSource = OALSimpleAudio.sharedInstance().playEffect(soundPath, volume, pitch, pan, loop);
+		if (soundSource == null) return -1;
 		if (soundSource.getSourceId() == -1) return -1;
 		streamIds.insert(0, soundSource.getSourceId());
 		return soundSource.getSourceId();
