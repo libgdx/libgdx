7f5d0d728a23d70790330833f525149a23692085
Daniel Holderbaum <danielholderbaum@gmail.com>
2015-02-14 15:32:38 +0100

    Fixed a NPE in HttpResponse.getResultAsString().

--- gdx/src/com/badlogic/gdx/net/NetJavaImpl.java
+++ gdx/src/com/badlogic/gdx/net/NetJavaImpl.java
@@ -71,6 +71,12 @@ public class NetJavaImpl {
 		@Override
 		public String getResultAsString () {
 			InputStream input = getInputStream();
+
+			// If the response does not contain any content, input will be null.
+			if (input == null) {
+				return "";
+			}
+
 			try {
 				return StreamUtils.copyStreamToString(input, connection.getContentLength());
 			} catch (IOException e) {
