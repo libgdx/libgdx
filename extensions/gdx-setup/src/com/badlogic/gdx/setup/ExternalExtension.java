512de78331ae1d85af8a787efc8d9fd436b5ed9d
TomSki <tomaszwojciechowski@hotmail.co.uk>
2015-02-14 09:26:54 +0000

    Support classifiers

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/ExternalExtension.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/ExternalExtension.java
@@ -52,7 +52,13 @@ public class ExternalExtension {
 		} else {
 			String[] arr = new String[dependencies.get(platformName).size()];
 			for (int i = 0; i < dependencies.get(platformName).size(); i++) {
-				arr[i] = dependencies.get(platformName).get(i) + ":" + version;
+				String dependencyString = dependencies.get(platformName).get(i);
+				if (dependencyString.split(":").length == 3) {
+					String[] split = dependencyString.split(":");
+					arr[i] = split[0] + ":" + split[1] + ":" + version + ":" + split[2];
+				} else {
+					arr[i] = dependencyString + ":" + version;
+				}
 			}
 			return arr;
 		}
