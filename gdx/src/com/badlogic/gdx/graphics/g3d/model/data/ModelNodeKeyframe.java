562c236a101d56832d9f4bb0ccf688a39eb9cfa9
Xoppa <contact@xoppa.com>
2015-02-15 19:04:08 +0100

    Separate NodeAnimation keyframes for each component. This allows a more flexible animation (e.g. the time of the keyframes for rotation doesn't have to match those of translation) This removes unneeded lerping (typically only rotation or translation is animated, its very rare to have all three components animated in the same keyframes) This prepares for g3dx version 0.2, which will have the components separated as well as custom interpolation (and perhaps tangents as well) per component

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelNodeKeyframe.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelNodeKeyframe.java
@@ -16,16 +16,9 @@
 
 package com.badlogic.gdx.graphics.g3d.model.data;
 
-import com.badlogic.gdx.math.Quaternion;
-import com.badlogic.gdx.math.Vector3;
-
-public class ModelNodeKeyframe {
+public class ModelNodeKeyframe<T> {
 	/** the timestamp of the keyframe in seconds **/
 	public float keytime;
-	/** the translation, in local space relative to the parent **/
-	public Vector3 translation;
-	/** the scale, in local space relative to the parent **/
-	public Vector3 scale;
-	/** the rotation, in local space relative to the parent **/
-	public Quaternion rotation;
+	/** the value of the keyframe */
+	public T value = null;
 }
