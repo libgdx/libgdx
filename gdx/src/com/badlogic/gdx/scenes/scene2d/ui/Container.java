eda451641fcea0210d8f099154314423e98baf79
NathanSweet <nathan.sweet@gmail.com>
2015-03-04 19:25:06 +0100

    Do nothing if setActor is called with same actor.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Container.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Container.java
@@ -153,6 +153,7 @@ public class Container<T extends Actor> extends WidgetGroup {
 	/** @param actor May be null. */
 	public void setActor (T actor) {
 		if (actor == this) throw new IllegalArgumentException("actor cannot be the Container.");
+		if (actor == this.actor) return;
 		if (this.actor != null) super.removeActor(this.actor);
 		this.actor = actor;
 		if (actor != null) super.addActor(actor);
