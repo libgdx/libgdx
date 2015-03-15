bc883c42ba600ed0695ecf85c8e5b4d425a846e0
Rob Bogie <bogie.rob@gmail.com>
2015-03-15 13:40:43 +0100

    Made it possible to automatically load native libraries for the arm architecture on linux.

--- extensions/gdx-jnigen/src/com/badlogic/gdx/jnigen/SharedLibraryFinder.java
+++ extensions/gdx-jnigen/src/com/badlogic/gdx/jnigen/SharedLibraryFinder.java
@@ -18,7 +18,7 @@ public interface SharedLibraryFinder {
 	 * @param nativesJar A ZipFile object, which gives the ability to walk through the containing files, and allows for pattern
 	 *           matching. May be null if no zipfile is used.
 	 * @return The name of the shared file, or null if none available */
-	String getSharedLibraryNameLinux (String sharedLibName, boolean is64Bit, ZipFile nativesJar);
+	String getSharedLibraryNameLinux (String sharedLibName, boolean is64Bit, boolean isArm, ZipFile nativesJar);
 
 	/** @param sharedLibName The name of the shared lib that is asked to be loaded.
 	 * @param is64Bit 
