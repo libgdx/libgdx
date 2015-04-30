d0121ac8d0c0693f3914cd2d65122ec8e91a1a8d
NathanSweet <nathan.sweet@gmail.com>
2015-04-30 20:27:50 +0200

    Fixed pref height for label font scale.
    closes #3073

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
@@ -227,7 +227,7 @@ public class Label extends Widget {
 
 	public float getPrefHeight () {
 		if (prefSizeInvalid) scaleAndComputePrefSize();
-		float height = prefSize.y - style.font.getDescent() * 2;
+		float height = prefSize.y - style.font.getDescent() * fontScaleY * 2;
 		Drawable background = style.background;
 		if (background != null) height += background.getTopHeight() + background.getBottomHeight();
 		return height;
