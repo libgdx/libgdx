355ce2ea2070f7dd3749154307e98fa71a39af23
Baraa Hamodi <bhamodi@uwaterloo.ca>
2015-05-10 00:49:16 -0700

    Update default Android API level and build tools to Android 5.0.

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -1,6 +1,5 @@
 package com.badlogic.gdx.setup;
 
-
 import java.util.HashMap;
 
 public class DependencyBank {
@@ -10,8 +9,8 @@ public class DependencyBank {
 	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
 	static String libgdxNightlyVersion = "1.6.1-SNAPSHOT";
 	static String roboVMVersion = "1.2.0";
-	static String buildToolsVersion = "20.0.0";
-	static String androidAPILevel = "20";
+	static String buildToolsVersion = "21.1.2";
+	static String androidAPILevel = "21";
 	static String gwtVersion = "2.6.0";
 
 	//Repositories
