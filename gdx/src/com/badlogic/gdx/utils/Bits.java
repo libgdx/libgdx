a34275f97bd9d28c5ea1a5d09e23f219b316775f
Antag99 <antag99@gmail.com>
2015-05-22 16:24:03 +0200

    Fix Bits.xor bug

--- gdx/src/com/badlogic/gdx/utils/Bits.java
+++ gdx/src/com/badlogic/gdx/utils/Bits.java
@@ -260,11 +260,7 @@ public class Bits {
 			bits[i] ^= other.bits[i];
 		}
 		
-		if (bits.length > commonWords) {
-			for (int i = other.bits.length, s = bits.length; s > i; i++) {
-				bits[i] = 0L;
-			}
-		} else if (commonWords < other.bits.length) {
+		if (commonWords < other.bits.length) {
 			checkCapacity(other.bits.length);
 			for (int i = commonWords, s = other.bits.length; s > i; i++) {
 				bits[i] = other.bits[i];
