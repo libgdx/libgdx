a92115869c1f335cd375dd5601484bbd17da7e62
NathanSweet <nathan.sweet@gmail.com>
2015-06-25 17:09:33 +0200

    Formatting.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/ParallelAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/ParallelAction.java
@@ -69,8 +69,7 @@ public class ParallelAction extends Action {
 			Array<Action> actions = this.actions;
 			for (int i = 0, n = actions.size; i < n && actor != null; i++) {
 				Action currentAction = actions.get(i);
-				if (currentAction.getActor()!=null && !currentAction.act(delta))
-					complete = false;
+				if (currentAction.getActor() != null && !currentAction.act(delta)) complete = false;
 				if (actor == null) return true; // This action was removed.
 			}
 			return complete;
