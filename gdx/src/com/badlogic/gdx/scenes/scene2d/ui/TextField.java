67c475175269e3d75ddd59e9146bead35a8f8827
NathanSweet <nathan.sweet@gmail.com>
2015-07-01 02:25:20 +0200

    Fixed when change event is fired.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
@@ -577,6 +577,7 @@ public class TextField extends Widget implements Disableable {
 	 * @return True if the text was changed. */
 	boolean changeText (String oldText, String newText) {
 		if (newText.equals(oldText)) return false;
+		text = newText;
 		ChangeEvent changeEvent = Pools.obtain(ChangeEvent.class);
 		boolean cancelled = fire(changeEvent);
 		text = cancelled ? oldText : newText;
