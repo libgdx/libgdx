aa805a4e81f29e303611a20f0f0711b43685cf5a
davebaol <davix7@yahoo.it>
2015-07-14 16:12:49 +0200

    Javadoc typos

--- gdx/src/com/badlogic/gdx/utils/reflect/ClassReflection.java
+++ gdx/src/com/badlogic/gdx/utils/reflect/ClassReflection.java
@@ -209,7 +209,7 @@ public final class ClassReflection {
 		return result;
 	}
 
-	/** Returns an {@link Annotation} object reflecting the annotation provided, or null of this class doesn't have such an
+	/** Returns an {@link Annotation} object reflecting the annotation provided, or null if this class doesn't have such an
 	 * annotation. This is a convenience function if the caller knows already which annotation type he's looking for. */
 	static public Annotation getAnnotation (Class c, Class<? extends java.lang.annotation.Annotation> annotationType) {
 		java.lang.annotation.Annotation annotation = c.getAnnotation(annotationType);
@@ -228,7 +228,7 @@ public final class ClassReflection {
 		return result;
 	}
 
-	/** Returns an {@link Annotation} object reflecting the annotation provided, or null of this class doesn't have such an
+	/** Returns an {@link Annotation} object reflecting the annotation provided, or null if this class doesn't have such an
 	 * annotation. This is a convenience function if the caller knows already which annotation type he's looking for. */
 	static public Annotation getDeclaredAnnotation (Class c, Class<? extends java.lang.annotation.Annotation> annotationType) {
 		java.lang.annotation.Annotation[] annotations = c.getDeclaredAnnotations();
