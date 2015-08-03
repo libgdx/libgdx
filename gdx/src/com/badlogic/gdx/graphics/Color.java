18269fbd1143a6d78cb6abe4a5b6cf2751ec1c6c
NathanSweet <nathan.sweet@gmail.com>
2015-08-03 02:10:00 +0200

    Added and organized colors.
    I don't want to go crazy and list every color, just wanted a couple options for each basic color.

--- gdx/src/com/badlogic/gdx/graphics/Color.java
+++ gdx/src/com/badlogic/gdx/graphics/Color.java
@@ -24,24 +24,45 @@ import com.badlogic.gdx.utils.NumberUtils;
  * @author mzechner */
 public class Color {
 	public static final Color CLEAR = new Color(0, 0, 0, 0);
-	public static final Color WHITE = new Color(1, 1, 1, 1);
 	public static final Color BLACK = new Color(0, 0, 0, 1);
-	public static final Color RED = new Color(1, 0, 0, 1);
-	public static final Color GREEN = new Color(0, 1, 0, 1);
+
+	public static final Color WHITE = new Color(0xffffffff);
+	public static final Color LIGHT_GRAY = new Color(0x696969ff);
+	public static final Color GRAY = new Color(0x7f7f7fff);
+	public static final Color DARK_GRAY = new Color(0x3f3f3fff);
+	public static final Color SLATE = new Color(0x708090ff);
+
 	public static final Color BLUE = new Color(0, 0, 1, 1);
-	public static final Color LIGHT_GRAY = new Color(0.75f, 0.75f, 0.75f, 1);
-	public static final Color GRAY = new Color(0.5f, 0.5f, 0.5f, 1);
-	public static final Color DARK_GRAY = new Color(0.25f, 0.25f, 0.25f, 1);
-	public static final Color PINK = new Color(1, 0.68f, 0.68f, 1);
-	public static final Color ORANGE = new Color(1, 0.78f, 0, 1);
-	public static final Color YELLOW = new Color(1, 1, 0, 1);
-	public static final Color MAGENTA = new Color(1, 0, 1, 1);
-	public static final Color CYAN = new Color(0, 1, 1, 1);
-	public static final Color OLIVE = new Color(0.5f, 0.5f, 0, 1);
-	public static final Color PURPLE = new Color(0.5f, 0, 0.5f, 1);
-	public static final Color MAROON = new Color(0.5f, 0, 0, 1);
-	public static final Color TEAL = new Color(0, 0.5f, 0.5f, 1);
 	public static final Color NAVY = new Color(0, 0, 0.5f, 1);
+	public static final Color ROYAL = new Color(0x4169e1ff);
+	public static final Color SKY = new Color(0x87ceebff);
+	public static final Color CYAN = new Color(0, 1, 1, 1);
+	public static final Color TEAL = new Color(0, 0.5f, 0.5f, 1);
+
+	public static final Color GREEN = new Color(0x00ff00ff);
+	public static final Color CHARTREUSE = new Color(0x7fff00ff);
+	public static final Color LIME = new Color(0x32cd32ff);
+	public static final Color FOREST = new Color(0x228b22ff);
+	public static final Color OLIVE = new Color(0x6b8e23ff);
+
+	public static final Color YELLOW = new Color(0xffff00ff);
+	public static final Color GOLD = new Color(0xffd700ff);
+	public static final Color GOLDENROD = new Color(0xdaa520ff);
+
+	public static final Color BROWN = new Color(0x8b4513ff);
+	public static final Color TAN = new Color(0xd2b48cff);
+	public static final Color FIREBRICK = new Color(0xb22222ff);
+
+	public static final Color RED = new Color(0xff0000ff);
+	public static final Color CORAL = new Color(0xff7f50ff);
+	public static final Color ORANGE = new Color(0xffa500ff);
+	public static final Color SALMON = new Color(0xfa8072ff);
+	public static final Color PINK = new Color(0xff69b4ff);
+	public static final Color MAGENTA = new Color(1, 0, 1, 1);
+
+	public static final Color PURPLE = new Color(0xa020f0ff);
+	public static final Color VIOLET = new Color(0xee82eeff);
+	public static final Color MAROON = new Color(0xb03060ff);
 
 	/** the red, green, blue and alpha components **/
 	public float r, g, b, a;
@@ -444,12 +465,12 @@ public class Color {
 		color.a = ((value & 0x000000ff)) / 255f;
 	}
 
-	/** Sets the Color components using the specified integer value in the format ARGB8888. This is the inverse to the argb8888(a, r,
-     * g, b) method
+	/** Sets the Color components using the specified integer value in the format ARGB8888. This is the inverse to the argb8888(a,
+	 * r, g, b) method
 	 *
 	 * @param color The Color to be modified.
 	 * @param value An integer color value in ARGB8888 format. */
-	public static void argb8888ToColor(Color color, int value) {
+	public static void argb8888ToColor (Color color, int value) {
 		color.a = ((value & 0xff000000) >>> 24) / 255f;
 		color.r = ((value & 0x00ff0000) >>> 16) / 255f;
 		color.g = ((value & 0x0000ff00) >>> 8) / 255f;
