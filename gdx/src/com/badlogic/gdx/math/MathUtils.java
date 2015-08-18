edc94ae50e2b47f344bac4089fd6090709d52faf
NathanSweet <nathan.sweet@gmail.com>
2015-08-19 00:32:21 +0200

    Javadoc

--- gdx/src/com/badlogic/gdx/math/MathUtils.java
+++ gdx/src/com/badlogic/gdx/math/MathUtils.java
@@ -80,22 +80,22 @@ public final class MathUtils {
 
 	// ---
 
-	/** Returns atan2 in radians, faster but less accurate than Math.atan2. Average error of 0.00231, largest error of 0.00488. */
+	/** Returns atan2 in radians, faster but less accurate than Math.atan2. Average error of 0.00231 radians (0.1323 degrees),
+	 * largest error of 0.00488 radians (0.2796 degrees). */
 	static public float atan2 (float y, float x) {
-		if (x == 0.0f) {
-			if (y > 0.0f) return PI / 2;
-			if (y == 0.0f) return 0.0f;
+		if (x == 0f) {
+			if (y > 0f) return PI / 2;
+			if (y == 0f) return 0f;
 			return -PI / 2;
 		}
-		final float atan;
-		final float z = y / x;
-		if (Math.abs(z) < 1.0f) {
-			atan = z / (1.0f + 0.28f * z * z);
-			if (x < 0.0f) return y < 0.0f ? atan - PI : atan + PI;
+		final float atan, z = y / x;
+		if (Math.abs(z) < 1f) {
+			atan = z / (1f + 0.28f * z * z);
+			if (x < 0f) return y < 0f ? atan - PI : atan + PI;
 			return atan;
 		}
 		atan = PI / 2 - z / (z * z + 0.28f);
-		return y < 0.0f ? atan - PI : atan;
+		return y < 0f ? atan - PI : atan;
 	}
 
 	// ---
