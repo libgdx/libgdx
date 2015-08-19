ed79aab101e03a03f2e5eabd2ce7a568e27f88c8
JS <realitix@gmail.com>
2015-08-19 17:11:32 +0200

    Update FrustumLightFilter
    Transform the frustum into BoundigBox until uses the points

--- gdx/src/com/badlogic/gdx/graphics/g3d/shadow/filter/FrustumLightFilter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shadow/filter/FrustumLightFilter.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g3d.Scene;
 import com.badlogic.gdx.graphics.g3d.environment.BaseLight;
 import com.badlogic.gdx.math.Frustum;
+import com.badlogic.gdx.math.collision.BoundingBox;
 
 public class FrustumLightFilter implements LightFilter {
 
 	protected Scene scene;
+	protected BoundingBox bb = new BoundingBox();
 
 	public FrustumLightFilter(Scene scene) {
 		this.scene = scene;
@@ -17,14 +19,16 @@ public class FrustumLightFilter implements LightFilter {
 	public boolean filter (int n, BaseLight light, Camera camera) {
 		Frustum f1 = scene.getCamera().frustum;
 		Frustum f2 = camera.frustum;
+		bb.inf();
 
-		boolean result = false;
 		for(int i = 0; i < f2.planePoints.length; i++) {
-			if(f1.pointInFrustum(f2.planePoints[i])) {
-				result = true;
-			}
+			bb.ext(f2.planePoints[i]);
 		}
 
-		return result;
+		if(f1.boundsInFrustum(bb)) {
+			return true;
+		}
+
+		return false;
 	}
 }
