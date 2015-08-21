9a58eef97f03cd683844f45d9e04475691a1eeac
badlogic <badlogicgames@gmail.com>
2015-08-21 10:37:00 +0200

    Updated versions for relesae

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -6,10 +6,10 @@ import java.util.HashMap;
 public class DependencyBank {
 
 	//Versions
-	static String libgdxVersion = "1.6.4";
+	static String libgdxVersion = "1.6.5";
 	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
-	static String libgdxNightlyVersion = "1.6.5-SNAPSHOT";
-	static String roboVMVersion = "1.5.0";
+	static String libgdxNightlyVersion = "1.6.6-SNAPSHOT";
+	static String roboVMVersion = "1.6.0";
 	static String buildToolsVersion = "20.0.0";
 	static String androidAPILevel = "20";
 	static String gwtVersion = "2.6.0";
