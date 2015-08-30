0bc16c1aba2908db1b40bf980fa42005f6e07c0c
NathanSweet <nathan.sweet@gmail.com>
2015-08-31 00:56:42 +0200

    Fixed resizing model window.
    Must have been broken for a while!
    
    closes #3341

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -95,7 +95,7 @@ public class Window extends Table {
 					int border = resizeBorder;
 					float width = getWidth(), height = getHeight();
 					edge = 0;
-					if (isResizable) {
+					if (isResizable && x >= 0 && x < width && y >= 0 && y < height) {
 						if (x < border) edge |= Align.left;
 						if (x > width - border) edge |= Align.right;
 						if (y < border) edge |= Align.bottom;
