eb173d1561dcad21e7fef544e81c964869b4d0c6
davebaol <davix7@yahoo.it>
2015-09-06 01:44:17 +0200

    Fixed misplaced javadoc and formatted code

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/utils/TimeUtils.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/utils/TimeUtils.java
@@ -19,10 +19,10 @@ package com.badlogic.gdx.utils;
 /** Wrapper around System.nanoTime() and System.currentTimeMillis(). Use this if you want to be compatible across all platforms!
  * @author mzechner */
 public final class TimeUtils {
-	/** @return The current value of the system timer, in nanoseconds. */
 
 	private static final long nanosPerMilli = 1000000;
 
+	/** @return The current value of the system timer, in nanoseconds. */
 	public static long nanoTime () {
 		return System.currentTimeMillis() * nanosPerMilli;
 	}
@@ -35,32 +35,28 @@ public final class TimeUtils {
 	/** Convert nanoseconds time to milliseconds
 	 * @param nanos must be nanoseconds
 	 * @return time value in milliseconds */
-	public static long nanosToMillis(long nanos) {
+	public static long nanosToMillis (long nanos) {
 		return nanos / nanosPerMilli;
 	}
 
 	/** Convert milliseconds time to nanoseconds
 	 * @param millis must be milliseconds
 	 * @return time value in nanoseconds */
-	public static long millisToNanos(long millis) {
+	public static long millisToNanos (long millis) {
 		return millis * nanosPerMilli;
 	}
 
-	/**
-	 * Get the time in nanos passed since a previous time
-	 * @param prevTime - must be nanoseconds
-	 * @return - time passed since prevTime in nanoseconds
-	 */
-	public static long timeSinceNanos(long prevTime) {
+	/** Get the time in nanos passed since a previous time
+	 * @param prevTime must be nanoseconds
+	 * @return time passed since prevTime in nanoseconds */
+	public static long timeSinceNanos (long prevTime) {
 		return nanoTime() - prevTime;
 	}
 
-	/**
-	 * Get the time in millis passed since a previous time
-	 * @param prevTime - must be milliseconds
-	 * @return - time passed since prevTime in milliseconds
-	 */
-	public static long timeSinceMillis(long prevTime) {
+	/** Get the time in millis passed since a previous time
+	 * @param prevTime must be milliseconds
+	 * @return time passed since prevTime in milliseconds */
+	public static long timeSinceMillis (long prevTime) {
 		return millis() - prevTime;
 	}
 }
