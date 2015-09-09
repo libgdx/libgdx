5817af1bc6ebfa713de4e32989bcb54bbd78f77d
Xoppa <contact@xoppa.com>
2015-09-09 21:05:12 +0200

    Add Matrix4#setFromEulerAnglesRad

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/math/Matrix4.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/math/Matrix4.java
@@ -864,6 +864,16 @@ public class Matrix4 implements Serializable {
 		quat.setEulerAngles(yaw, pitch, roll);
 		return set(quat);
 	}
+	
+	/** Sets this matrix to a rotation matrix from the given euler angles.
+	 * @param yaw the yaw in radians
+	 * @param pitch the pitch in radians
+	 * @param roll the roll in radians
+	 * @return This matrix */
+	public Matrix4 setFromEulerAnglesRad (float yaw, float pitch, float roll) {
+		quat.setEulerAnglesRad(yaw, pitch, roll);
+		return set(quat);
+	}
 
 	/** Sets this matrix to a scaling matrix
 	 * 
