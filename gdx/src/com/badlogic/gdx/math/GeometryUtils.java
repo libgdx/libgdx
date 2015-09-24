0a3ed69edd5aaa32c8b9342b13c00bc132733da2
pyros2097 <pyros2097@gmail.com>
2015-09-24 21:22:19 +0530

    dx13 and dy13 are assigned but not used

--- gdx/src/com/badlogic/gdx/math/GeometryUtils.java
+++ gdx/src/com/badlogic/gdx/math/GeometryUtils.java
@@ -98,7 +98,6 @@ public final class GeometryUtils {
 	static public boolean colinear (float x1, float y1, float x2, float y2, float x3, float y3) {
 		float dx21 = x2 - x1, dy21 = y2 - y1;
 		float dx32 = x3 - x2, dy32 = y3 - y2;
-		float dx13 = x1 - x3, dy13 = y1 - y3;
 		float det = dx32 * dy21 - dx21 * dy32;
 		return Math.abs(det) < MathUtils.FLOAT_ROUNDING_ERROR;
 	}
