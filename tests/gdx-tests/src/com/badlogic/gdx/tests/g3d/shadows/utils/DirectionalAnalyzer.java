1b166b4d1334d1d72df4397f6a5426b8c9f75fa9
JS <realitix@gmail.com>
2015-10-02 09:52:29 +0200

    ShadowSystem 1/6
    The shadow system pull request has been splitted in 6 parts.
    This is the first.
    
    1/6 -> Add interfaces.

---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/shadows/utils/DirectionalAnalyzer.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.badlogic.gdx.tests.g3d.shadows.utils;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g3d.environment.BaseLight;
+import com.badlogic.gdx.math.Frustum;
+import com.badlogic.gdx.math.Vector3;
+
+/** Directional Analyzer compute the camera's properties needed by a directional light
+ * @author realitix */
+public interface DirectionalAnalyzer {
+	/** Compute the good orthographicCamera dimension based on the frustum. Be careful, direction must be normalized.
+	 * @param light Current light
+	 * @param frustum Frustum of the main camera
+	 * @param direction Direction of the directional light
+	 * @param out Camera modified ( still needs to call update() )
+	 * @return Camera The camera for chaining */
+	public Camera analyze (BaseLight light, Frustum frustum, Vector3 direction, Camera out);
+}
---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/shadows/utils/NearFarAnalyzer.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.badlogic.gdx.tests.g3d.shadows.utils;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g3d.environment.BaseLight;
+import com.badlogic.gdx.math.Vector2;
+
+/** Nearfar Analyzer compute the near and far plane of a camera
+ * @author realitix */
+public interface NearFarAnalyzer {
+	/** Analyze near and far plane for the camera
+	 * @param light Current light
+	 * @param camera Light's camera
+	 * @return Vector2 x=near, y=far */
+	public Vector2 analyze (BaseLight light, Camera camera);
+}
