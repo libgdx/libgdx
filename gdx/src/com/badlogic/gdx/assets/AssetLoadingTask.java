1c29f642f579d97de6f9f9740647cc786e2ac12f
Xoppa <contact@xoppa.com>
2015-10-05 17:55:17 +0200

    Fixes #3467

--- gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java
+++ gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java
@@ -98,10 +98,10 @@ class AssetLoadingTask implements AsyncTask<Void> {
 			dependenciesLoaded = true;
 			dependencies = syncLoader.getDependencies(assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
 			if (dependencies == null) {
-				removeDuplicates(dependencies);
 				asset = syncLoader.load(manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
 				return;
 			}
+			removeDuplicates(dependencies);
 			manager.injectDependencies(assetDesc.fileName, dependencies);
 		} else {
 			asset = syncLoader.load(manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
