4df3227b849f1e8208edc0f70163eeb84dd84126
Spyce <Sebastianwiendlocha1@web.de>
2015-10-10 16:12:06 +0200

    forgot to import AudioAttributes

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidAudio.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidAudio.java
@@ -27,6 +27,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.media.SoundPool.Builder;
+import android.media.AudioAttributes;
 import android.os.Build;
 
 import com.badlogic.gdx.Audio;
