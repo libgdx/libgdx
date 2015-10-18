4fd2d1ff75f07d8e9c95c1db1ecda9f782322289
Xoppa <contact@xoppa.com>
2015-10-19 01:15:35 +0200

    Introduce #copy and #set methods for Node and NodePart to make it easier to use custom nodes, see #3186 Practically this makes them Cloneable and moves the responsibilty for updating the references to other nodes and materials to the caller. This removes the need for a map to keep track of those.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/NodePart.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/NodePart.java
@@ -66,4 +66,33 @@ public class NodePart {
 		out.bones = bones;
 		return out;
 	}
+
+	public NodePart copy () {
+		return new NodePart().set(this);
+	}
+
+	protected NodePart set (NodePart other) {
+		meshPart = new MeshPart(other.meshPart);
+		material = other.material;
+		enabled = other.enabled;
+		if (other.invBoneBindTransforms == null) {
+			invBoneBindTransforms = null;
+			bones = null;
+		} else {
+			if (invBoneBindTransforms == null)
+				invBoneBindTransforms = new ArrayMap<Node, Matrix4>(true, other.invBoneBindTransforms.size, Node.class, Matrix4.class);
+			else
+				invBoneBindTransforms.clear();
+			invBoneBindTransforms.putAll(other.invBoneBindTransforms);
+			
+			if (bones == null || bones.length != invBoneBindTransforms.size)
+				bones = new Matrix4[invBoneBindTransforms.size];
+			
+			for (int i = 0; i < bones.length; i++) {
+				if (bones[i] == null)
+					bones[i] = new Matrix4();
+			}
+		}
+		return this;
+	}
 }
