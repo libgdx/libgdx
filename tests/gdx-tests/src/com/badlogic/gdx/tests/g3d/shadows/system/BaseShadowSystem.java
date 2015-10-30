1ee472bdf1c423802893c850768fb81523912b70
JS <realitix@gmail.com>
2015-10-30 11:28:57 +0100

    Update BaseShadowSystem.
    Add scissor padding.

--- tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/shadows/system/BaseShadowSystem.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/shadows/system/BaseShadowSystem.java
@@ -388,7 +388,7 @@ public abstract class BaseShadowSystem implements ShadowSystem {
 		TextureRegion region = lp.region;
 		region.setTexture(frameBuffers[currentPass].getColorBufferTexture());
 		Gdx.gl.glViewport(r.x, r.y, r.width, r.height);
-		Gdx.gl.glScissor(r.x, r.y, r.width, r.height);
+		Gdx.gl.glScissor(r.x + 1, r.y + 1, r.width - 2, r.height - 2);
 		region.setRegion(r.x, r.y, r.width, r.height);
 
 		if (cameraViewport) {
