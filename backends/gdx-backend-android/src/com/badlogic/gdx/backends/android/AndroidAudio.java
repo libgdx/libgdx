0173fbf1ae66434812284b17392e236f4019d850
Justin Shapcott <support@mobidevelop.com>
2015-11-03 22:30:47 -0700

    Add method to create a music from FileDescriptor
    Closes #2970

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidAudio.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidAudio.java
@@ -16,6 +16,7 @@
 
 package com.badlogic.gdx.backends.android;
 
+import java.io.FileDescriptor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -135,6 +136,34 @@ public final class AndroidAudio implements Audio {
 
 	}
 
+	/** Creates a new Music instance from the provided FileDescriptor. It is the caller's responsibility to close the file
+	 * descriptor. It is safe to do so as soon as this call returns.
+	 * 
+	 * @param fd the FileDescriptor from which to create the Music
+	 * 
+	 * @see Audio#newMusic(FileHandle)
+	 */
+	public Music newMusic (FileDescriptor fd) {
+		if (soundPool == null) {
+			throw new GdxRuntimeException("Android audio is not enabled by the application config.");
+		}
+		
+		MediaPlayer mediaPlayer = new MediaPlayer();
+
+		try {
+			mediaPlayer.setDataSource(fd);
+			mediaPlayer.prepare();
+
+			AndroidMusic music = new AndroidMusic(this, mediaPlayer);
+			synchronized (musics) {
+				musics.add(music);
+			}
+			return music;
+		} catch (Exception ex) {
+			throw new GdxRuntimeException("Error loading audio from FileDescriptor", ex);
+		}
+	}
+	
 	/** {@inheritDoc} */
 	@Override
 	public Sound newSound (FileHandle file) {
