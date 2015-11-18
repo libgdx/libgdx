e1aa0e56415f76df6de67aaafc4ce3bc2cc4f9c1
NathanSweet <nathan.sweet@gmail.com>
2015-11-18 18:02:00 +0100

    Ensure LWJGL libs are extracted to the same directory.

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglNativesLoader.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglNativesLoader.java
@@ -30,7 +30,7 @@ public final class LwjglNativesLoader {
 	static public boolean load = true;
 
 	static {
-		System.setProperty("org.lwjgl.input.Mouse.allowNegativeMouseCoords", "true");		
+		System.setProperty("org.lwjgl.input.Mouse.allowNegativeMouseCoords", "true");
 
 		// Don't extract natives if using JWS.
 		try {
@@ -54,14 +54,14 @@ public final class LwjglNativesLoader {
 			if (isWindows) {
 				nativesDir = loader.extractFile(is64Bit ? "lwjgl64.dll" : "lwjgl.dll", null).getParentFile();
 				if (!LwjglApplicationConfiguration.disableAudio)
-					loader.extractFile(is64Bit ? "OpenAL64.dll" : "OpenAL32.dll", nativesDir.getName());
+					loader.extractFileTo(is64Bit ? "OpenAL64.dll" : "OpenAL32.dll", nativesDir);
 			} else if (isMac) {
 				nativesDir = loader.extractFile("liblwjgl.dylib", null).getParentFile();
-				if (!LwjglApplicationConfiguration.disableAudio) loader.extractFile("openal.dylib", nativesDir.getName());
+				if (!LwjglApplicationConfiguration.disableAudio) loader.extractFileTo("openal.dylib", nativesDir);
 			} else if (isLinux) {
 				nativesDir = loader.extractFile(is64Bit ? "liblwjgl64.so" : "liblwjgl.so", null).getParentFile();
 				if (!LwjglApplicationConfiguration.disableAudio)
-					loader.extractFile(is64Bit ? "libopenal64.so" : "libopenal.so", nativesDir.getName());
+					loader.extractFileTo(is64Bit ? "libopenal64.so" : "libopenal.so", nativesDir);
 			}
 		} catch (Throwable ex) {
 			throw new GdxRuntimeException("Unable to extract LWJGL natives.", ex);
