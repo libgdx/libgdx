47f03c66486802b0f5ad56ce03324bc1feebc4ee
NathanSweet <nathan.sweet@gmail.com>
2015-11-23 11:21:19 +0100

    extractFileTo has changed to take a directory, not a file.

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglNativesLoader.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglNativesLoader.java
@@ -53,22 +53,15 @@ public final class LwjglNativesLoader {
 		try {
 			if (isWindows) {
 				nativesDir = loader.extractFile(is64Bit ? "lwjgl64.dll" : "lwjgl.dll", null).getParentFile();
-				if (!LwjglApplicationConfiguration.disableAudio) {
-					String windowsOpenALFile = is64Bit ? "OpenAL64.dll" : "OpenAL32.dll";
-					loader.extractFileTo(windowsOpenALFile, new File(nativesDir, windowsOpenALFile));
-				}
+				if (!LwjglApplicationConfiguration.disableAudio)
+					loader.extractFileTo(is64Bit ? "OpenAL64.dll" : "OpenAL32.dll", nativesDir);
 			} else if (isMac) {
 				nativesDir = loader.extractFile("liblwjgl.dylib", null).getParentFile();
-				if (!LwjglApplicationConfiguration.disableAudio) {
-					String macOpenALFile = "openal.dylib";
-					loader.extractFileTo(macOpenALFile, new File(nativesDir, macOpenALFile));
-				}
+				if (!LwjglApplicationConfiguration.disableAudio) loader.extractFileTo("openal.dylib", nativesDir);
 			} else if (isLinux) {
 				nativesDir = loader.extractFile(is64Bit ? "liblwjgl64.so" : "liblwjgl.so", null).getParentFile();
-				if (!LwjglApplicationConfiguration.disableAudio) {
-					String linuxOpenALFile = is64Bit ? "libopenal64.so" : "libopenal.so";
-					loader.extractFileTo(linuxOpenALFile, new File(nativesDir, linuxOpenALFile));
-				}
+				if (!LwjglApplicationConfiguration.disableAudio)
+					loader.extractFileTo(is64Bit ? "libopenal64.so" : "libopenal.so", nativesDir);
 			}
 		} catch (Throwable ex) {
 			throw new GdxRuntimeException("Unable to extract LWJGL natives.", ex);
