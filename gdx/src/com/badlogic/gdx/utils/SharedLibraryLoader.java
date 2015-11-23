7156d7b9b7417792dc70348b46ab330a5b0e14cf
NathanSweet <nathan.sweet@gmail.com>
2015-11-23 11:18:13 +0100

    Fixed extractFileTo.

--- gdx/src/com/badlogic/gdx/utils/SharedLibraryLoader.java
+++ gdx/src/com/badlogic/gdx/utils/SharedLibraryLoader.java
@@ -166,9 +166,9 @@ public class SharedLibraryLoader {
 	/** Extracts the specified file into the temp directory if it does not already exist or the CRC does not match. If file
 	 * extraction fails and the file exists at java.library.path, that file is returned.
 	 * @param sourcePath The file to extract from the classpath or JAR.
-	 * @param extractedFile The location where the extracted file will be written. */
-	public void extractFileTo (String sourcePath, File extractedFile) throws IOException {
-		extractFile(sourcePath, crc(readFile(sourcePath)), extractedFile);
+	 * @param dir The location where the extracted file will be written. */
+	public void extractFileTo (String sourcePath, File dir) throws IOException {
+		extractFile(sourcePath, crc(readFile(sourcePath)), new File(dir, new File(sourcePath).getName()));
 	}
 
 	/** Returns a path to a file that can be written. Tries multiple locations and verifies writing succeeds.
