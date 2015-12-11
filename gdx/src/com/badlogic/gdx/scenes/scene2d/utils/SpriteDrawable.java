055abd9fe6df4e1c5b23273f0294f7ae8644c7e9
NathanSweet <nathan.sweet@gmail.com>
2015-12-11 04:11:50 +0100

    Better SpriteDrawable tinting.
    #3627

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
@@ -43,17 +43,21 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
 		draw(batch, x, y, width / 2f, height / 2f, width, height, 1f, 1f, 0f);
 	}
 
-	private static Color tmpColor = new Color();
 	public void draw (Batch batch, float x, float y, float originX, float originY, float width, float height, float scaleX,
 		float scaleY, float rotation) {
+
+		Color spriteColor = sprite.getColor();
+		float batchColor = batch.getPackedColor();
+		sprite.setColor(batch.getColor().mul(spriteColor));
+
 		sprite.setOrigin(originX, originY);
 		sprite.setRotation(rotation);
 		sprite.setScale(scaleX, scaleY);
 		sprite.setBounds(x, y, width, height);
-		Color color = sprite.getColor();
-		sprite.setColor(tmpColor.set(color).mul(batch.getColor()));
 		sprite.draw(batch);
-		sprite.setColor(color);
+
+		sprite.setColor(spriteColor);
+		batch.setColor(batchColor);
 	}
 
 	public void setSprite (Sprite sprite) {
