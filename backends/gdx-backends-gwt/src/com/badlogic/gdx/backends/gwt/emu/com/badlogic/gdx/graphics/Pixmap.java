dc79e3665837d2f4c9dc3a1b55e3cd71dc581a89
Justin Shapcott <support@mobidevelop.com>
2015-12-21 15:22:10 -0700

    Make fill Pixmap#fill emulation behave like core
    Closes #3660

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/graphics/Pixmap.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/graphics/Pixmap.java
@@ -229,6 +229,7 @@ public class Pixmap implements Disposable {
 
 	/** Fills the complete bitmap with the currently set color. */
 	public void fill () {
+		context.clearRect(0, 0, getWidth(), getHeight());
 		rectangle(0, 0, getWidth(), getHeight(), DrawType.FILL);
 	}
 
