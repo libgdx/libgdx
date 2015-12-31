ccb085942528866d753a2c1efd0e45814409a789
B4dT0bi <B4dT0bi@users.noreply.github.com>
2015-12-31 15:39:34 +0100

    Create AmazonFireGameController.java

---
+++ extensions/gdx-controllers/gdx-controllers/src/com/badlogic/gdx/controllers/mappings/AmazonFireGameController.java
@@ -0,0 +1,33 @@
+package com.badlogic.gdx.controllers.mappings;
+
+import com.badlogic.gdx.controllers.Controller;
+
+/**
+ * Button and axis indices for the Amazon Fire Game Controller {@link Controller}.
+ *
+ * @author Tobias Boese
+ */
+public class AmazonFireGameController {
+    public static final String ID = "Amazon Fire Game Controller";
+    public static final int BUTTON_A = 96;
+    public static final int BUTTON_B = 97;
+    public static final int BUTTON_Y = 100;
+    public static final int BUTTON_X = 99;
+    public static final int BUTTON_MENU = 82;
+    public static final int BUTTON_BACK = 4;
+    public static final int BUTTON_NEXT = 90;
+    public static final int BUTTON_PREV = 89;
+    public static final int BUTTON_PLAY_PAUSE = 85;
+    public static final int BUTTON_R1 = 89;
+    public static final int BUTTON_L1 = 102;
+
+    public static final int AXIS_TRIGGER_R2 = 4;
+    public static final int AXIS_TRIGGER_L2 = 5;
+    public static final int AXIS_DPAD_Y = 6;
+    public static final int AXIS_DPAD_X = 7;
+
+    public static final int AXIS_LEFT_STICK_Y = 0;
+    public static final int AXIS_LEFT_STICK_X = 1;
+    public static final int AXIS_RIGHT_STICK_Y = 3;
+    public static final int AXIS_RIGHT_STICK_X = 2;
+}
