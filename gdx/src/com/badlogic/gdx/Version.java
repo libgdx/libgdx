2c1bd52223df5005c22e499d78fbc095b8e34f6e
badlogic <badlogicgames@gmail.com>
2016-01-24 10:52:35 +0100

    Updated Version for next development iteration

--- gdx/src/com/badlogic/gdx/Version.java
+++ gdx/src/com/badlogic/gdx/Version.java
@@ -23,7 +23,7 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
  * @author mzechner */
 public class Version {
 	/** the current version of libgdx as a String in the major.minor.revision format **/
-	public static final String VERSION = "1.9.0";
+	public static final String VERSION = "1.9.1";
 
 	/** the current major version of libgdx **/
 	public static final int MAJOR;
