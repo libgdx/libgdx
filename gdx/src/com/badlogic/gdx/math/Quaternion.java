6e71dd4fd5db289306bf0b3336939c6e379f2c54
Xoppa <contact@xoppa.com>
2016-01-30 20:07:36 +0100

    Fixes #3808

--- gdx/src/com/badlogic/gdx/math/Quaternion.java
+++ gdx/src/com/badlogic/gdx/math/Quaternion.java
@@ -302,10 +302,10 @@ public class Quaternion implements Serializable {
 	 * @param w the w component of the other quaternion to multiply with
 	 * @return This quaternion for chaining */
 	public Quaternion mulLeft (final float x, final float y, final float z, final float w) {
-		final float newX = w * this.x + x * this.w + y * this.z - z * y;
-		final float newY = w * this.y + y * this.w + z * this.x - x * z;
-		final float newZ = w * this.z + z * this.w + x * this.y - y * x;
-		final float newW = w * this.w - x * this.x - y * this.y - z * z;
+		final float newX = w * this.x + x * this.w + y * this.z - z * this.y;
+		final float newY = w * this.y + y * this.w + z * this.x - x * this.z;
+		final float newZ = w * this.z + z * this.w + x * this.y - y * this.x;
+		final float newW = w * this.w - x * this.x - y * this.y - z * this.z;
 		this.x = newX;
 		this.y = newY;
 		this.z = newZ;
