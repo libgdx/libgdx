fb56dc38249a7d37fef5c732418616b18bbda1e9
J Fixby <github@jfixby.com>
2016-03-08 11:53:52 +0100

    Licensed under the Unlicense license

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/etc1/ETC1AtlasCompressorSettings.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/etc1/ETC1AtlasCompressorSettings.java
@@ -1,4 +1,22 @@
+/*******************************************************************************
+ * Licensed under the Unlicense license (the "License");
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this file, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * You may obtain a copy of the License at
+ * 
+ *   http://unlicense.org
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
 
+ ******************************************************************************/
 package com.badlogic.gdx.tools.etc1;
 
 public class ETC1AtlasCompressorSettings {
