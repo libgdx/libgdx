a34a08b1e31a7fe9b2b2c949a3a134cf0994fbb9
J Fixby <github@jfixby.com>
2016-03-08 15:48:55 +0100

    set license Apache License, Version 2.0

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/etc1/ETC1AtlasCompressor.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/etc1/ETC1AtlasCompressor.java
@@ -1,21 +1,17 @@
 /*******************************************************************************
- * Licensed under the Unlicense license (the "License");
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this file, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *   http://unlicense.org
+ *   http://www.apache.org/licenses/LICENSE-2.0
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
-
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 
 package com.badlogic.gdx.tools.etc1;
