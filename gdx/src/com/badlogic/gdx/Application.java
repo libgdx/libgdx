1c0589efb78505f77ed95c29f8b23bcf58bbe1ce
Jakub Kerber <kerber.jg@gmail.com>
2016-05-12 15:57:42 +0200

    Cleaned up ApplicationType#parseDesktopOS()

--- gdx/src/com/badlogic/gdx/Application.java
+++ gdx/src/com/badlogic/gdx/Application.java
@@ -109,12 +109,12 @@ public interface Application {
 		Windows, Linux, OSX, Android, iOS, BlackBerry10;
 
 		public static SystemType parseDesktopOS () {
-			String os = java.lang.System.getProperty("os.name").toLowerCase();
-			if (os.indexOf("win") >= 0)
+			String os = System.getProperty("os.name").toLowerCase();
+			if (os.contains("win"))
 				return Windows;
-			else if (os.indexOf("mac") >= 0)
+			else if (os.contains("mac"))
 				return OSX;
-			else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0)
+			else if (os.contains("nix") || os.contains("nux") || os.contains("aix"))
 				return Linux;
 			else
 				return null;
