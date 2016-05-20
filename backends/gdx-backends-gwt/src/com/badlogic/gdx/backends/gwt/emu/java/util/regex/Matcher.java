cc9d51eb4ac34b8066f4b18c03aa1259115f6f91
Vince <vinceangel3d@gmail.com>
2016-05-20 15:46:30 +0200

    Add useful method

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/java/util/regex/Matcher.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/java/util/regex/Matcher.java
@@ -44,3 +44,17 @@ public class Matcher {
 		return matchResult.getGroup(group);
 	}
 }
+
+    public static String quoteReplacement(String s) {
+        if ((s.indexOf('\\') == -1) && (s.indexOf('$') == -1))
+            return s;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '\\' || c == '$') {
+                sb.append('\\');
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
