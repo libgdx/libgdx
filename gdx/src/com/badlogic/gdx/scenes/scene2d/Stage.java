a6fd7f73788cfda82a7f869a1c793e8fc98bfaaa
NathanSweet <nathan.sweet@gmail.com>
2016-06-13 03:13:35 +0200

    Ensure any focus event is always balanced with an unfocus event.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
@@ -592,16 +592,16 @@ public class Stage extends InputAdapter implements Disposable {
 
 	/** Removes the touch, keyboard, and scroll focused actors. */
 	public void unfocusAll () {
-		scrollFocus = null;
-		keyboardFocus = null;
+		setScrollFocus(null);
+		setKeyboardFocus(null);
 		cancelTouchFocus();
 	}
 
 	/** Removes the touch, keyboard, and scroll focus for the specified actor and any descendants. */
 	public void unfocus (Actor actor) {
 		cancelTouchFocus(actor);
-		if (scrollFocus != null && scrollFocus.isDescendantOf(actor)) scrollFocus = null;
-		if (keyboardFocus != null && keyboardFocus.isDescendantOf(actor)) keyboardFocus = null;
+		if (scrollFocus != null && scrollFocus.isDescendantOf(actor)) setScrollFocus(null);
+		if (keyboardFocus != null && keyboardFocus.isDescendantOf(actor)) setKeyboardFocus(null);
 	}
 
 	/** Sets the actor that will receive key events.
