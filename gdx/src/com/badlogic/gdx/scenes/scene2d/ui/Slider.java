5f3b3408c57625da82b97bf75b3bb675955e46fa
NathanSweet <nathan.sweet@gmail.com>
2016-10-14 11:33:39 +0200

    Made Slider#snap protected.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Slider.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Slider.java
@@ -154,7 +154,7 @@ public class Slider extends ProgressBar {
 	}
 
 	/** Returns a snapped value. */
-	private float snap (float value) {
+	protected float snap (float value) {
 		if (snapValues == null) return value;
 		for (int i = 0; i < snapValues.length; i++) {
 			if (Math.abs(value - snapValues[i]) <= threshold) return snapValues[i];
