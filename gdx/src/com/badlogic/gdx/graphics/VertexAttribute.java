3e532c93b020e690fefab5c4070ca245d18a3e03
Cypherdare <contact@cyphercove.com>
2016-11-02 13:22:00 -0400

    VertexArray short support

--- gdx/src/com/badlogic/gdx/graphics/VertexAttribute.java
+++ gdx/src/com/badlogic/gdx/graphics/VertexAttribute.java
@@ -70,7 +70,9 @@ public final class VertexAttribute {
 	 * 
 	 * @param usage The attribute {@link Usage}, used for identification.
 	 * @param numComponents the number of components of this attribute, must be between 1 and 4.
-	 * @param type the OpenGL type of each component, e.g. {@link GL20#GL_FLOAT} or {@link GL20#GL_UNSIGNED_BYTE}
+	 * @param type the OpenGL type of each component, e.g. {@link GL20#GL_FLOAT} or {@link GL20#GL_UNSIGNED_BYTE}. Since {@link Mesh}
+	 * stores vertex data in 32bit floats, the total size of this attribute (type size times number of components) must be a 
+	 * multiple of four.
 	 * @param normalized For fixed types, whether the values are normalized to either -1f and +1f (signed) or 0f and +1f (unsigned) 
 	 * @param alias The alias used in a shader for this attribute. Can be changed after construction. */
 	public VertexAttribute (int usage, int numComponents, int type, boolean normalized, String alias) {
@@ -81,7 +83,9 @@ public final class VertexAttribute {
 	 * 
 	 * @param usage The attribute {@link Usage}, used for identification.
 	 * @param numComponents the number of components of this attribute, must be between 1 and 4.
-	 * @param type the OpenGL type of each component, e.g. {@link GL20#GL_FLOAT} or {@link GL20#GL_UNSIGNED_BYTE}
+	 * @param type the OpenGL type of each component, e.g. {@link GL20#GL_FLOAT} or {@link GL20#GL_UNSIGNED_BYTE}. Since {@link Mesh}
+	 * stores vertex data in 32bit floats, the total size of this attribute (type size times number of components) must be a 
+	 * multiple of four bytes.
 	 * @param normalized For fixed types, whether the values are normalized to either -1f and +1f (signed) or 0f and +1f (unsigned) 
 	 * @param alias The alias used in a shader for this attribute. Can be changed after construction.
 	 * @param unit Optional unit/index specifier, used for texture coordinates and bone weights */
