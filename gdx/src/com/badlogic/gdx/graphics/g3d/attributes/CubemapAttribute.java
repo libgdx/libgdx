2d5585b428c4c0a10bbc5138a9fa25935da5bf16
Daniel Ludwig <codi@code-disaster.com>
2016-11-09 17:54:52 +0100

    Fix use of cube map alias in render attributes and test shader. Fix various bugs in FrameBufferCubemapTest.

--- gdx/src/com/badlogic/gdx/graphics/g3d/attributes/CubemapAttribute.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/attributes/CubemapAttribute.java
@@ -22,7 +22,7 @@ import com.badlogic.gdx.graphics.g3d.utils.TextureDescriptor;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
 public class CubemapAttribute extends Attribute {
-	public final static String EnvironmentMapAlias = "environmentMapTexture";
+	public final static String EnvironmentMapAlias = "environmentCubemap";
 	public final static long EnvironmentMap = register(EnvironmentMapAlias);
 
 	protected static long Mask = EnvironmentMap;
@@ -46,7 +46,7 @@ public class CubemapAttribute extends Attribute {
 
 	public CubemapAttribute (final long type, final Cubemap texture) {
 		this(type);
-		textureDescription.texture = texture;
+		textureDescription.set(texture, texture.getMinFilter(), texture.getMagFilter(), texture.getUWrap(), texture.getVWrap());
 	}
 
 	public CubemapAttribute (final CubemapAttribute copyFrom) {
