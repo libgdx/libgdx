e8867d922155f747807a70f5aa34d2944d61ebb8
Darren Keese <dakeese@gmail.com>
2016-11-11 22:06:50 -0500

    Fix ProgressBar rounding and allow it to be disabled.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Slider.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Slider.java
@@ -34,8 +34,8 @@ import com.badlogic.gdx.utils.Pools;
  * <p>
  * {@link ChangeEvent} is fired when the slider knob is moved. Canceling the event will move the knob to where it was previously.
  * <p>
- * The preferred height of a slider is determined by the larger of the knob and background. The preferred width of a slider is
- * 140, a relatively arbitrary size.
+ * For a horizontal progress bar, its preferred height is determined by the larger of the knob and background, and the preferred width
+ * is 140, a relatively arbitrary size. These parameters are reversed for a vertical progress bar.
  * @author mzechner
  * @author Nathan Sweet */
 public class Slider extends ProgressBar {
@@ -53,7 +53,7 @@ public class Slider extends ProgressBar {
 		this(min, max, stepSize, vertical, skin.get(styleName, SliderStyle.class));
 	}
 
-	/** Creates a new slider. It's width is determined by the given prefWidth parameter, its height is determined by the maximum of
+	/** Creates a new slider. If horizontal, its width is determined by the prefWidth parameter, its height is determined by the maximum of
 	 * the height of either the slider {@link NinePatch} or slider handle {@link TextureRegion}. The min and max values determine
 	 * the range the values of this slider can take on, the stepSize parameter specifies the distance between individual values.
 	 * E.g. min could be 4, max could be 10 and stepSize could be 0.2, giving you a total of 30 values, 4.0 4.2, 4.4 and so on.
