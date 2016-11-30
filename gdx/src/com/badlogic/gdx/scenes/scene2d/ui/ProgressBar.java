26d6841914ff217211fb3d16ceec4a9e0409f46e
cypherdare <dakeese@gmail.com>
2016-11-30 10:00:41 -0500

    Fix #4446 (ProgressBar knobAfter)
    Fixes #4446

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ProgressBar.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ProgressBar.java
@@ -165,10 +165,10 @@ public class ProgressBar extends Widget implements Disableable {
 			if (knobAfter != null) {
 				if (round)
 					knobAfter.draw(batch, Math.round(x + (width - knobAfter.getMinWidth()) * 0.5f), Math.round(y + position + knobHeightHalf),
-						Math.round(knobAfter.getMinWidth()), Math.round(height - position + knobHeightHalf));
+						Math.round(knobAfter.getMinWidth()), Math.round(height - position - knobHeightHalf));
 				else
 					knobAfter.draw(batch, x + (width - knobAfter.getMinWidth()) * 0.5f, y + position + knobHeightHalf,
-						knobAfter.getMinWidth(), height - position + knobHeightHalf);
+						knobAfter.getMinWidth(), height - position - knobHeightHalf);
 			}
 			if (knob != null) {
 				if (round) 
@@ -216,10 +216,10 @@ public class ProgressBar extends Widget implements Disableable {
 			if (knobAfter != null) {
 				if (round)
 					knobAfter.draw(batch, Math.round(x + position + knobWidthHalf), Math.round(y + (height - knobAfter.getMinHeight()) * 0.5f),
-						Math.round(width - position + knobWidthHalf), Math.round(knobAfter.getMinHeight()));
+						Math.round(width - position - knobWidthHalf), Math.round(knobAfter.getMinHeight()));
 				else
 					knobAfter.draw(batch, x + position + knobWidthHalf, y + (height - knobAfter.getMinHeight()) * 0.5f,
-						width - position + knobWidthHalf, knobAfter.getMinHeight());
+						width - position - knobWidthHalf, knobAfter.getMinHeight());
 			}
 			if (knob != null) {
 				if (round)
