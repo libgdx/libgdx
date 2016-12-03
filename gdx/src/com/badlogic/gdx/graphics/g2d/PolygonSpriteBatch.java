28118b8d19166c2e078ff295dae6cbfc910dd0cf
cypherdare <dakeese@gmail.com>
2016-12-02 22:26:30 -0500

    Fix PolygonSpriteBatch error message

--- gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
@@ -123,7 +123,7 @@ public class PolygonSpriteBatch implements Batch {
 	public PolygonSpriteBatch (int maxVertices, int maxTriangles, ShaderProgram defaultShader) {
 		// 32767 is max vertex index.
 		if (maxVertices > 32767)
-			throw new IllegalArgumentException("Can't have more than 32767 vertices per batch: " + maxTriangles);
+			throw new IllegalArgumentException("Can't have more than 32767 vertices per batch: " + maxVertices);
 
 		Mesh.VertexDataType vertexDataType = Mesh.VertexDataType.VertexArray;
 		if (Gdx.gl30 != null) {
