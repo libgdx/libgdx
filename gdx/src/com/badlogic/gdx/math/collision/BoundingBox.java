825954dd03b75744dc66902d5068be6f4b909bab
Jakub Kerber <kerber.jg@gmail.com>
2016-12-07 11:30:16 +0100

    Updated the comment to reflect the changes

--- gdx/src/com/badlogic/gdx/math/collision/BoundingBox.java
+++ gdx/src/com/badlogic/gdx/math/collision/BoundingBox.java
@@ -207,7 +207,7 @@ public class BoundingBox implements Serializable {
 		return this.set(min.set(0, 0, 0), max.set(0, 0, 0));
 	}
 
-	/** Returns whether this bounding box is valid. This means that {@link #max} is greater than or equal to {@link #min}, with {@link #dim} greater than zero.
+	/** Returns whether this bounding box is valid. This means that {@link #max} is greater than or equal to {@link #min}.
 	 * @return True in case the bounding box is valid, false otherwise */
 	public boolean isValid () {
 		return min.x <= max.x && min.y <= max.y && min.z <= max.z;
