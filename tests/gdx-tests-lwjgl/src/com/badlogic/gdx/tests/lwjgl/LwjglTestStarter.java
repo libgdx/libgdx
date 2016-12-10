c65aa5f247995bbe71bc97adfc919efa0e4683a1
sarge <sausagefactory0@gmail.com>
2016-12-10 10:14:43 +0200

    necessary changes for running the test

--- tests/gdx-tests-lwjgl/src/com/badlogic/gdx/tests/lwjgl/LwjglTestStarter.java
+++ tests/gdx-tests-lwjgl/src/com/badlogic/gdx/tests/lwjgl/LwjglTestStarter.java
@@ -62,7 +62,7 @@ public class LwjglTestStarter extends JFrame {
 	 * @return {@code true} if the test was found and run, {@code false} otherwise
 	 */
 	public static boolean runTest (String testName) {
-		boolean useGL30 = false;
+		boolean useGL30 = true;
 		GdxTest test = GdxTests.newTest(testName);
 		if (test == null) {
 			return false;
@@ -72,10 +72,11 @@ public class LwjglTestStarter extends JFrame {
 		config.height = 480;
 		config.title = testName;
 		config.forceExit = false;
+		config.useHDPI = true;
 		if (useGL30) {
 			config.useGL30 = true;
-			ShaderProgram.prependVertexCode = "#version 140\n#define varying out\n#define attribute in\n";
-			ShaderProgram.prependFragmentCode = "#version 140\n#define varying in\n#define texture2D texture\n#define gl_FragColor fragColor\nout vec4 fragColor;\n";
+//			ShaderProgram.prependVertexCode = "#version 140\n#define varying out\n#define attribute in\n";
+//			ShaderProgram.prependFragmentCode = "#version 140\n#define varying in\n#define texture2D texture\n#define gl_FragColor fragColor\nout vec4 fragColor;\n";
 		} else {
 			config.useGL30 = false;
 			ShaderProgram.prependVertexCode = "";
