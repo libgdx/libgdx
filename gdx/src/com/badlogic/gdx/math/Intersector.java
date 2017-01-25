7414c565d009f9d27094cce3252f508c3721d7b1
Daniel Debert <ghandhikus@users.noreply.github.com>
2017-01-25 09:53:20 +0000

    raw overlaps to avoid unnecessary object creation

--- gdx/src/com/badlogic/gdx/math/Intersector.java
+++ gdx/src/com/badlogic/gdx/math/Intersector.java
@@ -887,6 +887,10 @@ public final class Intersector {
 		return r1.overlaps(r2);
 	}
 
+	public static boolean overlaps(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h) {
+ 		return r1x < r2x + r2w && r1x + r1w > r2x && r1y < r2y + r2h && r1y + r1h > r2y;
+ 	}
+
 	public static boolean overlaps (Circle c, Rectangle r) {
 		float closestX = c.x;
 		float closestY = c.y;
