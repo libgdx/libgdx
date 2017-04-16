48e2fb7fdb3470e36f2ee711f4d19b3f3446f68d
nevabyte <Carl@Carls-MacBook-Pro.local>
2017-04-16 16:15:41 +0100

    Added setX and setY methods with alignment argument
    Found myself looking for these methods so decided to create them

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Actor.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Actor.java
@@ -408,12 +408,42 @@ public class Actor {
 		}
 	}
 
+	/** Sets the x position using the specified {@link Align alignment}. Note this may set the position to non-integer
+	 * coordinates. */
+	public void setX (float x, int alignment) {
+
+		if ((alignment & right) != 0)
+			x -= width;
+		else if ((alignment & left) == 0) //
+			x -= width / 2;
+
+		if (this.x != x) {
+			this.x = x;
+			positionChanged();
+		}
+	}
+
 	/** Returns the Y position of the actor's bottom edge. */
 	public float getY () {
 		return y;
 	}
 
 	public void setY (float y) {
+		if (this.y != y) {
+			this.y = y;
+			positionChanged();
+		}
+	}
+
+	/** Sets the y position using the specified {@link Align alignment}. Note this may set the position to non-integer
+	 * coordinates. */
+	public void setY (float y, int alignment) {
+
+		if ((alignment & top) != 0)
+			y -= height;
+		else if ((alignment & bottom) == 0) //
+			y -= height / 2;
+
 		if (this.y != y) {
 			this.y = y;
 			positionChanged();
