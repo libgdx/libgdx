29a7329a2350b034cf79a6575b5da3338363e301
NathanSweet <nathan.sweet@gmail.com>
2017-05-29 00:10:05 +0200

    Paste files as string.
    Sorry LWJGL3 backend, you don't get this unless GLFW does it.

--- backends/gdx-backend-jglfw/src/com/badlogic/gdx/backends/jglfw/JglfwClipboard.java
+++ backends/gdx-backend-jglfw/src/com/badlogic/gdx/backends/jglfw/JglfwClipboard.java
@@ -23,30 +23,53 @@ import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
+import java.io.File;
+import java.util.List;
 
 /** Clipboard implementation for desktop that uses the system clipboard via the default AWT {@link Toolkit}.
  * @author mzechner */
 public class JglfwClipboard implements Clipboard, ClipboardOwner {
+	@Override
 	public String getContents () {
 		try {
 			java.awt.datatransfer.Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 			Transferable contents = clipboard.getContents(null);
-			if (contents != null && contents.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-				return (String)contents.getTransferData(DataFlavor.stringFlavor);
+			if (contents != null) {
+				if (contents.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+					try {
+						return (String)contents.getTransferData(DataFlavor.stringFlavor);
+					} catch (Exception ex) {
+					}
+				}
+				if (contents.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+					try {
+						List<File> files = (List)contents.getTransferData(DataFlavor.javaFileListFlavor);
+						StringBuilder buffer = new StringBuilder(128);
+						for (int i = 0, n = files.size(); i < n; i++) {
+							if (buffer.length() > 0) buffer.append('\n');
+							buffer.append(files.get(i).toString());
+						}
+						return buffer.toString();
+					} catch (RuntimeException ex) {
+					}
+				}
 			}
 		} catch (Exception ignored) { // Ignore JDK crashes sorting data flavors.
 		}
 		return "";
 	}
 
+	@Override
 	public void setContents (String content) {
 		try {
+			StringSelection stringSelection = new StringSelection(content);
 			java.awt.datatransfer.Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-			clipboard.setContents(new StringSelection(content), this);
+			clipboard.setContents(stringSelection, this);
 		} catch (Exception ignored) { // Ignore JDK crashes sorting data flavors.
 		}
 	}
 
-	public void lostOwnership (java.awt.datatransfer.Clipboard clipboard, Transferable transferable) {
+	@Override
+	public void lostOwnership (java.awt.datatransfer.Clipboard arg0, Transferable arg1) {
 	}
 }
