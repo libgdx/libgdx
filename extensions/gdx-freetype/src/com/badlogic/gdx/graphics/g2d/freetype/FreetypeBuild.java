32d470c4e3edba4b399ae18bdcb9e09592e3cc14
badlogic <badlogicgames@gmail.com>
2017-06-07 12:05:40 +0200

    Fixed up native builds, specifically iOS

--- extensions/gdx-freetype/src/com/badlogic/gdx/graphics/g2d/freetype/FreetypeBuild.java
+++ extensions/gdx-freetype/src/com/badlogic/gdx/graphics/g2d/freetype/FreetypeBuild.java
@@ -127,7 +127,7 @@ public class FreetypeBuild {
 		ios.cFlags += " -DFT2_BUILD_LIBRARY";
 		ios.cppFlags += " -DFT2_BUILD_LIBRARY";
 
-		new NativeCodeGenerator().generate();
+		new NativeCodeGenerator().generate("src", "bin:../../gdx/bin", "jni");
 		new AntScriptGenerator()
 			.generate(new BuildConfig("gdx-freetype"), win32home, win32, win64, lin32, lin64, mac, mac64, android, ios);
 // BuildExecutor.executeAnt("jni/build-windows32home.xml", "-v clean");
