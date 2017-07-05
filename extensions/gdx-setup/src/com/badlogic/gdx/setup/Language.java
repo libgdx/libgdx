1ecaa66a513abcc4944efc0b65dd4ae1df33b664
Wasin Thonkaew <haxpor@gmail.com>
2017-07-06 03:16:10 +0700

    Fix ios-moe of cannot find class in DexList
    In main build.gradle, it needs to have the following line inside
    "ios-moe" target as well.
    
    apply plugin: "kotlin"
    
    if it's not there, then an error of IOSMoeLauncher class cannot be found
    in DexList when running the application will be shown.

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/Language.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/Language.java
@@ -1,19 +1,21 @@
 package com.badlogic.gdx.setup;
 
 public enum Language {
-	JAVA ("java", "", "", "", "java;java;android;java,robovm;moe;gwt,war", true) ,
+	JAVA ("java", "", "", "", "java;java;android;java,robovm;moe;gwt,war", "_specifics/java", ".java", true) ,
 	KOTLIN ("kotlin", "ext.kotlinVersion = '1.1.3'", 
 			"classpath \"org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion\"", 
 			"compile \"org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion\"",
-			"kotlin;kotlin;android,kotlin-android;kotlin,robovm;moe; ",
+			"kotlin;kotlin;android,kotlin-android;kotlin,robovm;moe,kotlin; ",
+			"_specifics/kotlin",
+			".kt",
 			false);
 	
 	public final String[] corePlugins, desktopPlugins, androidPlugins, iosPlugins, iosmoePlugins, htmlPlugins;
 	public final String[][] platformPlugins;
-	public final String name, buildScript, buildScriptDependencies, dependencies;
+	public final String name, buildScript, buildScriptDependencies, dependencies, resourcePath, fileExtension;
 	public final boolean gwtSupported;
 	
-	private Language(String name, String buildScript, String buildScriptDependencies, String dependencies, String src, boolean gwtSupported) {
+	private Language(String name, String buildScript, String buildScriptDependencies, String dependencies, String src, String resourcePath, String fileExtension, boolean gwtSupported) {
 		this.name = name;
 		this.buildScript = buildScript;
 		this.buildScriptDependencies = buildScriptDependencies;
@@ -29,6 +31,8 @@ public enum Language {
 		this.iosPlugins = parts[3].split(",");
 		this.iosmoePlugins = parts[4].split(",");
 		this.htmlPlugins = parts[5].split(",");
+		this.resourcePath = resourcePath;
+		this.fileExtension = fileExtension;
 		this.gwtSupported = gwtSupported;
 	}
 }
