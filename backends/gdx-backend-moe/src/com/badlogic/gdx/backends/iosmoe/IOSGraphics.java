dafd49a672711f8a137d2d385bde6b4f04b00419
Roland Vigh <roland.vigh@migeran.com>
2017-07-24 05:26:07 +0200

    Added IOSGLKView application support (#4810)
    * Added IOSGLKView application support
    
    * Formated code with libgdx formatter

--- backends/gdx-backend-moe/src/com/badlogic/gdx/backends/iosmoe/IOSGraphics.java
+++ backends/gdx-backend-moe/src/com/badlogic/gdx/backends/iosmoe/IOSGraphics.java
@@ -16,6 +16,11 @@
 
 package com.badlogic.gdx.backends.iosmoe;
 
+import org.moe.natj.general.NatJ;
+import org.moe.natj.general.Pointer;
+import org.moe.natj.general.ann.ByValue;
+import org.moe.natj.objc.ann.Selector;
+
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
@@ -30,10 +35,7 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.glutils.GLVersion;
 import com.badlogic.gdx.utils.Array;
-import org.moe.natj.general.NatJ;
-import org.moe.natj.general.Pointer;
-import org.moe.natj.general.ann.ByValue;
-import org.moe.natj.objc.ann.Selector;
+
 import apple.NSObject;
 import apple.coregraphics.struct.CGPoint;
 import apple.coregraphics.struct.CGRect;
@@ -98,7 +100,10 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 		super(peer);
 	}
 
-	public IOSGraphics init (float scale, IOSApplication app, IOSApplicationConfiguration config, IOSInput input, boolean useGLES30) {
+	public IOSGraphics init (float scale, IOSApplication app, IOSApplicationConfiguration config, IOSInput input,
+		boolean useGLES30, IOSGLKView view) {
+		this.view = view;
+
 		init();
 		this.config = config;
 
@@ -108,7 +113,8 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 		height = (int)bounds.size().height();
 
 		if (useGLES30) {
-			context = EAGLContext.alloc().initWithAPI(EAGLRenderingAPI.GLES3);;
+			context = EAGLContext.alloc().initWithAPI(EAGLRenderingAPI.GLES3);
+			;
 			if (context != null)
 				gl20 = gl30 = new IOSGLES30();
 			else
@@ -120,7 +126,8 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 			gl30 = null;
 		}
 
-		view = IOSGLKView.alloc().init(this, new CGRect(new CGPoint(0, 0), new CGSize(bounds.size().width(), bounds.size().height())), context);
+		view.setContext(context);
+		view.setGraphics(this);
 
 		view.setDelegate(this);
 		view.setDrawableColorFormat(config.colorFormat);
@@ -163,8 +170,7 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 
 		String machineString = HWMachine.getMachineString();
 		IOSDevice device = IOSDevice.getDevice(machineString);
-		 if (device == null)
-		 	app.error(tag, "Machine ID: " + machineString + " not found, please report to LibGDX");
+		if (device == null) app.error(tag, "Machine ID: " + machineString + " not found, please report to LibGDX");
 		int ppi = device != null ? device.ppi : 163;
 		density = device != null ? device.ppi / 160f : scale;
 		ppiX = ppi;
@@ -181,9 +187,16 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 		return this;
 	}
 
+	public IOSGraphics init (float scale, IOSApplication app, IOSApplicationConfiguration config, IOSInput input,
+		boolean useGLES30) {
+		CGRect bounds = app.getBounds();
+		IOSGLKView view = IOSGLKView.alloc()
+			.init(new CGRect(new CGPoint(0, 0), new CGSize(bounds.size().width(), bounds.size().height())));
+		return init(scale, app, config, input, useGLES30, view);
+	}
+
 	public void resume () {
-		if (!appPaused)
-			return;
+		if (!appPaused) return;
 		appPaused = false;
 
 		Array<LifecycleListener> listeners = app.lifecycleListeners;
@@ -196,8 +209,7 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 	}
 
 	public void pause () {
-		if (appPaused)
-			return;
+		if (appPaused) return;
 		appPaused = true;
 
 		Array<LifecycleListener> listeners = app.lifecycleListeners;
@@ -443,8 +455,7 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 
 	@Override
 	public boolean supportsExtension (String extension) {
-		if (extensions == null)
-			extensions = Gdx.gl.glGetString(GL20.GL_EXTENSIONS);
+		if (extensions == null) extensions = Gdx.gl.glGetString(GL20.GL_EXTENSIONS);
 		return extensions.contains(extension);
 	}
 
@@ -453,8 +464,7 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 		if (isContinuous != this.isContinuous) {
 			this.isContinuous = isContinuous;
 			// start the GLKViewController if we go from non-continuous -> continuous
-			if (isContinuous)
-				viewController.setPaused(false);
+			if (isContinuous) viewController.setPaused(false);
 		}
 	}
 
@@ -468,8 +478,7 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 		isFrameRequested = true;
 		// start the GLKViewController if we are in non-continuous mode
 		// (we should already be started in continuous mode)
-		if (!isContinuous)
-			viewController.setPaused(false);
+		if (!isContinuous) viewController.setPaused(false);
 	}
 
 	@Override
@@ -496,11 +505,11 @@ public class IOSGraphics extends NSObject implements Graphics, GLKViewDelegate, 
 	}
 
 	@Override
-	public void setResizable(boolean resizable) {
+	public void setResizable (boolean resizable) {
 	}
 
 	@Override
-	public void setUndecorated(boolean undecorated) {
+	public void setUndecorated (boolean undecorated) {
 	}
 
 	private class IOSDisplayMode extends DisplayMode {
