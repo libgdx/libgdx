b8a97a5e48b9aeb90b2b8596d56a5c9239886401
Tom-Ski <tomwojciechowski@asidik.com>
2017-10-08 20:53:08 +0100

    Fix mac build

--- extensions/gdx-bullet/jni/src/bullet/BulletSoftBody/btSoftBody.h
+++ extensions/gdx-bullet/jni/src/bullet/BulletSoftBody/btSoftBody.h
@@ -617,7 +617,7 @@ public:
 		RayFromToCaster(const btVector3& rayFrom,const btVector3& rayTo,btScalar mxt);
 		void					Process(const btDbvtNode* leaf);
 
-		static inline btScalar	rayFromToTriangle(const btVector3& rayFrom,
+		static /*inline*/ btScalar	rayFromToTriangle(const btVector3& rayFrom,
 			const btVector3& rayTo,
 			const btVector3& rayNormalizedDirection,
 			const btVector3& a,
