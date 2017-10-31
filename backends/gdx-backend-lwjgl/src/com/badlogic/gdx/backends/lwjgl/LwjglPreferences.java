c09cbf861b26211373638388b7d0d98c97cf5e9a
Hugues Evrard <hevrard@users.noreply.github.com>
2017-10-31 09:24:57 +0000

    Indicate if flush() has succeeded

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglPreferences.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglPreferences.java
@@ -174,16 +174,19 @@ public class LwjglPreferences implements Preferences {
 	}
 
 	@Override
-	public void flush () {
+	public boolean flush () {
+		success = false;
 		OutputStream out = null;
 		try {
 			out = new BufferedOutputStream(file.write(false));
 			properties.storeToXML(out, null);
+			success = true;
 		} catch (Exception ex) {
 			throw new GdxRuntimeException("Error writing preferences: " + file, ex);
 		} finally {
 			StreamUtils.closeQuietly(out);
 		}
+		return success;
 	}
 
 	@Override
