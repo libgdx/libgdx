7163d4c5d0fcfac0ccb89b9c8033ea8cee9ef33e
Patryk Cisek <cisek.patryk389@gmail.com>
2017-12-07 02:40:29 +0100

    invert alignment to correct directions

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Actor.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Actor.java
@@ -413,9 +413,9 @@ public class Actor {
 	 * coordinates. */
 	public void setX (float x, int alignment) {
 
-		if ((alignment & right) != 0)
+		if ((alignment & left) != 0)
 			x -= width;
-		else if ((alignment & left) == 0) //
+		else if ((alignment & right) == 0) //
 			x -= width / 2;
 
 		if (this.x != x) {
@@ -440,9 +440,9 @@ public class Actor {
 	 * coordinates. */
 	public void setY (float y, int alignment) {
 
-		if ((alignment & top) != 0)
+		if ((alignment & bottom) != 0)
 			y -= height;
-		else if ((alignment & bottom) == 0) //
+		else if ((alignment & top) == 0) //
 			y -= height / 2;
 
 		if (this.y != y) {
@@ -473,14 +473,14 @@ public class Actor {
 	/** Sets the position using the specified {@link Align alignment}. Note this may set the position to non-integer
 	 * coordinates. */
 	public void setPosition (float x, float y, int alignment) {
-		if ((alignment & right) != 0)
+		if ((alignment & left) != 0)
 			x -= width;
-		else if ((alignment & left) == 0) //
+		else if ((alignment & right) == 0) //
 			x -= width / 2;
 
-		if ((alignment & top) != 0)
+		if ((alignment & bottom) != 0)
 			y -= height;
-		else if ((alignment & bottom) == 0) //
+		else if ((alignment & top) == 0) //
 			y -= height / 2;
 
 		if (this.x != x || this.y != y) {
