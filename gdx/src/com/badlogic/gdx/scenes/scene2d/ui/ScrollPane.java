03123873b0b11c37ba2d47fd49a62972eea701d0
sarkanyi <sarkanyi@users.noreply.github.com>
2018-03-05 20:31:15 +0200

    Fixes 5103. Items under a ScrollPane's fading scrollbar are not clickable even if the scrollbar is hidden (#5103) (#5107)

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ScrollPane.java
@@ -729,8 +729,8 @@ public class ScrollPane extends WidgetGroup {
 
 	public Actor hit (float x, float y, boolean touchable) {
 		if (x < 0 || x >= getWidth() || y < 0 || y >= getHeight()) return null;
-		if (scrollX && hScrollBounds.contains(x, y)) return this;
-		if (scrollY && vScrollBounds.contains(x, y)) return this;
+		if (scrollX && touchScrollH && hScrollBounds.contains(x, y)) return this;
+		if (scrollY && touchScrollV && vScrollBounds.contains(x, y)) return this;
 		return super.hit(x, y, touchable);
 	}
 
