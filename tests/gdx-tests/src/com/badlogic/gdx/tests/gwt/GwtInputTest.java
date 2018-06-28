4272490477a790ea58beaa12b8cc428e6f30dfae
allen-chin <allchin@ucdavis.edu>
2018-06-28 09:06:32 -0700

    Fix issue #5134 (#5167)
    * Fix issue #5134
    Maintains an IntSet of pressed keys to clear when the application or browser loses focus.
    
    * Fix issue #5134 now applies to Chrome/Edge/IE
    Changed target to window instead of document

--- tests/gdx-tests/src/com/badlogic/gdx/tests/gwt/GwtInputTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/gwt/GwtInputTest.java
@@ -16,7 +16,10 @@
 
 package com.badlogic.gdx.tests.gwt;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -25,10 +28,14 @@ import com.badlogic.gdx.tests.utils.GdxTest;
 
 public class GwtInputTest extends GdxTest {
 	ShapeRenderer renderer;
+	int x = 0;
+	int y = 0;
 
 	@Override
 	public void create () {
 		renderer = new ShapeRenderer();
+		Gdx.input.setInputProcessor(this);
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 	}
 
 	@Override
@@ -41,6 +48,47 @@ public class GwtInputTest extends GdxTest {
 		else
 			renderer.setColor(Color.GREEN);
 		renderer.rect(Gdx.input.getX() - 15, Gdx.graphics.getHeight() - Gdx.input.getY() - 15, 30, 30);
+		renderer.rect(x, y, 30, 30);
 		renderer.end();
+		
+		if (Gdx.input.isKeyPressed(Keys.ALT_LEFT)) {
+			Gdx.app.log("GwtInputTest", "key pressed: " + "ALT_LEFT");
+		}
+		if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)) {
+			Gdx.app.log("GwtInputTest", "key pressed: " + "CTRL_LEFT");
+		}
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= 1;
+		}
+
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += 1;
+		}
+
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			y += 1;
+		}
+
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y -= 1;
+		}
+	}
+
+	@Override
+	public boolean keyDown (int keycode) {
+		Gdx.app.log("GdxInputTest", "key down: " + keycode);
+		return super.keyDown(keycode);
+	}
+
+	@Override
+	public boolean keyTyped (char character) {
+		Gdx.app.log("GdxInputTest", "key typed: '" + character + "'");
+		return false;
+	}
+
+	@Override
+	public boolean keyUp (int keycode) {
+		Gdx.app.log("GdxInputTest", "key up: " + keycode);
+		return false;
 	}
 }
