5ca574c22a7c160f4cbaf20db469b7b8a415304b
Tom-Ski <tomaszwojciechowski@hotmail.co.uk>
2018-08-04 16:17:51 +0100

    extract PolygonSpriteBatch methods to PolyBatch interface

--- gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
@@ -56,7 +56,7 @@ import com.badlogic.gdx.utils.NumberUtils;
  * @author mzechner
  * @author Stefan Bachmann
  * @author Nathan Sweet */
-public class PolygonSpriteBatch implements Batch {
+public class PolygonSpriteBatch implements PolyBatch {
 	private Mesh mesh;
 
 	private final float[] vertices;
@@ -212,7 +212,7 @@ public class PolygonSpriteBatch implements Batch {
 		return color;
 	}
 
-	/** Draws a polygon region with the bottom left corner at x,y having the width and height of the region. */
+	@Override
 	public void draw (PolygonRegion region, float x, float y) {
 		if (!drawing) throw new IllegalStateException("PolygonSpriteBatch.begin must be called before draw.");
 
@@ -250,7 +250,7 @@ public class PolygonSpriteBatch implements Batch {
 		this.vertexIndex = vertexIndex;
 	}
 
-	/** Draws a polygon region with the bottom left corner at x,y and stretching the region to cover the given width and height. */
+	@Override
 	public void draw (PolygonRegion region, float x, float y, float width, float height) {
 		if (!drawing) throw new IllegalStateException("PolygonSpriteBatch.begin must be called before draw.");
 
@@ -291,10 +291,7 @@ public class PolygonSpriteBatch implements Batch {
 		this.vertexIndex = vertexIndex;
 	}
 
-	/** Draws the polygon region with the bottom left corner at x,y and stretching the region to cover the given width and height.
-	 * The polygon region is offset by originX, originY relative to the origin. Scale specifies the scaling factor by which the
-	 * polygon region should be scaled around originX, originY. Rotation specifies the angle of counter clockwise rotation of the
-	 * rectangle around originX, originY. */
+	@Override
 	public void draw (PolygonRegion region, float x, float y, float originX, float originY, float width, float height,
 		float scaleX, float scaleY, float rotation) {
 		if (!drawing) throw new IllegalStateException("PolygonSpriteBatch.begin must be called before draw.");
@@ -344,8 +341,7 @@ public class PolygonSpriteBatch implements Batch {
 		this.vertexIndex = vertexIndex;
 	}
 
-	/** Draws the polygon using the given vertices and triangles. Each vertices must be made up of 5 elements in this order: x, y,
-	 * color, u, v. */
+	@Override
 	public void draw (Texture texture, float[] polygonVertices, int verticesOffset, int verticesCount, short[] polygonTriangles,
 		int trianglesOffset, int trianglesCount) {
 		if (!drawing) throw new IllegalStateException("PolygonSpriteBatch.begin must be called before draw.");
