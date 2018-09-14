2088eb51556bb3d3dbd868eec8950cc4fbcbe7c1
abueide <contact@abueide.com>
2018-09-14 02:25:09 -0700

    Fix some formatting and accidental changes

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/BuildScriptHelper.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/BuildScriptHelper.java
@@ -59,31 +59,30 @@ public class BuildScriptHelper {
 		space(wr);
 	}
 
-	public static void addAllProjects(BufferedWriter wr) throws IOException {
-		 write(wr, "allprojects {");
-		 write(wr, "apply plugin: \"eclipse\"");
-		 write(wr, "apply plugin: \"idea\"");
-		 space(wr);
-		 write(wr, "version = '1.0'");
-		 write(wr, "ext {");
-		 write(wr, "appName = \"%APP_NAME%\"");
-		 write(wr, "gdxVersion = '" + DependencyBank.libgdxVersion + "'");
-		 write(wr, "roboVMVersion = '" + DependencyBank.roboVMVersion + "'");
-		 write(wr, "box2DLightsVersion = '" + DependencyBank.box2DLightsVersion + "'");
-		 write(wr, "ashleyVersion = '" + DependencyBank.ashleyVersion + "'");
-		 write(wr, "aiVersion = '" + DependencyBank.aiVersion + "'");
-		 write(wr, "}");
-		 space(wr);
-		 write(wr, "repositories {");
-		 write(wr, DependencyBank.mavenLocal);
-		 write(wr, DependencyBank.mavenCentral);
-		 write(wr, DependencyBank.google);
-		 write(wr, "maven { url \"" + DependencyBank.libGDXSnapshotsUrl + "\" }");
-		 write(wr, "maven { url \"" + DependencyBank.libGDXReleaseUrl + "\" }");
-		 write(wr, "}");
-		 write(wr, "}");
-
-}
+	public static void addAllProjects (BufferedWriter wr) throws IOException {
+		write(wr, "allprojects {");
+		write(wr, "apply plugin: \"eclipse\"");
+		write(wr, "apply plugin: \"idea\"");
+		space(wr);
+		write(wr, "version = '1.0'");
+		write(wr, "ext {");
+		write(wr, "appName = \"%APP_NAME%\"");
+		write(wr, "gdxVersion = '" + DependencyBank.libgdxVersion + "'");
+		write(wr, "roboVMVersion = '" + DependencyBank.roboVMVersion + "'");
+		write(wr, "box2DLightsVersion = '" + DependencyBank.box2DLightsVersion + "'");
+		write(wr, "ashleyVersion = '" + DependencyBank.ashleyVersion + "'");
+		write(wr, "aiVersion = '" + DependencyBank.aiVersion + "'");
+		write(wr, "}");
+		space(wr);
+		write(wr, "repositories {");
+		write(wr, DependencyBank.mavenLocal);
+		write(wr, DependencyBank.mavenCentral);
+		write(wr, DependencyBank.google);
+		write(wr, "maven { url \"" + DependencyBank.libGDXSnapshotsUrl + "\" }");
+		write(wr, "maven { url \"" + DependencyBank.libGDXReleaseUrl + "\" }");
+		write(wr, "}");
+		write(wr, "}");
+	}
 
 	public static void addProject(Language language, ProjectType project, List<Dependency> dependencies, BufferedWriter wr) throws IOException {
 		space(wr);
