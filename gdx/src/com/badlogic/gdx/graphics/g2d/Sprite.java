aa0d30e2dadbe36d4b549aca3c99c3725333815c
NathanSweet <nathan.sweet@gmail.com>
2018-09-27 15:42:14 +0200

    Fixed Sprite#setAlpha.

--- gdx/src/com/badlogic/gdx/graphics/g2d/Sprite.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/Sprite.java
@@ -208,17 +208,17 @@ public class Sprite extends TextureRegion {
 	}
 	
 	/** Sets the x position so that it is centered on the given x parameter */
-	public void setCenterX(float x){
+	public void setCenterX (float x) {
 		setX(x - width / 2);
 	}
-	
+
 	/** Sets the y position so that it is centered on the given y parameter */
-	public void setCenterY(float y){
+	public void setCenterY (float y) {
 		setY(y - height / 2);
 	}
-	
+
 	/** Sets the position so that the sprite is centered on (x, y) */
-	public void setCenter(float x, float y){
+	public void setCenter (float x, float y) {
 		setCenterX(x);
 		setCenterY(y);
 	}
@@ -286,15 +286,8 @@ public class Sprite extends TextureRegion {
 
 	/** Sets the alpha portion of the color used to tint this sprite. */
 	public void setAlpha (float a) {
-		int intBits = NumberUtils.floatToIntColor(vertices[C1]);
-		int alphaBits = (int)(255 * a) << 24;
-
-		// clear alpha on original color
-		intBits = intBits & 0x00FFFFFF;
-		// write new alpha
-		intBits = intBits | alphaBits;
-		color.set(intBits);
-		float color = NumberUtils.intToFloatColor(intBits);
+		color.a = a;
+		float color = this.color.toFloatBits();
 		vertices[C1] = color;
 		vertices[C2] = color;
 		vertices[C3] = color;
@@ -304,8 +297,7 @@ public class Sprite extends TextureRegion {
 	/** @see #setColor(Color) */
 	public void setColor (float r, float g, float b, float a) {
 		color.set(r, g, b, a);
-		int intBits = ((int)(255 * a) << 24) | ((int)(255 * b) << 16) | ((int)(255 * g) << 8) | ((int)(255 * r));
-		float color = NumberUtils.intToFloatColor(intBits);
+		float color = this.color.toFloatBits();
 		float[] vertices = this.vertices;
 		vertices[C1] = color;
 		vertices[C2] = color;
@@ -575,6 +567,12 @@ public class Sprite extends TextureRegion {
 	/** Returns the color of this sprite. If the returned instance is manipulated, {@link #setColor(Color)} must be called
 	 * afterward. */
 	public Color getColor () {
+		int intBits = NumberUtils.floatToIntColor(vertices[C1]);
+		Color color = this.color;
+		color.r = (intBits & 0xff) / 255f;
+		color.g = ((intBits >>> 8) & 0xff) / 255f;
+		color.b = ((intBits >>> 16) & 0xff) / 255f;
+		color.a = ((intBits >>> 24) & 0xff) / 255f;
 		return color;
 	}
 
