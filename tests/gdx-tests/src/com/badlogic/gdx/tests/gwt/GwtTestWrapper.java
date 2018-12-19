23604fee6cc56ca143e017d918c77be56855c8b4
Benjamin Schulte <bs@golfgl.de>
2018-12-19 20:53:12 +0100

    #5020 Fixed grammatical issue, changed IntArray to IntSet

--- tests/gdx-tests/src/com/badlogic/gdx/tests/gwt/GwtTestWrapper.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/gwt/GwtTestWrapper.java
@@ -420,7 +420,7 @@ public class GwtTestWrapper extends GdxTest {
 
 		@Override
 		public void setCatchKey(int keycode, boolean catchKey) {
-
+			input.setCatchKey(keycode, catchKey);
 		}
 
 		@Override
