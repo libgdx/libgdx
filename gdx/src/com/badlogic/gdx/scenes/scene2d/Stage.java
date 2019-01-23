100251a98dda9028cbac4e11d39225145366093c
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-01-23 16:55:13 -0700

    Remove redundant scissor calculation
    Reported by ryuukk_ on IRC

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Stage.java
@@ -746,7 +746,6 @@ public class Stage extends InputAdapter implements Disposable {
 	/** Calculates window scissor coordinates from local coordinates using the batch's current transformation matrix.
 	 * @see ScissorStack#calculateScissors(Camera, float, float, float, float, Matrix4, Rectangle, Rectangle) */
 	public void calculateScissors (Rectangle localRect, Rectangle scissorRect) {
-		viewport.calculateScissors(batch.getTransformMatrix(), localRect, scissorRect);
 		Matrix4 transformMatrix;
 		if (debugShapes != null && debugShapes.isDrawing())
 			transformMatrix = debugShapes.getTransformMatrix();
