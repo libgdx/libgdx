57719994a058e7b888db72de3716cbc79ebb0295
Tom-Ski <tomaszwojciechowski@hotmail.co.uk>
2019-03-13 07:54:16 +0000

    BitmapFontWriterTest

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -25,7 +25,7 @@ public class DependencyBank {
 	static String libgdxVersion = "1.9.9";
 	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
 	static String libgdxNightlyVersion = "1.9.10-SNAPSHOT";
-	static String roboVMVersion = "2.3.5";
+	static String roboVMVersion = "2.3.6";
 	static String moeVersion = "1.4.0";
 	static String buildToolsVersion = "28.0.3";
 	static String androidAPILevel = "27";
