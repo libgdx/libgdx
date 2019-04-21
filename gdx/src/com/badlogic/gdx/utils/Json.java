da36b9cbd87949b1a3ae2de44a898660bd76e0d8
NathanSweet <nathan.sweet@gmail.com>
2019-04-21 21:18:48 +0200

    Javadocs.

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -74,7 +74,7 @@ public class Json {
 	}
 
 	/** When true, fields in the JSON that are not found on the class will not throw a {@link SerializationException}. Default is
-	 * false. Default is false. */
+	 * false. */
 	public void setIgnoreUnknownFields (boolean ignoreUnknownFields) {
 		this.ignoreUnknownFields = ignoreUnknownFields;
 	}
