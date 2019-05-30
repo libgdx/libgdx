be4215ae30a8369e88003f406e27f2e1b258e2b2
Keval <therealchubbypanda@gmail.com>
2019-05-30 10:42:12 +0100

    Implemented monitor changing using monitor handle in config.

--- backends/gdx-backend-lwjgl3/src/com/badlogic/gdx/backends/lwjgl3/Lwjgl3Application.java
+++ backends/gdx-backend-lwjgl3/src/com/badlogic/gdx/backends/lwjgl3/Lwjgl3Application.java
@@ -465,7 +465,13 @@ public class Lwjgl3Application implements Application {
 				GLFW.glfwSetWindowPos(windowHandle, config.windowX, config.windowY);
 			}
 		} else if (config.windowMaximized) {
-			GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+			GLFWVidMode vidMode;
+			try {
+				vidmode = GLFW.glfwGetVideoMode(maximizedMonitor);
+			} catch (NullPointerException e) {
+				System.out.println("Monitor " + maximizedMonitor + " not found, using primary monitor!");
+				vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+			}
 			GLFW.glfwSetWindowPos(windowHandle, vidMode.width() / 2 - windowWidth / 2, vidMode.height() / 2 - windowHeight / 2);
 		}
 		if (config.windowIconPaths != null) {
