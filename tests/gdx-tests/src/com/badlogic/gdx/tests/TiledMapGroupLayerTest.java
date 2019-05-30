7f692fc9a53829b44a6aec2f59420babd348fd67
Thomas Creutzenberg <crossie@gmail.com>
2019-05-30 17:03:03 +0200

    BaseTmxMapLoader refactoring Mk2 (#5618)
    * BaseTmxMapLoader rectoring
    
    * Checkstyle

--- tests/gdx-tests/src/com/badlogic/gdx/tests/TiledMapGroupLayerTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/TiledMapGroupLayerTest.java
@@ -1,19 +1,22 @@
 package com.badlogic.gdx.tests;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapGroupLayer;
-import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.AtlasTmxMapLoader;
+import com.badlogic.gdx.maps.tiled.AtlasTmxMapLoader.AtlasTiledMapLoaderParameters;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.tests.utils.GdxTest;
 import com.badlogic.gdx.tests.utils.OrthoCamController;
-import com.badlogic.gdx.utils.GdxRuntimeException;
 
 public class TiledMapGroupLayerTest extends GdxTest {
 
@@ -21,17 +24,19 @@ public class TiledMapGroupLayerTest extends GdxTest {
 	private TiledMapRenderer renderer;
 	private OrthographicCamera camera;
 	private OrthoCamController cameraController;
+	private AssetManager assetManager;
 	private BitmapFont font;
 	private SpriteBatch batch;
+	String errorMessage;
+	private String fileName = "data/maps/tiled-groups/test.tmx";
 
-	@Override
-	public void create () {
+	@Override public void create () {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, (w / h) * 20, 20);
-		camera.position.set(10.0f, 2.5f, 0.0f);
+		camera.setToOrtho(false, (w / h) * 10, 10);
+		camera.zoom = 2;
 		camera.update();
 
 		cameraController = new OrthoCamController(camera);
@@ -40,50 +45,40 @@ public class TiledMapGroupLayerTest extends GdxTest {
 		font = new BitmapFont();
 		batch = new SpriteBatch();
 
-		map = new TmxMapLoader().load("data/maps/tiled/grouplayertest.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map, 1f / 32f);
+		AtlasTiledMapLoaderParameters params = new AtlasTiledMapLoaderParameters();
+		params.forceTextureFilters = true;
+		params.textureMinFilter = TextureFilter.Linear;
+		params.textureMagFilter = TextureFilter.Linear;
 
-		testMapProperty(map.getLayers().get("group1Visible"), true, 1f, 32, 0, true);
+		assetManager = new AssetManager();
+		assetManager.setErrorListener(new AssetErrorListener() {
+			@Override public void error (AssetDescriptor asset, Throwable throwable) {
+				errorMessage = throwable.getMessage();
+			}
+		});
 
-		MapGroupLayer group1Visible = (MapGroupLayer)map.getLayers().get("group1Visible");
-
-		testMapProperty(group1Visible.getLayers().get("group1ChildGroup"), true, 1f, 32, 0, true);
-		testMapProperty(group1Visible.getLayers().get("group1ChildVisible"), true, 1f, 0, 0, false);
-		testMapProperty(group1Visible.getLayers().get("group1ChildNotVisible"), false, 1f, 32, 0, false);
-
-
-		testMapProperty(map.getLayers().get("group2NotVisible"), false, 1f, 0, 0, true);
-		testMapProperty(map.getLayers().get("Layer1"), true, 1f, 0, 0, false);
+		assetManager.setLoader(TiledMap.class, new AtlasTmxMapLoader(new InternalFileHandleResolver()));
+		assetManager.load(fileName, TiledMap.class);
 	}
 
-	private void testMapProperty (MapLayer mapLayer, boolean visibleState, float opacity, int offsetX, int offsetY, boolean isGroup) {
-		boolean isValid = mapLayer.isVisible() == visibleState && opacity == mapLayer.getOpacity() && offsetX == mapLayer.getOffsetX() && mapLayer.getOffsetY() == mapLayer.getOffsetY();
-		boolean groupInstance = mapLayer instanceof MapGroupLayer;
-		if (isGroup && !groupInstance) {
-			isValid = false;
-		}
-		if (!isGroup && groupInstance) {
-			isValid = false;
-		}
-		if (!isValid) throw new GdxRuntimeException("Map layer " + mapLayer.getName() + " is not valid");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.55f, 0.55f, 0.55f, 1f);
+	@Override public void render () {
+		Gdx.gl.glClearColor(100f / 255f, 100f / 255f, 250f / 255f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		map.getLayers().get("group1Visible").setOffsetX(map.getLayers().get("group1Visible").getOffsetX() + 1);
-
 		camera.update();
-		renderer.setView(camera);
-		renderer.render();
+		assetManager.update();
+		if (renderer == null && assetManager.isLoaded(fileName)) {
+			map = assetManager.get(fileName);
+			renderer = new OrthogonalTiledMapRenderer(map, 1f / 32f);
+		} else if (renderer != null) {
+			renderer.setView(camera);
+			renderer.render();
+		}
+		batch.begin();
+		if (errorMessage != null) {
+			font.draw(batch, "ERROR (OK if running in GWT): " + errorMessage, 10, 50);
+			System.out.println(errorMessage);
+		}
+		font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
+		batch.end();
 	}
-
-	@Override
-	public void dispose () {
-		map.dispose();
-		batch.dispose();
-	}
-
 }
