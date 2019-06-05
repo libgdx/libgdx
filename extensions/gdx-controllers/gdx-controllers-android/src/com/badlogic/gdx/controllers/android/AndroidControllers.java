660dfcf930e1696f8a888368e1fd26766f3aadb8
Benjamin Schulte <bs@golfgl.de>
2019-06-05 11:13:17 +0200

    Controller Extension: Prevent crashes on Amazon FireTV, fixes #5277

--- extensions/gdx-controllers/gdx-controllers-android/src/com/badlogic/gdx/controllers/android/AndroidControllers.java
+++ extensions/gdx-controllers/gdx-controllers-android/src/com/badlogic/gdx/controllers/android/AndroidControllers.java
@@ -277,22 +277,27 @@ public class AndroidControllers implements LifecycleListener, ControllerManager,
 	}
 	
 	protected void addController(int deviceId, boolean sendEvent) {
-		InputDevice device = InputDevice.getDevice(deviceId);
-		if(!isController(device)) return;
-		String name = device.getName();
-		AndroidController controller = new AndroidController(deviceId, name);
-		controllerMap.put(deviceId, controller);
-		if(sendEvent) {
-			synchronized(eventQueue) {
-				AndroidControllerEvent event = eventPool.obtain();
-				event.type = AndroidControllerEvent.CONNECTED;
-				event.controller = controller;
-				eventQueue.add(event);
+		try {
+			InputDevice device = InputDevice.getDevice(deviceId);
+			if (!isController(device)) return;
+			String name = device.getName();
+			AndroidController controller = new AndroidController(deviceId, name);
+			controllerMap.put(deviceId, controller);
+			if (sendEvent) {
+				synchronized (eventQueue) {
+					AndroidControllerEvent event = eventPool.obtain();
+					event.type = AndroidControllerEvent.CONNECTED;
+					event.controller = controller;
+					eventQueue.add(event);
+				}
+			} else {
+				controllers.add(controller);
 			}
-		} else {
-			controllers.add(controller);
+			Gdx.app.log(TAG, "added controller '" + name + "'");
+		} catch (RuntimeException e) {
+			// this exception is sometimes thrown by getDevice().
+			// we can't use this device anyway, so ignore it and move on
 		}
-		Gdx.app.log(TAG, "added controller '" + name + "'");
 	}
 	
 	protected void removeController(int deviceId) {
