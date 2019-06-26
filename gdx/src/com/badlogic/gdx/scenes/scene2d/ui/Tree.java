c2f07ed68e34f79ffd90a05e8cf6fbaaae45abc7
NathanSweet <nathan.sweet@gmail.com>
2019-06-26 19:26:55 +0200

    [scene2d.ui] Tree, update over node without needing to move the mouse.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
@@ -124,6 +124,11 @@ public class Tree<N extends Node, V> extends WidgetGroup {
 				return false;
 			}
 
+			public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {
+				super.enter(event, x, y, pointer, fromActor);
+				setOverNode(getNodeAt(y));
+			}
+
 			public void exit (InputEvent event, float x, float y, int pointer, Actor toActor) {
 				super.exit(event, x, y, pointer, toActor);
 				if (toActor == null || !toActor.isDescendantOf(Tree.this)) setOverNode(null);
