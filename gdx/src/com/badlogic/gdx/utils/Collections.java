b03febe38686ed3078235e843b701fca5b843fab
Nathan Sweet <nathan.sweet@gmail.com>
2019-06-27 08:59:36 +0200

    Update Collections.java

--- gdx/src/com/badlogic/gdx/utils/Collections.java
+++ gdx/src/com/badlogic/gdx/utils/Collections.java
@@ -2,8 +2,9 @@ package com.badlogic.gdx.utils;
 
 public class Collections {
 
-	/** When true, {@link Iterable#iterator()} in this class and others will allocate a new iterator for each invocation. When
-	 * false, the iterator is reused and nested use will throw an exception. Default is false. */
+	/** When true, {@link Iterable#iterator()} for {@link Array}, {@link ObjectMap}, and other collections will allocate a new
+	 * iterator for each invocation. When false, the iterator is reused and nested use will throw an exception. Default is
+	 * false. */
 	public static boolean allocateIterators;
 
 }
