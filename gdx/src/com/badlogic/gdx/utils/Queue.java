cfbf3aca974d1efe507734313b5d1bc9e1cfd49a
NathanSweet <nathan.sweet@gmail.com>
2019-07-19 01:25:10 +0200

    [collections] Added Queue#toString(String) to match Array.

--- gdx/src/com/badlogic/gdx/utils/Queue.java
+++ gdx/src/com/badlogic/gdx/utils/Queue.java
@@ -330,8 +330,8 @@ public class Queue<T> implements Iterable<T> {
 
 	/** Returns an iterator for the items in the queue. Remove is supported.
 	 * <p>
-	 * If {@link Collections#allocateIterators} is false, the same iterator instance is returned each time this method is called. Use the
-	 * {@link QueueIterator} constructor for nested or multithreaded iteration. */
+	 * If {@link Collections#allocateIterators} is false, the same iterator instance is returned each time this method is called.
+	 * Use the {@link QueueIterator} constructor for nested or multithreaded iteration. */
 	public Iterator<T> iterator () {
 		if (Collections.allocateIterators) return new QueueIterator(this, true);
 		if (iterable == null) iterable = new QueueIterable(this);
@@ -353,6 +353,19 @@ public class Queue<T> implements Iterable<T> {
 			sb.append(", ").append(values[i]);
 		}
 		sb.append(']');
+		return sb.toString();
+	}
+
+	public String toString (String separator) {
+		if (size == 0) return "";
+		final T[] values = this.values;
+		final int head = this.head;
+		final int tail = this.tail;
+
+		StringBuilder sb = new StringBuilder(64);
+		sb.append(values[head]);
+		for (int i = (head + 1) % values.length; i != tail; i = (i + 1) % values.length)
+			sb.append(separator).append(values[i]);
 		return sb.toString();
 	}
 
