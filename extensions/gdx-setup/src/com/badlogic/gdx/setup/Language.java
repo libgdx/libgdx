3228131ed510149292a03bb0d90ba664d0287be0
Tom-Ski <tomaszwojciechowski@hotmail.co.uk>
2019-08-01 20:56:12 +0100

    Fix #5731 Upgrade kotlin to 1.3.41

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/Language.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/Language.java
@@ -2,9 +2,9 @@ package com.badlogic.gdx.setup;
 
 public enum Language {
 	JAVA ("java", "", "", "", "java-library;java-library;android;java-library,robovm;java-library,moe;java-library,gwt,war", true) ,
-	KOTLIN ("kotlin", "ext.kotlinVersion = '1.2.71'",
+	KOTLIN ("kotlin", "ext.kotlinVersion = '1.3.41'",
 			"classpath \"org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion\"", 
-			"compile \"org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion\"",
+			"api \"org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion\"",
 			"kotlin;kotlin;android,kotlin-android;kotlin,robovm;moe; ",
 			false);
 	
