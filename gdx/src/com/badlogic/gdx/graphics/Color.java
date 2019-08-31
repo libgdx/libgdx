6d08f14e3016c19c3481953a1d5074a44570b54e
AndEditor7 <35620587+AndEditor7@users.noreply.github.com>
2019-08-31 15:30:00 +1000

    Noticed toIntBits() wasn't the same what it was for toHexString.

--- gdx/src/com/badlogic/gdx/graphics/Color.java
+++ gdx/src/com/badlogic/gdx/graphics/Color.java
@@ -323,7 +323,8 @@ public class Color {
 
 	/** Returns the color encoded as hex string with the format RRGGBBAA. */
 	public String toString () {
-		String value = Integer.toHexString(toIntBits());
+		String value = Integer
+			.toHexString(((int)(255 * r) << 24) | ((int)(255 * g) << 16) | ((int)(255 * b) << 8) | ((int)(255 * a)));
 		while (value.length() < 8)
 			value = "0" + value;
 		return value;
