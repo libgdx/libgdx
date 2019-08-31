4a4d1b5cc90da641e5419a8b3c75ebe0c54ce9d1
Ryan Stout <ryanastout@gmail.com>
2019-08-31 17:18:03 -0500

    use absolute path instead of just filename

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/flame/TextureLoaderPanel.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/flame/TextureLoaderPanel.java
@@ -53,7 +53,7 @@ public class TextureLoaderPanel extends EditorPanel {
 				if(file != null){
 					TextureAtlas atlas = editor.load(file.getAbsolutePath(), TextureAtlas.class, null,  null);
 					if(atlas != null){
-						editor.setAtlas(atlas, file.getName());
+						editor.setAtlas(atlas, file.getAbsolutePath());
 					}
 				}
 			}
