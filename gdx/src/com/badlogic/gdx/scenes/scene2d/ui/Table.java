742390b374393a45496afa25b336409fbbcc0063
NathanSweet <nathan.sweet@gmail.com>
2019-10-07 09:34:16 +0200

    [scene2d.ui] Table#getRow now returns -1 when over the table but not over a row (used to return the last row).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -684,7 +684,7 @@ public class Table extends WidgetGroup {
 		return align;
 	}
 
-	/** Returns the row index for the y coordinate, or -1 if there are no cells.
+	/** Returns the row index for the y coordinate, or -1 if not over a row.
 	 * @param y The y coordinate, where 0 is the top of the table. */
 	public int getRow (float y) {
 		Array<Cell> cells = this.cells;
@@ -692,13 +692,12 @@ public class Table extends WidgetGroup {
 		y += getPadTop();
 		int i = 0, n = cells.size;
 		if (n == 0) return -1;
-		if (n == 1) return 0;
 		while (i < n) {
 			Cell c = cells.get(i++);
-			if (c.actorY + c.computedPadTop < y) break;
+			if (c.actorY + c.computedPadTop < y) return row;
 			if (c.endRow) row++;
 		}
-		return Math.min(row, rows - 1);
+		return -1;
 	}
 
 	public void setSkin (Skin skin) {
