6b552bf47e8b3d777b7b7cccd97c8527e3862246
NathanSweet <nathan.sweet@gmail.com>
2019-11-22 12:05:58 +0100

    [scene2d.ui] Added ProgressBar#isAnimating().

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ProgressBar.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ProgressBar.java
@@ -358,6 +358,10 @@ public class ProgressBar extends Widget implements Disableable {
 		this.disabled = disabled;
 	}
 
+	public boolean isAnimating () {
+		return animateTime > 0;
+	}
+
 	public boolean isDisabled () {
 		return disabled;
 	}
