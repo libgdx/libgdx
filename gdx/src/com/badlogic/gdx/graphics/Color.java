c3bc0d18c0a5be7786fe6519cbbd240e75448f87
NathanSweet <nathan.sweet@gmail.com>
2019-11-25 11:34:01 +0100

    Avoid needless boxing in Color.

--- gdx/src/com/badlogic/gdx/graphics/Color.java
+++ gdx/src/com/badlogic/gdx/graphics/Color.java
@@ -335,10 +335,10 @@ public class Color {
 	 * @see #toString() */
 	public static Color valueOf (String hex) {
 		hex = hex.charAt(0) == '#' ? hex.substring(1) : hex;
-		int r = Integer.valueOf(hex.substring(0, 2), 16);
-		int g = Integer.valueOf(hex.substring(2, 4), 16);
-		int b = Integer.valueOf(hex.substring(4, 6), 16);
-		int a = hex.length() != 8 ? 255 : Integer.valueOf(hex.substring(6, 8), 16);
+		int r = Integer.parseInt(hex.substring(0, 2), 16);
+		int g = Integer.parseInt(hex.substring(2, 4), 16);
+		int b = Integer.parseInt(hex.substring(4, 6), 16);
+		int a = hex.length() != 8 ? 255 : Integer.parseInt(hex.substring(6, 8), 16);
 		return new Color(r / 255f, g / 255f, b / 255f, a / 255f);
 	}
 
