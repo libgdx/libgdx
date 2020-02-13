c3508de3087ce2c54a3a080e8d63478e26f8f48f
NathanSweet <nathan.sweet@gmail.com>
2020-02-13 21:14:20 +0100

    Fixed script to copy Eclipse settings.

--- gdx/.settings/copy-to-other-projects.sh
+++ gdx/.settings/copy-to-other-projects.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-files=(org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs)
-cp -t ../../backends/gdx-backend-headless/.settings $files
-cp -t ../../backends/gdx-backend-lwjgl/.settings $files
-cp -t ../../backends/gdx-backend-lwjgl3/.settings $files
-cp -t ../../extensions/gdx-box2d/gdx-box2d/.settings $files
-cp -t ../../extensions/gdx-bullet/.settings $files
-cp -t ../../extensions/gdx-controllers/gdx-controllers/.settings $files
-cp -t ../../extensions/gdx-controllers/gdx-controllers-desktop/.settings $files
-cp -t ../../extensions/gdx-controllers/gdx-controllers-lwjgl3/.settings $files
-cp -t ../../extensions/gdx-freetype/.settings $files
-cp -t ../../extensions/gdx-jnigen/.settings $files
-cp -t ../../extensions/gdx-tools/.settings $files
-cp -t ../../tests/gdx-tests/.settings $files
-cp -t ../../tests/gdx-tests-lwjgl/.settings $files
-cp -t ../../tests/gdx-tests-lwjgl3/.settings $files
+cp -t ../../backends/gdx-backend-headless/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../backends/gdx-backend-lwjgl/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../backends/gdx-backend-lwjgl3/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-box2d/gdx-box2d/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-bullet/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-controllers/gdx-controllers/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-controllers/gdx-controllers-desktop/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-controllers/gdx-controllers-lwjgl3/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-freetype/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-jnigen/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../extensions/gdx-tools/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../tests/gdx-tests/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../tests/gdx-tests-lwjgl/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
+cp -t ../../tests/gdx-tests-lwjgl3/.settings org.eclipse.jdt.core.prefs org.eclipse.jdt.ui.prefs
