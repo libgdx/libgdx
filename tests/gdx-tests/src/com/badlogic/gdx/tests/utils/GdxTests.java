882fd77dbec63ed9720ea622e65ba6353a02a588
MGSX <germain.aubert@gmail.com>
2020-03-07 21:13:19 +0100

    Fix DefaultTextureBinder replacing WEIGHTED strategy by LRU (#5942)
    * added LRU strategy for DefaultTextureBinder
    
    * remove WEIGHTED strategy from DefaultTextureBinder in favor to LRU
    
    * update changes
    
    * remove unused parameter

--- tests/gdx-tests/src/com/badlogic/gdx/tests/utils/GdxTests.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/utils/GdxTests.java
@@ -74,6 +74,7 @@ import com.badlogic.gdx.tests.g3d.SkeletonTest;
 import com.badlogic.gdx.tests.g3d.TangentialAccelerationTest;
 import com.badlogic.gdx.tests.g3d.TextureArrayTest;
 import com.badlogic.gdx.tests.g3d.TextureRegion3DTest;
+import com.badlogic.gdx.tests.g3d.utils.DefaultTextureBinderTest;
 import com.badlogic.gdx.tests.gles2.HelloTriangle;
 import com.badlogic.gdx.tests.gles2.SimpleVertexShader;
 import com.badlogic.gdx.tests.net.NetAPITest;
@@ -125,6 +126,7 @@ public class GdxTests {
 		CullTest.class,
 		CursorTest.class,
 		DecalTest.class,
+		DefaultTextureBinderTest.class,
 		DelaunayTriangulatorTest.class,
 		DeltaTimeTest.class,
 		DirtyRenderingTest.class,
