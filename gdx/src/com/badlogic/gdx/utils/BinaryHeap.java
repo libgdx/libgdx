da431af48b3d95b55c349c0c7f07b335fb8cf6aa
Nathan Sweet <nathan.sweet@gmail.com>
2020-03-29 17:59:26 +0200

    Fixed BinaryHeap remove when the node is not the first.
    closes #5984

--- gdx/src/com/badlogic/gdx/utils/BinaryHeap.java
+++ gdx/src/com/badlogic/gdx/utils/BinaryHeap.java
@@ -81,20 +81,28 @@ public class BinaryHeap<T extends BinaryHeap.Node> {
 	/** Removes the first item in the heap and returns it. This is the item with the lowest value (or highest value if this heap is
 	 * configured as a max heap). */
 	public T pop () {
-		return remove(0);
+		Node removed = nodes[0];
+		if (--size > 0) {
+			nodes[0] = nodes[size];
+			nodes[size] = null;
+			down(0);
+		} else
+			nodes[0] = null;
+		return (T)removed;
 	}
 
 	public T remove (T node) {
-		return remove(node.index);
-	}
-
-	private T remove (int index) {
-		Node[] nodes = this.nodes;
-		Node removed = nodes[index];
-		nodes[index] = nodes[--size];
-		nodes[size] = null;
-		if (size > 0 && index < size) down(index);
-		return (T)removed;
+		if (--size > 0) {
+			Node moved = nodes[size];
+			nodes[node.index] = moved;
+			nodes[size] = null;
+			if (moved.value < node.value ^ isMaxHeap)
+				up(node.index);
+			else
+				down(node.index);
+		} else
+			nodes[0] = null;
+		return node;
 	}
 
 	/** Returns true if the heap has one or more items. */
