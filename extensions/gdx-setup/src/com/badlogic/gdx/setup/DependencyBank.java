6a0fa71a293bc0fd1e639623c0192b263eb60212
Benjamin Schulte <bs@golfgl.de>
2020-08-30 10:44:49 +0200

    Updated all definitions of RoboVM version to 2.3.10-SNAPSHOT, caused by #6132

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -25,7 +25,7 @@ public class DependencyBank {
 	static String libgdxVersion = "1.9.11";
 	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
 	static String libgdxNightlyVersion = "1.9.12-SNAPSHOT";
-	static String roboVMVersion = "2.3.8";
+	static String roboVMVersion = "2.3.10-SNAPSHOT";
 	static String moeVersion = "1.4.0";
 	static String buildToolsVersion = "29.0.2";
 	static String androidAPILevel = "29";
