f4d8de4878abac7cfa70acbdb3ada95a5d8c4c71
Kai Denzel <kai.denzel@uni-ulm.de>
2020-09-13 02:02:38 +0200

    -Fixes bug in IntersectorOverlapConvexPolygonsTest class.

--- tests/gdx-tests/src/com/badlogic/gdx/tests/IntersectorOverlapConvexPolygonsTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/IntersectorOverlapConvexPolygonsTest.java
@@ -18,8 +18,8 @@ public class IntersectorOverlapConvexPolygonsTest extends GdxTest {
     private OrthographicCamera camera;
     private ShapeRenderer shapeRenderer;
 
-    private float shapeWidth = 10f;
-    private float shapeHeight = 10f;
+    private float shapeWidth = 1f;
+    private float shapeHeight = 1f;
     //the vertices of the 2 shapes. Feel free to play with the values. It must be a convex Polygon.
     private float[] vertsShape1 = {0f, 0f, shapeWidth, 0f, shapeWidth, shapeHeight};
     private float[] vertsShape2 = {0f, 0f, shapeWidth *2, 0f, shapeWidth *2, shapeHeight *2,0, shapeHeight};
@@ -38,18 +38,17 @@ public class IntersectorOverlapConvexPolygonsTest extends GdxTest {
         camera.setToOrtho(false);
         camera.position.set(0, 0, 0);
         shapeRenderer = new ShapeRenderer();
-        shapeRenderer.setAutoShapeType(true);
         //set inital position
         shape1.setVertices(vertsShape1);
         shape2.setVertices(vertsShape2);
         shape1.setPosition(0, 0);
-        shape2.setPosition(10, 0);
+        shape2.setPosition(1, 0);
     }
 
     @Override
     public void resize(int width, int height) {
-        camera.viewportWidth = 80;
-        camera.viewportHeight = 60;
+        camera.viewportWidth = 8;
+        camera.viewportHeight = 6;
     }
 
     private void update(float deltaTime) {
@@ -85,18 +84,15 @@ public class IntersectorOverlapConvexPolygonsTest extends GdxTest {
         shapeRenderer.polygon(shape1.getTransformedVertices());
         shapeRenderer.setColor(Color.CYAN);
         shapeRenderer.polygon(shape2.getTransformedVertices());
-        //Translate the axis to position 0/0 and show the direction as red line
+        //Show axis starting at position 0/0 in pointing direction
         shapeRenderer.setColor(Color.RED);
-        shapeRenderer.line(0, 0, (mtv.normal.x * 100)-mtv.normal.x, (mtv.normal.y * 100)-mtv.normal.y);
-        //Translate the axis to position 0/0 and show the opposite direction as dark red line
+        shapeRenderer.line(0, 0, mtv.normal.x * 100, mtv.normal.y * 100);
+        //Show axis starting at position 0/0 in opposite direction
         shapeRenderer.setColor(Color.DARK_GRAY);
-        shapeRenderer.line(0, 0, (mtv.normal.x * -100)-mtv.normal.x, (mtv.normal.y * -100)-mtv.normal.y);
-        //Translate the axis to position 0/0 and show the depth as green line
+        shapeRenderer.line(0, 0, mtv.normal.x * -100, mtv.normal.y * -100);
+        //Show depth on axis in pointing direction
         shapeRenderer.setColor(Color.GREEN);
-        shapeRenderer.line(0, 0, (mtv.normal.x * mtv.depth)-mtv.normal.x, (mtv.normal.y * mtv.depth)-mtv.normal.y);
-        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
-        //Show origin at 0,0 as green dot
-        shapeRenderer.circle(0, 0, 0.2f);
+        shapeRenderer.line(0, 0, mtv.normal.x * mtv.depth, mtv.normal.y * mtv.depth);
         shapeRenderer.end();
     }
 }
