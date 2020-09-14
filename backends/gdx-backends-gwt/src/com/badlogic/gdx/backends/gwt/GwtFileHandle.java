libgdx/libgdx

commit 8539c3baf6a901f5272666d7305cb1234cbb27f3

Added md5 hash to file name for GWT preloader (#4314)
* Added md5 hash to file name for GWT preloader

* Fixed merge mistakes

* Reverted renaming of filewrapper

* GWT MD5 code improvements

* GWT MD5 Fixes problem with naming of files without extension

* GWT Added test for lazy loading, fixed other tests on GWT due to problems with files without extension

* Update CHANGES file

Co-authored-by: Benjamin Schulte <bs@golfgl.de>

backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/Preloader.java
@@ -55,7 +55,8 @@ public class Preloader {
 	private ObjectMap<String, Asset> stillToFetchAssets = new ObjectMap<String, Asset>();
 
 	public static class Asset {
-		public Asset (String url, AssetType type, long size, String mimeType) {
+		public Asset(String file, String url, AssetType type, long size, String mimeType) {
+			this.file = file;
 			this.url = url;
 			this.type = type;
 			this.size = size;
@@ -66,6 +67,7 @@ public class Preloader {
 		public boolean failed;
 		public boolean downloadStarted;
 		public long loaded;
+		public final String file;
 		public final String url;
 		public final AssetType type;
 		public final long size;
@@ -122,7 +124,7 @@ public class Preloader {
 
 	public void preload (final String assetFileUrl, final PreloaderCallback callback) {
 
-		loader.loadText(baseUrl + assetFileUrl, new AssetLoaderListener<String>() {
+        loader.loadText(baseUrl + assetFileUrl + "?etag=" + System.currentTimeMillis(), new AssetLoaderListener<String>() {
 			@Override
 			public void onProgress (double amount) {
 			}
@@ -136,29 +138,37 @@ public class Preloader {
 				Array<Asset> assets = new Array<Asset>(lines.length);
 				for (String line : lines) {
 					String[] tokens = line.split(":");
-					if (tokens.length != 5) {
+					if (tokens.length != 6) {
 						throw new GdxRuntimeException("Invalid assets description file.");
 					}
+
+					String assetTypeCode = tokens[0];
+					String assetPathOrig = tokens[1];
+					String assetPathMd5 = tokens[2];
+					long size = Long.parseLong(tokens[3]);
+					String assetMimeType = tokens[4];
+					boolean assetPreload = tokens[5].equals("1");
+
+
 					AssetType type = AssetType.Text;
-					if (tokens[0].equals("i")) type = AssetType.Image;
-					if (tokens[0].equals("b")) type = AssetType.Binary;
-					if (tokens[0].equals("a")) type = AssetType.Audio;
-					if (tokens[0].equals("d")) type = AssetType.Directory;
-					long size = Long.parseLong(tokens[2]);
+					if (assetTypeCode.equals("i")) type = AssetType.Image;
+					if (assetTypeCode.equals("b")) type = AssetType.Binary;
+					if (assetTypeCode.equals("a")) type = AssetType.Audio;
+					if (assetTypeCode.equals("d")) type = AssetType.Directory;
 					if (type == AssetType.Audio && !loader.isUseBrowserCache()) {
 						size = 0;
 					}
-					Asset asset = new Asset(tokens[1].trim(), type, size, tokens[3]);
-					if (tokens[4].equals("1") || asset.url.startsWith("com/badlogic/"))
-						assets.add(asset);
-					else
-						stillToFetchAssets.put(asset.url, asset);
+					Asset asset = new Asset(assetPathOrig.trim(), assetPathMd5.trim(), type, size, assetMimeType);
+                    if (assetPreload || asset.file.startsWith("com/badlogic/"))
+                        assets.add(asset);
+                    else
+                        stillToFetchAssets.put(asset.file, asset);
 				}
 				final PreloaderState state = new PreloaderState(assets);
 				for (int i = 0; i < assets.size; i++) {
 					final Asset asset = assets.get(i);
-					
-					if (contains(asset.url)) {
+
+					if (contains(asset.file)) {
 						asset.loaded = asset.size;
 						asset.succeed = true;
 						continue;
@@ -174,7 +184,7 @@ public class Preloader {
 						@Override
 						public void onFailure () {
 							asset.failed = true;
-							callback.error(asset.url);
+							callback.error(asset.file);
 							callback.update(state);
 						}
 						@Override
@@ -190,16 +200,16 @@ public class Preloader {
 		});
 	}
 
-	public void preloadSingleFile(final String url) {
-		if (!isNotFetchedYet(url))
+	public void preloadSingleFile(final String file) {
+		if (!isNotFetchedYet(file))
 			return;
 
-		final Asset asset = stillToFetchAssets.get(url);
+		final Asset asset = stillToFetchAssets.get(file);
 
 		if (asset.downloadStarted)
 			return;
 
-		Gdx.app.log("Preloader", "Downloading " + baseUrl + asset.url);
+		Gdx.app.log("Preloader", "Downloading " + baseUrl + asset.file);
 
 		asset.downloadStarted = true;
 
@@ -211,12 +221,12 @@ public class Preloader {
 			@Override
 			public void onFailure () {
 				asset.failed = true;
-				stillToFetchAssets.remove(url);
+				stillToFetchAssets.remove(file);
 			}
 			@Override
 			public void onSuccess (Object result) {
 				putAssetInMap(result, asset);
-				stillToFetchAssets.remove(url);
+				stillToFetchAssets.remove(file);
 				asset.succeed = true;
 			}
 		});
@@ -226,79 +236,79 @@ public class Preloader {
 	protected void putAssetInMap(Object result, Asset asset) {
 		switch (asset.type) {
 		case Text:
-			texts.put(asset.url, (String) result);
+			texts.put(asset.file, (String) result);
 			break;
 		case Image:
-			images.put(asset.url, (ImageElement) result);
+			images.put(asset.file, (ImageElement) result);
 			break;
 		case Binary:
-			binaries.put(asset.url, (Blob) result);
+			binaries.put(asset.file, (Blob) result);
 			break;
 		case Audio:
-			audio.put(asset.url, (Blob) result);
+			audio.put(asset.file, (Blob) result);
 			break;
 		case Directory:
-			directories.put(asset.url, null);
+			directories.put(asset.file, null);
 			break;
 		}
 	}
 
-	public InputStream read (String url) {
-		if (texts.containsKey(url)) {
+	public InputStream read(String file) {
+		if (texts.containsKey(file)) {
 			try {
-				return new ByteArrayInputStream(texts.get(url).getBytes("UTF-8"));
+				return new ByteArrayInputStream(texts.get(file).getBytes("UTF-8"));
 			} catch (UnsupportedEncodingException e) {
 				return null;
 			}
 		}
-		if (images.containsKey(url)) {
+		if (images.containsKey(file)) {
 			return new ByteArrayInputStream(new byte[1]); // FIXME, sensible?
 		}
-		if (binaries.containsKey(url)) {
-			return binaries.get(url).read();
+		if (binaries.containsKey(file)) {
+			return binaries.get(file).read();
 		}
-		if (audio.containsKey(url)) {
-			return audio.get(url).read();
+		if (audio.containsKey(file)) {
+			return audio.get(file).read();
 		}
 		return null;
 	}
 
-	public boolean contains (String url) {
-		return texts.containsKey(url) || images.containsKey(url) || binaries.containsKey(url) || audio.containsKey(url) || directories.containsKey(url);
+	public boolean contains(String file) {
+		return texts.containsKey(file) || images.containsKey(file) || binaries.containsKey(file) || audio.containsKey(file) || directories.containsKey(file);
 	}
 
-	public boolean isNotFetchedYet (String url) {
-		return stillToFetchAssets.containsKey(url);
+    public boolean isNotFetchedYet (String file) {
+        return stillToFetchAssets.containsKey(file);
+    }
+
+    public boolean isText(String file) {
+		return texts.containsKey(file);
 	}
 
-	public boolean isText (String url) {
-		return texts.containsKey(url);
+	public boolean isImage(String file) {
+		return images.containsKey(file);
 	}
 
-	public boolean isImage (String url) {
-		return images.containsKey(url);
+	public boolean isBinary(String file) {
+		return binaries.containsKey(file);
 	}
 
-	public boolean isBinary (String url) {
-		return binaries.containsKey(url);
+	public boolean isAudio(String file) {
+		return audio.containsKey(file);
 	}
 
-	public boolean isAudio (String url) {
-		return audio.containsKey(url);
+	public boolean isDirectory(String file) {
+		return directories.containsKey(file);
 	}
 
-	public boolean isDirectory (String url) {
-		return directories.containsKey(url);
+	private boolean isChild(String path, String file) {
+		return path.startsWith(file) && (path.indexOf('/', file.length() + 1) < 0);
 	}
 
-	private boolean isChild(String path, String url) {
-		return path.startsWith(url) && (path.indexOf('/', url.length() + 1) < 0);
-	}
-
-	public FileHandle[] list (String url) {
+	public FileHandle[] list(String file) {
 		Array<FileHandle> files = new Array<FileHandle>();
 		for (String path : texts.keys()) {
-			if (isChild(path, url)) {
+			if (isChild(path, file)) {
 				files.add(new GwtFileHandle(this, path, FileType.Internal));
 			}
 		}
@@ -307,10 +317,10 @@ public class Preloader {
 		return list;
 	}
 
-	public FileHandle[] list (String url, FileFilter filter) {
+	public FileHandle[] list(String file, FileFilter filter) {
 		Array<FileHandle> files = new Array<FileHandle>();
 		for (String path : texts.keys()) {
-			if (isChild(path, url) && filter.accept(new File(path))) {
+			if (isChild(path, file) && filter.accept(new File(path))) {
 				files.add(new GwtFileHandle(this, path, FileType.Internal));
 			}
 		}
@@ -319,10 +329,10 @@ public class Preloader {
 		return list;
 	}
 
-	public FileHandle[] list (String url, FilenameFilter filter) {
+	public FileHandle[] list(String file, FilenameFilter filter) {
 		Array<FileHandle> files = new Array<FileHandle>();
 		for (String path : texts.keys()) {
-			if (isChild(path, url) && filter.accept(new File(url), path.substring(url.length() + 1))) {
+			if (isChild(path, file) && filter.accept(new File(file), path.substring(file.length() + 1))) {
 				files.add(new GwtFileHandle(this, path, FileType.Internal));
 			}
 		}
@@ -331,10 +341,10 @@ public class Preloader {
 		return list;
 	}
 
-	public FileHandle[] list (String url, String suffix) {
+	public FileHandle[] list(String file, String suffix) {
 		Array<FileHandle> files = new Array<FileHandle>();
 		for (String path : texts.keys()) {
-			if (isChild(path, url) && path.endsWith(suffix)) {
+			if (isChild(path, file) && path.endsWith(suffix)) {
 				files.add(new GwtFileHandle(this, path, FileType.Internal));
 			}
 		}
@@ -343,22 +353,22 @@ public class Preloader {
 		return list;
 	}
 
-	public long length (String url) {
-		if (texts.containsKey(url)) {
+	public long length(String file) {
+		if (texts.containsKey(file)) {
 			try {
-				return texts.get(url).getBytes("UTF-8").length;
+				return texts.get(file).getBytes("UTF-8").length;
 			} catch (UnsupportedEncodingException e) {
-				return texts.get(url).getBytes().length;
+				return texts.get(file).getBytes().length;
 			}
 		}
-		if (images.containsKey(url)) {
+		if (images.containsKey(file)) {
 			return 1; // FIXME, sensible?
 		}
-		if (binaries.containsKey(url)) {
-			return binaries.get(url).length();
+		if (binaries.containsKey(file)) {
+			return binaries.get(file).length();
 		}
-		if (audio.containsKey(url)) {
-			return audio.get(url).length();
+		if (audio.containsKey(file)) {
+			return audio.get(file).length();
 		}
 		return 0;
 	}

backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/PreloaderBundleGenerator.java
@@ -16,11 +16,11 @@
 
 package com.badlogic.gdx.backends.gwt.preloader;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.math.BigInteger;
 import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -36,6 +36,7 @@ import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.user.rebind.ClassSourceFileComposerFactory;
 import com.google.gwt.user.rebind.SourceWriter;
+import org.apache.commons.io.IOUtils;
 
 /** Copies assets from the path specified in the modules gdx.assetpath configuration property to the war/ folder and generates the
  * assets.txt file. The type of a file is determined by an {@link AssetFilter}, which is either created by instantiating the class
@@ -43,10 +44,12 @@ import com.google.gwt.user.rebind.SourceWriter;
  * @author mzechner */
 public class PreloaderBundleGenerator extends Generator {
 	private class Asset {
+		String filePathOrig;
 		FileWrapper file;
 		AssetType type;
 
-		public Asset (FileWrapper file, AssetType type) {
+		public Asset(String filePathOrig, FileWrapper file, AssetType type) {
+			this.filePathOrig = filePathOrig;
 			this.file = file;
 			this.type = type;
 		}
@@ -91,10 +94,12 @@ public class PreloaderBundleGenerator extends Generator {
 			if (assetFilter.accept(classpathFile, false)) {
 				try {
 					InputStream is = context.getClass().getClassLoader().getResourceAsStream(classpathFile);
-					FileWrapper dest = target.child(classpathFile);
-					dest.write(is, false);
-					assets.add(new Asset(dest, assetFilter.getType(dest.path())));
+					byte[] bytes = IOUtils.toByteArray(is);
 					is.close();
+					FileWrapper origFile = target.child(classpathFile);
+					FileWrapper destFile = target.child(fileNameWithMd5(origFile, bytes));
+					destFile.writeBytes(bytes, false);
+					assets.add(new Asset(origFile.path(), destFile, assetFilter.getType(destFile.path())));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}			
@@ -106,7 +111,7 @@ public class PreloaderBundleGenerator extends Generator {
 			String bundleName = assetFilter.getBundleName(asset.file.path());
 			if (bundleName == null) {
 				bundleName = "assets";
-			}			
+			}
 			ArrayList<Asset> bundleAssets = bundles.get(bundleName);
 			if (bundleAssets == null) {
 				bundleAssets = new ArrayList<Asset>();
@@ -115,21 +120,26 @@ public class PreloaderBundleGenerator extends Generator {
 			bundleAssets.add(asset);
 		}
 
+		// Write the tokens for Preloader.preload()
 		for (Entry<String, ArrayList<Asset>> bundle : bundles.entrySet()) {
 			StringBuilder sb = new StringBuilder();
 			for (Asset asset : bundle.getValue()) {
-				String path = asset.file.path().replace('\\', '/').replace(assetOutputPath, "").replaceFirst("assets/", "");
-				if (path.startsWith("/")) path = path.substring(1);
+                String pathOrig = asset.filePathOrig.replace('\\', '/').replace(assetOutputPath, "").replaceFirst("assets/", "");
+				if (pathOrig.startsWith("/")) pathOrig = pathOrig.substring(1);
+                String pathMd5 = asset.file.path().replace('\\', '/').replace(assetOutputPath, "").replaceFirst("assets/", "");
+                if (pathMd5.startsWith("/")) pathMd5 = pathMd5.substring(1);
 				sb.append(asset.type.code);
 				sb.append(":");
-				sb.append(path);
+				sb.append(pathOrig);
 				sb.append(":");
+                sb.append(pathMd5);
+                sb.append(":");
 				sb.append(asset.file.isDirectory() ? 0 : asset.file.length());
 				sb.append(":");
 				String mimetype = URLConnection.guessContentTypeFromName(asset.file.name());
 				sb.append(mimetype == null ? "application/unknown" : mimetype);
                 sb.append(":");
-                sb.append(asset.file.isDirectory() || assetFilter.preload(path) ? '1' : '0');
+                sb.append(asset.file.isDirectory() || assetFilter.preload(pathOrig) ? '1' : '0');
 				sb.append("\n");
 			}
 			target.child(bundle.getKey() + ".txt").writeString(sb.toString(), false);
@@ -137,29 +147,31 @@ public class PreloaderBundleGenerator extends Generator {
 		return createDummyClass(logger, context);
 	}
 
-	private void copyFile (FileWrapper source, FileWrapper dest, AssetFilter filter, ArrayList<Asset> assets) {
-		if (!filter.accept(dest.path(), false)) return;
+	private void copyFile(FileWrapper source, String filePathOrig, FileWrapper dest, AssetFilter filter, ArrayList<Asset> assets) {
+		if (!filter.accept(filePathOrig, false)) return;
 		try {
-			assets.add(new Asset(dest, filter.getType(dest.path())));
+			assets.add(new Asset(filePathOrig, dest, filter.getType(dest.path())));
 			dest.write(source.read(), false);
 		} catch (Exception ex) {
 			throw new GdxRuntimeException("Error copying source file: " + source + "\n" //
-				+ "To destination: " + dest, ex);
+					+ "To destination: " + dest, ex);
 		}
 	}
 
-	private void copyDirectory (FileWrapper sourceDir, FileWrapper destDir, AssetFilter filter, ArrayList<Asset> assets) {
+	private void copyDirectory(FileWrapper sourceDir, FileWrapper destDir, AssetFilter filter, ArrayList<Asset> assets) {
 		if (!filter.accept(destDir.path(), true)) return;
-		assets.add(new Asset(destDir, AssetType.Directory));
+		assets.add(new Asset(destDir.path(), destDir, AssetType.Directory));
 		destDir.mkdirs();
 		FileWrapper[] files = sourceDir.list();
 		for (int i = 0, n = files.length; i < n; i++) {
 			FileWrapper srcFile = files[i];
-			FileWrapper destFile = destDir.child(srcFile.name());
-			if (srcFile.isDirectory())
+			if (srcFile.isDirectory()) {
+				FileWrapper destFile = destDir.child(srcFile.name());
 				copyDirectory(srcFile, destFile, filter, assets);
-			else
-				copyFile(srcFile, destFile, filter, assets);
+			} else {
+				FileWrapper destFile = destDir.child(fileNameWithMd5(srcFile, srcFile.readBytes()));
+				copyFile(srcFile, destDir.child(srcFile.name()).path(), destFile, filter, assets);
+			}
 		}
 	}
 
@@ -268,4 +280,24 @@ public class PreloaderBundleGenerator extends Generator {
 		sourceWriter.commit(logger);
 		return packageName + "." + className;
 	}
+
+	private static String fileNameWithMd5(FileWrapper fw, byte[] bytes) {
+		String md5;
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			digest.update(bytes);
+			md5 = String.format("%032x", new BigInteger(1, digest.digest()));
+		} catch (NoSuchAlgorithmException e) {
+			// Fallback
+			md5 = String.valueOf(System.currentTimeMillis());
+		}
+
+		String nameWithMd5 = fw.nameWithoutExtension() + "-" + md5;
+		String extension = fw.extension();
+		if (!extension.isEmpty() || fw.name().endsWith(".")) {
+			nameWithMd5 = nameWithMd5 + "." + extension;
+		}
+		return nameWithMd5;
+	}
+
 }

tests/gdx-tests-gwt/res/com/badlogic/gdx/tests/gwt/GdxTestsGwt.gwt.xml
@@ -7,5 +7,6 @@
 	<inherits name="com.badlogic.gdx.controllers.controllers-gwt"/>
 	<set-configuration-property name="gdx.assetpath" value="../gdx-tests-android/assets"/>
 	<set-configuration-property name='xsiframe.failIfScriptTag' value='FALSE'/>
-	<entry-point class='com.badlogic.gdx.tests.gwt.client.GwtTestStarter' />	
+	<entry-point class='com.badlogic.gdx.tests.gwt.client.GwtTestStarter' />
+	<set-configuration-property name="gdx.assetfilterclass" value="com.badlogic.gdx.tests.gwt.client.GwtTestAssetFilter"/>
 </module>

tests/gdx-tests-gwt/src/com/badlogic/gdx/tests/gwt/client/GwtTestAssetFilter.java
@@ -1,0 +1,10 @@
+package com.badlogic.gdx.tests.gwt.client;
+
+import com.badlogic.gdx.backends.gwt.preloader.DefaultAssetFilter;
+
+public class GwtTestAssetFilter extends DefaultAssetFilter {
+    @Override
+    public boolean preload(String file) {
+        return !file.contains("gwt_lazy");
+    }
+}

tests/gdx-tests/src/com/badlogic/gdx/tests/AssetManagerTest.java
@@ -81,7 +81,7 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 // Gdx.app.setLogLevel(Logger.DEBUG);
 		start = TimeUtils.nanoTime();
 		tex1 = new Texture("data/animation.png");
-		tex2 = new TextureAtlas(Gdx.files.internal("data/pack"));
+		tex2 = new TextureAtlas(Gdx.files.internal("data/pack.atlas"));
 		font2 = new BitmapFont(Gdx.files.internal("data/verdana39.fnt"), false);
 // tex3 = new Texture("data/test.etc1");
 // map = TiledLoader.createMap(Gdx.files.internal("data/tiledmap/tilemap csv.tmx"));
@@ -91,9 +91,11 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 		System.out.println("plain took: " + (TimeUtils.nanoTime() - start) / 1000000000.0f);
 
 		start = TimeUtils.nanoTime();
+		// this is a test for lazy loading on GWT
+		manager.load("data/animation_gwt_lazy.png", Texture.class);
 		manager.load("data/animation.png", Texture.class);
 // manager.load("data/pack1.png", Texture.class);
-		manager.load("data/pack", TextureAtlas.class);
+		manager.load("data/pack.atlas", TextureAtlas.class);
 // manager.load("data/verdana39.png", Texture.class);
 		manager.load("data/verdana39.fnt", BitmapFont.class);
 // manager.load("data/multipagefont.fnt", BitmapFont.class);
@@ -119,9 +121,10 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 // renderer.dispose();
 		shader.dispose();
 
+		manager.unload("data/animation_gwt_lazy.png");
 		manager.unload("data/animation.png");
 // manager.unload("data/pack1.png");
-		manager.unload("data/pack");
+		manager.unload("data/pack.atlas");
 // manager.unload("data/verdana39.png");
 		manager.unload("data/verdana39.fnt");
 // manager.unload("data/multipagefont.fnt");
@@ -163,10 +166,11 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 		else batch.setShader(null);
 		
 		batch.begin();
+		if (manager.isLoaded("data/animation_gwt_lazy.png")) batch.draw(manager.get("data/animation_gwt_lazy.png", Texture.class), 100, 100);
 		if (manager.isLoaded("data/animation.png")) batch.draw(manager.get("data/animation.png", Texture.class), 100, 100);
 		if (manager.isLoaded("data/verdana39.png")) batch.draw(manager.get("data/verdana39.png", Texture.class), 300, 100);
-		if (manager.isLoaded("data/pack"))
-			batch.draw(manager.get("data/pack", TextureAtlas.class).findRegion("particle-star"), 164, 100);
+		if (manager.isLoaded("data/pack.atlas"))
+			batch.draw(manager.get("data/pack.atlas", TextureAtlas.class).findRegion("particle-star"), 164, 100);
 		if (manager.isLoaded("data/verdana39.fnt"))
 			manager.get("data/verdana39.fnt", BitmapFont.class).draw(batch, "This is a test", 100, 80);
 		if (manager.isLoaded("data/multipagefont.fnt"))

tests/gdx-tests/src/com/badlogic/gdx/tests/BitmapFontMetricsTest.java
@@ -36,7 +36,7 @@ public class BitmapFontMetricsTest extends GdxTest {
 	@Override
 	public void create () {
 		spriteBatch = new SpriteBatch();
-		atlas = new TextureAtlas("data/pack");
+		atlas = new TextureAtlas("data/pack.atlas");
 		smallFont = new BitmapFont();
 		font = new BitmapFont(Gdx.files.internal("data/verdana39.fnt"), atlas.findRegion("verdana39"), false);
 		font = new BitmapFont(Gdx.files.internal("data/arial-32-pad.fnt"), false);

commit 5e76472a07423a67bded7da50ddbbb4d7eca0d3b

Fix sound and music not playing because of changed file names (#6187)
caused by #4314

backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtFileHandle.java
@@ -56,6 +56,11 @@ public class GwtFileHandle extends FileHandle {
 		this.file = fixSlashes(path);
 	}
 
+	/** @return The full url to an asset, e.g. http://localhost:8080/assets/data/shotgun-e5f56587d6f025bff049632853ae4ff9.ogg */
+	public String getAssetUrl() {
+		return preloader.baseUrl + preloader.assetNames.get(file, file);
+	}
+
 	public String path () {
 		return file;
 	}

backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/Preloader.java
@@ -53,6 +53,7 @@ public class Preloader {
 	public ObjectMap<String, String> texts = new ObjectMap<String, String>();
 	public ObjectMap<String, Blob> binaries = new ObjectMap<String, Blob>();
 	private ObjectMap<String, Asset> stillToFetchAssets = new ObjectMap<String, Asset>();
+	public ObjectMap<String, String> assetNames = new ObjectMap<String, String>();
 
 	public static class Asset {
 		public Asset(String file, String url, AssetType type, long size, String mimeType) {
@@ -159,6 +160,7 @@ public class Preloader {
 						size = 0;
 					}
 					Asset asset = new Asset(assetPathOrig.trim(), assetPathMd5.trim(), type, size, assetMimeType);
+					assetNames.put(asset.file, asset.url);
                     if (assetPreload || asset.file.startsWith("com/badlogic/"))
                         assets.add(asset);
                     else

backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/webaudio/WebAudioAPIManager.java
@@ -20,12 +20,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.LifecycleListener;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.GwtFileHandle;
 import com.badlogic.gdx.backends.gwt.preloader.AssetDownloader;
-import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.files.FileHandle;
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.dom.client.CanvasElement;
 import com.google.gwt.media.client.Audio;
 import com.google.gwt.typedarrays.shared.ArrayBuffer;
 import com.google.gwt.typedarrays.shared.Int8Array;
@@ -163,7 +161,7 @@ public class WebAudioAPIManager implements LifecycleListener {
 	public Sound createSound (FileHandle fileHandle) {
 		final WebAudioAPISound newSound = new WebAudioAPISound(audioContext, globalVolumeNode, audioControlGraphPool);
 
-		String url = ((GwtApplication)Gdx.app).getBaseUrl() + fileHandle.path();
+		String url = ((GwtFileHandle) fileHandle).getAssetUrl();
 
 		XMLHttpRequest request = XMLHttpRequest.create();
 		request.setOnReadyStateChange(new ReadyStateChangeHandler() {
@@ -192,7 +190,7 @@ public class WebAudioAPIManager implements LifecycleListener {
 	}
 
 	public Music createMusic (FileHandle fileHandle) {
-		String url = ((GwtApplication)Gdx.app).getBaseUrl() + fileHandle.path();
+		String url = ((GwtFileHandle) fileHandle).getAssetUrl();
 
 		Audio audio = Audio.createIfSupported();
 		audio.setSrc(url);