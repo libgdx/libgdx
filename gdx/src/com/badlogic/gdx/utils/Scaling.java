6b7c194085baf4b5ec7b7231024a143429a24bf4
NathanSweet <nathan.sweet@gmail.com>
2020-11-14 16:22:14 -0800

    Clean up.

--- gdx/src/com/badlogic/gdx/utils/Scaling.java
+++ gdx/src/com/badlogic/gdx/utils/Scaling.java
@@ -24,19 +24,18 @@ public enum Scaling {
 	/** Scales the source to fit the target while keeping the same aspect ratio. This may cause the source to be smaller than the
 	 * target in one direction. */
 	fit,
+	/** Scales the source to fit the target while keeping the same aspect ratio, but the source is not scaled at all if smaller in
+	 * both directions. This may cause the source to be smaller than the target in one or both directions. */
+	contain,
 	/** Scales the source to fill the target while keeping the same aspect ratio. This may cause the source to be larger than the
 	 * target in one direction. */
 	fill,
-	/** Scales the source to fill the target in the x direction while keeping the same aspect ratio. This may cause the source to be
-	 * smaller or larger than the target in the y direction. */
+	/** Scales the source to fill the target in the x direction while keeping the same aspect ratio. This may cause the source to
+	 * be smaller or larger than the target in the y direction. */
 	fillX,
-	/** Scales the source to fill the target in the y direction while keeping the same aspect ratio. This may cause the source to be
-	 * smaller or larger than the target in the x direction. */
+	/** Scales the source to fill the target in the y direction while keeping the same aspect ratio. This may cause the source to
+	 * be smaller or larger than the target in the x direction. */
 	fillY,
-	/** Scales the source to be contained within the target while keeping the same aspect ratio. This may cause the source to be
-	 * smaller than the target in one direction. If the source is smaller than the target on both sides it will not be scaled at
-	 * all. */
-	contain,
 	/** Scales the source to fill the target. This may cause the source to not keep the same aspect ratio. */
 	stretch,
 	/** Scales the source to fill the target in the x direction, without changing the y direction. This may cause the source to not
@@ -62,6 +61,15 @@ public enum Scaling {
 			temp.y = sourceHeight * scale;
 			break;
 		}
+		case contain: {
+			float targetRatio = targetHeight / targetWidth;
+			float sourceRatio = sourceHeight / sourceWidth;
+			float scale = targetRatio > sourceRatio ? targetWidth / sourceWidth : targetHeight / sourceHeight;
+			if (scale > 1) scale = 1;
+			temp.x = sourceWidth * scale;
+			temp.y = sourceHeight * scale;
+			break;
+		}
 		case fill: {
 			float targetRatio = targetHeight / targetWidth;
 			float sourceRatio = sourceHeight / sourceWidth;
@@ -78,17 +86,6 @@ public enum Scaling {
 		}
 		case fillY: {
 			float scale = targetHeight / sourceHeight;
-			temp.x = sourceWidth * scale;
-			temp.y = sourceHeight * scale;
-			break;
-		}
-		case contain: {
-			float targetRatio = targetHeight / targetWidth;
-			float sourceRatio = sourceHeight / sourceWidth;
-			float scale = targetRatio > sourceRatio ? targetWidth / sourceWidth : targetHeight / sourceHeight;
-			if (scale > 1) {
-				scale = 1;   // If we're going to scale up, then don't.
-			}
 			temp.x = sourceWidth * scale;
 			temp.y = sourceHeight * scale;
 			break;
