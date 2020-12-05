1fd195bb335fc6cd86e46ade1c259ec71d3437bd
Benjamin Schulte <bs@golfgl.de>
2020-12-05 11:07:27 +0100

    Fix formatting

--- gdx/src/com/badlogic/gdx/Input.java
+++ gdx/src/com/badlogic/gdx/Input.java
@@ -252,18 +252,18 @@ public interface Input {
 		public static final int F10 = 253;
 		public static final int F11 = 254;
 		public static final int F12 = 255;
-		public static final int F13	= 183;
-		public static final int F14	= 184;
-		public static final int F15	= 185;
-		public static final int F16	= 186;
-		public static final int F17	= 187;
-		public static final int F18	= 188;
-		public static final int F19	= 189;
-		public static final int F20	= 190;
-		public static final int F21	= 191;
-		public static final int F22	= 192;
-		public static final int F23	= 193;
-		public static final int F24	= 194;
+		public static final int F13 = 183;
+		public static final int F14 = 184;
+		public static final int F15 = 185;
+		public static final int F16 = 186;
+		public static final int F17 = 187;
+		public static final int F18 = 188;
+		public static final int F19 = 189;
+		public static final int F20 = 190;
+		public static final int F21 = 191;
+		public static final int F22 = 192;
+		public static final int F23 = 193;
+		public static final int F24 = 194;
 
 		public static final int MAX_KEYCODE = 255;
 
