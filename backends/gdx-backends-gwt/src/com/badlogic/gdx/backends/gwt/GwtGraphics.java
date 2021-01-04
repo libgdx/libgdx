e92e3066925785e7f966b2ae45b273f01ac546bf
obigu <torinthechosen@gmail.com>
2021-01-04 17:25:30 +0100

    Add an Abstract Graphics class (#6336)

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtGraphics.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/GwtGraphics.java
@@ -16,6 +16,7 @@
 
 package com.badlogic.gdx.backends.gwt;
 
+import com.badlogic.gdx.AbstractGraphics;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
@@ -34,7 +35,7 @@ import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.webgl.client.WebGLContextAttributes;
 import com.google.gwt.webgl.client.WebGLRenderingContext;
 
-public class GwtGraphics implements Graphics {
+public class GwtGraphics extends AbstractGraphics {
 
 	/* Enum values from http://www.w3.org/TR/screen-orientation. Filtered based on what the browsers actually support. */
 	public enum OrientationLockType {
@@ -516,11 +517,6 @@ public class GwtGraphics implements Graphics {
 	public void setVSync (boolean vsync) {
 	}
 
-	@Override
-	public float getDensity () {
-		return (getPpiX()) / 160;
-	}
-
 	/**
 	 * See https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio for more information
 	 *
@@ -543,11 +539,6 @@ public class GwtGraphics implements Graphics {
 
 	@Override
 	public void requestRendering () {
-	}
-
-	@Override
-	public float getRawDeltaTime () {
-		return getDeltaTime();
 	}
 
 	@Override
