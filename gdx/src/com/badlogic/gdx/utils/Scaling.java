8d876bd802e75807130729e2b3791ef944156b87
Nathan Sweet <nathan.sweet@gmail.com>
2021-02-12 15:12:54 +0100

    Scaling formatting.
    #6391

--- gdx/src/com/badlogic/gdx/utils/Scaling.java
+++ gdx/src/com/badlogic/gdx/utils/Scaling.java
@@ -21,10 +21,15 @@ import com.badlogic.gdx.math.Vector2;
 /** Various scaling types for fitting one rectangle into another.
  * @author Nathan Sweet */
 public abstract class Scaling {
+	protected static final Vector2 temp = new Vector2();
+
+	/** Returns the size of the source scaled to the target. Note the same Vector2 instance is always returned and should never be
+	 * cached. */
+	public abstract Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight);
+
 	/** Scales the source to fit the target while keeping the same aspect ratio. This may cause the source to be smaller than the
 	 * target in one direction. */
 	public static final Scaling fit = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float targetRatio = targetHeight / targetWidth;
 			float sourceRatio = sourceHeight / sourceWidth;
@@ -34,10 +39,10 @@ public abstract class Scaling {
 			return temp;
 		}
 	};
+
 	/** Scales the source to fill the target while keeping the same aspect ratio. This may cause the source to be larger than the
 	 * target in one direction. */
 	public static final Scaling fill = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float targetRatio = targetHeight / targetWidth;
 			float sourceRatio = sourceHeight / sourceWidth;
@@ -47,10 +52,10 @@ public abstract class Scaling {
 			return temp;
 		}
 	};
+
 	/** Scales the source to fill the target in the x direction while keeping the same aspect ratio. This may cause the source to
 	 * be smaller or larger than the target in the y direction. */
 	public static final Scaling fillX = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float scale = targetWidth / sourceWidth;
 			temp.x = sourceWidth * scale;
@@ -58,10 +63,10 @@ public abstract class Scaling {
 			return temp;
 		}
 	};
+
 	/** Scales the source to fill the target in the y direction while keeping the same aspect ratio. This may cause the source to
 	 * be smaller or larger than the target in the x direction. */
 	public static final Scaling fillY = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			float scale = targetHeight / sourceHeight;
 			temp.x = sourceWidth * scale;
@@ -69,48 +74,42 @@ public abstract class Scaling {
 			return temp;
 		}
 	};
+
 	/** Scales the source to fill the target. This may cause the source to not keep the same aspect ratio. */
 	public static final Scaling stretch = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = targetWidth;
 			temp.y = targetHeight;
 			return temp;
 		}
 	};
+
 	/** Scales the source to fill the target in the x direction, without changing the y direction. This may cause the source to not
 	 * keep the same aspect ratio. */
 	public static final Scaling stretchX = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = targetWidth;
 			temp.y = sourceHeight;
 			return temp;
 		}
 	};
+
 	/** Scales the source to fill the target in the y direction, without changing the x direction. This may cause the source to not
 	 * keep the same aspect ratio. */
 	public static final Scaling stretchY = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = sourceWidth;
 			temp.y = targetHeight;
 			return temp;
 		}
 	};
+
 	/** The source is not scaled. */
 	public static final Scaling none = new Scaling() {
-		@Override
 		public Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight) {
 			temp.x = sourceWidth;
 			temp.y = sourceHeight;
 			return temp;
 		}
 	};
-
-	protected static final Vector2 temp = new Vector2();
-
-	/** Returns the size of the source scaled to the target. Note the same Vector2 instance is always returned and should never be
-	 * cached. */
-	public abstract Vector2 apply (float sourceWidth, float sourceHeight, float targetWidth, float targetHeight);
 }
