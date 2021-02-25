2e2f6ed17765c824ae79948fe5536fa236fe29a1
Ondřej Fibich <ondrej.fibich@gmail.com>
2021-02-25 10:02:46 +0100

    Fixes #6433: queuing network tasks if configuration option `maxNetThreads` is set to non-default value (other than MAX_INTEGER).
    If non-default value is set then the ThreadPoolExecutor used by NetJavaImpl is created with:
    - core pool size to `maxNetThreads` (instead of 0)
    - enables `ThreadPoolExecutor#allowCoreThreadTimeOut` to remove core pool after the specified timeout
    - work queue type `LinkedBlockingQueue` (instead of SynchronousQueue which does not accept any tasks to be queued)
    
    The behavior if `maxNetThreads` is set to default value remain unchanged. Thus this change should not cause any regression.

--- gdx/src/com/badlogic/gdx/net/NetJavaImpl.java
+++ gdx/src/com/badlogic/gdx/net/NetJavaImpl.java
@@ -25,7 +25,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -123,7 +123,7 @@ public class NetJavaImpl {
 		}
 	}
 
-	private final ExecutorService executorService;
+	private final ThreadPoolExecutor executorService;
 	final ObjectMap<HttpRequest, HttpURLConnection> connections;
 	final ObjectMap<HttpRequest, HttpResponseListener> listeners;
 
@@ -132,9 +132,11 @@ public class NetJavaImpl {
 	}
 
 	public NetJavaImpl (int maxThreads) {
-		executorService = new ThreadPoolExecutor(0, maxThreads,
+		final boolean isCachedPool = maxThreads == Integer.MAX_VALUE;
+		executorService = new ThreadPoolExecutor(
+				isCachedPool ? 0 : maxThreads, maxThreads,
 				60L, TimeUnit.SECONDS,
-				new SynchronousQueue<Runnable>(),
+				isCachedPool ? new SynchronousQueue<Runnable>() : new LinkedBlockingQueue<Runnable>(),
 				new ThreadFactory() {
 					AtomicInteger threadID = new AtomicInteger();
 					@Override
@@ -144,6 +146,7 @@ public class NetJavaImpl {
 						return thread;
 					}
 				});
+		executorService.allowCoreThreadTimeOut(!isCachedPool);
 		connections = new ObjectMap<HttpRequest, HttpURLConnection>();
 		listeners = new ObjectMap<HttpRequest, HttpResponseListener>();
 	}
