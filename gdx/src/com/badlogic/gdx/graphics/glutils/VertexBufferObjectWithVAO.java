bebd74532e6e98343d06451b59b31c7d28d975e8
RUSshy <ismaalove@gmail.com>
2021-03-01 16:20:00 +0100

    Let's not forget to unbind

--- gdx/src/com/badlogic/gdx/graphics/glutils/VertexBufferObjectWithVAO.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/VertexBufferObjectWithVAO.java
@@ -248,6 +248,7 @@ public class VertexBufferObjectWithVAO implements VertexData {
 	public void unbind (final ShaderProgram shader, final int[] locations) {
 		GL30 gl = Gdx.gl30;
 		gl.glBindVertexArray(0);
+		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
 		isBound = false;
 	}
 
