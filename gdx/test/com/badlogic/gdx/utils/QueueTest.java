538c8f9c8b3a13e1b4832909a1f6531fa7160f70
Colin Geukes <colingeukes@gmail.com>
2021-03-07 14:26:49 +0100

    Improve test cases of Queue

--- gdx/test/com/badlogic/gdx/utils/QueueTest.java
+++ gdx/test/com/badlogic/gdx/utils/QueueTest.java
@@ -4,12 +4,26 @@ package com.badlogic.gdx.utils;
 import static org.junit.Assert.*;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class QueueTest {
-	@Test
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void initializeWithType() {
+        Queue<String> queue = new Queue<String>(1, String.class);
+        queue.addFirst("a");
+        assertEquals("a", queue.get(0));
+    }
+
+    @Test
 	public void addFirstAndLastTest() {
 		Queue<Integer> queue = new Queue<Integer>();
 		queue.addFirst(1);
@@ -342,6 +356,19 @@ public class QueueTest {
 		assertTrue(q.toString().equals("[4, 5, 6, 7]"));
 	}
 
+    @Test
+    public void toStringTestWithSeperator () {
+	    String separator = ";";
+        Queue<Integer> q = new Queue<Integer>(1);
+        assertEquals(q.toString(separator), "");
+        q.addLast(4);
+        assertEquals(q.toString(separator),"4");
+        q.addLast(5);
+        q.addLast(6);
+        q.addLast(7);
+        assertEquals(q.toString(separator),"4;5;6;7");
+    }
+
 	@Test
 	public void hashEqualsTest () {
 		Queue<Integer> q1 = new Queue<Integer>();
@@ -374,7 +401,222 @@ public class QueueTest {
 		}
 	}
 
-	private void assertEqualsAndHash (Queue<?> q1, Queue<?> q2) {
+	@Test
+    public void emptyQueueLast(){
+        Queue<Integer> queue = new Queue<>();
+
+        // Queue is empty and an error should be thrown.
+        exception.expect(NoSuchElementException.class);
+        queue.last();
+    }
+
+    @Test
+    public void emptyQueueRemoveFirst(){
+        Queue<Integer> queue = new Queue<>();
+
+        // Queue is empty and an error should be thrown.
+        exception.expect(NoSuchElementException.class);
+        queue.removeFirst();
+    }
+
+    @Test
+    public void emptyQueueRemoveLast(){
+        Queue<Integer> queue = new Queue<>();
+
+        // Queue is empty and an error should be thrown.
+        exception.expect(NoSuchElementException.class);
+        queue.removeLast();
+    }
+
+    @Test
+    public void emptyQueueFirst(){
+        Queue<Integer> queue = new Queue<>();
+
+        // Queue is empty and an error should be thrown.
+        exception.expect(NoSuchElementException.class);
+        queue.first();
+    }
+
+    @Test
+    public void indexOfNotFound() {
+        Queue<Integer> queue = new Queue<>();
+
+        // Check an existing element
+	    queue.addFirst(1);
+        assertEquals(0, queue.indexOf(1, true));
+
+        // Check a non existing element.
+        assertEquals(-1, queue.indexOf(42, true));
+    }
+
+    @Test
+    public void removeValueFound() {
+        Queue<Integer> queue = new Queue<>();
+
+        // Check an existing element
+        queue.addFirst(1);
+        assertEquals(0, queue.indexOf(1, true));
+
+        // Remove the element and check again for existence.
+        assertTrue(queue.removeValue(1, true));
+        assertEquals(-1, queue.indexOf(1, true));
+    }
+
+    @Test
+    public void removeNotFound() {
+        Queue<Integer> queue = new Queue<>();
+
+        // Check an existing element
+        assertEquals(-1, queue.indexOf(1, true));
+
+        // Remove the element and check again for existence.
+        assertFalse(queue.removeValue(1, true));
+    }
+
+
+    @Test
+    public void ensureCapacityFit() {
+	    // Create the queue.
+        Queue<String> queue = new Queue<String>(2, String.class);
+
+        // Enough space, does not change.
+        assertEquals(2, queue.values.length);
+        queue.ensureCapacity(2);
+        assertEquals(2, queue.values.length);
+    }
+
+    @Test
+    public void ensureCapacityNotFit() {
+        // Create the queue.
+        Queue<String> queue = new Queue<>(2, String.class);
+
+        // Add an element.
+        queue.addFirst("first");
+
+        // Enough space, does not change.
+        assertEquals(2, queue.values.length);
+        queue.ensureCapacity(2);
+        assertEquals(3, queue.values.length);
+    }
+
+    @Test
+    public void isEmpty() {
+        // Create the queue
+        Queue<String> queue = new Queue<>();
+
+        assertTrue(queue.isEmpty());
+        queue.addFirst("element");
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void notEmpty() {
+        // Create the queue
+        Queue<String> queue = new Queue<>();
+
+        assertFalse(queue.notEmpty());
+        queue.addFirst("element");
+        assertTrue(queue.notEmpty());
+    }
+
+    @Test
+    public void equalsIdentitySameObject(){
+	    Queue<String> queue = new Queue<>();
+        assertTrue(queue.equalsIdentity(queue));
+    }
+
+    @Test
+    public void equalsIdentityDifferentObject(){
+        Queue<String> queue = new Queue<>();
+
+        assertFalse(queue.equalsIdentity(null));
+        assertFalse(queue.equalsIdentity("different"));
+    }
+
+    @Test
+    public void equalsIdentityDifferentQueueSameObjects(){
+        // Initialize queues.
+	    Queue<String> queue1 = new Queue<>();
+        Queue<String> queue2 = new Queue<>();
+
+        // Check empty equality
+        assertTrue(queue1.equalsIdentity(queue2));
+        assertTrue(queue2.equalsIdentity(queue1));
+
+        // Check inequality.
+        queue1.addLast("1");
+        assertFalse(queue1.equalsIdentity(queue2));
+        assertFalse(queue2.equalsIdentity(queue1));
+
+        // Check equality.
+        queue2.addLast("1");
+        assertTrue(queue1.equalsIdentity(queue2));
+        assertTrue(queue2.equalsIdentity(queue1));
+    }
+
+    @Test
+    public void initializeQueueIterator() {
+	    // Initialize queue and iterator.
+        Queue<String> queue = new Queue<>();
+        queue.addLast("remove");
+        Queue.QueueIterator<String> iterator = new Queue.QueueIterator<>(queue);
+
+        // Allow remove is set to true, thus test a remove case.
+        assertEquals(1, queue.size);
+        iterator.next();
+        iterator.remove();
+        assertEquals(0, queue.size);
+    }
+
+    @Test
+    public void queueIteratorInvalidHasNext() {
+        // Initialize queue and iterator.
+        Queue<Integer> queue = new Queue<>();
+        Queue.QueueIterator<Integer> iterator = new Queue.QueueIterator<>(queue);
+        iterator.valid = false;
+
+        // GdxRuntimeException will be thrown by check hasNext of invalid iterator.
+        exception.expect(GdxRuntimeException.class);
+        boolean hasNext = iterator.hasNext();
+    }
+
+    @Test
+    public void queueIteratorInvalidNext() {
+        // Initialize queue and iterator.
+        Queue<String> queue = new Queue<>();
+        Queue.QueueIterator<String> iterator = new Queue.QueueIterator<>(queue);
+        iterator.valid = false;
+        queue.addLast("1");
+
+        // GdxRuntimeException will be thrown by check hasNext of invalid iterator.
+        exception.expect(GdxRuntimeException.class);
+        String next = iterator.next();
+    }
+
+    @Test
+    public void queueIteratorReset() {
+        // Initialize queue and iterator.
+        Queue<String> queue = new Queue<>();
+        queue.addLast("element");
+        Queue.QueueIterator<String> iterator = new Queue.QueueIterator<>(queue);
+
+        assertEquals(0, iterator.index);
+        assertEquals("element", iterator.next());
+        assertEquals(1, iterator.index);
+        iterator.reset();
+        assertEquals(0, iterator.index);
+    }
+
+    @Test
+    public void queueIteratorGetIterator() {
+        // Initialize queue and iterator.
+        Queue<String> queue = new Queue<>();
+        Queue.QueueIterator<String> iterator = new Queue.QueueIterator<>(queue);
+
+        assertEquals(iterator, iterator.iterator());
+    }
+
+    private void assertEqualsAndHash (Queue<?> q1, Queue<?> q2) {
 		assertEquals(q1, q2);
 		assertEquals("Hash codes are not equal", q1.hashCode(), q2.hashCode());
 	}
