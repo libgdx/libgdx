58208e255bb6ac910f8239591c394998d218a9a9
Mario Zechner <badlogicgames@gmail.com>
2021-03-15 10:51:05 +0100

    Add iOS arm32 builds back in, remove tvOS builds.

--- backends/gdx-backend-robovm/build-objectal.sh
+++ backends/gdx-backend-robovm/build-objectal.sh
@@ -12,22 +12,15 @@ tar xvfz $BUILD_DIR/objectal.tar.gz -C $BUILD_DIR --strip-components 1
 
 XCODEPROJ=$BUILD_DIR/ObjectAL/ObjectAL.xcodeproj
 
+xcodebuild -project $XCODEPROJ -arch armv7  -sdk iphoneos         CONFIGURATION_BUILD_DIR=$BUILD_DIR/armv7  OTHER_CFLAGS="-fembed-bitcode -target armv7-apple-ios9.0.0"
 xcodebuild -project $XCODEPROJ -arch arm64  -sdk iphoneos         CONFIGURATION_BUILD_DIR=$BUILD_DIR/arm64  OTHER_CFLAGS="-fembed-bitcode -target arm64-apple-ios9.0.0"
 xcodebuild -project $XCODEPROJ -arch x86_64 -sdk iphonesimulator  CONFIGURATION_BUILD_DIR=$BUILD_DIR/x86_64 OTHER_CFLAGS="-target x86_64-apple-ios9.0.0"
-xcodebuild -project $XCODEPROJ -arch arm64  -sdk appletvos        CONFIGURATION_BUILD_DIR=$BUILD_DIR/tvos-arm64 OTHER_CFLAGS="-fembed-bitcode -target arm64-apple-tvos9.0.0"
-xcodebuild -project $XCODEPROJ -arch x86_64 -sdk appletvsimulator CONFIGURATION_BUILD_DIR=$BUILD_DIR/tvos-x86_64 OTHER_CFLAGS="-target x86_64-apple-tvos9.0.0"
 
-lipo $BUILD_DIR/arm64/libObjectAL.a \
+lipo $BUILD_DIR/armv7/libObjectAL.a \
+     $BUILD_DIR/arm64/libObjectAL.a \
      $BUILD_DIR/x86_64/libObjectAL.a \
      -create \
      -output $BUILD_DIR/libObjectAL.a
 
-cp $BUILD_DIR/libObjectAL.a $BASE/../../gdx/libs/ios32/
-
-lipo $BUILD_DIR/tvos-arm64/libObjectAL.a \
-     $BUILD_DIR/tvos-x86_64/libObjectAL.a \
-     -create \
-     -output $BUILD_DIR/libObjectAL.a.tvos
-
 mkdir -p $BASE/../../gdx/libs/ios32/
-cp $BUILD_DIR/libObjectAL.a.tvos $BASE/../../gdx/libs/ios32/
+cp $BUILD_DIR/libObjectAL.a $BASE/../../gdx/libs/ios32/
