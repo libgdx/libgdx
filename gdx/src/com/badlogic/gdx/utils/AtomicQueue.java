7a7bdc1201fa726224398f55bae06ca5f7db3629
Colin Geukes <colingeukes@gmail.com>
2021-03-22 14:34:12 +0100

    Revert style changes

--- gdx/src/com/badlogic/gdx/utils/AtomicQueue.java
+++ gdx/src/com/badlogic/gdx/utils/AtomicQueue.java
@@ -29,34 +29,35 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
  * not call these methods.
  * @author Matthias Mann */
 public class AtomicQueue<T> {
-    private final AtomicInteger writeIndex = new AtomicInteger();
-    private final AtomicInteger readIndex = new AtomicInteger();
-    private final AtomicReferenceArray<T> queue;
+	private final AtomicInteger writeIndex = new AtomicInteger();
+	private final AtomicInteger readIndex = new AtomicInteger();
+	private final AtomicReferenceArray<T> queue;
 
-    public AtomicQueue (int capacity) {
-        queue = new AtomicReferenceArray(capacity);
-    }
+	public AtomicQueue (int capacity) {
+		queue = new AtomicReferenceArray(capacity);
+	}
 
-    private int next (int idx) {
-        return (idx + 1) % queue.length();
-    }
+	private int next (int idx) {
+		return (idx + 1) % queue.length();
+	}
 
-    public boolean put (@Null T value) {
-        int write = writeIndex.get();
-        int read = readIndex.get();
-        int next = next(write);
-        if (next == read) return false;
-        queue.set(write, value);
-        writeIndex.set(next);
-        return true;
-    }
+	public boolean put (@Null T value) {
+		int write = writeIndex.get();
+		int read = readIndex.get();
+		int next = next(write);
+		if (next == read) return false;
+		queue.set(write, value);
+		writeIndex.set(next);
+		return true;
+	}
 
-    public @Null T poll () {
-        int read = readIndex.get();
-        int write = writeIndex.get();
-        if (read == write) return null;
-        T value = queue.get(read);
-        readIndex.set(next(read));
-        return value;
-    }
+	public @Null
+	T poll () {
+		int read = readIndex.get();
+		int write = writeIndex.get();
+		if (read == write) return null;
+		T value = queue.get(read);
+		readIndex.set(next(read));
+		return value;
+	}
 }
