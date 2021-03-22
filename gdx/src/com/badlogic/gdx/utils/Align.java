f65c43664d8df582473d264976afadd9b284bc73
Colin Geukes <colingeukes@gmail.com>
2021-03-22 14:25:42 +0100

    Revert ALL documentation changes

--- gdx/src/com/badlogic/gdx/utils/Align.java
+++ gdx/src/com/badlogic/gdx/utils/Align.java
@@ -16,113 +16,58 @@
 
 package com.badlogic.gdx.utils;
 
-/**
- * Provides bit flag constants for alignment.
- *
- * @author Nathan Sweet
- */
+/** Provides bit flag constants for alignment.
+ * @author Nathan Sweet */
 public class Align {
-    static public final int center = 0x1;
-    static public final int top = 0x10;
-    static public final int bottom = 0x100;
-    static public final int left = 0x1000;
-    static public final int right = 0x10000;
+	static public final int center = 1 << 0;
+	static public final int top = 1 << 1;
+	static public final int bottom = 1 << 2;
+	static public final int left = 1 << 3;
+	static public final int right = 1 << 4;
 
-    static public final int topLeft = top | left;
-    static public final int topRight = top | right;
-    static public final int bottomLeft = bottom | left;
-    static public final int bottomRight = bottom | right;
+	static public final int topLeft = top | left;
+	static public final int topRight = top | right;
+	static public final int bottomLeft = bottom | left;
+	static public final int bottomRight = bottom | right;
 
-    /**
-     * Return true if the Align is left.
-     *
-     * @param align The Align you want to check.
-     * @return true if Align is left, else false.
-     */
-    static public final boolean isLeft (int align) {
-        return (align & left) != 0;
-    }
+	static public final boolean isLeft (int align) {
+		return (align & left) != 0;
+	}
 
-    /**
-     * Return true if the Align is right.
-     *
-     * @param align The Align you want to check.
-     * @return true if Align is right, else false.
-     */
-    static public final boolean isRight (int align) {
-        return (align & right) != 0;
-    }
+	static public final boolean isRight (int align) {
+		return (align & right) != 0;
+	}
 
-    /**
-     * Return true if the Align is top.
-     *
-     * @param align The Align you want to check.
-     * @return true if Align is top, else false.
-     */
-    static public final boolean isTop (int align) {
-        return (align & top) != 0;
-    }
+	static public final boolean isTop (int align) {
+		return (align & top) != 0;
+	}
 
-    /**
-     * Return true if the Align is bottom.
-     *
-     * @param align The Align you want to check.
-     * @return true if Align is bottom, else false.
-     */
-    static public final boolean isBottom (int align) {
-        return (align & bottom) != 0;
-    }
+	static public final boolean isBottom (int align) {
+		return (align & bottom) != 0;
+	}
 
-    /**
-     * Return true if the Align is left, center, right.
-     *
-     * @param align The Align you want to check.
-     * @return true if Align is left, center, or right, else false.
-     */
-    static public final boolean isCenterVertical (int align) {
-        return (align & top) == 0 && (align & bottom) == 0;
-    }
+	static public final boolean isCenterVertical (int align) {
+		return (align & top) == 0 && (align & bottom) == 0;
+	}
 
-    /**
-     * Return true if the Align is top, center, bottom.
-     *
-     * @param align The Align you want to check.
-     * @return true if Align is top, center, or bottom, else false.
-     */
-    static public final boolean isCenterHorizontal (int align) {
-        return (align & left) == 0 && (align & right) == 0;
-    }
+	static public final boolean isCenterHorizontal (int align) {
+		return (align & left) == 0 && (align & right) == 0;
+	}
 
-    /**
-     * Transform an align to a fully written alignment string.
-     * The return is in the way  of (y,x), where
-     * y can be either (top, bottom, center) and
-     * x can be either (top, bottom, center)
-     *
-     * @param align The align you want to transform to a written representation.
-     * @return a string representation in written words of the align.
-     */
-    static public String toString (int align) {
-        // Create string buffer of maximum return size.
-        StringBuilder buffer = new StringBuilder(13);
-
-        // Write the vertical representation.
-        if ((align & top) != 0)
-            buffer.append("top,");
-        else if ((align & bottom) != 0)
-            buffer.append("bottom,");
-        else
-            buffer.append("center,");
-
-        // Write the horizontal representation.
-        if ((align & left) != 0)
-            buffer.append("left");
-        else if ((align & right) != 0)
-            buffer.append("right");
-        else
-            buffer.append("center");
-
-        // Return the written representation.
-        return buffer.toString();
-    }
+	static public String toString (int align) {
+		StringBuilder buffer = new StringBuilder(13);
+		if ((align & top) != 0)
+			buffer.append("top,");
+		else if ((align & bottom) != 0)
+			buffer.append("bottom,");
+		else
+			buffer.append("center,");
+		if ((align & left) != 0)
+			buffer.append("left");
+		else if ((align & right) != 0)
+			buffer.append("right");
+		else
+			buffer.append("center");
+		return buffer.toString();
+	}
 }
