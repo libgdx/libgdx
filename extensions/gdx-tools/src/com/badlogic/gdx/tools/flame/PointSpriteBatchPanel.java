6ed1366853911bc5a43c918a7e6179033ed913c5
Germain AUBERT <germain.aubert@gmail.com>
2021-04-08 11:35:35 +0200

    improved PointSpriteParticleBatch options

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/flame/PointSpriteBatchPanel.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/flame/PointSpriteBatchPanel.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package com.badlogic.gdx.tools.flame;
 
 import java.awt.GridBagConstraints;
@@ -12,52 +28,32 @@ import javax.swing.JLabel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.particles.ParticleShader.AlignMode;
 import com.badlogic.gdx.graphics.g3d.particles.ParticleSorter;
-import com.badlogic.gdx.graphics.g3d.particles.batches.BillboardParticleBatch;
+import com.badlogic.gdx.graphics.g3d.particles.batches.PointSpriteParticleBatch;
 
-/** @author Inferno */
-public class BillboardBatchPanel extends EditorPanel<BillboardParticleBatch> {
-	JComboBox alignCombo;
-	JCheckBox useGPUBox;
+public class PointSpriteBatchPanel extends EditorPanel<PointSpriteParticleBatch> {
+
 	JComboBox sortCombo;
 	JComboBox srcBlendFunction, destBlendFunction;
 
-	public BillboardBatchPanel (FlameMain particleEditor3D, BillboardParticleBatch renderer) {
-		super(particleEditor3D, "Billboard Batch", "Renderer used to draw billboards particles.");
+	public PointSpriteBatchPanel (FlameMain particleEditor3D, PointSpriteParticleBatch renderer) {
+		super(particleEditor3D, "Point sprite Batch", "Renderer used to draw point sprite particles.");
 		initializeComponents(renderer);
 		setValue(renderer);
 	}
 
-	private void initializeComponents (BillboardParticleBatch renderer) {
-		//Align
-		alignCombo = new JComboBox();
-		alignCombo.setModel(new DefaultComboBoxModel(AlignModeWrapper.values()));
-		alignCombo.setSelectedItem(AlignModeWrapper.find(renderer.getAlignMode()));
-		alignCombo.addActionListener(new ActionListener() {
-			public void actionPerformed (ActionEvent event) {
-				AlignModeWrapper align = (AlignModeWrapper)alignCombo.getSelectedItem();
-				editor.getBillboardBatch().setAlignMode(align.mode);
-			}
-		});
-		
-		//Cpu/Gpu
-		useGPUBox = new JCheckBox();
-		useGPUBox.setSelected(renderer.isUseGPU());
-		useGPUBox.addChangeListener(new ChangeListener() {
-			public void stateChanged (ChangeEvent event) {
-				editor.getBillboardBatch().setUseGpu(useGPUBox.isSelected());
-			}
-		}); 
-		
-		//Sort
+	private void initializeComponents (PointSpriteParticleBatch renderer) {
+
+		// Sort
 		sortCombo = new JComboBox();
 		sortCombo.setModel(new DefaultComboBoxModel(SortMode.values()));
 		sortCombo.setSelectedItem(SortMode.find(renderer.getSorter()));
 		sortCombo.addActionListener(new ActionListener() {
 			public void actionPerformed (ActionEvent event) {
 				SortMode mode = (SortMode)sortCombo.getSelectedItem();
-				editor.getBillboardBatch().setSorter(mode.sorter);
+				editor.getPointSpriteBatch().setSorter(mode.sorter);
 			}
 		});
 
@@ -68,7 +64,7 @@ public class BillboardBatchPanel extends EditorPanel<BillboardParticleBatch> {
 		srcBlendFunction.addActionListener(new ActionListener() {
 			public void actionPerformed (ActionEvent event) {
 				BlendFunction blend = (BlendFunction)srcBlendFunction.getSelectedItem();
-				editor.getBillboardBatch().getBlendingAttribute().sourceFunction = blend.blend;
+				editor.getPointSpriteBatch().getBlendingAttribute().sourceFunction = blend.blend;
 			}
 		});
 
@@ -79,24 +75,16 @@ public class BillboardBatchPanel extends EditorPanel<BillboardParticleBatch> {
 		destBlendFunction.addActionListener(new ActionListener() {
 			public void actionPerformed (ActionEvent event) {
 				BlendFunction blend = (BlendFunction)destBlendFunction.getSelectedItem();
-				editor.getBillboardBatch().getBlendingAttribute().destFunction = blend.blend;
+				editor.getPointSpriteBatch().getBlendingAttribute().destFunction = blend.blend;
 			}
 		});
 
 		int i = 0;
-		Insets insets = new Insets(6, 0, 0, 0);
-		contentPanel.add(new JLabel("Align"),
-			new GridBagConstraints(0, i, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
-		contentPanel.add(alignCombo,
-			new GridBagConstraints(1, i++, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
-		contentPanel.add(new JLabel("Use GPU"),
-			new GridBagConstraints(0, i, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
-		contentPanel.add(useGPUBox,
-			new GridBagConstraints(1, i++, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
+		Insets insets = new Insets(3, 0, 0, 0);
 		contentPanel.add(new JLabel("Sort"),
 			new GridBagConstraints(0, i, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
 		contentPanel.add(sortCombo,
-			new GridBagConstraints(1, i++, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
+			new GridBagConstraints(1, i++, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
 		contentPanel.add(new JLabel("Blending Src"),
 			new GridBagConstraints(0, i, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
 		contentPanel.add(srcBlendFunction,
@@ -104,7 +92,7 @@ public class BillboardBatchPanel extends EditorPanel<BillboardParticleBatch> {
 		contentPanel.add(new JLabel("Blending Dest"),
 			new GridBagConstraints(0, i, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
 		contentPanel.add(destBlendFunction,
-			new GridBagConstraints(1, i++, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE,insets, 0, 0));
+			new GridBagConstraints(1, i++, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
 	}
 
 }
