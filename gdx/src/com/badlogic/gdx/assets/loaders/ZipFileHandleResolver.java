ac987d44017f483e3ea1358def2bbf83d8c70720
Andrey Fadeev <aaafdv@gmail.com>
2021-04-20 17:59:50 +1000

    [6212]: Add zipfile handle

---
+++ gdx/src/com/badlogic/gdx/assets/loaders/ZipFileHandleResolver.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.badlogic.gdx.assets.loaders;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.files.ZipFileHandle;
+
+import java.util.zip.ZipFile;
+
+public class ZipFileHandleResolver implements FileHandleResolver {
+	private final ZipFile archive;
+
+	public ZipFileHandleResolver (ZipFile archive) {
+		this.archive = archive;
+	}
+
+	@Override
+	public FileHandle resolve (String fileName) {
+		return new ZipFileHandle(archive, fileName);
+	}
+}
