89b4c86cf91d8d8af8ca51d015ec0365d9000d59
crykn <crykn@users.noreply.github.com>
2021-05-23 10:23:53 +0200

    Replace link to old blog; closes #6538.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEffect.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEffect.java
@@ -32,7 +32,8 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.StreamUtils;
 
-/** See <a href="http://www.badlogicgames.com/wordpress/?p=1255">http://www.badlogicgames.com/wordpress/?p=1255</a>
+/** See <a href=
+ * "https://web.archive.org/web/20200427191041/http://www.badlogicgames.com/wordpress/?p=12555">http://www.badlogicgames.com/wordpress/?p=12555</a>
  * @author mzechner */
 public class ParticleEffect implements Disposable {
 	private final Array<ParticleEmitter> emitters;
