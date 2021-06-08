26848da41afd761a1e3e2626632cc0de77680272
Tim Visser <tvisser@optimizers.nl>
2021-06-08 19:30:50 +0200

    Prevented the freeing of all renderables when setting a new Texture on a BillboardParticleBatch

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/BillboardParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/BillboardParticleBatch.java
@@ -275,7 +275,6 @@ public class BillboardParticleBatch extends BufferedParticleBatch<BillboardContr
 	}
 
 	public void setTexture (Texture texture) {
-		renderablePool.freeAll(renderables);
 		renderables.clear();
 		for (int i = 0, free = renderablePool.getFree(); i < free; ++i) {
 			Renderable renderable = renderablePool.obtain();
