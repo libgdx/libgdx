8a5e939d0336694c3305ad050d3cb594840406cd
Julien Villegas <julien.villegas@gmail.com>
2021-06-13 17:44:53 +0200

    Added Skin load in the AsserManager test
    Added Skin load in the AsserManager test

--- tests/gdx-tests/src/com/badlogic/gdx/tests/AssetManagerTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/AssetManagerTest.java
@@ -16,9 +16,6 @@
 
 package com.badlogic.gdx.tests;
 
-import java.nio.IntBuffer;
-import java.util.Locale;
-
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
@@ -36,14 +33,21 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.tests.utils.GdxTest;
 import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import java.nio.IntBuffer;
+import java.util.Locale;
+
 public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 	AssetManager manager;
 	BitmapFont font;
+	Skin skin;
+	Label skinLabel;
 	SpriteBatch batch;
 	int frame = 0;
 	int reloads = 0;
@@ -62,6 +66,8 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 		Texture.setAssetManager(manager);
 		batch = new SpriteBatch();
 		font = new BitmapFont(Gdx.files.internal("data/font.fnt"), false);
+		skin = new Skin();
+
 	}
 
 	boolean diagnosed = false;
@@ -108,7 +114,7 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 				vertexFile = "data/g2d/batchCommon.vert";
 			}
 		});
-		
+		manager.load("data/uiskin.json", Skin.class);
 	}
 
 	private void unload () {
@@ -132,6 +138,7 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 // manager.unload("data/tiledmap/tilemap csv.tmx");
 		manager.unload("data/i18n/message2");
 		manager.unload("data/g2d/monochrome.frag");
+		manager.unload("data/uiskin.json");
 	}
 
 	private void invalidateTexture (Texture texture) {
@@ -170,17 +177,25 @@ public class AssetManagerTest extends GdxTest implements AssetErrorListener {
 		if (manager.isLoaded("data/verdana39.png")) batch.draw(manager.get("data/verdana39.png", Texture.class), 300, 100);
 		if (manager.isLoaded("data/pack.atlas"))
 			batch.draw(manager.get("data/pack.atlas", TextureAtlas.class).findRegion("particle-star"), 164, 100);
-		if (manager.isLoaded("data/verdana39.fnt"))
+		if (manager.isLoaded("data/verdana39.fnt")) {
 			manager.get("data/verdana39.fnt", BitmapFont.class).draw(batch, "This is a test", 100, 80);
-		if (manager.isLoaded("data/multipagefont.fnt"))
+		}
+		if (manager.isLoaded("data/multipagefont.fnt")) {
 			manager.get("data/multipagefont.fnt", BitmapFont.class).draw(batch, "This is a test qpRPN multi page!", 100, 80);
-
+		}
 // System.out.println(Arrays.toString(manager.getAssetNames().items));
 
 // if (manager.isLoaded("data/test.etc1")) batch.draw(manager.get("data/test.etc1", Texture.class), 0, 0);
 // if (manager.isLoaded("data/tiledmap/tilemap csv.tmx")) manager.get("data/tiledmap/tilemap csv.tmx",
 // TileMapRenderer.class).render();
-		if (manager.isLoaded("data/i18n/message2")) font.draw(batch, manager.get("data/i18n/message2", I18NBundle.class).get("msg"), 100, 400);
+		if (manager.isLoaded("data/i18n/message2")){
+			font.draw(batch, manager.get("data/i18n/message2", I18NBundle.class).get("msg"), 100, 400);
+		}
+		if (manager.isLoaded("data/uiskin.json")){
+			skin = manager.get("data/uiskin.json",Skin.class);
+			skinLabel = new Label("label from a Asset Manager skin",skin,"default");
+			font.draw(batch, "Skin Loaded", 100, 420);
+		}
 
 		font.draw(batch, "loaded: " + manager.getProgress() + ", reloads: " + reloads, 0, 30);
 		batch.end();
