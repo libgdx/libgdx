3d9b43fd9462f61d2879218d0b53d4cca6618dfe
berstanio <sebastian_grunwald@web.de>
2021-06-13 23:44:21 +0200

    Expanded Reflection config based on the Pools.get/Pools.obtain calls.

--- backends/gdx-backend-moe/src/org/moe/svm/ConfigCollectionFeature.java
+++ backends/gdx-backend-moe/src/org/moe/svm/ConfigCollectionFeature.java
@@ -1,10 +1,19 @@
 package org.moe.svm;
 
+import com.badlogic.gdx.Net;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
+import com.badlogic.gdx.utils.Array;
 import com.oracle.svm.core.jni.JNIRuntimeAccess;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
+import com.badlogic.gdx.scenes.scene2d.actions.*;
 
 import java.nio.*;
 
@@ -18,6 +27,8 @@ public class ConfigCollectionFeature implements Feature {
             RuntimeReflection.register(GlyphLayout.class.getConstructor());
             RuntimeReflection.register(GlyphLayout.GlyphRun.class.getConstructor());
             RuntimeReflection.register(Color.class.getConstructor());
+            RuntimeReflection.register(Table.DebugRect.class.getConstructor());
+            RuntimeReflection.register(AddAction.class.getConstructor(), RemoveAction.class.getConstructor(), MoveToAction.class.getConstructor(), MoveByAction.class.getConstructor(), SizeToAction.class.getConstructor(), SizeByAction.class.getConstructor(), ScaleToAction.class.getConstructor(), ScaleByAction.class.getConstructor(), RotateToAction.class.getConstructor(), RotateByAction.class.getConstructor(), ColorAction.class.getConstructor(), AlphaAction.class.getConstructor(), VisibleAction.class.getConstructor(), TouchableAction.class.getConstructor(), RemoveActorAction.class.getConstructor(), DelayAction.class.getConstructor(), TimeScaleAction.class.getConstructor(), SequenceAction.class.getConstructor(), ParallelAction.class.getConstructor(), RepeatAction.class.getConstructor(), RunnableAction.class.getConstructor(), LayoutAction.class.getConstructor(), AfterAction.class.getConstructor(), AddListenerAction.class.getConstructor(), RemoveListenerAction.class.getConstructor(), Array.class.getConstructor(), Rectangle.class.getConstructor(), ChangeListener.ChangeEvent.class.getConstructor(), Net.HttpRequest.class.getConstructor(), InputEvent.class.getConstructor(), Stage.TouchFocus.class.getConstructor(), FocusListener.FocusEvent.class.getConstructor());
         }catch (NoSuchMethodException e){
             e.printStackTrace();
         }
