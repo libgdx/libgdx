fc22beff7a6bec606a435c3ff9d4ab15345ab392
Frosty-J <james@elite-20.04>
2021-08-02 13:58:28 +0100

    Remove refresh.png (unused since commit 57a2f24)

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/GdxSetup.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/GdxSetup.java
@@ -303,7 +303,6 @@ public class GdxSetup {
 			project.files.add(new ProjectFile("html/GdxDefinitionSuperdev", "html/src/" + packageDir + "/GdxDefinitionSuperdev.gwt.xml", true));
 			project.files.add(new ProjectFile("html/war/index", "html/webapp/index.html", true));
 			project.files.add(new ProjectFile("html/war/styles.css", "html/webapp/styles.css", false));
-			project.files.add(new ProjectFile("html/war/refresh.png", "html/webapp/refresh.png", false));
 			project.files.add(new ProjectFile("html/war/WEB-INF/web.xml", "html/webapp/WEB-INF/web.xml", true));
 		}
 
