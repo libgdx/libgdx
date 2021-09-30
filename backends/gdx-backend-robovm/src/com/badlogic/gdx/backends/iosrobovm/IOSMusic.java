ee777421c2a1bec008b5e670914f3f319a7e9214
obigu <1794492+obigu@users.noreply.github.com>
2021-09-30 15:27:41 +0200

    Improve iOS Music loading (#6654)

--- backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSMusic.java
+++ backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSMusic.java
@@ -26,10 +26,13 @@ import com.badlogic.gdx.backends.iosrobovm.objectal.OALAudioTrack;
 /** @author Niklas Therning */
 public class IOSMusic implements Music {
 	private final OALAudioTrack track;
+	private final String filePath;
+	private boolean initialized;
 	OnCompletionListener onCompletionListener;
 
-	public IOSMusic (OALAudioTrack track) {
+	public IOSMusic (OALAudioTrack track, String filePath) {
 		this.track = track;
+		this.filePath = filePath;
 		this.track.setDelegate(new AVAudioPlayerDelegateAdapter() {
 			@Override
 			public void didFinishPlaying (NSObject player, boolean success) {
@@ -51,7 +54,17 @@ public class IOSMusic implements Music {
 		if (track.isPaused()) {
 			track.setPaused(false);
 		} else if (!track.isPlaying()) {
-			track.play();
+			// Check https://github.com/kstenerud/ObjectAL-for-iPhone/blob/master/ObjectAL/ObjectAL/AudioTrack/OALAudioTrack.m
+			// OALAudioTrack needs to execute preloadURL() once to store the file path. From then on we avoid
+			// calling it again to avoid instantiating a new AVAudioPlayer every time.
+			if (!initialized) {
+				initialized = track.playFile(filePath);
+				if (!initialized) {
+					Gdx.app.error("IOSMusic", "Unable to initialize music " + filePath);
+				}
+			} else {
+				track.play();
+			}
 		}
 	}
 
@@ -116,6 +129,12 @@ public class IOSMusic implements Music {
 	@Override
 	public void setOnCompletionListener (OnCompletionListener listener) {
 		this.onCompletionListener = listener;
+	}
+
+	/** Calling this method preloads audio buffers and acquires the audio hardware necessary for playback. Can be noticeable on
+	 * latency critical scenarios. Call with some time before {@link #play()}. */
+	public boolean preload () {
+		return initialized = track.preloadFile(filePath);
 	}
 
 }
