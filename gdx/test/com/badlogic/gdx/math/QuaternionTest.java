b0d2ee913402e379b56a7a884d83c818f80bb998
Joshua Skelton <joshua.skelton@gmail.com>
2021-11-06 16:20:09 -0700

    Spaces to tabs

--- gdx/test/com/badlogic/gdx/math/QuaternionTest.java
+++ gdx/test/com/badlogic/gdx/math/QuaternionTest.java
@@ -6,18 +6,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class QuaternionTest {
-    private static final float epsilon = Float.MIN_NORMAL;
+	private static final float epsilon = Float.MIN_NORMAL;
 
-    @Test
-    public void testEulerPrecision () {
-        float yaw = -5f;
-        float pitch = 42.00055f;
-        float roll = 164.32f;
+	@Test
+	public void testEulerPrecision () {
+		float yaw = -5f;
+		float pitch = 42.00055f;
+		float roll = 164.32f;
 
-        Quaternion q = new Quaternion().setEulerAngles(yaw, pitch, roll);
+		Quaternion q = new Quaternion().setEulerAngles(yaw, pitch, roll);
 
-        assertEquals(yaw, q.getYaw(), epsilon);
-        assertEquals(pitch, q.getPitch(), epsilon);
-        assertEquals(roll, q.getRoll(), epsilon);
-    }
+		assertEquals(yaw, q.getYaw(), epsilon);
+		assertEquals(pitch, q.getPitch(), epsilon);
+		assertEquals(roll, q.getRoll(), epsilon);
+	}
 }
