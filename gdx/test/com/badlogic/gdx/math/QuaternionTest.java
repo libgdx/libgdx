85e2ed609bce801310201f4193e466aeaad83a5a
Joshua Skelton <joshua.skelton@gmail.com>
2021-11-12 14:32:32 -0800

    Adding test for multiple rotations

--- gdx/test/com/badlogic/gdx/math/QuaternionTest.java
+++ gdx/test/com/badlogic/gdx/math/QuaternionTest.java
@@ -6,10 +6,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class QuaternionTest {
-	private static final float epsilon = Float.MIN_NORMAL;
+	private static final float epsilon = 0.0001f;
 
 	@Test
-	public void testEulerPrecision () {
+	public void testRoundTrip () {
 		float yaw = -5f;
 		float pitch = 42.00055f;
 		float roll = 164.32f;
@@ -19,5 +19,18 @@ public class QuaternionTest {
 		assertEquals(yaw, q.getYaw(), epsilon);
 		assertEquals(pitch, q.getPitch(), epsilon);
 		assertEquals(roll, q.getRoll(), epsilon);
+	}
+
+	@Test
+	public void testMultipleRotations () {
+		Quaternion q = new Quaternion();
+		Quaternion rot = new Quaternion().setEulerAngles(4f, 0, 0);
+
+		for (int i = 0; i < 90; i++) {
+			q.mul(rot);
+			q.setEulerAngles(q.getYaw(), q.getPitch(), q.getRoll());
+		}
+
+		assertEquals(0f, q.getYaw(), epsilon);
 	}
 }
