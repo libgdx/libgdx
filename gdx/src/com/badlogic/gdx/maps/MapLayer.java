c775db9638a09ae44a00c56b76b9571a141e6696
klausm <simon.klausner@swarovski.com>
2021-12-06 15:59:04 +0100

    (#6480) remove unused method

--- gdx/src/com/badlogic/gdx/maps/MapLayer.java
+++ gdx/src/com/badlogic/gdx/maps/MapLayer.java
@@ -104,10 +104,6 @@ public class MapLayer {
 		this.parallaxY = parallaxY;
 	}
 
-	public boolean hasParallaxScrolling() {
-		return parallaxX != 1f || parallaxY != 1f;
-	}
-
 	/** set the renderOffsetDirty state to true, when this layer or any parents' offset has changed **/
 	public void invalidateRenderOffset () {
 		renderOffsetDirty = true;
