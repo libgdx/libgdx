cbf51a65ed651aa5d1e161fb5bb96791564434dd
GitHub Action <action@github.com>
2022-01-13 19:39:42 +0000

    Apply formatter

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
@@ -16,7 +16,6 @@
 
 package com.badlogic.gdx.scenes.scene2d.ui;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Null;
