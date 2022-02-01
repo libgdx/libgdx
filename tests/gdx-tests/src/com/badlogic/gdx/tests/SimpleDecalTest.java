cb2eff931270e76e4eb0edca36b2035deff405f6
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2022-02-01 01:38:13 +0000

    Undo wildcard import (bad IntelliJ, bad!)

--- tests/gdx-tests/src/com/badlogic/gdx/tests/SimpleDecalTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/SimpleDecalTest.java
@@ -17,7 +17,11 @@
 package com.badlogic.gdx.tests;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
 import com.badlogic.gdx.graphics.g3d.decals.Decal;
