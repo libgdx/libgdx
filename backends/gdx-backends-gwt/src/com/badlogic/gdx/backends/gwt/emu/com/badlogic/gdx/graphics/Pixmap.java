95e0b15a900cd063d9e65cb89d8f022dc819c734
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2022-03-09 21:06:52 +0000

    Didn't measure peformance difference; put `+ 0` back for clarity

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/graphics/Pixmap.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/emu/com/badlogic/gdx/graphics/Pixmap.java
@@ -474,7 +474,7 @@ public class Pixmap implements Disposable {
 		ensureCanvasExists();
 		if (pixels == null) pixels = context.getImageData(0, 0, width, height).getData();
 		int i = x * 4 + y * width * 4;
-		int r = pixels.get(i) & 0xff;
+		int r = pixels.get(i + 0) & 0xff;
 		int g = pixels.get(i + 1) & 0xff;
 		int b = pixels.get(i + 2) & 0xff;
 		int a = pixels.get(i + 3) & 0xff;
