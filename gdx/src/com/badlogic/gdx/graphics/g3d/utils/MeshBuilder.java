9facde437575772d7df04b98299119b9361ca1c0
Germain AUBERT <germain.aubert@gmail.com>
2022-04-12 16:11:11 +0200

    prevent out of limit mesh

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/MeshBuilder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/MeshBuilder.java
@@ -260,7 +260,7 @@ public class MeshBuilder implements MeshPartBuilder {
 
 	/** End building the mesh and returns the mesh */
 	public Mesh end () {
-		return end(new Mesh(true, vertices.size / stride, indices.size, attributes));
+		return end(new Mesh(true, Math.min(vertices.size / stride, MAX_VERTICES), indices.size, attributes));
 	}
 
 	/** Clears the data being built up until now, including the vertices, indices and all parts. Must be called in between the call
