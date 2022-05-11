117f73fac51fa22b258633d9ed0bcfe8e2c09b79
Tomski <tomwojciechowski@asidik.com>
2022-05-11 17:44:11 +0100

    prep for 1.11.0 release (breaking changes)

--- extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
+++ extensions/gdx-setup/src/com/badlogic/gdx/setup/DependencyBank.java
@@ -21,9 +21,9 @@ import java.util.HashMap;
 public class DependencyBank {
 
 	// Versions
-	static String libgdxVersion = "1.10.0";
+	static String libgdxVersion = "1.11.0";
 	// Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
-	static String libgdxNightlyVersion = "1.10.1-SNAPSHOT";
+	static String libgdxNightlyVersion = "1.11.0-SNAPSHOT";
 	static String roboVMVersion = "2.3.16";
 	static String buildToolsVersion = "29.0.3";
 	static String androidAPILevel = "30";
