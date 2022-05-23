abf5ebbd76b5658e67ba75c76158d8caad8a7584
GitHub Action <action@github.com>
2022-05-23 22:29:33 +0000

    Apply formatter

--- gdx/test/com/badlogic/gdx/utils/SortedIntListTest.java
+++ gdx/test/com/badlogic/gdx/utils/SortedIntListTest.java
@@ -1,3 +1,4 @@
+
 package com.badlogic.gdx.utils;
 
 import org.junit.Assert;
@@ -6,15 +7,15 @@ import org.junit.Test;
 public class SortedIntListTest {
 
 	@Test
-	public void testIteratorWithAllocation(){
+	public void testIteratorWithAllocation () {
 		Collections.allocateIterators = true;
-		try{
+		try {
 			SortedIntList<String> list = new SortedIntList<String>();
 			list.insert(0, "hello");
 			Assert.assertEquals(1, list.size);
 			Assert.assertEquals("hello", list.get(0));
 			Assert.assertEquals("hello", list.iterator().next().value);
-		}finally{
+		} finally {
 			Collections.allocateIterators = false;
 		}
 	}
