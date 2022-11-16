ab31a4fdf956ff76780f861a980771af8a29a20a
GitHub Action <action@github.com>
2022-11-16 14:46:26 +0000

    Apply formatter

--- backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSGLES30.java
+++ backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSGLES30.java
@@ -22,7 +22,8 @@ public class IOSGLES30 extends IOSGLES20 implements GL30 {
 
 	public native void glDrawRangeElements (int mode, int start, int end, int count, int type, int offset);
 
-	public native void glTexImage2D (int target, int level, int xoffset, int yoffset, int width, int height, int format, int type, int offset);
+	public native void glTexImage2D (int target, int level, int xoffset, int yoffset, int width, int height, int format, int type,
+		int offset);
 
 	public void glTexImage3D (int target, int level, int internalformat, int width, int height, int depth, int border, int format,
 		int type, Buffer pixels) {
@@ -45,7 +46,8 @@ public class IOSGLES30 extends IOSGLES20 implements GL30 {
 	public native void glTexImage3D (int target, int level, int internalformat, int width, int height, int depth, int border,
 		int format, int type, int offset);
 
-	public native void glTexSubImage2D (int target, int level, int xoffset, int yoffset, int width, int height, int format, int type, int offset);
+	public native void glTexSubImage2D (int target, int level, int xoffset, int yoffset, int width, int height, int format,
+		int type, int offset);
 
 	public void glTexSubImage3D (int target, int level, int xoffset, int yoffset, int zoffset, int width, int height, int depth,
 		int format, int type, Buffer pixels) {
--- tests/gdx-tests/src/com/badlogic/gdx/tests/gles3/PixelBufferObjectTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/gles3/PixelBufferObjectTest.java
@@ -1,3 +1,4 @@
+
 package com.badlogic.gdx.tests.gles3;
 
 import java.nio.Buffer;
@@ -15,7 +16,7 @@ import com.badlogic.gdx.tests.utils.GdxTest;
 import com.badlogic.gdx.tests.utils.GdxTestConfig;
 import com.badlogic.gdx.utils.BufferUtils;
 
-@GdxTestConfig(requireGL30=true)
+@GdxTestConfig(requireGL30 = true)
 public class PixelBufferObjectTest extends GdxTest {
 
 	static class PBOUpload {
@@ -29,16 +30,16 @@ public class PixelBufferObjectTest extends GdxTest {
 		protected Buffer mappedBuffer;
 		protected int pboHandle;
 		private final boolean useSubImage;
-		
+
 		public PBOUpload (boolean useSubImage) {
 			super();
 			this.useSubImage = useSubImage;
 		}
 
-		public void start(){
+		public void start () {
 			lock = new ReentrantLock();
 			lock.lock();
-			
+
 			new Thread(new Runnable() {
 				@Override
 				public void run () {
@@ -46,10 +47,10 @@ public class PixelBufferObjectTest extends GdxTest {
 					pixmap = new Pixmap(Gdx.files.internal("data/badlogic.jpg"));
 					pixmapSizeBytes = pixmap.getWidth() * pixmap.getHeight() * 3;
 					pixmapReady = true;
-					
+
 					// wait for PBO initialization (need to be done in GLThread)
 					lock.lock();
-					
+
 					// Transfer data from pixmap to PBO
 					ByteBuffer data = pixmap.getPixels();
 					data.rewind();
@@ -58,41 +59,44 @@ public class PixelBufferObjectTest extends GdxTest {
 				}
 			}).start();
 		}
-		
-		public void update(){
-		// first step: once we get pixmap size, we can create both texture and PBO
-			if(pixmapReady && texture == null){
+
+		public void update () {
+			// first step: once we get pixmap size, we can create both texture and PBO
+			if (pixmapReady && texture == null) {
 				texture = new Texture(pixmap.getWidth(), pixmap.getHeight(), pixmap.getFormat());
-				
+
 				pboHandle = Gdx.gl.glGenBuffer();
 				Gdx.gl.glBindBuffer(GL30.GL_PIXEL_UNPACK_BUFFER, pboHandle);
 				Gdx.gl.glBufferData(GL30.GL_PIXEL_UNPACK_BUFFER, pixmapSizeBytes, null, GL30.GL_STREAM_DRAW);
-				
-				mappedBuffer = Gdx.gl30.glMapBufferRange(GL30.GL_PIXEL_UNPACK_BUFFER, 0, pixmapSizeBytes, GL30.GL_MAP_WRITE_BIT | GL30.GL_MAP_UNSYNCHRONIZED_BIT);
+
+				mappedBuffer = Gdx.gl30.glMapBufferRange(GL30.GL_PIXEL_UNPACK_BUFFER, 0, pixmapSizeBytes,
+					GL30.GL_MAP_WRITE_BIT | GL30.GL_MAP_UNSYNCHRONIZED_BIT);
 				Gdx.gl.glBindBuffer(GL30.GL_PIXEL_UNPACK_BUFFER, 0);
 
 				lock.unlock();
 			}
 			// second step: once async transfer is complete, we can transfer to the texture and cleanup
-			if(!textureReady && pboTransferComplete){
-				
+			if (!textureReady && pboTransferComplete) {
+
 				// transfer data to texture (GL Thread)
 				Gdx.gl.glBindBuffer(GL30.GL_PIXEL_UNPACK_BUFFER, pboHandle);
 				Gdx.gl30.glUnmapBuffer(GL30.GL_PIXEL_UNPACK_BUFFER);
-				
-				Gdx.gl.glBindTexture (GL20.GL_TEXTURE_2D, texture.getTextureObjectHandle());
-				
+
+				Gdx.gl.glBindTexture(GL20.GL_TEXTURE_2D, texture.getTextureObjectHandle());
+
 				// for testing purpose: use glTexSubImage2D or glTexImage2D
-				if(useSubImage){
-					Gdx.gl30.glTexSubImage2D(GL20.GL_TEXTURE_2D, 0, 0, 0, pixmap.getWidth(), pixmap.getHeight(), pixmap.getGLFormat(), pixmap.getGLType(), 0);
-				}else{
-					Gdx.gl30.glTexImage2D(GL20.GL_TEXTURE_2D, 0, pixmap.getGLInternalFormat(), pixmap.getWidth(), pixmap.getHeight(), 0, pixmap.getGLFormat(), pixmap.getGLType(), 0);
+				if (useSubImage) {
+					Gdx.gl30.glTexSubImage2D(GL20.GL_TEXTURE_2D, 0, 0, 0, pixmap.getWidth(), pixmap.getHeight(), pixmap.getGLFormat(),
+						pixmap.getGLType(), 0);
+				} else {
+					Gdx.gl30.glTexImage2D(GL20.GL_TEXTURE_2D, 0, pixmap.getGLInternalFormat(), pixmap.getWidth(), pixmap.getHeight(),
+						0, pixmap.getGLFormat(), pixmap.getGLType(), 0);
 				}
-				
+
 				Gdx.gl.glBindTexture(GL20.GL_TEXTURE_2D, 0);
-				
+
 				Gdx.gl.glBindBuffer(GL30.GL_PIXEL_UNPACK_BUFFER, 0);
-				
+
 				// cleanup
 				mappedBuffer = null;
 				Gdx.gl.glDeleteBuffer(pboHandle);
@@ -102,45 +106,42 @@ public class PixelBufferObjectTest extends GdxTest {
 				textureReady = true;
 			}
 		}
-		
-		public boolean isTextureReady(){
+
+		public boolean isTextureReady () {
 			return textureReady;
 		}
-		
-		public Texture getTexture(){
+
+		public Texture getTexture () {
 			return texture;
 		}
 	}
-	
+
 	private SpriteBatch batch;
 	private PBOUpload demo1, demo2;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		
+
 		demo1 = new PBOUpload(false);
 		demo1.start();
 		demo2 = new PBOUpload(true);
 		demo2.start();
-		
+
 	}
-	
+
 	@Override
-	public void render() {
+	public void render () {
 		demo1.update();
 		demo2.update();
-		
+
 		batch.begin();
-		if(demo1.isTextureReady()){
-			batch.draw(demo1.getTexture(), 
-				0, 0, 
-				Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		if (demo1.isTextureReady()) {
+			batch.draw(demo1.getTexture(), 0, 0, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 		}
-		if(demo2.isTextureReady()){
-			batch.draw(demo2.getTexture(), 
-				Gdx.graphics.getWidth()/2, 0, 
-				Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		if (demo2.isTextureReady()) {
+			batch.draw(demo2.getTexture(), Gdx.graphics.getWidth() / 2, 0, Gdx.graphics.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2);
 		}
 		batch.end();
 	}
