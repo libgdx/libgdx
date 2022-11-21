7259594aca5a9579b589979879bdc8f0586a46f0
James <wjamesfl@gmail.com>
2022-11-21 15:26:37 -0500

    Add WebGL2 objects

---
+++ backends/gdx-backends-gwt/src/com/google/gwt/webgl/client/WebGLSync.java
@@ -0,0 +1,11 @@
+package com.google.gwt.webgl.client;
+
+/**
+ * @author JamesTKhan
+ * @version November 18, 2022
+ */
+public class WebGLSync extends WebGLObject {
+
+    protected WebGLSync() {
+    }
+}
