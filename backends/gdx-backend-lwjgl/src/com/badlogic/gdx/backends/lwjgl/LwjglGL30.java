605e63608e093070c0005eec92bfb73d7c20706d
MGSX <germain.aubert@gmail.com>
2022-11-23 08:58:48 +0100

    GLES3 added glTexImage2D and glTexSubImage2D with offset parameter (#6394)
    * GLES3 added glTexImage2D and glTexSubImage2D with offset parameter
    
    * fix formatting
    
    * Generate MobiVM MetalANGLE backend
    
    * Apply formatter
    
    * Update android to not import GL20
    Generate Header for iosgl30.h
    fix isssues in iosgl30.cpp
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Tom Wojciechowski <tomwojciechowski@asidik.com>

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglGL30.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglGL30.java
@@ -63,6 +63,12 @@ class LwjglGL30 extends LwjglGL20 implements com.badlogic.gdx.graphics.GL30 {
 	}
 
 	@Override
+	public void glTexImage2D (int target, int level, int internalformat, int width, int height, int border, int format, int type,
+		int offset) {
+		GL11.glTexImage2D(target, level, internalformat, width, height, border, format, type, offset);
+	}
+
+	@Override
 	public void glTexImage3D (int target, int level, int internalformat, int width, int height, int depth, int border, int format,
 		int type, Buffer pixels) {
 		if (pixels == null)
@@ -86,6 +92,12 @@ class LwjglGL30 extends LwjglGL20 implements com.badlogic.gdx.graphics.GL30 {
 	public void glTexImage3D (int target, int level, int internalformat, int width, int height, int depth, int border, int format,
 		int type, int offset) {
 		GL12.glTexImage3D(target, level, internalformat, width, height, depth, border, format, type, offset);
+	}
+
+	@Override
+	public void glTexSubImage2D (int target, int level, int xoffset, int yoffset, int width, int height, int format, int type,
+		int offset) {
+		GL11.glTexSubImage2D(target, level, xoffset, yoffset, width, height, format, type, offset);
 	}
 
 	@Override
