52fad2fce5a3d073cab64782df43a78ed61a46e6
BoBIsHere86 <rck86@hotmail.com>
2023-01-30 09:49:31 -0500

    Added ability to use the repeatx and repeaty functionality as found introduced in tiled 1.8 in orthomaps, will add the rest next. Added new GDXTest for this, also added test orthomap.

--- gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
@@ -271,6 +271,9 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
 			}
 			if (flipY) y = mapHeightInPixels - y;
 
+			boolean repeatX = element.getIntAttribute("repeatx", 0) == 1;
+			boolean repeatY = element.getIntAttribute("repeaty", 0) == 1;
+
 			TextureRegion texture = null;
 
 			Element image = element.getChildByName("image");
@@ -282,7 +285,7 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
 				y -= texture.getRegionHeight();
 			}
 
-			TiledMapImageLayer layer = new TiledMapImageLayer(texture, x, y);
+			TiledMapImageLayer layer = new TiledMapImageLayer(texture, x, y,repeatX,repeatY);
 
 			loadBasicLayerInfo(layer, element);
 
