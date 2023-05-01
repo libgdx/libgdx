39662768b40a75921431cccb976a667e6a851182
tim <kalhatianin@gmail.com>
2023-05-01 13:11:08 +0200

    after `./gradlew spotlessApply`

--- tests/gdx-tests/src/com/badlogic/gdx/tests/TiledMapGroupLayerWithImagelayerTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/TiledMapGroupLayerWithImagelayerTest.java
@@ -1,3 +1,4 @@
+
 package com.badlogic.gdx.tests;
 
 import com.badlogic.gdx.Gdx;
@@ -20,67 +21,67 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class TiledMapGroupLayerWithImagelayerTest extends GdxTest {
 
-    private TiledMap map;
-    private TiledMapRenderer renderer;
-    private OrthographicCamera camera;
-    private OrthoCamController cameraController;
-    private AssetManager assetManager;
-    private BitmapFont font;
-    private SpriteBatch batch;
-    String errorMessage;
-    private String fileName = "data/maps/tiled-groups-imagelayer/test.tmx";
+	private TiledMap map;
+	private TiledMapRenderer renderer;
+	private OrthographicCamera camera;
+	private OrthoCamController cameraController;
+	private AssetManager assetManager;
+	private BitmapFont font;
+	private SpriteBatch batch;
+	String errorMessage;
+	private String fileName = "data/maps/tiled-groups-imagelayer/test.tmx";
 
-    @Override
-    public void create () {
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
+	@Override
+	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, (w / h) * 10, 10);
-        camera.zoom = 2;
-        camera.update();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, (w / h) * 10, 10);
+		camera.zoom = 2;
+		camera.update();
 
-        cameraController = new OrthoCamController(camera);
-        Gdx.input.setInputProcessor(cameraController);
+		cameraController = new OrthoCamController(camera);
+		Gdx.input.setInputProcessor(cameraController);
 
-        font = new BitmapFont();
-        batch = new SpriteBatch();
+		font = new BitmapFont();
+		batch = new SpriteBatch();
 
-        AtlasTmxMapLoader.AtlasTiledMapLoaderParameters params = new AtlasTmxMapLoader.AtlasTiledMapLoaderParameters();
-        params.forceTextureFilters = true;
-        params.textureMinFilter = Texture.TextureFilter.Linear;
-        params.textureMagFilter = Texture.TextureFilter.Linear;
+		AtlasTmxMapLoader.AtlasTiledMapLoaderParameters params = new AtlasTmxMapLoader.AtlasTiledMapLoaderParameters();
+		params.forceTextureFilters = true;
+		params.textureMinFilter = Texture.TextureFilter.Linear;
+		params.textureMagFilter = Texture.TextureFilter.Linear;
 
-        assetManager = new AssetManager();
-        assetManager.setErrorListener(new AssetErrorListener() {
-            @Override
-            public void error (AssetDescriptor asset, Throwable throwable) {
-                errorMessage = throwable.getMessage();
-            }
-        });
+		assetManager = new AssetManager();
+		assetManager.setErrorListener(new AssetErrorListener() {
+			@Override
+			public void error (AssetDescriptor asset, Throwable throwable) {
+				errorMessage = throwable.getMessage();
+			}
+		});
 
-        assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
-        assetManager.load(fileName, TiledMap.class);
-    }
+		assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+		assetManager.load(fileName, TiledMap.class);
+	}
 
-    @Override
-    public void render () {
-        ScreenUtils.clear(100f / 255f, 100f / 255f, 250f / 255f, 1f);
-        camera.update();
-        assetManager.update(16);
-        if (renderer == null && assetManager.isLoaded(fileName)) {
-            map = assetManager.get(fileName);
-            renderer = new OrthogonalTiledMapRenderer(map, 1f / 32f);
-        } else if (renderer != null) {
-            renderer.setView(camera);
-            renderer.render();
-        }
-        batch.begin();
-        if (errorMessage != null) {
-            font.draw(batch, "ERROR (OK if running in GWT): " + errorMessage, 10, 50);
-            System.out.println(errorMessage);
-        }
-        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
-        batch.end();
-    }
+	@Override
+	public void render () {
+		ScreenUtils.clear(100f / 255f, 100f / 255f, 250f / 255f, 1f);
+		camera.update();
+		assetManager.update(16);
+		if (renderer == null && assetManager.isLoaded(fileName)) {
+			map = assetManager.get(fileName);
+			renderer = new OrthogonalTiledMapRenderer(map, 1f / 32f);
+		} else if (renderer != null) {
+			renderer.setView(camera);
+			renderer.render();
+		}
+		batch.begin();
+		if (errorMessage != null) {
+			font.draw(batch, "ERROR (OK if running in GWT): " + errorMessage, 10, 50);
+			System.out.println(errorMessage);
+		}
+		font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
+		batch.end();
+	}
 }
