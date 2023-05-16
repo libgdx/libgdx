a621fe83cd166769ec1120faa3224130fbf0a4a6
obigu <torinthechosen@gmail.com>
2023-05-16 15:30:02 +0200

    Upgrade min iOS deployment target to 11

--- backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSApplication.java
+++ backends/gdx-backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSApplication.java
@@ -21,7 +21,6 @@ import java.io.File;
 import com.badlogic.gdx.ApplicationLogger;
 import com.badlogic.gdx.backends.iosrobovm.objectal.OALIOSAudio;
 import org.robovm.apple.coregraphics.CGRect;
-import org.robovm.apple.foundation.Foundation;
 import org.robovm.apple.foundation.NSMutableDictionary;
 import org.robovm.apple.foundation.NSObject;
 import org.robovm.apple.foundation.NSProcessInfo;
@@ -434,12 +433,7 @@ public class IOSApplication implements Application {
 
 			@Override
 			public boolean hasContents () {
-				if (Foundation.getMajorSystemVersion() >= 10) {
-					return UIPasteboard.getGeneralPasteboard().hasStrings();
-				}
-
-				String contents = getContents();
-				return contents != null && !contents.isEmpty();
+				return UIPasteboard.getGeneralPasteboard().hasStrings();
 			}
 
 			@Override
