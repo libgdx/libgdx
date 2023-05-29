3350a03513844e6586eabddce1ee1be0a98265c8
Berstanio <berstaniodev@gmail.com>
2023-05-29 22:23:52 +0200

    Two CI fixes (#7156)
    * fix: Don't use java 8 features for MetalANGLE generator
    
    * fix: Fix formatting

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -18,7 +18,6 @@ package com.badlogic.gdx.graphics.g2d;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Comparator;
 
 import com.badlogic.gdx.Files.FileType;
