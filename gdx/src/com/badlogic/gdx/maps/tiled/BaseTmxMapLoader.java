4d945764ac370d3e472c83969550e214b44aba2f
JohnLavender474 <jml62608@uga.edu>
2023-09-20 13:23:01 -0400

    Successfully created and ran test

--- gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
@@ -1,6 +1,7 @@
 
 package com.badlogic.gdx.maps.tiled;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetLoaderParameters;
 import com.badlogic.gdx.assets.loaders.AsynchronousAssetLoader;
@@ -76,8 +77,14 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
     protected int mapHeightInPixels;
 
     protected TiledMap map;
-    protected ObjectMap<Integer, MapProperties> tiledObjIdToMapPropsMap;
-    protected Queue<Runnable> runOnEnd;
+    /**
+     * MapProperty instances are mapped to the id of the respective tiled object
+     */
+    protected ObjectMap<Integer, MapProperties> idToPropsMap;
+    /**
+     * Logic to delay until the end of the [loadTiledMap] method
+     */
+    protected Queue<Runnable> runOnEndOfLoadTiledMap;
 
     public BaseTmxMapLoader(FileHandleResolver resolver) {
         super(resolver);
@@ -110,8 +117,8 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
      */
     protected TiledMap loadTiledMap(FileHandle tmxFile, P parameter, ImageResolver imageResolver) {
         this.map = new TiledMap();
-        this.tiledObjIdToMapPropsMap = new ObjectMap<>();
-        this.runOnEnd = new Queue<>();
+        this.idToPropsMap = new ObjectMap<>();
+        this.runOnEndOfLoadTiledMap = new Queue<>();
 
         if (parameter != null) {
             this.convertObjectToTileSpace = parameter.convertObjectToTileSpace;
@@ -198,8 +205,9 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
             }
         }
 
-        while (!runOnEnd.isEmpty()) {
-            runOnEnd.removeFirst().run();
+        while (!runOnEndOfLoadTiledMap.isEmpty()) {
+            // Gdx.app.log("BaseTmxMapLoader", "Running on end of load tiled map");
+            runOnEndOfLoadTiledMap.removeFirst().run();
         }
 
         return map;
@@ -456,27 +464,69 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
             Element properties = element.getChildByName("properties");
             if (properties != null) {
                 loadProperties(object.getProperties(), properties);
-                tiledObjIdToMapPropsMap.put(id, object.getProperties());
+
+                // Gdx.app.log("BaseTmxMapLoader", "Putting props for id [" + id + "]");
+                /*
+                 * After loading the properties of the object, put a mapping entry where the key is the object id and
+                 *  the value is the
+                 * object's properties
+                 */
+                idToPropsMap.put(id, object.getProperties());
             }
             objects.add(object);
         }
     }
 
-    protected void loadProperties(MapProperties properties, Element element) {
+    protected void loadProperties(final MapProperties properties, Element element) {
         if (element == null)
             return;
         if (element.getName().equals("properties")) {
             for (Element property : element.getChildrenByName("property")) {
-                String name = property.getAttribute("name", null);
+                final String name = property.getAttribute("name", null);
+                final String type = property.getAttribute("type", null);
                 String value = property.getAttribute("value", null);
-                String type = property.getAttribute("type", null);
+
                 if (value == null) {
                     value = property.getText();
                 }
+
                 if (type != null && type.equals("object")) {
-                    final String _value = value;
-                    runOnEnd.addFirst(() -> properties.put(name,
-                            tiledObjIdToMapPropsMap.get(Integer.parseInt(_value))));
+                    /*
+                     * Wait until the end of the [loadTiledMap] method to fetch the map props for "object". This is
+                     * because the map
+                     * properties of the object being pointed to might not have been loaded yet.
+                     */
+                    try {
+                        // Gdx.app.log("BaseTmxMapLoader", "Parsing property [" + name + "] of type \"object\"");
+                        /*
+                         * Value should be the id of the object being pointed to. Nevertheless, the try-catch block
+                         * safeguards in case
+                         * faulty data in inputted.
+                         */
+                        final int _value = Integer.parseInt(value);
+
+                        /*
+                         * Create a [Runnable] to fetch the map properties of the object being pointed to and add it
+                         * to this object's
+                         * properties.
+                         */
+                        // Gdx.app.log("BaseTmxMapLoader", "Creating runnable to fetch obj with id [" + _value + "] " +
+                                // "to put in props of obj with id [" + properties.get("id", Integer.class) + "]");
+                        Runnable fetchMapProps = new Runnable() {
+                            @Override
+                            public void run() {
+                                MapProperties props = idToPropsMap.get(_value);
+                                properties.put(name, props);
+                            }
+                        };
+
+                        // [Runnable] should not run until the end of the [loadTiledMap] method.
+                        // Gdx.app.log("BaseTmxMapLoader", "Put runnable in queue...");
+                        runOnEndOfLoadTiledMap.addFirst(fetchMapProps);
+                    } catch (Exception exception) {
+                        Gdx.app.error("BaseTmxMapLoader", "Error parsing property [" + name + "] of type \"object\"",
+                                exception);
+                    }
                 } else {
                     Object castValue = castProperty(name, value, type);
                     properties.put(name, castValue);
@@ -501,8 +551,8 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
             return Color.valueOf(opaqueColor + alpha);
         } else {
             throw new GdxRuntimeException(
-                    "Wrong type given for property " + name + ", given : " + type + ", supported : string, bool, int," +
-                            " float, color");
+                    "Wrong type given for property " + name + ", given : " + type + ", supported : string, bool, int,"
+                            + " float, color");
         }
 
     }
@@ -554,8 +604,7 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
                         else if (compression.equals("zlib"))
                             is = new BufferedInputStream(new InflaterInputStream(new ByteArrayInputStream(bytes)));
                         else
-                            throw new GdxRuntimeException("Unrecognised compression (" + compression + ") for TMX " +
-                                    "Layer Data");
+                            throw new GdxRuntimeException("Unrecognised compression (" + compression + ") for TMX " + "Layer Data");
 
                         byte[] temp = new byte[4];
                         for (int y = 0; y < height; y++) {
@@ -568,8 +617,7 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters> ex
                                     read += curr;
                                 }
                                 if (read != temp.length)
-                                    throw new GdxRuntimeException("Error Reading TMX Layer Data: Premature end of " +
-                                            "tile data");
+                                    throw new GdxRuntimeException("Error Reading TMX Layer Data: Premature end of " + "tile data");
                                 ids[y * width + x] = unsignedByteToInt(temp[0]) | unsignedByteToInt(temp[1]) << 8
                                         | unsignedByteToInt(temp[2]) << 16 | unsignedByteToInt(temp[3]) << 24;
                             }
---
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/TiledMapObjectPropertyTest.java
@@ -0,0 +1,189 @@
+package com.badlogic.gdx.tests;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.tests.utils.OrthoCamController;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.ObjectSet;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.Iterator;
+
+/**
+ * Test for successful loading of "object" properties for tiled objects in a map. The value of the "object"
+ * property is the id of another tiled object. This test ensures that the "object" property is correctly
+ * loaded and points to the correct object.
+ */
+public class TiledMapObjectPropertyTest extends GdxTest {
+
+    private TiledMap map;
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
+    private OrthographicCamera camera;
+    private TiledMapRenderer mapRenderer;
+    private boolean error;
+
+    @Override
+    public void create() {
+        Gdx.app.log("TiledMapObjectPropertyTest", "Running test...");
+        try {
+            float w = Gdx.graphics.getWidth();
+            float h = Gdx.graphics.getHeight();
+
+            camera = new OrthographicCamera();
+            camera.setToOrtho(false, (w / h) * 512, 512);
+            camera.zoom = .5f;
+            camera.update();
+
+            OrthoCamController cameraController = new OrthoCamController(camera);
+            Gdx.input.setInputProcessor(cameraController);
+
+            Gdx.app.log("TiledMapObjectPropertyTest", "Load map...");
+            map = new TmxMapLoader().load("data/maps/tiled-objects/test-object-properties.tmx");
+            Gdx.app.log("TiledMapObjectPropertyTest", "Finish loading map!");
+
+            batch = new SpriteBatch();
+            shapeRenderer = new ShapeRenderer();
+            mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+            MapObjects objects = map.getLayers().get("Objects").getObjects();
+
+            ObjectMap<Integer, MapProperties> idPropMap = new ObjectMap<>();
+            for (MapObject object : objects) {
+                int id = object.getProperties().get("id", Integer.class);
+                idPropMap.put(id, object.getProperties());
+            }
+
+            for (MapObject object : objects) {
+                int id = object.getProperties().get("id", Integer.class);
+                Gdx.app.log("TiledMapObjectPropertyTest", "Testing object with id " + id);
+                MapProperties props = idPropMap.get(id);
+
+                switch (id) {
+                    case 1:
+                        test(props, 2, idPropMap);
+                        test(props, 5, idPropMap);
+                        test(props, 1, idPropMap);
+                        break;
+                    case 2:
+                        test(props, 3, idPropMap);
+                        test(props, 4, idPropMap);
+                        break;
+                    case 3:
+                        test(props, 2, idPropMap);
+                        break;
+                    case 4:
+                        test(props, 1, idPropMap);
+                        break;
+                    case 5:
+                    case 6:
+                        Iterator<String> propKeysIterator = props.getKeys();
+                        ObjectSet<String> propKeys = new ObjectSet<>();
+                        while (propKeysIterator.hasNext()) {
+                            propKeys.add(propKeysIterator.next());
+                        }
+                        if (propKeys.size != 6) {
+                            throw new RuntimeException("Object with id " + id + " should have six keys " +
+                                    "but has " + propKeys);
+                        }
+                }
+            }
+
+            for (ObjectMap.Entry<Integer, MapProperties> entry : idPropMap.entries()) {
+                int id = entry.key;
+                MapProperties props = entry.value;
+
+                System.out.println("Object with id " + id + " has \"object\" properties: ");
+
+                Iterator<String> propKeysIterator = props.getKeys();
+                Iterator<Object> propValuesIterator = props.getValues();
+
+                while (propKeysIterator.hasNext() && propValuesIterator.hasNext()) {
+                    Object value = propValuesIterator.next();
+                    String key = propKeysIterator.next();
+                    if (!key.contains("Points_To_ID_")) {
+                        continue;
+                    }
+
+                    if (value instanceof MapProperties) {
+                        MapProperties nestedProps = (MapProperties) value;
+                        value = "props of object with id " + nestedProps.get("id", Integer.class);
+                    }
+
+                    System.out.println("\t" + key + " = " + value);
+                }
+
+            }
+
+            Gdx.app.log("TiledMapObjectPropertyTest", "Successfully ran test!");
+        } catch (Exception e) {
+            Gdx.app.error("TiledMapObjectPropertyTest", "Failed to run test!", e);
+            e.printStackTrace();
+            error = true;
+        }
+    }
+
+    @Override
+    public void render() {
+        if (error) {
+            Gdx.app.error("TiledMapObjectPropertyTest", "Failed to run test!");
+            Gdx.app.exit();
+        }
+
+        ScreenUtils.clear(0.55f, 0.55f, 0.55f, 1f);
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        batch.setProjectionMatrix(camera.combined);
+
+        shapeRenderer.setColor(Color.BLUE);
+        Gdx.gl20.glLineWidth(2);
+        MapLayer layer = map.getLayers().get("Objects");
+        for (MapObject mapObject : layer.getObjects()) {
+            if (!mapObject.isVisible())
+                continue;
+            if (mapObject instanceof RectangleMapObject) {
+                shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+                Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
+                shapeRenderer.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+                shapeRenderer.end();
+            }
+        }
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        shapeRenderer.dispose();
+    }
+
+    private void test(MapProperties props, int idToObjProp, ObjectMap<Integer, MapProperties> idPropMap) {
+        String key = "Points_To_ID_" + idToObjProp;
+        Gdx.app.log("TiledMapObjectPropertyTest", "Testing property " + key);
+
+        if (!props.containsKey(key)) {
+            throw new RuntimeException("Missing property: " + key);
+        }
+
+        MapProperties otherProps = idPropMap.get(idToObjProp);
+        if (otherProps != idPropMap.get(idToObjProp)) {
+            throw new RuntimeException("Property " + key + " does not point to the correct object");
+        }
+    }
+}
