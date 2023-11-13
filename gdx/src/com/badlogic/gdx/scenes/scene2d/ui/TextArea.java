3e4e865db0eff62ea79e35d8bd91734f6ae09020
GitHub Action <action@github.com>
2023-11-13 12:20:20 +0000

    Apply formatter

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java
@@ -21,7 +21,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
