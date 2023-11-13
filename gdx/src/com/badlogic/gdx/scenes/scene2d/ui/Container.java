80773da58175485462e5a94c199fcee749deb94e
Nathan Sweet <nathan.sweet@gmail.com>
2023-11-13 11:11:15 -0400

    Removed unused imports.
    Related: #7276, #7278

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Container.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Container.java
@@ -4,7 +4,6 @@ package com.badlogic.gdx.scenes.scene2d.ui;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
