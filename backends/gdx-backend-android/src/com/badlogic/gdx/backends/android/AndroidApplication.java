886efa9f393e75352eff4bdf938398dfa128594a
obigu <torinthechosen@gmail.com>
2024-02-03 22:56:04 +0100

    Fix compilation error

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
@@ -16,10 +16,12 @@
 
 package com.badlogic.gdx.backends.android;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.Handler;
