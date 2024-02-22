bdee5296bb20308cb4a3b70cdcbedfae7037dc99
obigu <torinthechosen@gmail.com>
2024-02-22 15:23:28 +0100

    Make Pixmap safe to dispose if already disposed

--- gdx/src/com/badlogic/gdx/graphics/Pixmap.java
+++ gdx/src/com/badlogic/gdx/graphics/Pixmap.java
@@ -391,7 +391,7 @@ public class Pixmap implements Disposable {
 
 	/** Releases all resources associated with this Pixmap. */
 	public void dispose () {
-		if (disposed) throw new GdxRuntimeException("Pixmap already disposed!");
+		if (disposed) Gdx.app.error("Pixmap", "Pixmap already disposed!");
 		pixmap.dispose();
 		disposed = true;
 	}
