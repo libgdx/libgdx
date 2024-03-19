096a88765ca699709f9afa98535ecce5e8d5e283
Jevon Wright <jevon@jevon.org>
2024-03-19 17:35:23 +1300

    Fix code style in Batch.java

--- gdx/src/com/badlogic/gdx/graphics/g2d/Batch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/Batch.java
@@ -155,8 +155,7 @@ public interface Batch extends Disposable {
 	 * rectangle is offset by originX, originY relative to the origin. Scale specifies the scaling factor by which the rectangle
 	 * should be scaled around originX, originY. Rotation specifies the angle of counter clockwise rotation of the rectangle around
 	 * originX, originY. 
-	 * @param rotation rotation in degrees
-	 */
+	 * @param rotation rotation in degrees */
 	public void draw (TextureRegion region, float x, float y, float originX, float originY, float width, float height,
 		float scaleX, float scaleY, float rotation);
 
