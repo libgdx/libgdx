b7fb0d0ecf9da9febbb4222008ced06b23998808
Thomas Creutzenberg <crossie@gmail.com>
2024-07-25 11:02:00 +0200

    Added FPSLogger.setBound(bound)

--- gdx/src/com/badlogic/gdx/graphics/FPSLogger.java
+++ gdx/src/com/badlogic/gdx/graphics/FPSLogger.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 /** A simple helper class to log the frames per seconds achieved. Just invoke the {@link #log()} method in your rendering method.
  * The output will be logged once per second.
- * 
+ *
  * @author mzechner */
 public class FPSLogger {
 	long startTime;
@@ -33,6 +33,10 @@ public class FPSLogger {
 
 	/** @param bound only logs when they frames per second are less than the bound */
 	public FPSLogger (int bound) {
+		setBound(bound);
+	}
+
+	public void setBound (int bound) {
 		this.bound = bound;
 		startTime = TimeUtils.nanoTime();
 	}
