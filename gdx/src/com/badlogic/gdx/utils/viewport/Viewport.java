118ddc53eac5662ec07e33ba9a5fb6c03bde8de1
Tom Wojciechowski <tomwojciechowski@asidik.com>
2024-10-02 10:32:48 +0100

    update docs on unproject methods and pickRay for touch coordinates over screen coordinates

--- gdx/src/com/badlogic/gdx/utils/viewport/Viewport.java
+++ gdx/src/com/badlogic/gdx/utils/viewport/Viewport.java
@@ -66,14 +66,16 @@ public abstract class Viewport {
 		apply(centerCamera);
 	}
 
-	/** Transforms the specified screen coordinate to world coordinates.
+	/** Transforms the specified touch coordinate to world coordinates.
+	 * The x- and y-coordinate of vec are assumed to be in touch coordinates (origin is the top left corner, y
+	 * 	 * pointing down, x pointing to the right)
 	 * @return The vector that was passed in, transformed to world coordinates.
 	 * @see Camera#unproject(Vector3) */
-	public Vector2 unproject (Vector2 screenCoords) {
-		tmp.set(screenCoords.x, screenCoords.y, 1);
+	public Vector2 unproject (Vector2 touchCoords) {
+		tmp.set(touchCoords.x, touchCoords.y, 1);
 		camera.unproject(tmp, screenX, screenY, screenWidth, screenHeight);
-		screenCoords.set(tmp.x, tmp.y);
-		return screenCoords;
+		touchCoords.set(tmp.x, tmp.y);
+		return touchCoords;
 	}
 
 	/** Transforms the specified world coordinate to screen coordinates.
