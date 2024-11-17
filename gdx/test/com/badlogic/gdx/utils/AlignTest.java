56bafa5f690e20710db069e3a6d101a3641fa1ce
GitHub Action <action@github.com>
2024-11-17 19:48:53 +0000

    Apply formatter

--- gdx/test/com/badlogic/gdx/utils/AlignTest.java
+++ gdx/test/com/badlogic/gdx/utils/AlignTest.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  ******************************************************************************/
 
-
 package com.badlogic.gdx.utils;
 
 import static org.junit.Assert.*;
