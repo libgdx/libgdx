22c6b133e3e405137b24176a5da8a3df1250622d
Julien <julien.abraul-guilherme@imt-atlantique.net>
2024-11-20 11:14:12 +0100

    AlignTest / SortTest / without BinaryHeap

--- gdx/test/com/badlogic/gdx/utils/AlignTest.java
+++ gdx/test/com/badlogic/gdx/utils/AlignTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2024 Julien Abraul Guilherme.
+ * Copyright 2024 see AUTHORS file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
