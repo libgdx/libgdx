dbc3a8428ce83bbd6385974897532c9bd63770e2
Julien <julien.abraul-guilherme@imt-atlantique.net>
2024-11-23 17:07:57 +0100

    Withdrawal testCombinations

--- gdx/test/com/badlogic/gdx/utils/AlignTest.java
+++ gdx/test/com/badlogic/gdx/utils/AlignTest.java
@@ -29,72 +29,61 @@ import org.junit.Test;
 
 public class AlignTest {
 
-    @Test
-    public void testIsLeft () {
-        assertTrue(Align.isLeft(Align.left));
-        assertFalse(Align.isLeft(Align.right));
-        assertTrue(Align.isLeft(Align.topLeft));
-        assertFalse(Align.isLeft(Align.topRight));
-    }
+	@Test
+	public void testIsLeft () {
+		assertTrue(Align.isLeft(Align.left));
+		assertFalse(Align.isLeft(Align.right));
+		assertTrue(Align.isLeft(Align.topLeft));
+		assertFalse(Align.isLeft(Align.topRight));
+	}
 
-    @Test
-    public void testIsRight () {
-        assertTrue(Align.isRight(Align.right));
-        assertFalse(Align.isRight(Align.left));
-        assertTrue(Align.isRight(Align.topRight));
-        assertFalse(Align.isRight(Align.topLeft));
-    }
+	@Test
+	public void testIsRight () {
+		assertTrue(Align.isRight(Align.right));
+		assertFalse(Align.isRight(Align.left));
+		assertTrue(Align.isRight(Align.topRight));
+		assertFalse(Align.isRight(Align.topLeft));
+	}
 
-    @Test
-    public void testIsTop () {
-        assertTrue(Align.isTop(Align.top));
-        assertFalse(Align.isTop(Align.bottom));
-        assertTrue(Align.isTop(Align.topLeft));
-        assertFalse(Align.isTop(Align.bottomLeft));
-        assertTrue(Align.isTop(Align.topRight));
-        assertFalse(Align.isTop(Align.bottomRight));
-    }
+	@Test
+	public void testIsTop () {
+		assertTrue(Align.isTop(Align.top));
+		assertFalse(Align.isTop(Align.bottom));
+		assertTrue(Align.isTop(Align.topLeft));
+		assertFalse(Align.isTop(Align.bottomLeft));
+		assertTrue(Align.isTop(Align.topRight));
+		assertFalse(Align.isTop(Align.bottomRight));
+	}
 
-    @Test
-    public void testIsBottom () {
-        assertTrue(Align.isBottom(Align.bottom));
-        assertFalse(Align.isBottom(Align.top));
-        assertTrue(Align.isBottom(Align.bottomLeft));
-        assertFalse(Align.isBottom(Align.topLeft));
-        assertTrue(Align.isBottom(Align.bottomRight));
-        assertFalse(Align.isBottom(Align.topRight));
-    }
+	@Test
+	public void testIsBottom () {
+		assertTrue(Align.isBottom(Align.bottom));
+		assertFalse(Align.isBottom(Align.top));
+		assertTrue(Align.isBottom(Align.bottomLeft));
+		assertFalse(Align.isBottom(Align.topLeft));
+		assertTrue(Align.isBottom(Align.bottomRight));
+		assertFalse(Align.isBottom(Align.topRight));
+	}
 
-    @Test public void testIsCenterVertical() {
-        assertTrue(Align.isCenterVertical(Align.center));
-        assertFalse(Align.isCenterVertical(Align.top));
-        assertFalse(Align.isCenterVertical(Align.bottom));
-        assertFalse(Align.isCenterVertical(Align.topLeft));
-        assertFalse(Align.isCenterVertical(Align.topRight));
-        assertFalse(Align.isCenterVertical(Align.bottomLeft));
-        assertFalse(Align.isCenterVertical(Align.bottomRight));
-    }
+	@Test
+	public void testIsCenterVertical () {
+		assertTrue(Align.isCenterVertical(Align.center));
+		assertFalse(Align.isCenterVertical(Align.top));
+		assertFalse(Align.isCenterVertical(Align.bottom));
+		assertFalse(Align.isCenterVertical(Align.topLeft));
+		assertFalse(Align.isCenterVertical(Align.topRight));
+		assertFalse(Align.isCenterVertical(Align.bottomLeft));
+		assertFalse(Align.isCenterVertical(Align.bottomRight));
+	}
 
-    @Test public void testIsCenterHorizontal() {
-        assertTrue(Align.isCenterHorizontal(Align.center));
-        assertFalse(Align.isCenterHorizontal(Align.left));
-        assertFalse(Align.isCenterHorizontal(Align.right));
-        assertFalse(Align.isCenterHorizontal(Align.topLeft));
-        assertFalse(Align.isCenterHorizontal(Align.topRight));
-        assertFalse(Align.isCenterHorizontal(Align.bottomLeft));
-        assertFalse(Align.isCenterHorizontal(Align.bottomRight));
-    }
-
-    @Test
-    public void testCombinations () {
-        assertTrue(Align.isLeft(Align.bottomLeft));
-        assertTrue(Align.isRight(Align.bottomRight));
-        assertTrue(Align.isTop(Align.topRight));
-        assertTrue(Align.isBottom(Align.bottomRight));
-
-        assertFalse(Align.isLeft(Align.topRight));
-        assertFalse(Align.isRight(Align.topLeft));
-        assertFalse(Align.isTop(Align.bottomLeft));
-        assertFalse(Align.isBottom(Align.topLeft));
-    }
+	@Test
+	public void testIsCenterHorizontal () {
+		assertTrue(Align.isCenterHorizontal(Align.center));
+		assertFalse(Align.isCenterHorizontal(Align.left));
+		assertFalse(Align.isCenterHorizontal(Align.right));
+		assertFalse(Align.isCenterHorizontal(Align.topLeft));
+		assertFalse(Align.isCenterHorizontal(Align.topRight));
+		assertFalse(Align.isCenterHorizontal(Align.bottomLeft));
+		assertFalse(Align.isCenterHorizontal(Align.bottomRight));
+	}
 }
