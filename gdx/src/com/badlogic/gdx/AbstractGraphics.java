2a0ee36df061ee32ba01801cafcf0752c2044836
ER-RAHMAOUY Abderrahmane <146597745+Yuri69420@users.noreply.github.com>
2024-11-27 14:43:13 +0100

    Update AbstractGraphics.java
    Deletion of javadoc because super method already does

--- gdx/src/com/badlogic/gdx/AbstractGraphics.java
+++ gdx/src/com/badlogic/gdx/AbstractGraphics.java
@@ -5,25 +5,19 @@ package com.badlogic.gdx;
  * and density calculations. */
 public abstract class AbstractGraphics implements Graphics {
 
-	/** Delegates to {@link #getDeltaTime()} for the actual time calculation.
-	 * @return the time span between the current frame and the last frame in seconds. */
 	@Override
 	public float getRawDeltaTime () {
 		return getDeltaTime();
 	}
 
-	/** Calculates and returns the screen density based on the pixel-per-inch value along the X-axis. The density is derived by
-	 * dividing the PPI by a standard 160 PPI, and defaults to 1 if PPI is invalid.
-	 * @return the Density Independent Pixel factor of the display. */
+	
 	@Override
 	public float getDensity () {
 		float ppiX = getPpiX();
 		return (ppiX > 0 && ppiX <= Float.MAX_VALUE) ? ppiX / 160f : 1f;
 	}
 
-	/** Computes and returns the scaling factor for the back buffer compared to the display width. This is the ratio of the back
-	 * buffer width to the display width.
-	 * @return amount of pixels per logical pixel (point) */
+
 	@Override
 	public float getBackBufferScale () {
 		return getBackBufferWidth() / (float)getWidth();
