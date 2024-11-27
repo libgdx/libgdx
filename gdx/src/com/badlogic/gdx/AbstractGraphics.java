c91dfdebef946d9b972bbbbff40ba63ef6be9728
GitHub Action <action@github.com>
2024-11-27 13:44:20 +0000

    Apply formatter

--- gdx/src/com/badlogic/gdx/AbstractGraphics.java
+++ gdx/src/com/badlogic/gdx/AbstractGraphics.java
@@ -10,13 +10,11 @@ public abstract class AbstractGraphics implements Graphics {
 		return getDeltaTime();
 	}
 
-	
 	@Override
 	public float getDensity () {
 		float ppiX = getPpiX();
 		return (ppiX > 0 && ppiX <= Float.MAX_VALUE) ? ppiX / 160f : 1f;
 	}
-
 
 	@Override
 	public float getBackBufferScale () {
