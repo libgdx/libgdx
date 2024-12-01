86a1dd18c32bb764be96fd75a865915d3f282032
XyreinDev <64452678+XyreinDev@users.noreply.github.com>
2024-12-01 15:14:58 +0000

    Removed unused imports

--- extensions/gdx-box2d/gdx-box2d/src/com/badlogic/gdx/physics/box2d/AABBUtil.java
+++ extensions/gdx-box2d/gdx-box2d/src/com/badlogic/gdx/physics/box2d/AABBUtil.java
@@ -1,12 +1,6 @@
 package com.badlogic.gdx.physics.box2d;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.Shape;
-import com.badlogic.gdx.physics.box2d.Transform;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 
 public class AABBUtil {
     /** Sets the lower and upper bounds of the specified fixture to the provided Vector2 values.
