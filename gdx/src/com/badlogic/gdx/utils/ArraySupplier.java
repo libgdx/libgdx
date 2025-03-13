04e273eae15e4de6ddfe68755603933be258581b
Berstanio <berstaniodev@gmail.com>
2025-03-13 11:39:39 +0100

    feat: Add new ArraySupplier

---
+++ gdx/src/com/badlogic/gdx/utils/ArraySupplier.java
@@ -0,0 +1,14 @@
+package com.badlogic.gdx.utils;
+
+/** An interface that is used to create arrays. Even tho not annotated with "FunctionalInterface", it can act as
+ * one. <br>
+ * You can use a constructor reference or lambda as a ArraySupplier, such as with {@code MyClass[]::new} or
+ * {@code (size) -> new MyClass[size]}. */
+public interface ArraySupplier<T> {
+    /**
+     * A default array supplier that creates an Object[].
+     */
+    ArraySupplier<?> OBJECT_ARRAY_SUPPLIER = Object[]::new;
+
+    T get (int size);
+}
