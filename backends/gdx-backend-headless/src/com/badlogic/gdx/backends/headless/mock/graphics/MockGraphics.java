6c67484631fff704c20d5f392859f778955fe7ff
A-Taylor123 <alyssataylor5612306@gmail.com>
2025-03-31 17:13:03 -0700

    fixing things?

--- backends/gdx-backend-headless/src/com/badlogic/gdx/backends/headless/mock/graphics/MockGraphics.java
+++ backends/gdx-backend-headless/src/com/badlogic/gdx/backends/headless/mock/graphics/MockGraphics.java
@@ -222,6 +222,10 @@ public class MockGraphics extends AbstractGraphics {
 	public void setForegroundFPS (int fps) {
 		this.targetRenderInterval = (long)(fps <= 0 ? (fps == 0 ? 0 : -1) : ((1F / fps) * 1000000000F));
 	}
+	@Override
+	public void setBackgroundFPS (int fps) {
+
+	}
 
 	public long getTargetRenderInterval () {
 		return targetRenderInterval;
