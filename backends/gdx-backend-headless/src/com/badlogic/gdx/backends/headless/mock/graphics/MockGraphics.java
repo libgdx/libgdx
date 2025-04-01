f3d3b0619e60a75b04c2a3616e655c9740ec082d
GitHub Action <action@github.com>
2025-04-01 00:14:30 +0000

    Apply formatter

--- backends/gdx-backend-headless/src/com/badlogic/gdx/backends/headless/mock/graphics/MockGraphics.java
+++ backends/gdx-backend-headless/src/com/badlogic/gdx/backends/headless/mock/graphics/MockGraphics.java
@@ -222,6 +222,7 @@ public class MockGraphics extends AbstractGraphics {
 	public void setForegroundFPS (int fps) {
 		this.targetRenderInterval = (long)(fps <= 0 ? (fps == 0 ? 0 : -1) : ((1F / fps) * 1000000000F));
 	}
+
 	@Override
 	public void setBackgroundFPS (int fps) {
 
