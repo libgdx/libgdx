fd8daecf5e3253830c43bf504f7ee05e1ba4fd9d
BoBIsHere86 <55356015+BoBIsHere86@users.noreply.github.com>
2025-05-03 21:12:18 -0400

    Added a test which loads an assorted pick of maps from other tests. All using the TiledMapLoader.

--- tests/gdx-tests/src/com/badlogic/gdx/tests/utils/GdxTests.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/utils/GdxTests.java
@@ -325,6 +325,7 @@ public class GdxTests {
 		TiledMapJsonAtlasAssetManagerTest.class,
 		TiledMapJsonFormatTest.class,
 		TiledMapJsonObjectLoadingTest.class,
+		TiledMapUniversalLoaderTest.class,
 		TimerTest.class,
 		TimeUtilsTest.class,
 		TouchpadTest.class,
