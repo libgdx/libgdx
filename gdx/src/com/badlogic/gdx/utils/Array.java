libgdx/libgdx

commit 34cc595deb4ac09ee476c6b1aba1b805f4dc81a7

Migrate away from ArrayReflection array instantiation (#7605)
* feat: Add new ArraySupplier

* feat: Migrate Queue to ArraySupplier

* feat: Migrate ArrayMap to ArraySupplier

* feat: Migrate Array to ArraySupplier

* feat: Make Animation not use array reflection

* fix: Fix formatting

* fix: Fix CumulativeDistribution use

* fix: Fix gwt

* feat: Add CHANGES entry

* fix: Make CumulativeDistribution use method reference for array creation

* fix: Make CumulativeDistribution.CumulativeValue private

* feat: Imporve ArraySupplier Object[] creation

gdx/src/com/badlogic/gdx/utils/Array.java
@@ -52,20 +52,38 @@ public class Array<T> implements Iterable<T> {
 	 *           memory copy.
 	 * @param capacity Any elements added beyond this will cause the backing array to be grown. */
 	public Array (boolean ordered, int capacity) {
+		this(ordered, capacity, ArraySupplier.object());
+	}
+
+	/** Creates a new array with {@link #items} with the specified supplier.
+	 * @param ordered If false, methods that remove elements may change the order of other elements in the array, which avoids a
+	 *           memory copy.
+	 * @param capacity Any elements added beyond this will cause the backing array to be grown. */
+	public Array (boolean ordered, int capacity, ArraySupplier<T[]> arraySupplier) {
 		this.ordered = ordered;
-		items = (T[])new Object[capacity];
+		items = arraySupplier.get(capacity);
+	}
+
+	/** Creates an ordered array with {@link #items} with the specified supplier and a capacity of 16. */
+	public Array (ArraySupplier<T[]> arraySupplier) {
+		this(true, 16, arraySupplier);
 	}
 
 	/** Creates a new array with {@link #items} of the specified type.
 	 * @param ordered If false, methods that remove elements may change the order of other elements in the array, which avoids a
 	 *           memory copy.
-	 * @param capacity Any elements added beyond this will cause the backing array to be grown. */
+	 * @param capacity Any elements added beyond this will cause the backing array to be grown.
+	 *
+	 * @deprecated Use {@link Array#Array(boolean, int, ArraySupplier)} instead */
+	@Deprecated
 	public Array (boolean ordered, int capacity, Class arrayType) {
-		this.ordered = ordered;
-		items = (T[])ArrayReflection.newInstance(arrayType, capacity);
+		this(ordered, capacity, size -> (T[])ArrayReflection.newInstance(arrayType, size));
 	}
 
-	/** Creates an ordered array with {@link #items} of the specified type and a capacity of 16. */
+	/** Creates an ordered array with {@link #items} of the specified type and a capacity of 16.
+	 *
+	 * @deprecated Use {@link Array#Array(ArraySupplier)} instead */
+	@Deprecated
 	public Array (Class arrayType) {
 		this(true, 16, arrayType);
 	}
@@ -73,10 +91,10 @@ public class Array<T> implements Iterable<T> {
 	/** Creates a new array containing the elements in the specified array. The new array will have the same type of backing array
 	 * and will be ordered if the specified array is ordered. The capacity is set to the number of elements, so any subsequent
 	 * elements added will cause the backing array to be grown. */
-	public Array (Array<? extends T> array) {
-		this(array.ordered, array.size, array.items.getClass().getComponentType());
+	public Array (Array<T> array) {
+		items = Arrays.copyOf(array.items, array.size);
+		ordered = array.ordered;
 		size = array.size;
-		System.arraycopy(array.items, 0, items, 0, size);
 	}
 
 	/** Creates a new ordered array containing the elements in the specified array. The new array will have the same type of
@@ -91,9 +109,9 @@ public class Array<T> implements Iterable<T> {
 	 * @param ordered If false, methods that remove elements may change the order of other elements in the array, which avoids a
 	 *           memory copy. */
 	public Array (boolean ordered, T[] array, int start, int count) {
-		this(ordered, count, array.getClass().getComponentType());
+		items = Arrays.copyOfRange(array, start, start + count);
+		this.ordered = ordered;
 		size = count;
-		System.arraycopy(array, start, items, 0, size);
 	}
 
 	public void add (T value) {
@@ -466,11 +484,8 @@ public class Array<T> implements Iterable<T> {
 
 	/** Creates a new backing array with the specified size containing the current items. */
 	protected T[] resize (int newSize) {
-		T[] items = this.items;
-		T[] newItems = (T[])ArrayReflection.newInstance(items.getClass().getComponentType(), newSize);
-		System.arraycopy(items, 0, newItems, 0, Math.min(size, newItems.length));
-		this.items = newItems;
-		return newItems;
+		items = Arrays.copyOf(items, newSize);
+		return items;
 	}
 
 	/** Sorts this array. The array elements must implement {@link Comparable}. This method is not thread safe (uses
@@ -570,11 +585,19 @@ public class Array<T> implements Iterable<T> {
 	}
 
 	/** Returns the items as an array. Note the array is typed, so the {@link #Array(Class)} constructor must have been used.
-	 * Otherwise use {@link #toArray(Class)} to specify the array type. */
+	 * Otherwise use {@link #toArray(ArraySupplier)} to specify the array type. */
 	public T[] toArray () {
-		return (T[])toArray(items.getClass().getComponentType());
+		return Arrays.copyOf(items, size);
 	}
 
+	public T[] toArray (ArraySupplier<T[]> arraySupplier) {
+		T[] result = arraySupplier.get(size);
+		System.arraycopy(items, 0, result, 0, size);
+		return result;
+	}
+
+	/** @deprecated Use {@link Array#toArray(ArraySupplier)} instead */
+	@Deprecated
 	public <V> V[] toArray (Class<V> type) {
 		V[] result = (V[])ArrayReflection.newInstance(type, size);
 		System.arraycopy(items, 0, result, 0, size);
@@ -651,12 +674,28 @@ public class Array<T> implements Iterable<T> {
 		return buffer.toString();
 	}
 
-	/** @see #Array(Class) */
+	/** @see #Array(ArraySupplier) */
+	static public <T> Array<T> of (ArraySupplier<T[]> arraySupplier) {
+		return new Array<>(arraySupplier);
+	}
+
+	/** @see #Array(boolean, int, ArraySupplier) */
+	static public <T> Array<T> of (boolean ordered, int capacity, ArraySupplier<T[]> arraySupplier) {
+		return new Array<>(ordered, capacity, arraySupplier);
+	}
+
+	/** @see #Array(Class)
+	 *
+	 * @deprecated Use {@link Array#of(ArraySupplier)} */
+	@Deprecated
 	static public <T> Array<T> of (Class<T> arrayType) {
 		return new Array(arrayType);
 	}
 
-	/** @see #Array(boolean, int, Class) */
+	/** @see #Array(boolean, int, Class)
+	 *
+	 * @deprecated Use {@link Array#of(boolean, int, ArraySupplier)} */
+	@Deprecated
 	static public <T> Array<T> of (boolean ordered, int capacity, Class<T> arrayType) {
 		return new Array(ordered, capacity, arrayType);
 	}

backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
@@ -55,8 +55,7 @@ public class AndroidApplication extends Activity implements AndroidApplicationBa
 	protected boolean firstResume = true;
 	protected final Array<Runnable> runnables = new Array<Runnable>();
 	protected final Array<Runnable> executedRunnables = new Array<Runnable>();
-	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<LifecycleListener>(
-		LifecycleListener.class);
+	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<>(LifecycleListener[]::new);
 	private final Array<AndroidEventListener> androidEventListeners = new Array<AndroidEventListener>();
 	protected int logLevel = LOG_INFO;
 	protected ApplicationLogger applicationLogger;

backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidDaydream.java
@@ -54,8 +54,7 @@ public class AndroidDaydream extends DreamService implements AndroidApplicationB
 	protected boolean firstResume = true;
 	protected final Array<Runnable> runnables = new Array<Runnable>();
 	protected final Array<Runnable> executedRunnables = new Array<Runnable>();
-	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<LifecycleListener>(
-		LifecycleListener.class);
+	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<>(LifecycleListener[]::new);
 	protected int logLevel = LOG_INFO;
 	protected ApplicationLogger applicationLogger;
 

backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidFragmentApplication.java
@@ -42,8 +42,7 @@ public class AndroidFragmentApplication extends Fragment implements AndroidAppli
 	protected boolean firstResume = true;
 	protected final Array<Runnable> runnables = new Array<Runnable>();
 	protected final Array<Runnable> executedRunnables = new Array<Runnable>();
-	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<LifecycleListener>(
-		LifecycleListener.class);
+	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<>(LifecycleListener[]::new);
 	private final Array<AndroidEventListener> androidEventListeners = new Array<AndroidEventListener>();
 	protected int logLevel = LOG_INFO;
 	protected ApplicationLogger applicationLogger;

backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidLiveWallpaper.java
@@ -48,8 +48,7 @@ public class AndroidLiveWallpaper implements AndroidApplicationBase {
 	protected boolean firstResume = true;
 	protected final Array<Runnable> runnables = new Array<Runnable>();
 	protected final Array<Runnable> executedRunnables = new Array<Runnable>();
-	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<LifecycleListener>(
-		LifecycleListener.class);
+	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<>(LifecycleListener[]::new);
 	protected int logLevel = LOG_INFO;
 	protected ApplicationLogger applicationLogger;
 	protected volatile Color[] wallpaperColors = null;

backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglApplication.java
@@ -52,8 +52,7 @@ public class LwjglApplication implements LwjglApplicationBase {
 	protected boolean running = true;
 	protected final Array<Runnable> runnables = new Array<Runnable>();
 	protected final Array<Runnable> executedRunnables = new Array<Runnable>();
-	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<LifecycleListener>(
-		LifecycleListener.class);
+	protected final SnapshotArray<LifecycleListener> lifecycleListeners = new SnapshotArray<>(LifecycleListener[]::new);
 	protected int logLevel = LOG_INFO;
 	protected ApplicationLogger applicationLogger;
 	protected String preferencesdir;

backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/audio/OpenALLwjglAudio.java
@@ -50,7 +50,7 @@ public class OpenALLwjglAudio implements LwjglAudio {
 	private OpenALSound[] recentSounds;
 	private int mostRecentSound = -1;
 
-	Array<OpenALMusic> music = new Array(false, 1, OpenALMusic.class);
+	Array<OpenALMusic> music = new Array<>(false, 1, OpenALMusic[]::new);
 	boolean noDevice = false;
 
 	public OpenALLwjglAudio () {

backends/gdx-backend-lwjgl3/src/com/badlogic/gdx/backends/lwjgl3/audio/OpenALLwjgl3Audio.java
@@ -65,7 +65,7 @@ public class OpenALLwjgl3Audio implements Lwjgl3Audio {
 	private String preferredOutputDevice = null;
 	private Thread observerThread;
 
-	Array<OpenALMusic> music = new Array(false, 1, OpenALMusic.class);
+	Array<OpenALMusic> music = new Array<>(false, 1, OpenALMusic[]::new);
 	long device;
 	long context;
 	boolean noDevice = false;

commit 10a824c1d5a751e69468fe7937d039041fefd671

fix: Fix accidental API breakage in https://github.com/libgdx/libgdx/pull/7605

gdx/src/com/badlogic/gdx/utils/Array.java
@@ -91,7 +91,7 @@ public class Array<T> implements Iterable<T> {
 	/** Creates a new array containing the elements in the specified array. The new array will have the same type of backing array
 	 * and will be ordered if the specified array is ordered. The capacity is set to the number of elements, so any subsequent
 	 * elements added will cause the backing array to be grown. */
-	public Array (Array<T> array) {
+	public Array (Array<? extends T> array) {
 		items = Arrays.copyOf(array.items, array.size);
 		ordered = array.ordered;
 		size = array.size;