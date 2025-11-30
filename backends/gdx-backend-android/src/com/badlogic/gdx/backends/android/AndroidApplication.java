930f64df8ab8d6d1a5d6d622bad468242d4858f7
obigu <torinthechosen@gmail.com>
2025-12-01 00:50:36 +0100

    Good practice to null check on destroy

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/AndroidApplication.java
@@ -265,7 +265,9 @@ public class AndroidApplication extends Activity implements AndroidApplicationBa
 
 	@Override
 	protected void onDestroy () {
-		audio.dispose();
+        if (audio != null) {
+            audio.dispose();
+        }
 		keyboardHeightProvider.close();
 		super.onDestroy();
 	}
