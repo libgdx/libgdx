2cd6f818f28301f0ff1ddc0454ef92458b621c69
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2026-08-23 02:24:49 +0100

    Double space is annoying me

--- gdx/src/com/badlogic/gdx/net/HttpStatus.java
+++ gdx/src/com/badlogic/gdx/net/HttpStatus.java
@@ -199,7 +199,7 @@ public class HttpStatus {
 	/** {@code 510 Not Extended} (An HTTP Extension Framework - RFC 2774, p. 10, section 7) */
 	public static final int SC_NOT_EXTENDED = 510;
 
-	/** {@code  511 Network Authentication Required} (Additional HTTP Status Codes - RFC 6585) */
+	/** {@code 511 Network Authentication Required} (Additional HTTP Status Codes - RFC 6585) */
 	public static final int SC_NETWORK_AUTHENTICATION_REQUIRED = 511;
 
 }
